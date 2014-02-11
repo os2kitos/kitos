c04fb3e984bc2995d94512d4678775df34615ef9
slp <slp@it-minds.dk>
2014-02-11 10:55:25 +0100

    Testing first commit

--- UI.MVC4/Controllers/HomeController.cs
+++ UI.MVC4/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace UI.MVC4.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Your app description page!";
 
             return View();
         }
