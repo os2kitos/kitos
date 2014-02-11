d5f2cc09537689f84e5551ade15813ffaad39836
slp <slp@it-minds.dk>
2014-02-11 16:37:41 +0100

    Added simple user, admin and login actions to Home controller to test new membership provider.

--- UI.MVC4/Controllers/HomeController.cs
+++ UI.MVC4/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using UI.MVC4.Models;
 
 namespace UI.MVC4.Controllers
 {
@@ -25,6 +27,54 @@ namespace UI.MVC4.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel loginModel, string returnUrl)
+        {
+            if (ModelState.IsValid && Membership.ValidateUser(loginModel.Username, loginModel.Password))
+            {
+                FormsAuthentication.SetAuthCookie(loginModel.Username, loginModel.RememberMe);
+
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            // If we got this far, we couldn't authenticate, redisplay form
+            return View(loginModel);
+        }
+
+        [Authorize(Roles = "User")]
+        public ActionResult UserPage()
+        {
+            ViewBag.Message = "Hello user: " + User.Identity.Name;
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminPage()
+        {
+            ViewBag.Message = "The truth is out there, Scully!";
 
             return View();
         }
