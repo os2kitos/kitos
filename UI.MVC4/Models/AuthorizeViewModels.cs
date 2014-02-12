f527a71e2b3535ecbfa49318d578c2352a983c94
slp <slp@it-minds.dk>
2014-02-12 16:16:21 +0100

    Forgot new authorize view models

---
+++ UI.MVC4/Models/AuthorizeViewModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI.MVC4.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Brugernavn")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Forbliv logget ind?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Display(Name = "Brugernavn eller email adresse")]
+        public string UserIdentifier { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        public bool Retry { get; set; }
+        public string Hash { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Nyt password")]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        [Display(Name = "Bekræft nyt password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
