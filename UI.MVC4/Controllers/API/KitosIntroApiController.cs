61d175ba363443031f95e07ec25c89b9f34d07e9
slp <slp@it-minds.dk>
2014-02-18 16:06:43 +0100

    Added XeditableReturn model to handle RESTFUL updating via X-editable js plugin.
    Added new KITOS Intro page api controller which uses a repository.

---
+++ UI.MVC4/Controllers/API/KitosIntroApiController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Core.DomainModel.Text;
+using Core.DomainServices;
+using Infrastructure.DataAccess;
+using UI.MVC4.Models;
+
+namespace UI.MVC4.Controllers.API
+{
+    public class KitosIntroApiController : ApiController
+    {
+        private readonly IGenericRepository<KitosIntro> _repository;
+
+        public KitosIntroApiController()
+        {
+            //yeah yeah, I know, we should use DI
+            _repository = new FakeKitosIntroRepository();
+        }
+
+        // GET api/kitosintroapi
+        public IEnumerable<KitosIntro> Get()
+        {
+            return _repository.Get();
+        }
+
+        // GET api/kitosintroapi/5
+        public KitosIntro Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        // POST api/kitosintroapi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/kitosintroapi/5
+        public void Put(int id, [FromBody] XeditableReturnModel returnModel)
+        {
+            _repository.Update(new KitosIntro()
+                {
+                    Id = id,
+                    Text = returnModel.Value
+                });
+        }
+
+        // DELETE api/kitosintroapi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
