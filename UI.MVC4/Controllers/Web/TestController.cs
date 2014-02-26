ae662e63230d618e565e9b69bde3104f43c822a4
slp <slp@it-minds.dk>
2014-02-26 11:22:14 +0100

    Added test controller for simple manual testing of API.

---
+++ UI.MVC4/Controllers/Web/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.MVC4.Controllers.Web
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
