e0d76cb6f5eeaa679c484d666e0a495d5c23eb07
esd <esd@it-minds.dk>
2014-03-05 17:57:26 +0100

    Changed module names to use new option interface

--- Core.DomainModel/ItSupportModuleName.cs
+++ Core.DomainModel/ItSupportModuleName.cs
@@ -2,16 +2,20 @@
 
 namespace Core.DomainModel
 {
-    public class ItSupportModuleName : IEntity<int>
+    public class ItSupportModuleName : IOptionEntity<Config>
     {
         public ItSupportModuleName()
         {
-            this.Configs = new List<Config>();
+            this.IsActive = true;
+            this.References = new List<Config>();
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsSuggestion { get; set; }
+        public string Note { get; set; }
 
-        public virtual ICollection<Config> Configs { get; set; }
+        public virtual ICollection<Config> References { get; set; }
     }
 }
