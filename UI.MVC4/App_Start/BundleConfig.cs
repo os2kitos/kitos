b3c5ee6c3f18a99d4b4a2e956aa5f55365395ddf
slp <slp@it-minds.dk>
2014-03-07 14:50:50 +0100

    Added angularjs packages. Removed jQuery and all dependent packages. Updated BundleConfig. Added new MVC controller.

--- UI.MVC4/App_Start/BundleConfig.cs
+++ UI.MVC4/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace UI.MVC4
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
@@ -31,35 +31,27 @@ namespace UI.MVC4
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                         "~/Scripts/form.control.js"));
 
+             * 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+             * 
+             */
+
+            bundles.Add(new ScriptBundle("~/Scripts/angular").Include(
+                "~/Scripts/angular.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap-cosmo.*", 
                 "~/Content/carousel.css",
-                "~/Content/bootstrap3-wysihtml5/bootstrap3-wysihtml5.css",
-                "~/Content/pnotify/*.css",
-                "~/Content/css/select2.css",
-                "~/Content/select2-bootstrap.css",
+                //"~/Content/bootstrap3-wysihtml5/bootstrap3-wysihtml5.css",
+                //"~/Content/pnotify/*.css",
+                //"~/Content/css/select2.css",
+                //"~/Content/select2-bootstrap.css",
                 "~/Content/kitos.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
---
+++ UI.MVC4/Controllers/Web/AngularController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.MVC4.Controllers.Web
+{
+    public class AngularController : Controller
+    {
+        //
+        // GET: /Angular/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
