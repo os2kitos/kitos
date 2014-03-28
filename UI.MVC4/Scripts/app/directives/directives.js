84cc5ef2a0e37512019a434a6eb344910f44ab25
slp <slp@it-minds.dk>
2014-03-28 15:55:04 +0100

    Added add-user button to select-user directive

--- UI.MVC4/Scripts/app/directives/directives.js
+++ UI.MVC4/Scripts/app/directives/directives.js
@@ -22,9 +22,79 @@
             };
         }]);
 
+
+    app.directive('addUser', ['$http', '$modal', function($http, $modal) {
+        return {
+            scope: {
+                userResult: '=?addUser',
+                selectResult: '=?forSelect2'
+            },
+            replace: true,
+            templateUrl: 'partials/directives/add-user-button.html',
+            link: function(scope, element, attr) {
+
+                scope.open = function() {
+                    var modal = $modal.open({
+                        templateUrl: 'partials/directives/add-user-modal.html',
+                        controller: ['$scope', 'growl', '$modalInstance', function($scope, growl, $modalInstance) {
+
+                            $scope.newUser = {};
+
+                            $scope.addUser = function() {
+
+                                if ($scope.newUser.form.$invalid) return;
+
+                                var name = $scope.newUser.name;
+                                var email = $scope.newUser.email;
+
+                                var data = {
+                                    'Name': name,
+                                    'Email': email
+                                };
+
+                                $scope.newUser.submitting = true;
+
+                                $http.post("api/user", data).success(function(result) {
+                                    growl.addSuccessMessage(name + " er oprettet i KITOS");
+
+                                    $modalInstance.close(result.Response);
+                                }).error(function(result) {
+                                    $scope.newUser.submitting = false;
+                                    growl.addErrorMessage("Fejl! " + name + " blev ikke oprettet i KITOS!");
+                                });
+                            };
+
+                            $scope.cancel = function() {
+                                $modalInstance.dismiss('cancel');
+                            };
+                        }]
+                    });
+
+                    modal.result.then(function(userResult) {
+                        scope.userResult = userResult;
+
+                        scope.selectResult = {
+                            id: userResult.Id,
+                            text: userResult.Name
+                        };
+                    }, function() {
+                        scope.userResult = null;
+                        scope.selectResult = {
+                            id: null,
+                            text: null
+                        };
+                    });
+                };
+
+            }
+        };
+    }]);
+    
     app.directive('selectUser', ['$rootScope', '$http', function ($rootScope, $http) {
         return {
-            scope: {},
+            scope: {
+                userModel: '='
+            },
             replace: true,
             templateUrl: 'partials/directives/select-user.html',
             controller: ['$scope', function($scope) {
@@ -62,126 +132,7 @@
                     }
                 };
             }]
-            /*
-            link: function (scope, element, attr) {
-                scope.$parent = $rootScope;
-
-                console.log(Select2);
-                scope.Select2 = Select2;
-
-                scope.selectUserOptions = {
-                    minimumInputLength: 1,
-                    initSelection: function(elem, callback) {
-                    },
-                    ajax: {
-                        data: function(term, page) {
-                            return { query: term };
-                        },
-                        quietMillis: 500,
-                        transport: function(queryParams) {
-                            //console.log(queryParams);
-                            var res = $http.get('api/user?q=' + queryParams.data.query).then(queryParams.success);
-                            res.abort = function() {
-                                return null;
-                            };
-
-                            return res;
-                        },
-                        results: function(data, page) {
-                            console.log(data);
-                            var results = [];
-
-                            _.each(data.data.Response, function(user) {
-                                //Save to cache
-                                $rootScope.users[user.Id] = user;
-
-                                results.push({
-                                    id: user.Id,
-                                    text: user.Name
-                                });
-                            });
-
-                            return { results: results };
-                        }
-                    }
-                };
-
-                console.log(scope);
-
-            }*/
         };
-    }
-
-    ]);
+    }]);
     
-
-    app.directive('addUser', ['$http', '$modal', function ($http, $modal) {
-        return {
-            scope: {
-                userResult: '=?addUser',
-                selectResult: '=?forSelect2'
-            },
-            replace: true,
-            templateUrl: 'partials/directives/add-user-button.html',
-            link: function (scope, element, attr) {
-
-                scope.open = function() {
-                    var modal = $modal.open({
-                        templateUrl: 'partials/directives/add-user-modal.html',
-                        controller: ['$scope', 'growl', '$modalInstance', function ($scope, growl, $modalInstance) {
-
-                            $scope.newUser = {};
-
-                            $scope.addUser = function () {
-                                
-                                if ($scope.newUser.form.$invalid) return;
-
-                                var name = $scope.newUser.name;
-                                var email = $scope.newUser.email;
-
-                                var data = {
-                                    'Name': name,
-                                    'Email': email
-                                };
-
-                                $scope.newUser.submitting = true;
-                                
-                                $http.post("api/user", data).success(function (result) {
-                                    growl.addSuccessMessage(name + " er oprettet i KITOS");
-
-                                    $modalInstance.close(result.Response);
-                                }).error(function (result)
-                                {
-                                    $scope.newUser.submitting = false;
-                                    growl.addErrorMessage("Fejl! " + name + " blev ikke oprettet i KITOS!");
-                                });
-                            };
-
-                            $scope.cancel = function() {
-                                $modalInstance.dismiss('cancel');
-                            };
-                        }]
-                    });
-
-                    modal.result.then(function (userResult) {
-                            scope.userResult = userResult;
-                        
-                            scope.selectResult = {
-                                id: userResult.Id,
-                                text: userResult.Name
-                            };
-                    }, function() {
-                        scope.userResult = null;
-                        scope.selectResult = {
-                            id: null,
-                            text: null
-                        };
-                    });
-                };
-
-            }
-        };
-    }
-
-    ]);
 })(angular, app);
