332f5caf52e98f48b026272deaa0438b52bbe44f
slp <slp@it-minds.dk>
2014-04-04 16:46:38 +0200

    renamed it support link to organization

--- Infrastructure.DataAccess/Migrations/201404041443474_Initial.Designer.cs
+++ Infrastructure.DataAccess/Migrations/201404041443474_Initial.Designer.cs
@@ -13,7 +13,7 @@ namespace Infrastructure.DataAccess.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201404041407118_Initial"; }
+            get { return "201404041443474_Initial"; }
         }
         
         string IMigrationMetadata.Source
