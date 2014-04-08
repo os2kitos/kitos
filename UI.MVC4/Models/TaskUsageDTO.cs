dfae56b42321501a309f055f560309dc2711ed0e
slp <slp@it-minds.dk>
2014-04-08 13:56:18 +0200

    Missing files

---
+++ UI.MVC4/Models/TaskUsageDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace UI.MVC4.Models
+{
+    public class TaskUsageDTO
+    {
+        public int TaskRefId { get; set; }
+        public int OrgUnitId { get; set; }
+
+        public bool Starred { get; set; }
+        public int TechnologyStatus { get; set; }
+        public int UsageStatus { get; set; }
+
+        /*
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string TaskKey { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<int> HandledByOrgUnit { get; set; }*/
+    }
+}
