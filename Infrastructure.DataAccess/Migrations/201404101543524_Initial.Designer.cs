1b2b25dea1f44f9b17cf768f382ef2012d9865e5
esd <esd@it-minds.dk>
2014-04-10 17:47:52 +0200

    Updated migritaions

--- Infrastructure.DataAccess/Migrations/201404101543524_Initial.Designer.cs
+++ Infrastructure.DataAccess/Migrations/201404101543524_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Infrastructure.DataAccess.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.0-30225")]
-    public sealed partial class Comment : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Comment));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201404101213509_Comment"; }
+            get { return "201404101543524_Initial"; }
         }
         
         string IMigrationMetadata.Source
--- Infrastructure.DataAccess/Migrations/201404101543524_Initial.cs
+++ Infrastructure.DataAccess/Migrations/201404101543524_Initial.cs
@@ -3,7 +3,7 @@ namespace Infrastructure.DataAccess.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Comment : DbMigration
+    public partial class Initial : DbMigration
     {
         public override void Up()
         {
