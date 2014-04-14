dc9507f9958574be1f11506afdfc901b2d3e5a0b
slp <slp@it-minds.dk>
2014-04-14 13:20:26 +0200

    added focusOn and autofocus directives

--- UI.MVC4/Scripts/app/directives/directives.js
+++ UI.MVC4/Scripts/app/directives/directives.js
@@ -177,6 +177,49 @@
         };
     }]);
     
+    /* http://stackoverflow.com/questions/14833326/how-to-set-focus-in-angularjs */
+    app.directive('focusOn', ['$timeout', function ($timeout) {
+        return function (scope, elem, attr) {
+            scope.$on('focusOn', function (e, name) {
+                if (name === attr.focusOn) {
+                    $timeout(function() {
+                        elem[0].focus();
+                    });
+                }
+            });
+        };
+    }]);
+
+    /* http://stackoverflow.com/questions/14833326/how-to-set-focus-in-angularjs */
+    app.factory('focus', function ($rootScope, $timeout) {
+        return function(name) {
+            $timeout(function() {
+                $rootScope.$broadcast('focusOn', name);
+            });
+        };
+    });
+
+
+    app.directive('autofocus', ['$timeout', function($timeout) {
+        return function (scope, elem, attr) {
+            scope.$on('autofocus', function (e) {
+                $timeout(function () {
+                    elem[0].focus();
+                });
+            });
+        };
+    }]);
+    
+    /* http://stackoverflow.com/questions/14833326/how-to-set-focus-in-angularjs */
+    app.factory('autofocus', function ($rootScope, $timeout) {
+        return function () {
+            $timeout(function () {
+                $rootScope.$broadcast('autofocus');
+            });
+        };
+    });
+
+
     /* from http://stackoverflow.com/questions/11540157/using-comma-as-list-separator-with-angularjs */
     app.filter('joinBy', function () {
         return function (input, delimiter) {
