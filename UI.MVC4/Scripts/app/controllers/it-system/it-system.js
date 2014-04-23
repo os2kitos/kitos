c67b5b7a83ecb8def6897586ea1b44e20f6b5822
slp <slp@it-minds.dk>
2014-04-23 11:33:44 +0200

    BAM

---
+++ UI.MVC4/Scripts/app/controllers/it-system/it-system.js
@@ -0,0 +1,30 @@
+﻿(function (ng, app) {
+    
+    var subnav = [
+            { state: 'index', text: 'Overblik' },
+            { state: 'index', text: 'Tilknyt IT system' },
+            { state: 'edit-it-system', text: 'Opret IT system' },
+            { state: 'index', text: 'Rapport' },
+    ];
+
+    app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+        $stateProvider.state('edit-it-system', {
+            url: '/system/add',
+            templateUrl: 'partials/it-system/edit-system.html',
+            controller: 'system.AddCtrl'
+        });
+
+    }]);
+
+
+    app.controller('system.AddCtrl',
+        ['$rootScope', '$scope', '$http', 'notify',
+            function($rootScope, $scope, $http, notify) {
+                $rootScope.page.title = 'Opret IT system';
+                $rootScope.page.subnav = subnav;
+
+            }]);
+
+
+})(angular, app);
