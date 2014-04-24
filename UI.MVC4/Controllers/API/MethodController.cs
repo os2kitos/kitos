7f548f627ca9c1cc4b2b582d65951eef92351171
slp <slp@it-minds.dk>
2014-04-24 11:58:23 +0200

    Removed some unused and faulty get overrides

--- UI.MVC4/Controllers/API/MethodController.cs
+++ UI.MVC4/Controllers/API/MethodController.cs
@@ -11,10 +11,5 @@ namespace UI.MVC4.Controllers.API
             : base(repository)
         {
         }
-
-        protected override IEnumerable<Method> GetAllQuery()
-        {
-            return Repository.Get(x => x.IsActive);
-        }
     }
 }
--- UI.MVC4/Controllers/API/ProjectTypeController.cs
+++ UI.MVC4/Controllers/API/ProjectTypeController.cs
@@ -11,10 +11,5 @@ namespace UI.MVC4.Controllers.API
             : base(repository)
         {
         }
-
-        protected override IEnumerable<ProjectType> GetAllQuery()
-        {
-            return Repository.Get(x => x.IsActive);
-        }
     }
 }
