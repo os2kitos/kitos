ad89c5bd88e58b54e0b3ebb612fb1681fdbe49bb
esd <esd@it-minds.dk>
2014-04-28 16:01:39 +0200

    Created nested ui view

---
+++ UI.MVC4/Scripts/app/controllers/it-system/it-system-wishes.js
@@ -0,0 +1,13 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-system-usage.wishes', {
+            url: '/wishes',
+            templateUrl: 'partials/it-system/tab-wishes.html',
+            controller: 'system.EditWishes'
+        });
+    }]);
+
+    app.controller('system.EditWishes', [function () {
+        
+    }]);
+})(angular, app);
