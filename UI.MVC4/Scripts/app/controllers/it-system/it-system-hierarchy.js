513d5b59ef2ebf5997f108bd7914655ab36445e1
esd <esd@it-minds.dk>
2014-04-29 11:02:16 +0200

    Added placeholder partial, ctrl. Added tab link.

---
+++ UI.MVC4/Scripts/app/controllers/it-system/it-system-hierarchy.js
@@ -0,0 +1,17 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-system-usage.hierarchy', {
+            url: '/hierarchy',
+            templateUrl: 'partials/it-system/tab-hierarchy.html',
+            controller: 'system.EditHierarchy',
+            
+        });
+    }]);
+
+    app.controller('system.EditHierarchy', ['$rootScope', '$scope', '$http', '$state', '$stateParams', 'notify', 'wishes', function ($rootScope, $scope, $http, $state, $stateParams, notify, wishes) {
+        $scope.user = $rootScope.user;
+        $scope.wishes = wishes;
+
+        
+    }]);
+})(angular, app);
