3a7491ed0e12b3f7e80e1410a752d7408630548c
slp <slp@it-minds.dk>
2014-04-30 17:27:20 +0200

    Added search function on organization controller

--- UI.MVC4/Controllers/API/OrganizationController.cs
+++ UI.MVC4/Controllers/API/OrganizationController.cs
@@ -1,4 +1,6 @@
-﻿using Core.DomainModel;
+﻿using System;
+using System.Net.Http;
+using Core.DomainModel;
 using Core.DomainServices;
 using UI.MVC4.Models;
 
@@ -11,6 +13,19 @@ namespace UI.MVC4.Controllers.API
         public OrganizationController(IGenericRepository<Organization> repository, IOrganizationService organizationService) : base(repository)
         {
             _organizationService = organizationService;
+        }
+
+        public HttpResponseMessage GetBySearch(string q)
+        {
+            try
+            {
+                var orgs = Repository.Get(org => org.Name.StartsWith(q));
+                return Ok(Map(orgs));
+            }
+            catch (Exception e)
+            {
+                return Error(e);
+            }
         }
 
         protected override Organization PostQuery(Organization item)
