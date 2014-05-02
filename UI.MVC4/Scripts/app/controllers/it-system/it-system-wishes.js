ece34fda3a73b94df1b742a12c2383640db79a7d
slp <slp@it-minds.dk>
2014-05-02 16:16:40 +0200

    $rootScope.user to userService

--- UI.MVC4/Scripts/app/controllers/it-system/it-system-wishes.js
+++ UI.MVC4/Scripts/app/controllers/it-system/it-system-wishes.js
@@ -22,34 +22,34 @@
 
     app.controller('system.EditWishes', ['$rootScope', '$scope', '$http', '$state', '$stateParams', 'notify', 'wishes', 'user',
         function ($rootScope, $scope, $http, $state, $stateParams, notify, wishes, user) {
-        $scope.user = $rootScope.user;
-        $scope.wishes = wishes;
+            $scope.user = user;
+            $scope.wishes = wishes;
 
-        function clear() {
-            $scope.text = '';
-            $scope.isPublic = false;
-        }
+            function clear() {
+                $scope.text = '';
+                $scope.isPublic = false;
+            }
 
-        $scope.save = function () {
-            var payload = {
-                text: $scope.text,
-                isPublic: $scope.isPublic,
-                userId: user.id,
-                itSystemUsageId: $stateParams.id
-            };
+            $scope.save = function () {
+                var payload = {
+                    text: $scope.text,
+                    isPublic: $scope.isPublic,
+                    userId: user.id,
+                    itSystemUsageId: $stateParams.id
+                };
 
-            var msg = notify.addInfoMessage("Gemmer... ");
-            $http.post('api/wish', payload).success(function() {
-                msg.toSuccessMessage("Ønsket er gemt!");
-                clear();
-                $state.transitionTo($state.current, $stateParams, {
-                    reload: true,
-                    inherit: false,
-                    notify: true
+                var msg = notify.addInfoMessage("Gemmer... ");
+                $http.post('api/wish', payload).success(function () {
+                    msg.toSuccessMessage("Ønsket er gemt!");
+                    clear();
+                    $state.transitionTo($state.current, $stateParams, {
+                        reload: true,
+                        inherit: false,
+                        notify: true
+                    });
+                }).error(function () {
+                    msg.toErrorMessage("Fejl! Ønsket kunne ikke gemmes!");
                 });
-            }).error(function() {
-                msg.toErrorMessage("Fejl! Ønsket kunne ikke gemmes!");
-            });
-        };
-    }]);
+            };
+        }]);
 })(angular, app);
