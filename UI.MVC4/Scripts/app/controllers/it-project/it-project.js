958c21a85b8ef6d5245951eb93db4d40f021cbbf
slp <slp@it-minds.dk>
2014-05-05 14:10:01 +0200

    Added edit project html and js files; added first markup for basic data on edit project page

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project.js
@@ -0,0 +1,36 @@
+﻿(function(ng, app) {
+
+    var subnav = [
+            { state: 'edit-it-project', text: 'IT Projekt' }
+    ];
+
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider.state('edit-it-project', {
+            url: '/project/edit/{id:[0-9]+}',
+            templateUrl: 'partials/it-project/edit-it-project.html',
+            controller: 'project.EditCtrl',
+            resolve: {
+                //itProject: ['$http', '$stateParams', function($http, $stateParams) {
+                //    return $http.get("api/itproject/" + $stateParams.id)
+                //        .then(function(result) {
+                //            return result.data.response;
+                //        });
+                //}]
+            }
+        });
+
+    }]);
+
+
+    app.controller('project.EditCtrl',
+        ['$rootScope', '$scope',
+            function ($rootScope, $scope){
+                $rootScope.page.title = 'IT Projekt';
+                $rootScope.page.subnav = subnav;
+
+
+
+            }]);
+
+})(angular, app);
