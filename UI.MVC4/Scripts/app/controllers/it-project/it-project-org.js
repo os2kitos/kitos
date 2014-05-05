95da398224fed4af7ed4379fa5b2fa85487600d0
esd <esd@it-minds.dk>
2014-05-05 15:59:01 +0200

    Added boilerplate

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-org.js
@@ -0,0 +1,16 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('edit-it-project.org', {
+            url: '/org',
+            templateUrl: 'partials/it-project/tab-org.html',
+            controller: 'project.EditOrg',
+            resolve: {
+                
+            }
+        });
+    }]);
+
+    app.controller('project.EditOrg', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
+        
+    }]);
+})(angular, app);
