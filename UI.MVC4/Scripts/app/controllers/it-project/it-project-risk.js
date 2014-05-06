0d552ca6560bda87f22d329d28227e3a7800a9b1
slp <slp@it-minds.dk>
2014-05-07 01:03:30 +0200

    Added markup for input fields; Began implementing it project risk.js

--- UI.MVC4/Scripts/app/controllers/it-project/it-project-risk.js
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-risk.js
@@ -1,18 +1,54 @@
 ﻿(function (ng, app) {
     app.config(['$stateProvider', function ($stateProvider) {
-        $stateProvider.state('edit-it-project.org', {
+        $stateProvider.state('edit-it-project.risk', {
             url: '/risk',
             templateUrl: 'partials/it-project/tab-risk.html',
             controller: 'project.EditRiskCtrl',
             resolve: {
-
+                risks: ['$http', 'itProject', function ($http, itProject) {
+                        return $http.get('api/risk/?getByProject&projectId=' + itProject.id)
+                            .then(function (result) {
+                                return result.data.response;
+                            });
+                    }]
             }
         });
     }]);
 
-    app.controller('project.EditRiskCtrl', ['$scope', '$http', '$stateParams', 'notify',
-        function($scope, $http, $stateParams, notify) {
+    app.controller('project.EditRiskCtrl', ['$scope', '$http', '$stateParams', 'notify', 'risks',
+        function($scope, $http, $stateParams, notify, risks) {
 
+            _.each(risks, function(risk) {
+                risk.show = true;
+                risk.userForSelect = {
+                    id: risk.responsibleUserId,
+                    text: risk.responsibleUser.name
+                };
+            });
+
+            $scope.risks = risks;
+
+            function resetNewRisk() {
+                $scope.newRisk = {
+                    consequence: 1,
+                    probability: 1
+                };
+            }
+
+            resetNewRisk();
+
+            $scope.product = function (risk) {
+
+                return risk.consequence * risk.probability;
+            };
+
+            $scope.save = function(risk) {
+                 
+                if (!risk.name || !risk.action) {
+                    
+                }
+
+            };
 
         }
     ]);
