e4945a0d63457ce8718aa999d65ea3395f92fde4
esd <esd@it-minds.dk>
2014-05-06 10:53:37 +0200

    Added boilerplate

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-itsys.js
@@ -0,0 +1,20 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('edit-it-project.itsys', {
+            url: '/itsys',
+            templateUrl: 'partials/it-project/tab-itsys.html',
+            controller: 'project.EditItsysCtrl',
+            resolve: {
+
+            }
+        });
+    }]);
+
+    app.controller('project.EditItsysCtrl',
+    ['$rootScope', '$scope', 'itProject',
+        function ($rootScope, $scope, itProject) {
+
+
+
+        }]);
+})(angular, app);
