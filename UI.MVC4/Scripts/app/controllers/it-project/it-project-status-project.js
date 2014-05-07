59d312d32295e24156de076b017561a02a2630c0
slp <slp@it-minds.dk>
2014-05-07 17:10:10 +0200

    Added the first html and js for status project tab

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-status-project.js
@@ -0,0 +1,14 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('edit-it-project.status-project', {
+            url: '/status-project',
+            templateUrl: 'partials/it-project/tab-status-project.html',
+            controller: 'project.EditStatusProjectCtrl'
+        });
+    }]);
+
+    app.controller('project.EditStatusProjectCtrl',
+    ['$scope', 'itProject',
+        function ($scope, itProject) {
+        }]);
+})(angular, app);
