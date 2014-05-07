593b78ecb376b28be4449222ba01be9c67d8a54a
slp <slp@it-minds.dk>
2014-05-07 12:41:24 +0200

    Added save, delete, update js functions

--- UI.MVC4/Scripts/app/controllers/it-project/it-project-risk.js
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-risk.js
@@ -18,35 +18,110 @@
     app.controller('project.EditRiskCtrl', ['$scope', '$http', '$stateParams', 'notify', 'risks',
         function($scope, $http, $stateParams, notify, risks) {
 
-            _.each(risks, function(risk) {
+            var projectId = $stateParams.id;
+
+            $scope.risks = [];
+
+            function watchUser(risk) {
+
+                function getUserId() {
+                    return risk.userForSelect.id;
+                }
+
+                $scope.$watch(getUserId, function (newVal, oldVal) {
+
+                    if (!newVal || newVal === oldVal) return;
+
+                    $http({
+                        method: 'PATCH',
+                        url: risk.updateUrl,
+                        data: {
+                            'responsibleUserId': newVal
+                        }
+                    }).success(function() {
+                        risk.responsibleUserId = risk.userForSelect.id;
+                        risk.responsibleUser = risk.userForSelect.user;
+
+                        notify.addSuccessMessage("Feltet er opdateret.");
+                    }).error(function () {
+                        notify.addErrorMessage("Fejl! Feltet kunne ikke ændres!");
+                    });
+
+                });
+
+
+            }
+
+            function pushRisk(risk) {
                 risk.show = true;
                 risk.userForSelect = {
                     id: risk.responsibleUserId,
                     text: risk.responsibleUser.name
                 };
-            });
 
-            $scope.risks = risks;
+                risk.updateUrl = "api/risk/" + risk.id;
 
-            function resetNewRisk() {
-                $scope.newRisk = {
-                    consequence: 1,
-                    probability: 1
-                };
+                watchUser(risk);
+
+                $scope.risks.push(risk);
             }
-
-            resetNewRisk();
+            _.each(risks, pushRisk);
 
             $scope.product = function (risk) {
 
                 return risk.consequence * risk.probability;
             };
 
-            $scope.save = function(risk) {
-                 
-                if (!risk.name || !risk.action) {
-                    
-                }
+            $scope.delete = function(risk) {
+                $http.delete(risk.updateUrl).success(function(result) {
+                    risk.show = false;
+
+                    notify.addSuccessMessage("Rækken er slettet");
+                }).error(function() {
+
+                    notify.addErrorMessage("Fejl! Kunne ikke slette!");
+                });
+            };
+
+            function resetNewRisk() {
+                $scope.newRisk = {
+                    consequence: 1,
+                    probability: 1
+                };
+                $scope.userForSelect = null;
+            }
+
+            resetNewRisk();
+
+            $scope.saveNewRisk = function() {
+                var risk = $scope.newRisk;
+
+                //name, action or user shouldn't be null or empty
+                if (!risk.name || !risk.action || !risk.userForSelect) return;
+                
+                var data = {
+                    itProjectId: projectId,
+                    name: risk.name,
+                    action: risk.action,
+                    probability: risk.probability,
+                    consequence: risk.consequence,
+                    responsibleUserId: risk.userForSelect.id
+                };
+
+                var msg = notify.addInfoMessage("Gemmer række", false);
+                $http.post("api/risk", data)
+                    .success(function (result) {
+
+                        var responseRisk = result.response;
+                        responseRisk.responsibleUser = risk.userForSelect.user;
+                        pushRisk(responseRisk);
+                        resetNewRisk();
+
+                        msg.toSuccessMessage("Rækken er gemt");
+                    })
+                    .error(function() {
+                        msg.toErrorMessage("Fejl! Prøv igen");
+                    });
 
             };
 
