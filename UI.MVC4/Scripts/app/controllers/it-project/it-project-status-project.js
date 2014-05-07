c82e39cac7065bd0946615769a4cfa0719fb119f
slp <slp@it-minds.dk>
2014-05-07 19:02:28 +0200

    Added markup for phases and activities table

--- UI.MVC4/Scripts/app/controllers/it-project/it-project-status-project.js
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-status-project.js
@@ -10,5 +10,10 @@
     app.controller('project.EditStatusProjectCtrl',
     ['$scope', 'itProject',
         function ($scope, itProject) {
+            $scope.project = itProject;
+
+            $scope.project.phases = [itProject.phase1, itProject.phase2, itProject.phase3, itProject.phase4, itProject.phase5];
+
+            console.log($scope.project);
         }]);
 })(angular, app);
