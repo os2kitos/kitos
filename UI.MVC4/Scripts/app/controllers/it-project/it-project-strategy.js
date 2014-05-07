a45c56205655cacc35eba38c9fc7b6c1e2bc1410
esd <esd@it-minds.dk>
2014-05-07 12:34:40 +0200

    Added boilerplate files

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-strategy.js
@@ -0,0 +1,15 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('edit-it-project.strategy', {
+            url: '/strategy',
+            templateUrl: 'partials/it-project/tab-strategy.html',
+            controller: 'project.EditStrategyCtrl'
+        });
+    }]);
+
+    app.controller('project.EditStrategyCtrl',
+    ['$scope', 'itProject',
+        function ($scope, itProject) {
+            
+        }]);
+})(angular, app);
