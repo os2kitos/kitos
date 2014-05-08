187309737bc649d1f04fb0ea2eb456351e4adc28
esd <esd@it-minds.dk>
2014-05-08 14:11:23 +0200

    Fixed issue with list not updating.

--- UI.MVC4/Scripts/app/controllers/it-project/it-project-org.js
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-org.js
@@ -11,6 +11,9 @@
                 selectedOrgUnits: ['itProject', function (itProject) {
                     return itProject.usedByOrgUnits;
                 }],
+                responsibleOrgUnitId: ['itProject', function (itProject) {
+                    return itProject.responsibleOrgUnitId;
+                }],
                 orgUnitsTree: ['$http', 'itProject', function ($http, itProject) {
                     return $http.get('api/organizationunit/?organization=' + itProject.organizationId)
                         .then(function (result) {
@@ -21,53 +24,67 @@
         });
     }]);
 
-    app.controller('project.EditOrgCtrl', ['$scope', '$http', '$stateParams', 'notify', 'isTransversal', 'orgUnitsTree', 'selectedOrgUnits', function ($scope, $http, $stateParams, notify, isTransversal, orgUnitsTree, selectedOrgUnits) {
-        $scope.orgUnitsTree = orgUnitsTree;
-        $scope.isTransversal = isTransversal;
-        $scope.selectedOrgUnits = selectedOrgUnits;
-        var projectId = $stateParams.id;
-        $scope.patchUrl = 'api/itproject/' + projectId;
+    app.controller('project.EditOrgCtrl',
+        ['$scope', '$http', '$stateParams', 'notify', 'isTransversal', 'orgUnitsTree', 'selectedOrgUnits', 'responsibleOrgUnitId',
+            function ($scope, $http, $stateParams, notify, isTransversal, orgUnitsTree, selectedOrgUnits, responsibleOrgUnitId) {
+                $scope.orgUnitsTree = orgUnitsTree;
+                $scope.isTransversal = isTransversal;
+                $scope.selectedOrgUnits = selectedOrgUnits;
+                $scope.responsibleOrgUnitId = responsibleOrgUnitId;
+                var projectId = $stateParams.id;
+                $scope.patchUrl = 'api/itproject/' + projectId;
 
-        $scope.save = function (obj) {
-            var msg = notify.addInfoMessage("Gemmer... ");
-            if (obj.selected) {
-                $http.post('api/itproject/' + projectId + '?organizationunit=' + obj.id)
-                    .success(function () {
-                        msg.toSuccessMessage("Gemt!");
-                    })
-                    .error(function() {
-                        msg.toErrorMessage("Fejl! Kunne ikke gemmes!");
-                    });
-            } else {
-                $http.delete('api/itproject/' + projectId + '?organizationunit=' + obj.id)
-                    .success(function() {
-                        msg.toSuccessMessage("Gemt!");
-                    })
-                    .error(function() {
-                        msg.toErrorMessage("Fejl! Kunne ikke gemmes!");
-                    });
-            }
-        };
+                $scope.save = function(obj) {
+                    var msg = notify.addInfoMessage("Gemmer... ");
+                    if (obj.selected) {
+                        $http.post('api/itproject/' + projectId + '?organizationunit=' + obj.id)
+                            .success(function() {
+                                msg.toSuccessMessage("Gemt!");
+                                $scope.selectedOrgUnits.push(obj);
+                            })
+                            .error(function() {
+                                msg.toErrorMessage("Fejl! Kunne ikke gemmes!");
+                            });
+                    } else {
+                        $http.delete('api/itproject/' + projectId + '?organizationunit=' + obj.id)
+                            .success(function() {
+                                msg.toSuccessMessage("Gemt!");
 
-        function searchTree(element, matchingId) {
-            if (element.id == matchingId) {
-                return element;
-            } else if (element.children != null) {
-                var result = null;
-                for (var i = 0; result == null && i < element.children.length; i++) {
-                    result = searchTree(element.children[i], matchingId);
+                                var indexOf;
+                                var found = _.filter($scope.selectedOrgUnits, function(element, index) {
+                                    var equal = element.id == obj.id;
+                                    if (equal) indexOf = index;
+                                    return equal;
+                                });
+                                if (found) $scope.selectedOrgUnits.splice(indexOf, 1);
+                            })
+                            .error(function() {
+                                msg.toErrorMessage("Fejl! Kunne ikke gemmes!");
+                            });
+                    }
+                };
+
+                function searchTree(element, matchingId) {
+                    if (element.id == matchingId) {
+                        return element;
+                    } else if (element.children != null) {
+                        var result = null;
+                        for (var i = 0; result == null && i < element.children.length; i++) {
+                            result = searchTree(element.children[i], matchingId);
+                        }
+                        return result;
+                    }
+                    return null;
                 }
-                return result;
-            }
-            return null;
-        }
 
-        var selectedOrgUnitIds = _.pluck(selectedOrgUnits, 'id');
-        _.each(selectedOrgUnitIds, function (id) {
-            var found = searchTree(orgUnitsTree[0], id);
-            if (found) {
-                found.selected = true;
+                var selectedOrgUnitIds = _.pluck(selectedOrgUnits, 'id');
+                _.each(selectedOrgUnitIds, function(id) {
+                    var found = searchTree(orgUnitsTree[0], id);
+                    if (found) {
+                        found.selected = true;
+                    }
+                });
             }
-        });
-    }]);
+        ]
+    );
 })(angular, app);
