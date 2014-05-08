d932cf35d3a87b6c44f238a300efb0813a6bf32a
esd <esd@it-minds.dk>
2014-05-08 16:23:56 +0200

    Added http calls

--- UI.MVC4/Scripts/app/controllers/it-project/it-project-catalog.js
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-catalog.js
@@ -3,12 +3,32 @@
         $stateProvider.state('it-project.catalog', {
             url: '/catalog',
             templateUrl: 'partials/it-project/catalog.html',
-            controller: 'project.EditCatalogCtrl'
+            controller: 'project.CatalogCtrl',
+            resolve: {
+                user: ['userService', function(userService) {
+                    return userService.getUser();
+                }]
+            }
         });
     }]);
 
-    app.controller('project.EditCatalogCtrl',
-        ['$scope', function($scope) {
-
-        }]);
+    app.controller('project.CatalogCtrl',
+        ['$scope', '$http', 'user', function ($scope, $http, user) {
+            $scope.create = function () {
+                var orgUnitId = user.defaultOrganizationUnitId;
+                var payload = {
+                    responsibleOrgUnitId: orgUnitId
+                };
+                $http.post('api/itproject', payload)
+                    .sucess(function (result) {
+                        var projectId = result.id;
+                        // add users default org unit to the new project
+                        $http.post('api/itproject/' + projectId + '?organizationunit=' + orgUnitId);
+                    })
+                    .error(function() {
+                        
+                    });
+            };
+        }]
+    );
 })(angular, app);
