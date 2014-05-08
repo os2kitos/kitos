03ea31a4ba1ad4f0230c8caaddd504b695e22697
esd <esd@it-minds.dk>
2014-05-08 18:10:22 +0200

    Added ui routing for top- and subnav

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-portfolio.js
@@ -0,0 +1,15 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-project.portfolio', {
+            url: '/portfolio',
+            templateUrl: 'partials/it-project/portfolio.html',
+            controller: 'project.EditPortfolioCtrl'
+        });
+    }]);
+
+    app.controller('project.EditPortfolioCtrl',
+    ['$scope',
+        function ($scope) {
+            
+        }]);
+})(angular, app);
