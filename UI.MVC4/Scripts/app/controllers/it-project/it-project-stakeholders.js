8e5620e6bcfcfb7943eb1c7e2d5a8cfd10336d52
slp <slp@it-minds.dk>
2014-05-13 17:37:51 +0200

    Added html and js files for stakeholders.

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-stakeholders.js
@@ -0,0 +1,25 @@
+﻿(function(ng, app) {
+
+    app.config(['$stateProvider', function($stateProvider) {
+
+        $stateProvider.state('it-project.edit.stakeholders', {
+            url: '/stakeholders',
+            templateUrl: 'partials/it-project/tab-stakeholders.html',
+            controller: 'project.EditStakeholdersCtrl',
+            resolve: {
+                                
+            }
+        });
+    }]);
+
+    app.controller('project.EditStakeholdersCtrl',
+    ['$rootScope', '$scope', 'itProject',
+        function($rootScope, $scope, itProject) {
+
+
+
+        }]);
+
+
+})(angular, app);
+    
