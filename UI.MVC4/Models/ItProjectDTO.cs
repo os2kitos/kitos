9da7df775ea38b372a360cff042fa16e68ee4db3
slp <slp@it-minds.dk>
2014-05-13 18:30:34 +0200

    Added averageRisk calculation to DTO

--- UI.MVC4/Models/ItProjectDTO.cs
+++ UI.MVC4/Models/ItProjectDTO.cs
@@ -35,6 +35,7 @@ namespace UI.MVC4.Models
         public IEnumerable<EconomyYearDTO> EconomyYears { get; set; } 
         public IEnumerable<ItSystemDTO> ItSystems { get; set; }
         public IEnumerable<TaskRefDTO> TaskRefs { get; set; }
+        public IEnumerable<RiskDTO> Risks { get; set; }
 
 
         #region Status project tab
@@ -95,6 +96,11 @@ namespace UI.MVC4.Models
         public int Bc
         {
             get { return EconomyYears.Sum(year => year.TotalBudget); }
+        }
+
+        public double AverageRisk
+        {
+            get { return Risks.Average(risk => risk.Consequence*risk.Probability); }
         }
     }
 }
