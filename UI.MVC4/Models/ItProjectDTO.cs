ffea92ed86732ea5637360cc6738659af40f8c25
esd <esd@it-minds.dk>
2014-05-13 18:37:50 +0200

    Added new fields to model and dto.

--- UI.MVC4/Models/ItProjectDTO.cs
+++ UI.MVC4/Models/ItProjectDTO.cs
@@ -19,6 +19,8 @@ namespace UI.MVC4.Models
         public string Description { get; set; }
         public AccessModifier AccessModifier { get; set; }
         public ItProjectPriority Priority { get; set; }
+        public bool IsPriorityLocked { get; set; }
+        public ItProjectPriority PriorityPf { get; set; }
         public bool IsArchived { get; set; }
 
         public int? AssociatedProgramId { get; set; }
