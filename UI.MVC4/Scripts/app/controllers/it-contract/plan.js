1571cc846393b0c1f439514a15c4874c320c4387
esd <esd@it-minds.dk>
2014-05-20 14:51:42 +0200

    Added navigation and boilerplate for tabs

---
+++ UI.MVC4/Scripts/app/controllers/it-contract/plan.js
@@ -0,0 +1,17 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-contract.plan', {
+            url: '/plan',
+            templateUrl: 'partials/it-contract/plan.html',
+            controller: 'contract.PlanCtrl',
+            resolve: {
+
+            }
+        });
+    }]);
+
+    app.controller('contract.PlanCtrl', ['$scope', '$http', 'notify',
+            function ($scope, $http, notify) {
+
+            }]);
+})(angular, app);
---
+++ UI.MVC4/Scripts/app/controllers/it-contract/subnav.js
@@ -0,0 +1,18 @@
+﻿(function(ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-contract', {
+            url: '/contract',
+            abstract: true,
+            template: '<ui-view/>',
+            controller: ['$rootScope', function($rootScope) {
+                $rootScope.page.title = 'IT Kontrakt';
+                $rootScope.page.subnav = [
+                    { state: 'it-contract.overview', text: 'Oversigt' },
+                    { state: 'it-contract.plan', text: 'Udbudsplan' },
+                    { state: 'it-contract.catalog', text: 'IT Kontrakt katalog' },
+                    { state: 'it-contract.edit', text: 'IT Kontrakt' }
+                ];
+            }]
+        });
+    }]);
+})(angular, app);
