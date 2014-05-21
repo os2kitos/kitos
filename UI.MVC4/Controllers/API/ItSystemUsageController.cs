c11bc8ca1ce7d175fafcbc8f55a8cf78e9e036d8
slp <slp@it-minds.dk>
2014-05-21 11:21:32 +0200

    Updated ItSystemUSageController to use the new ItSystemUsageService

--- UI.MVC4/Controllers/API/ItSystemUsageController.cs
+++ UI.MVC4/Controllers/API/ItSystemUsageController.cs
@@ -14,12 +14,14 @@ namespace UI.MVC4.Controllers.API
     {
         private readonly IGenericRepository<OrganizationUnit> _orgUnitRepository;
         private readonly IGenericRepository<TaskRef> _taskRepository;
+        private readonly IItSystemUsageService _itSystemUsageService;
 
-        public ItSystemUsageController(IGenericRepository<ItSystemUsage> repository, IGenericRepository<OrganizationUnit> orgUnitRepository, IGenericRepository<TaskRef> taskRepository) 
+        public ItSystemUsageController(IGenericRepository<ItSystemUsage> repository, IGenericRepository<OrganizationUnit> orgUnitRepository, IGenericRepository<TaskRef> taskRepository, IItSystemUsageService itSystemUsageService) 
             : base(repository)
         {
             _orgUnitRepository = orgUnitRepository;
             _taskRepository = taskRepository;
+            _itSystemUsageService = itSystemUsageService;
         }
 
         public HttpResponseMessage GetByOrganization(int organizationId)
@@ -59,12 +61,9 @@ namespace UI.MVC4.Controllers.API
                     && usage.OrganizationId == dto.OrganizationId).Any())
                     return Conflict("Usage already exist");
 
-                var item = Map(dto);
-                item.ObjectOwner = KitosUser;
-                Repository.Insert(item);
-                Repository.Save();
+                var sysUsage = _itSystemUsageService.Add(dto.ItSystemId, dto.OrganizationId, KitosUser);
 
-                return Created(Map(item), new Uri(Request.RequestUri + "?itSystemId=" + dto.ItSystemId + "&organizationId" + dto.OrganizationId));
+                return Created(Map(sysUsage), new Uri(Request.RequestUri + "?itSystemId=" + dto.ItSystemId + "&organizationId" + dto.OrganizationId));
 
             }
             catch (Exception e)
