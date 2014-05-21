4cfb52d03b31c44a0e0d755e25993cd5edfa89e5
slp <slp@it-minds.dk>
2014-05-21 17:06:00 +0200

    Started implemented markup and js for tab-systems

---
+++ UI.MVC4/Scripts/app/controllers/it-contract/it-contract-systems.js
@@ -0,0 +1,17 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-contract.edit.systems', {
+            url: '/edit/{id:[0-9]+}',
+            templateUrl: 'partials/it-contract/tab-systems.html',
+            controller: 'contract.EditSystemsCtrl',
+            resolve: {
+
+            }
+        });
+    }]);
+
+    app.controller('contract.EditSystemsCtrl', ['$scope', '$http', 'notify',
+            function ($scope, $http, notify) {
+
+            }]);
+})(angular, app);
