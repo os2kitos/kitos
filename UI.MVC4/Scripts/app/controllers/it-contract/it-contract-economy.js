4394fed0a974ce1d668316a1691c7d393c9d0f3f
slp <slp@it-minds.dk>
2014-05-26 13:24:27 +0200

    Added html and js files.

---
+++ UI.MVC4/Scripts/app/controllers/it-contract/it-contract-economy.js
@@ -0,0 +1,18 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-contract.edit.economy', {
+            url: '/economy',
+            templateUrl: 'partials/it-contract/tab-economy.html',
+            controller: 'contract.EditEconomyCtrl',
+            resolve: {
+            }
+        });
+    }]);
+
+    app.controller('contract.EditEconomyCtrl', ['$scope', '$http', 'notify', 'contract',
+        function ($scope, $http, notify, contract) {
+
+
+        }]);
+
+})(angular, app);
