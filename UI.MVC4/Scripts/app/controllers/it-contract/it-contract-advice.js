17e100b0f04dc53a6eeaa2b98625339775ab443f
slp <slp@it-minds.dk>
2014-05-27 15:41:59 +0200

    Added html and js for advice

---
+++ UI.MVC4/Scripts/app/controllers/it-contract/it-contract-advice.js
@@ -0,0 +1,24 @@
+﻿(function(ng, app) {
+    app.config(['$stateProvider', function($stateProvider) {
+        $stateProvider.state('it-contract.edit.advice', {
+            url: '/advice',
+            templateUrl: 'partials/it-contract/tab-advice.html',
+            controller: 'contract.EditAdviceCtrl',
+            resolve: {
+                itContractRoles: ['$http', function ($http) {
+                    return $http.get("api/itcontractrole/")
+                        .then(function (result) {
+                            return result.data.response;
+                        });
+                }]
+            }
+        });
+    }]);
+
+    app.controller('contract.EditAdviceCtrl', ['$scope', '$http', 'notify', 'contract', 'itContractRoles',
+        function ($scope, $http, notify, contract, itContractRights, itContractRoles) {
+
+
+        }]);
+
+})(angular, app);
