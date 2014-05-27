4f57c73438f4fc5bb07e0776ea7578568246a8ca
esd <esd@it-minds.dk>
2014-05-27 17:40:58 +0200

    Moved classes to their own file

---
+++ Core.DomainModel/ItContract/PaymentModel.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Core.DomainModel.ItContract
+{
+    public class PaymentModel : IOptionEntity<ItContract>
+    {
+        public PaymentModel()
+        {
+            References = new List<ItContract>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsSuggestion { get; set; }
+        public string Note { get; set; }
+        public virtual ICollection<ItContract> References { get; set; }
+    }
+}
