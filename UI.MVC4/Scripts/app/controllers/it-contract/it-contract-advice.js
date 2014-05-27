30a06c32788b32011f1bd3a3141d8f7428a4c37a
slp <slp@it-minds.dk>
2014-05-27 17:20:56 +0200

    Can now add row; can now change the row

--- UI.MVC4/Scripts/app/controllers/it-contract/it-contract-advice.js
+++ UI.MVC4/Scripts/app/controllers/it-contract/it-contract-advice.js
@@ -16,8 +16,53 @@
     }]);
 
     app.controller('contract.EditAdviceCtrl', ['$scope', '$http', 'notify', 'contract', 'itContractRoles',
-        function ($scope, $http, notify, contract, itContractRights, itContractRoles) {
+        function ($scope, $http, notify, contract, itContractRoles) {
+            console.log(itContractRoles);
+            $scope.itContractRoles = itContractRoles;
 
+            var baseUrl = "api/advice";
+            
+            var advices = [];
+            $scope.advices = advices;
+            
+            function pushAdvice(advice) {
+                advice.show = true;
+                advice.updateUrl = baseUrl + "/" + advice.id;
+
+                advice.delete = function () {
+                    var msg = notify.addInfoMessage("Sletter rækken...", false);
+                    
+                    $http.delete(advice.updateUrl).success(function(result) {
+                        advice.show = false;
+                        msg.toSuccessMessage("Rækken er slettet!");
+                    }).error(function () {
+                        msg.toErrorMessage("Fejl! Rækken kunne ikke slettes!");
+                    });
+                };
+
+                advices.push(advice);
+            }
+
+            _.each(contract.advices, pushAdvice);
+
+            function newAdvice() {
+
+                var data = {
+                    itContractId: contract.id,
+                    isActive: true
+                };
+
+                var msg = notify.addInfoMessage("Tilføjer ny række...", false);
+
+                $http.post(baseUrl, data).success(function(result) {
+                    pushAdvice(result.response);
+                    msg.toSuccessMessage("Rækken er tilføjet!");
+                }).error(function() {
+                    msg.toErrorMessage("Fejl! Kunne ikke tilføje række!");
+                });
+            }
+
+            $scope.newAdvice = newAdvice;
 
         }]);
 
