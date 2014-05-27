88fc3dce51c1d3380b83a0c94a703da9d4d2f5e9
esd <esd@it-minds.dk>
2014-05-27 18:56:18 +0200

    Added View, ngCtrl

---
+++ UI.MVC4/Scripts/app/controllers/it-contract/it-contract-hierarchy.js
@@ -0,0 +1,20 @@
+﻿(function(ng, app) {
+    app.config(['$stateProvider', function($stateProvider) {
+        $stateProvider.state('it-contract.edit.hierarchy', {
+            url: '/hierarchy',
+            templateUrl: 'partials/it-contract/tab-hierarchy.html',
+            controller: 'contract.EditHierarchyCtrl',
+            resolve: {
+                
+            }
+        });
+    }]);
+
+    app.controller('contract.EditHierarchyCtrl',
+        ['$scope', '$http', 'notify',
+            function($scope, $http, notify) {
+
+            }
+        ]
+    );
+})(angular, app);
