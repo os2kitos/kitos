055a27edad2ff425e0b8e1650c4d39eea62f3216
esd <esd@it-minds.dk>
2014-05-28 15:13:30 +0200

    Fixed missing nullable type on prop in dto. Clicking the create it contract tab, now works.

--- UI.MVC4/Scripts/app/controllers/it-contract/it-contract-catalog.js
+++ UI.MVC4/Scripts/app/controllers/it-contract/it-contract-catalog.js
@@ -5,13 +5,19 @@
             templateUrl: 'partials/it-contract/it-contract-catalog.html',
             controller: 'contract.CatalogCtrl',
             resolve: {
-
+                user: ['userService', function (userService) {
+                    return userService.getUser();
+                }]
             }
         });
     }]);
 
-    app.controller('contract.CatalogCtrl', ['$scope', '$http', 'notify',
-            function ($scope, $http, notify) {
-
+    app.controller('contract.CatalogCtrl', ['$scope', '$http', '$state', 'user',
+            function ($scope, $http, $state, user) {
+                var orgId = user.currentOrganizationId;
+                $http.post('api/itcontract', { organizationId: orgId }).success(function (result) {
+                    var contract = result.response;
+                    $state.go('it-contract.edit', { id: contract.id });
+                });
             }]);
 })(angular, app);
