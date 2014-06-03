55a557554b2f0ff208f84f98e46eee8724f5cc49
slp <slp@it-minds.dk>
2014-06-03 18:32:20 +0200

    Added ID to DTO

--- UI.MVC4/Models/ItContractSystemDTO.cs
+++ UI.MVC4/Models/ItContractSystemDTO.cs
@@ -11,6 +11,7 @@ namespace UI.MVC4.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ItContractId { get; set; }
 
         public string ContractTypeName { get; set; }
 
