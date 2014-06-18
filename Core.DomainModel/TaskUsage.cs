3a78a73a92fa5aec4614146d5161ca5d4db55e2c
esd <esd@it-minds.dk>
2014-06-18 17:43:05 +0200

    Removed unused directives

--- Core.DomainModel/TaskUsage.cs
+++ Core.DomainModel/TaskUsage.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Core.DomainModel
+﻿namespace Core.DomainModel
 {
     public class TaskUsage : Entity
     {
