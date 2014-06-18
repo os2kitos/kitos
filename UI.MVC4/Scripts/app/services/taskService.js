d5cabe8bf727fa999443ffd974b33eec3bf3418d
slp <slp@it-minds.dk>
2014-06-18 15:33:19 +0200

    added task service

---
+++ UI.MVC4/Scripts/app/services/taskService.js
@@ -0,0 +1,17 @@
+﻿(function (ng, app) {
+    
+    app.factory('taskService', ['$http', '$q', '$rootScope', function ($http, $q, $rootScope) {
+
+        function loadRoots() {
+            
+        }
+
+
+        return {
+            
+        };
+
+    }]);
+
+
+})(angular, app);
