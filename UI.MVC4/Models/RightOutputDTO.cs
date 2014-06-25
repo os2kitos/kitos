94fe296d4fcccce21364d2e5855e90827b82e343
esd <esd@it-minds.dk>
2014-06-25 10:41:48 +0200

    Changed supplier to role selector

--- UI.MVC4/Models/RightOutputDTO.cs
+++ UI.MVC4/Models/RightOutputDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace UI.MVC4.Models
+﻿namespace UI.MVC4.Models
 {
     public class RightOutputDTO
     {
