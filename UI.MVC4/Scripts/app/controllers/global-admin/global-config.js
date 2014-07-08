b40cfd584be541478f8a042e87df7e8032f73303
esd <esd@it-minds.dk>
2014-07-08 13:14:47 +0200

    More cleaning up

--- UI.MVC4/Scripts/app/controllers/global-admin/global-config.js
+++ UI.MVC4/Scripts/app/controllers/global-admin/global-config.js
@@ -1,12 +1,12 @@
 ﻿(function(ng, app) {
     var subnav = [
-        { state: "config-org", text: "Organisation" },
-        { state: "config-project", text: "IT Projekt" },
-        { state: "config-system", text: "IT System" },
-        { state: "config-contract", text: "IT Kontrakt" }
+        { state: 'config-org', text: 'Organisation' },
+        { state: 'config-project', text: 'IT Projekt' },
+        { state: 'config-system', text: 'IT System' },
+        { state: 'config-contract', text: 'IT Kontrakt' }
     ];
 
-    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    app.config(['$stateProvider', function($stateProvider) {
 
         $stateProvider.state('config-org', {
             url: '/global-config/org',
@@ -31,22 +31,22 @@
         });
     }]);
 
-    app.controller('globalConfig.OrgCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
+    app.controller('globalConfig.OrgCtrl', ['$rootScope', '$scope', function($rootScope) {
         $rootScope.page.title = 'Global konfiguration';
         $rootScope.page.subnav = subnav;
     }]);
 
-    app.controller('globalConfig.ProjectCtrl', ['$rootScope', '$scope', '$timeout', '$state', '$stateParams', 'Restangular', 'notify', function ($rootScope, $scope, $timeout, $state, $stateParams, Restangular, notify) {
+    app.controller('globalConfig.ProjectCtrl', ['$rootScope', function ($rootScope) {
         $rootScope.page.title = 'Global konfiguration';
         $rootScope.page.subnav = subnav;
     }]);
 
-    app.controller('globalConfig.SystemCtrl', ['$rootScope', '$scope', 'Restangular', 'notify', function($rootScope, $scope, Restangular, notify) {
+    app.controller('globalConfig.SystemCtrl', ['$rootScope', function($rootScope) {
         $rootScope.page.title = 'Global konfiguration';
         $rootScope.page.subnav = subnav;
     }]);
 
-    app.controller('globalConfig.ContractCtrl', ['$rootScope', '$scope', 'Restangular', 'notify', function ($rootScope, $scope, Restangular, notify) {
+    app.controller('globalConfig.ContractCtrl', ['$rootScope', function ($rootScope) {
         $rootScope.page.title = 'Global konfiguration';
         $rootScope.page.subnav = subnav;
     }]);
