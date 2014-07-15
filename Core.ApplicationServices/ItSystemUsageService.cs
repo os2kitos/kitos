2ed33f7e1a6023b1552e40192e52d664b3ffd7d2
slp-itminds <slp@it-minds.dk>
2014-07-15 11:29:14 +0200

    Fixed LastChangedByUser bug

--- Core.ApplicationServices/ItSystemUsageService.cs
+++ Core.ApplicationServices/ItSystemUsageService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Core.DomainModel;
 using Core.DomainModel.ItSystem;
 using Core.DomainServices;
@@ -56,7 +52,8 @@ namespace Core.ApplicationServices
                                                              new DataRowUsage()
                                                                  {
                                                                      DataRowId = dataRow.Id,
-                                                                     ObjectOwner = objectOwner
+                                                                     ObjectOwner = objectOwner,
+                                                                     LastChangedByUser = objectOwner
                                                                  }).ToList();
 
             var usage = new InterfaceUsage()
