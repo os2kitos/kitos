5e842b6999e6f97c18f795e596f3e995afe94ffb
slp-itminds <slp@it-minds.dk>
2014-07-16 14:52:59 +0200

    Added new tab 'phases' to ItProject

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-phases.js
@@ -0,0 +1,74 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-project.edit.phases', {
+            url: '/phases',
+            templateUrl: 'partials/it-project/tab-phases.html',
+            controller: 'project.EditPhasesCtrl',
+            resolve: {
+                // re-resolve data from parent cause changes here wont cascade to it
+                project: ['$http', '$stateParams', function ($http, $stateParams) {
+                    return $http.get("api/itproject/" + $stateParams.id)
+                        .then(function (result) {
+                            return result.data.response;
+                        });
+                }]
+            }
+        });
+    }]);
+
+    app.controller('project.EditPhasesCtrl',
+    ['$scope', '$http', 'notify', '$modal', 'project',
+        function ($scope, $http, notify, $modal, project) {
+            $scope.project = project;
+            $scope.project.updateUrl = "api/itproject/" + project.id;
+
+            //Setup phases
+            $scope.project.phases = [project.phase1, project.phase2, project.phase3, project.phase4, project.phase5];
+            var prevPhase = null;
+            _.each($scope.project.phases, function (phase) {
+                phase.updateUrl = "api/activity/" + phase.id;
+                phase.prevPhase = prevPhase;
+                prevPhase = phase;
+            });
+
+            function patch(url, field, value) {
+                var payload = {};
+                payload[field] = value;
+
+                return $http({
+                    method: 'PATCH',
+                    url: url,
+                    data: payload
+                });
+            }
+
+            $scope.updateSelectedPhase = function (phase) {
+                patch($scope.project.updateUrl, "currentPhaseId", phase.id)
+                    .success(function (result) {
+                        notify.addSuccessMessage("Feltet er opdateret");
+                        $scope.project.currentPhaseId = phase.id;
+                    })
+                    .error(function () {
+                        notify.addErrorMessage("Fejl!");
+                    });
+            };
+
+
+            $scope.updatePhaseDate = function (phase) {
+                //Update start date of the current phase
+                patch(phase.updateUrl, "startDate", phase.startDate)
+                    .success(function (result) {
+                        //Also update end date of the previous phase
+                        patch(phase.prevPhase.updateUrl, "endDate", phase.startDate).success(function () {
+                            notify.addSuccessMessage("Feltet er opdateret");
+                        }).error(function () {
+                            notify.addErrorMessage("Fejl!");
+                        });
+
+                    }).error(function () {
+                        notify.addErrorMessage("Fejl!");
+                    });
+            };
+
+        }]);
+})(angular, app);
