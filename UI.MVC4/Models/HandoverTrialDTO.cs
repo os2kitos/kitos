6053d8397df5ea28519c1ae7682871c45e396f85
esd <esd@it-minds.dk>
2014-07-22 12:54:02 +0200

    Added HandoverTrial controller

---
+++ UI.MVC4/Models/HandoverTrialDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UI.MVC4.Models
+{
+    public class HandoverTrialDTO
+    {
+        public DateTime? Expected { get; set; }
+        public DateTime? Approved { get; set; }
+        public int ItContractId { get; set; }
+        public int? HandoverTrialTypeId { get; set; }
+    }
+}
