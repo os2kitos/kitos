4a081fee5e061cf4b355f581f9e54be7aeb223ce
esd <esd@it-minds.dk>
2014-07-22 16:42:10 +0200

    Changed layout according to new mockup. Added new option to global and local config.

--- UI.MVC4/Models/HandoverTrialDTO.cs
+++ UI.MVC4/Models/HandoverTrialDTO.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using Newtonsoft.Json;
+using UI.MVC4.Filters;
 
 namespace UI.MVC4.Models
 {
     public class HandoverTrialDTO
     {
+        public int Id { get; set; }
+        [JsonConverter(typeof(CustomDateTimeConverter))]
         public DateTime? Expected { get; set; }
+        [JsonConverter(typeof(CustomDateTimeConverter))]
         public DateTime? Approved { get; set; }
         public int ItContractId { get; set; }
         public int? HandoverTrialTypeId { get; set; }
