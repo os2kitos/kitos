8d683f698ac39bc3c38792084fcdfca50850a8c6
esditm <esd@it-minds.dk>
2014-09-02 15:59:05 +0200

    Added filter to shorten strings

--- UI.MVC4/Scripts/app/directives/filters.js
+++ UI.MVC4/Scripts/app/directives/filters.js
@@ -48,4 +48,21 @@
             return input;
         };
     });
+
+    /**
+     * Same as https://docs.angularjs.org/api/ng/filter/limitTo
+     * But it adds ... at the end if limit is exceeded
+     * 
+     * Only works on strings though
+     */
+    app.filter('limitToDots', ['limitToFilter', function (limitToFilter) {
+        return function (input, limit) {
+            var str = limitToFilter(input, limit);
+
+            if (input.length > limit)
+                str += '...';
+
+            return str;
+        }
+    }]);
 })(angular, app);
