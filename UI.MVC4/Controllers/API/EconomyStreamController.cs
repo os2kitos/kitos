5a864f3c96fd845ca0fbddd032e6ab3ccbea6958
esditm <esd@it-minds.dk>
2014-09-09 09:39:42 +0200

    Removed unused directives

--- UI.MVC4/Controllers/API/EconomyStreamController.cs
+++ UI.MVC4/Controllers/API/EconomyStreamController.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using Core.DomainModel.ItContract;
+﻿using Core.DomainModel.ItContract;
 using Core.DomainServices;
 using UI.MVC4.Models;
 
