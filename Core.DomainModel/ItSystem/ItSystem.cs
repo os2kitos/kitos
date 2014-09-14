da692f58587363c91ff7baa1be4c25ca772e7b2e
esditm <esd@it-minds.dk>
2014-09-14 13:05:19 +0200

    Refactoring domain model...

--- Core.DomainModel/ItSystem/ItSystem.cs
+++ Core.DomainModel/ItSystem/ItSystem.cs
@@ -5,36 +5,21 @@ namespace Core.DomainModel.ItSystem
     /// <summary>
     /// Represents an it system.
     /// </summary>
-    public class ItSystem : Entity, IHasAccessModifier, IHierarchy<ItSystem>
+    public class ItSystem : ItSystemBase, IHasAccessModifier, IHierarchy<ItSystem>
     {
         public ItSystem()
         {
-            this.ExposedInterfaces = new List<ItSystem>();
-            this.CanUseInterfaces = new List<ItSystem>();
-            this.CanBeUsedBy = new List<ItSystem>();
+            this.ExposedInterfaces = new List<ItInterface>();
+            this.CanUseInterfaces = new List<ItInterface>();
             this.Children = new List<ItSystem>();
             this.TaskRefs = new List<TaskRef>();
             this.Usages = new List<ItSystemUsage>();
             this.Wishes = new List<Wish>();
             this.TaskRefs = new List<TaskRef>();
             this.Overviews = new List<ItSystemUsage>();
-            this.DataRows = new List<DataRow>();
-            this.InterfaceLocalUsages = new List<InterfaceUsage>();
-            this.InterfaceLocalExposure = new List<InterfaceExposure>();
             this.InfrastructureUsage = new List<InterfaceUsage>();
         }
         
-        public string Name { get; set; }
-        /// <summary>
-        /// Gets or sets the user defined system identifier.
-        /// </summary>
-        /// <remarks>
-        /// This identifier is NOT the primary key.
-        /// </remarks>
-        /// <value>
-        /// The user defined system identifier.
-        /// </value>
-        public string SystemId { get; set; }
         public int? BelongsToId { get; set; }
         /// <summary>
         /// Gets or sets the organization the system belongs to.
@@ -46,37 +31,15 @@ namespace Core.DomainModel.ItSystem
         /// The organization the it system belongs to.
         /// </value>
         public virtual Organization BelongsTo { get; set; }
-        public AccessModifier AccessModifier { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
-        public int? ExposedById { get; set; }
-        /// <summary>
-        /// Gets or sets it system that exposes this interface instance.
-        /// </summary>
-        /// <remarks>
-        /// Should only be set/used if this instance's <see cref="AppType"/> is an interface.
-        /// </remarks>
-        /// <value>
-        /// The it system that exposes this instance.
-        /// </value>
-        public virtual ItSystem ExposedBy { get; set; }
+        
         /// <summary>
         /// Gets or sets interfaces this instance exposes.
         /// </summary>
         /// <value>
         /// Exposed interfaces.
         /// </value>
-        public virtual ICollection<ItSystem> ExposedInterfaces { get; set; }
-        /// <summary>
-        /// Gets or sets it systems that can use this instance.
-        /// </summary>
-        /// <remarks>
-        /// Should only be set/used if this instance's <see cref="AppType"/> is an interface.
-        /// </remarks>
-        /// <value>
-        /// It systems that can used by this instance.
-        /// </value>
-        public virtual ICollection<ItSystem> CanBeUsedBy { get; set; }
+        public virtual ICollection<ItInterface> ExposedInterfaces { get; set; }
+        
         /// <summary>
         /// Gets or sets interfaces that can use this instance.
         /// </summary>
@@ -86,7 +49,8 @@ namespace Core.DomainModel.ItSystem
         /// <value>
         /// Usable interfaces.
         /// </value>
-        public virtual ICollection<ItSystem> CanUseInterfaces { get; set; }
+        public virtual ICollection<ItInterface> CanUseInterfaces { get; set; }
+        
         /// <summary>
         /// Gets or sets the sub (child) it systems.
         /// </summary>
@@ -94,6 +58,7 @@ namespace Core.DomainModel.ItSystem
         /// The children.
         /// </value>
         public virtual ICollection<ItSystem> Children { get; set; }
+        
         public int? ParentId { get; set; }
         /// <summary>
         /// Gets or sets the parent (master) it system.
@@ -102,34 +67,7 @@ namespace Core.DomainModel.ItSystem
         /// The parent.
         /// </value>
         public virtual ItSystem Parent { get; set; }
-        public int OrganizationId { get; set; }
-        /// <summary>
-        /// Gets or sets the organization this instance was created under.
-        /// </summary>
-        /// <value>
-        /// The organization.
-        /// </value>
-        public virtual Organization Organization { get; set; }
-        /// <summary>
-        /// Gets or sets the usages (binding between system and org).
-        /// </summary>
-        /// <value>
-        /// The usages.
-        /// </value>
-        public virtual ICollection<ItSystemUsage> Usages { get; set; }
-
-        public int? AppTypeId { get; set; }
-        /// <summary>
-        /// Gets or sets the type of the application.
-        /// </summary>
-        /// <remarks>
-        /// This is an important property as it changes what properties are relevant.
-        /// </remarks>
-        /// <value>
-        /// The type of the application.
-        /// </value>
-        public virtual AppType AppType { get; set; }
-
+        
         public int? BusinessTypeId { get; set; }
         /// <summary>
         /// Gets or sets the type of the business option.
@@ -139,59 +77,11 @@ namespace Core.DomainModel.ItSystem
         /// </value>
         public virtual BusinessType BusinessType { get; set; }
 
-        #region Interface "Snitflade" data
-
-        public int? InterfaceId { get; set; }
-        /// <summary>
-        /// Gets or sets the interface option.
-        /// Provides details about an it system of type interface.
-        /// </summary>
-        /// <value>
-        /// The interface option.
-        /// </value>
-        public virtual Interface Interface { get; set; }
-
-        public int? InterfaceTypeId { get; set; }
-        /// <summary>
-        /// Gets or sets the type of the interface.
-        /// Provides details about an it system of type interface.
-        /// </summary>
-        /// <value>
-        /// The type of the interface.
-        /// </value>
-        public virtual InterfaceType InterfaceType { get; set; }
-
-        public int? TsaId { get; set; }
-        public virtual Tsa Tsa { get; set; }
-
-        public int? MethodId { get; set; }
-        public virtual Method Method { get; set; }
-
-        public virtual ICollection<DataRow> DataRows { get; set; } 
-
-        #endregion
-
         public virtual ICollection<Wish> Wishes { get; set; }
 
         public virtual ICollection<TaskRef> TaskRefs { get; set; }
 
         public virtual ICollection<ItSystemUsage> Overviews { get; set; }
-
-        /// <summary>
-        /// Gets or sets local usages of the system, in case the system is an interface.
-        /// </summary>
-        /// <value>
-        /// The interface local usages.
-        /// </value>
-        public virtual ICollection<InterfaceUsage> InterfaceLocalUsages { get; set; }
-
-        /// <summary>
-        /// Gets or sets local exposure of the system, in case the system is an interface.
-        /// </summary>
-        /// <value>
-        /// The interface local exposure.
-        /// </value>
-        public virtual ICollection<InterfaceExposure> InterfaceLocalExposure { get; set; }
 
         /// <summary>
         /// Gets or sets local infrastructure usages of the system, in case the system is not an interface.
---
+++ Core.DomainModel/ItSystem/ItSystemBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Core.DomainModel.ItSystem
+{
+    public abstract class ItSystemBase : Entity
+    {
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the user defined system identifier.
+        /// </summary>
+        /// <remarks>
+        /// This identifier is NOT the primary key.
+        /// </remarks>
+        /// <value>
+        /// The user defined system identifier.
+        /// </value>
+        public string ItSystemId { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public AccessModifier AccessModifier { get; set; }
+
+        public int OrganizationId { get; set; }
+        /// <summary>
+        /// Gets or sets the organization this instance was created under.
+        /// </summary>
+        /// <value>
+        /// The organization.
+        /// </value>
+        public virtual Organization Organization { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usages (binding between system and org).
+        /// </summary>
+        /// <value>
+        /// The usages.
+        /// </value>
+        public virtual ICollection<ItSystemUsage> Usages { get; set; }
+    }
+}
