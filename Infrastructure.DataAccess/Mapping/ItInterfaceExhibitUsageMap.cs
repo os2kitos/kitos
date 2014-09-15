e9abf6f21aff26aca9e48357d391123b8305de17
esditm <esd@it-minds.dk>
2014-09-15 14:19:57 +0200

    Added missing file

---
+++ Infrastructure.DataAccess/Mapping/ItInterfaceExhibitUsageMap.cs
@@ -0,0 +1,25 @@
+﻿using Core.DomainModel.ItSystemUsage;
+
+namespace Infrastructure.DataAccess.Mapping
+{
+    class ItInterfaceExhibitUsageMap : EntityMap<ItInterfaceExhibitUsage>
+    {
+        public ItInterfaceExhibitUsageMap()
+        {
+            // Table & Column Mappings
+            this.ToTable("ItInterfaceExhibitUsage");
+
+            this.HasRequired(t => t.ItSystemUsage)
+                .WithMany(d => d.ItInterfaceExhibitUsages)
+                .HasForeignKey(t => t.ItSystemUsageId);
+
+            this.HasRequired(t => t.ItInterfaceExhibit)
+                .WithMany(d => d.ItInterfaceExhibitUsage)
+                .HasForeignKey(t => t.ItInterfaceExhibitId);
+
+            this.HasOptional(t => t.ItContract)
+                .WithMany(d => d.AssociatedInterfaceExposures)
+                .HasForeignKey(t => t.ItContractId);
+        }
+    }
+}
