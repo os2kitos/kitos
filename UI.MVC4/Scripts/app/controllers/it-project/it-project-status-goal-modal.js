67a262a0bb27c080bbd3b7066b2c7882bb4f316c
esditm <esd@it-minds.dk>
2014-10-06 14:02:43 +0200

    Altered status goal modal to be a form modal

---
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-status-goal-modal.js
@@ -0,0 +1,94 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('it-project.edit.status-goal.modal', {
+            url: '/modal/:goalId',
+            onEnter: ['$state', '$stateParams', '$modal', 'goalTypes', 'project',
+                function ($state, $stateParams, $modal, goalTypes, project) {
+                    $modal.open({
+                        size: 'lg',
+                        templateUrl: 'partials/it-project/modal-goal-edit.html',
+                        // fade in instead of slide from top, fixes strange cursor placement in IE
+                        // http://stackoverflow.com/questions/25764824/strange-cursor-placement-in-modal-when-using-autofocus-in-internet-explorer
+                        windowClass: 'modal fade in',
+                        resolve: {
+                            goalId: function() {
+                                return $stateParams.goalId;
+                            },
+                            // workaround to get data inherited from parent state
+                            goalTypes: function() {
+                                return goalTypes;
+                            },
+                            // workaround to get data inherited from parent state
+                            project: function () {
+                                return project;
+                            },
+                            goal: ['$http', function($http) {
+                                var id = $stateParams.goalId;
+                                if (id) {
+                                    return $http.get('api/goal/' + id)
+                                        .then(function(result) {
+                                            return result.data.response;
+                                        });
+                                }
+                                return null;
+                            }]
+                        },
+                        controller: 'project.statusGoalModalCtrl'
+                    }).result.then(function () {
+                        // OK
+                        // GOTO parent state and reload
+                        $state.go('^', null, { reload: true });
+                    }, function () {
+                        // Cancel
+                        // GOTO parent state
+                        $state.go('^');
+                    });
+                }
+            ]
+        });
+    }]);
+
+    app.controller('project.statusGoalModalCtrl',
+    ['$scope', '$http', 'autofocus', 'goal', 'notify', 'goalId', 'goalTypes', 'project',
+        function ($scope, $http, autofocus, goal, notify, goalId, goalTypes, project) {
+            var isNewGoal = goal == null;
+            
+            // set to empty object if falsy
+            $scope.goal = goal ? goal : {};
+            $scope.goalTypes = goalTypes;
+
+            autofocus();
+
+            $scope.opened = {};
+            $scope.open = function ($event, datepicker) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                $scope.opened[datepicker] = true;
+            };
+
+            $scope.dismiss = function () {
+                $scope.$dismiss();
+            };
+
+            $scope.save = function () {
+                var payload = $scope.goal;
+                payload.goalStatusId = project.goalStatus.id,
+                delete payload.id;
+                delete payload.objectOwnerId;
+                delete payload.objectOwner;
+
+                var msg = notify.addInfoMessage("Gemmer ændringer...", false);
+                $http({
+                    method: isNewGoal ? 'POST' : 'PATCH',
+                    url: 'api/goal/' + goalId,
+                    data: payload
+                }).success(function () {
+                    msg.toSuccessMessage("Ændringerne er gemt!");
+                    $scope.$close(true);
+                }).error(function () {
+                    msg.toErrorMessage("Ændringerne kunne ikke gemmes!");
+                });
+            };
+        }]);
+})(angular, app);
--- UI.MVC4/Scripts/app/controllers/it-project/it-project-status-goal.js
+++ UI.MVC4/Scripts/app/controllers/it-project/it-project-status-goal.js
@@ -1,29 +1,35 @@
-﻿(function (ng, app) {
-    app.config(['$stateProvider', function ($stateProvider) {
-        $stateProvider.state('it-project.edit.status-goal', {
-            url: '/status-goal',
-            templateUrl: 'partials/it-project/tab-status-goal.html',
-            controller: 'project.EditStatusGoalCtrl',
-            resolve: {
-                // re-resolve data from parent cause changes here wont cascade to it
-                project: ['$http', '$stateParams', function ($http, $stateParams) {
-                    return $http.get("api/itproject/" + $stateParams.id)
-                        .then(function (result) {
-                            return result.data.response;
-                        });
-                }],
-                goalTypes: ['$http', function($http) {
-                    return $http.get("api/goalType").then(function(result) {
-                        return result.data.response;
-                    });
-                }]
-            }
-        });
-    }]);
+﻿(function(ng, app) {
+    app.config([
+        '$stateProvider', function($stateProvider) {
+            $stateProvider.state('it-project.edit.status-goal', {
+                url: '/status-goal',
+                templateUrl: 'partials/it-project/tab-status-goal.html',
+                controller: 'project.EditStatusGoalCtrl',
+                resolve: {
+                    // re-resolve data from parent cause changes here wont cascade to it
+                    project: [
+                        '$http', '$stateParams', function($http, $stateParams) {
+                            return $http.get("api/itproject/" + $stateParams.id)
+                                .then(function(result) {
+                                    return result.data.response;
+                                });
+                        }
+                    ],
+                    goalTypes: [
+                        '$http', function($http) {
+                            return $http.get("api/goalType").then(function(result) {
+                                return result.data.response;
+                            });
+                        }
+                    ]
+                }
+            });
+        }
+    ]);
 
-    app.controller('project.EditStatusGoalCtrl',
-    ['$scope', '$http', 'notify', '$modal', 'project', 'goalTypes',
-        function ($scope, $http, notify, $modal, project, goalTypes) {
+    app.controller('project.EditStatusGoalCtrl', [
+        '$scope', '$http', 'notify', '$modal', '$state', 'project', 'goalTypes',
+        function ($scope, $http, notify, $modal, $state, project, goalTypes) {
             $scope.goalStatus = project.goalStatus;
             $scope.goalStatus.updateUrl = "api/goalStatus/" + project.goalStatus.id;
 
@@ -34,6 +40,7 @@
             };
 
             $scope.goals = [];
+
             function addGoal(goal) {
                 //add goals means show goal in list
                 goal.show = true;
@@ -44,30 +51,24 @@
                     prevEntry = goal;
                     return;
                 }
-                
+
                 //otherwise:
 
                 //easy-access functions
                 goal.edit = function() {
-                    editGoal(goal);
+                    $state.go('.modal', { goalId: goal.id });
                 };
-                
-                goal.delete = function() {
 
+                goal.delete = function() {
                     var msg = notify.addInfoMessage("Sletter... ");
                     $http.delete(goal.updateUrl).success(function() {
-
                         goal.show = false;
-
                         msg.toSuccessMessage("Slettet!");
-
                     }).error(function() {
-
                         msg.toErrorMessage("Fejl! Kunne ikke slette!");
                     });
-
                 };
-                
+
                 goal.updateUrl = "api/goal/" + goal.id;
                 $scope.goals.push(goal);
             }
@@ -84,48 +85,15 @@
                     data: payload
                 });
             }
-            
-            $scope.updateStatusDate = function () {
+
+            $scope.updateStatusDate = function() {
                 patch($scope.goalStatus.updateUrl, "statusDate", $scope.project.statusDate)
-                    .success(function () {
+                    .success(function() {
                         notify.addSuccessMessage("Feltet er opdateret");
-                    }).error(function () {
+                    }).error(function() {
                         notify.addErrorMessage("Fejl!");
                     });
             };
-
-            $scope.addGoal = function() {
-                $http.post("api/goal", {
-                    goalStatusId: project.goalStatus.id,
-                    goalTypeId: 1
-                }).success(function(result) {
-                    notify.addSuccessMessage("Nyt mål tilføjet!");
-
-                    addGoal(result.response);
-
-                }).error(function() {
-                    notify.addErrorMessage("Kunne ikke oprette nyt mål!");
-                });
-            };
-
-            function editGoal(goal) {
-                var modal = $modal.open({
-                    size: 'lg',
-                    templateUrl: 'partials/it-project/modal-goal-edit.html',
-                    controller: ['$scope', 'autofocus', function ($modalScope, autofocus) {
-                        autofocus();
-                        $modalScope.goal = goal;
-                        $modalScope.goalTypes = goalTypes;
-                                                
-                        $modalScope.opened = {};
-                        $modalScope.open = function ($event, datepicker) {
-                            $event.preventDefault();
-                            $event.stopPropagation();
-
-                            $modalScope.opened[datepicker] = true;
-                        };
-                    }]
-                });
-            }
-        }]);
+        }
+    ]);
 })(angular, app);
