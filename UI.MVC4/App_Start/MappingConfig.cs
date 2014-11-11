de1b79f7e762f643c71491e2a505ef802c6b02c2
esditm <esd@it-minds.dk>
2014-11-11 11:54:26 +0100

    Reverted not needed change

--- UI.MVC4/App_Start/MappingConfig.cs
+++ UI.MVC4/App_Start/MappingConfig.cs
@@ -261,7 +261,7 @@ namespace UI.MVC4.App_Start
                 .ForMember(dest => dest.MainContractId, opt => opt.MapFrom(src => src.MainContract.ItContractId))
                 .ForMember(dest => dest.MainContractIsActive, opt => opt.MapFrom(src => src.MainContract.ItContract.IsActive))
                 .ForMember(dest => dest.InterfaceExhibitCount, opt => opt.MapFrom(src => src.ItSystem.ItInterfaceExhibits.Count))
-                .ForMember(dest => dest.InterfaceUseCount, opt => opt.MapFrom(src => src.ItSystem.CanUseInterfaces.Count()))
+                .ForMember(dest => dest.InterfaceUseCount, opt => opt.MapFrom(src => src.ItSystem.CanUseInterfaces.Count))
                 .ForMember(dest => dest.ActiveInterfaceUseCount, opt => opt.MapFrom(src => src.ItSystem.CanUseInterfaces.Sum(x => x.InterfaceUsages.Count(y => y.ItContract.IsActive))))
                 .ReverseMap()
                 .ForMember(dest => dest.OrgUnits, opt => opt.Ignore())
