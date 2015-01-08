06ed361a44c1c2fe8b9a949027153af18f9610e6
esditm <esd@it-minds.dk>
2015-01-08 11:07:50 +0100

    Implemented MOX test

---
+++ UI.MVC4/Scripts/App/Controllers/mox/mox-order.js
@@ -0,0 +1,21 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('mox.order', {
+            url: '/order',
+            templateUrl: 'partials/mox/mox-order.html',
+            controller: 'mox.OrderCtrl',
+            resolve: {
+                user: [
+                    'userService', function (userService) {
+                        return userService.getUser();
+                    }
+                ]
+            }
+        });
+    }]);
+
+    app.controller('mox.OrderCtrl', ['$scope', '$http', 'notify', 'user',
+            function ($scope, $http, notify, user) {
+                
+            }]);
+})(angular, app);
---
+++ UI.MVC4/Scripts/App/Controllers/mox/mox-overview.js
@@ -0,0 +1,17 @@
+﻿(function (ng, app) {
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('mox.overview', {
+            url: '/overview',
+            templateUrl: 'partials/mox/mox-overview.html',
+            controller: 'mox.OverviewCtrl',
+            resolve: {
+                
+            }
+        });
+    }]);
+
+    app.controller('mox.OverviewCtrl', ['$scope', '$http', 'notify',
+            function ($scope, $http, notify) {
+
+            }]);
+})(angular, app);
