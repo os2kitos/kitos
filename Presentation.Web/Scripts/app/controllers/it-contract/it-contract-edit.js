e0231bb9720ea4b2bdaaa38dac4e9b74a3ac5b03
flemmo87 <flemmo87@gmail.com>
2015-02-13 12:50:26 +0100

    switched more user.name references to use user.qualifiedName instead.

--- Presentation.Web/Scripts/app/controllers/it-contract/it-contract-edit.js
+++ Presentation.Web/Scripts/app/controllers/it-contract/it-contract-edit.js
@@ -243,7 +243,7 @@
                 if (signer) {
                     userForSelect = {
                         id: signer.id,
-                        text: signer.name
+                        text: signer.qualifiedName
                     };
                 }
 
--- Presentation.Web/Scripts/app/directives/directives.js
+++ Presentation.Web/Scripts/app/directives/directives.js
@@ -277,7 +277,7 @@
 
                                         results.push({
                                             id: user.id, //select2 mandatory
-                                            text: user.name, //select2 mandatory
+                                            text: user.qualifiedName, //select2 mandatory
                                             user: user //not mandatory, for extra info when formatting
                                         });
                                     });
