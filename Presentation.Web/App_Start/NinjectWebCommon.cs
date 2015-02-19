02aa33ab7105a2df3a5c47164303229c2b935993
flemmo87 <flemmo87@gmail.com>
2015-02-19 16:37:57 +0100

    removed obsolete stuff + renamed webconfig-property from "Dev" to "Test"

--- Presentation.Web/App_Start/NinjectWebCommon.cs
+++ Presentation.Web/App_Start/NinjectWebCommon.cs
@@ -77,8 +77,7 @@ namespace Presentation.Web.App_Start
             kernel.Bind<IUserService>().To<UserService>().InRequestScope()
                 .WithConstructorArgument("ttl", Settings.Default.ResetPasswordTTL)
                 .WithConstructorArgument("baseUrl", Settings.Default.BaseUrl)
-                .WithConstructorArgument("mailSuffix", Settings.Default.MailSuffix)
-                .WithConstructorArgument("environment", Settings.Default.Environment);
+                .WithConstructorArgument("mailSuffix", Settings.Default.MailSuffix);
             kernel.Bind<IOrgUnitService>().To<OrgUnitService>().InRequestScope();
             kernel.Bind<IAdminService>().To<AdminService>().InRequestScope();
             kernel.Bind<IOrganizationService>().To<OrganizationService>().InRequestScope();
