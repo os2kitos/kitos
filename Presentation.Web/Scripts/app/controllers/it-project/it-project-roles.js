9581e67db8b260f2cc32bccd8df280752b3d7a8e
flemmo87 <flemmo87@gmail.com>
2015-02-20 15:27:00 +0100

    reverted indenting further

--- Presentation.Web/Scripts/app/controllers/it-project/it-project-roles.js
+++ Presentation.Web/Scripts/app/controllers/it-project/it-project-roles.js
@@ -8,22 +8,19 @@
             controller: 'project.EditRolesCtrl',
             resolve: {
                 // re-resolve data from parent cause changes here wont cascade to it
-                project: [
-                    '$http', '$stateParams', function($http, $stateParams) {
+                project: ['$http', '$stateParams', function($http, $stateParams) {
                         return $http.get("api/itproject/" + $stateParams.id)
                             .then(function(result) {
                                 return result.data.response;
                             });
                 }],
-                itProjectRights: [
-                    '$http', '$stateParams', function($http, $stateParams) {
+                itProjectRights: ['$http', '$stateParams', function($http, $stateParams) {
                         return $http.get("api/itprojectrights/" + $stateParams.id)
                             .then(function(result) {
                                 return result.data.response;
                             });
                 }],
-                itProjectRoles: [
-                    '$http', function($http) {
+                itProjectRoles: ['$http', function($http) {
                         return $http.get("api/itprojectrole/?nonsuggestions=")
                             .then(function(result) {
                                 return result.data.response;
