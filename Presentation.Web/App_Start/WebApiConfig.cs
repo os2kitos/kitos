329a276a5a6748b3e9cf2716e34940186c81cbe4
flemmo87 <flemmo87@gmail.com>
2015-02-24 16:21:17 +0100

    Added odata controller + odata routing

--- Presentation.Web/App_Start/WebApiConfig.cs
+++ Presentation.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,15 @@
-﻿using System.Web.Http;
+﻿using System.EnterpriseServices;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using Core.DomainModel;
+using Core.DomainModel.ItContract;
+using Core.DomainModel.ItProject;
+using Core.DomainModel.ItSystem;
+using Core.DomainModel.ItSystemUsage;
+using Infrastructure.DataAccess;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Presentation.Web
 {
@@ -16,6 +27,86 @@ namespace Presentation.Web
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
             //config.EnableQuerySupport();
+
+            //OData
+            var builder = new ODataModelBuilder();
+
+            //builder.EntitySet<AdminRight>("AdminRights");
+            //builder.EntitySet<AdminRole>("AdminRoles");
+            //builder.EntitySet<Advice>("Advices");
+            //builder.EntitySet<AgreementElement>("AgreementElements");
+            //builder.EntitySet<ArchiveType>("ArchiveTypes");
+            //builder.EntitySet<BusinessType>("BusinessTypes");
+            //builder.EntitySet<Communication>("Communications");
+            //builder.EntitySet<Config>("Configs");
+            //builder.EntitySet<ContractTemplate>("ContractTemplates");
+            //builder.EntitySet<ContractType>("ContractTypes");
+            //builder.EntitySet<DataType>("DataTypes");
+            //builder.EntitySet<DataRow>("DataRows");
+            //builder.EntitySet<DataRowUsage>("DataRowUsages");
+            //builder.EntitySet<EconomyYear>("EconomyYears");
+            //builder.EntitySet<EconomyStream>("EconomyStrams");
+            //builder.EntitySet<Frequency>("Frequencies");
+            //builder.EntitySet<Goal>("Goals");
+            //builder.EntitySet<GoalStatus>("GoalStatus");
+            //builder.EntitySet<GoalType>("GoalTypes");
+            //builder.EntitySet<Handover>("Handovers");
+            //builder.EntitySet<HandoverTrial>("HandoverTrials");
+            //builder.EntitySet<HandoverTrialType>("HandoverTrialTypes");
+            //builder.EntitySet<Interface>("Interfaces");
+            //builder.EntitySet<ItInterfaceUse>("ItInterfaceUses");
+            //builder.EntitySet<InterfaceUsage>("InterfaceUsages");
+            //builder.EntitySet<ItInterfaceExhibit>("ItInterfaceExhibits");
+            //builder.EntitySet<ItInterfaceExhibitUsage>("InterfaceExhibtUsages");
+            //builder.EntitySet<InterfaceType>("InterfaceTypes");
+            //builder.EntitySet<ItContract>("ItContracts");
+            //builder.EntitySet<ItContractItSystemUsage>("ItContractItSystemUsages");
+            //builder.EntitySet<ItContractRight>("ItContractRights");
+            //builder.EntitySet<ItContractRole>("ItContractRoles");
+            //builder.EntitySet<ItProject>("ItProjects");
+            //builder.EntitySet<ItProjectPhase>("ItProjectPhases");
+            //builder.EntitySet<ItProjectStatus>("ItProjectStatuses");
+            //builder.EntitySet<ItProjectRight>("ItProjectRights");
+            //builder.EntitySet<ItProjectRole>("ItProjectRoles");
+            //builder.EntitySet<ItProjectOrgUnitUsage>("ItProjectOrgUnitUsages");
+            //builder.EntitySet<ItSystemUsageOrgUnitUsage>("ItSystemUsageOrgUnitUsages");
+            builder.EntitySet<ItSystem>("ItSystems").EntityType.HasKey(x => x.Id);
+            //builder.EntitySet<ItSystemUsage>("ItSystemUsages");
+            //builder.EntitySet<ItSystemRight>("ItSystemRights");
+            //builder.EntitySet<ItSystemRole>("ItSystemRoles");
+            //builder.EntitySet<ItSystemTypeOption>("ItSystemTypeOptions");
+            //builder.EntitySet<Method>("Methods");
+            //builder.EntitySet<OptionExtend>("OptionExtention");
+            //builder.EntitySet<Organization>("Organizations");
+            //builder.EntitySet<OrganizationUnit>("OrganizationUnits");
+            //builder.EntitySet<OrganizationRight>("OrganizationRights");
+            //builder.EntitySet<OrganizationRole>("OrganizationRoles");
+            //builder.EntitySet<PasswordResetRequest>("PasswordResetRequests");
+            //builder.EntitySet<PaymentFreqency>("PaymentFreqencies");
+            //builder.EntitySet<PaymentMilestone>("PaymentMilestones");
+            //builder.EntitySet<PaymentModel>("PaymentModels");
+            //builder.EntitySet<PriceRegulation>("PriceRegulations");
+            //builder.EntitySet<ProcurementStrategy>("ProcurementStrategies");
+            //builder.EntitySet<ItProjectType>("ProjectTypes");
+            //builder.EntitySet<PurchaseForm>("PurchaseForms");
+            //builder.EntitySet<Risk>("Risks");
+            //builder.EntitySet<SensitiveDataType>("SensitiveDataTypes");
+            //builder.EntitySet<Stakeholder>("Stakeholders");
+            //builder.EntitySet<TerminationDeadline>("TerminationDeadlines");
+            //builder.EntitySet<TaskRef>("TaskRefs");
+            //builder.EntitySet<TaskUsage>("TaskUsages");
+            //builder.EntitySet<Text>("Texts");
+            //builder.EntitySet<Tsa>("Tsas");
+            //builder.EntitySet<User>("Users");
+            //builder.EntitySet<Wish>("Wishes");
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.MapODataServiceRoute(
+                routeName: "odata",
+                routePrefix: "odata",
+                model: builder.GetEdmModel());
         }
     }
 }
---
+++ Presentation.Web/Controllers/OData/ItSystemController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using Core.DomainModel;
+using Core.DomainModel.ItSystem;
+using Core.DomainServices;
+using Infrastructure.DataAccess;
+
+namespace Presentation.Web.Controllers.OData
+{
+    public class ItSystemsController : ODataController
+    {
+        private readonly IGenericRepository<TaskRef> _taskRepository;
+        private readonly IItSystemService _systemService;
+        private readonly IGenericRepository<ItSystem> _itSystemRepository;
+        public ItSystemsController(IGenericRepository<ItSystem> itSystemRepository, IGenericRepository<ItSystem> repository, IGenericRepository<TaskRef> taskRepository, IItSystemService systemService)
+        {
+            _taskRepository = taskRepository;
+            _systemService = systemService;
+            _itSystemRepository = itSystemRepository;
+        }
+        
+        KitosContext db = new KitosContext();
+
+        private bool SystemExists(int key)
+        {
+            return db.ItSystems.Any(s => s.Id == key);
+        }
+
+        [EnableQuery]
+        public IQueryable<ItSystem> Get()
+        {
+            return _itSystemRepository.AsQueryable();
+        }
+
+        [EnableQuery]
+        public SingleResult<ItSystem> Get([FromODataUri] int key)
+        {
+            IQueryable<ItSystem> result = db.ItSystems.Where(x => x.Id == key);
+            return SingleResult.Create(result);
+        }
+
+        public async Task<IHttpActionResult> Post(ItSystem itSystem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.ItSystems.Add(itSystem);
+            await db.SaveChangesAsync();
+            return Created(itSystem);
+        }
+
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<ItSystem> itSystem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var entity = await db.ItSystems.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            itSystem.Patch(entity);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SystemExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(entity);
+        }
+
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, ItSystem update)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (key != update.Id)
+            {
+                return BadRequest();
+            }
+            db.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SystemExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(update);
+        }
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var itSystem = await db.ItSystems.FindAsync(key);
+            if (itSystem == null)
+            {
+                return NotFound();
+            }
+            db.ItSystems.Remove(itSystem);
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+        
+
+    }
+}
