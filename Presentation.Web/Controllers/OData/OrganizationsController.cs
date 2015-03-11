88324f4fb77af9cafaa385eed2fe48292d3feb26
esditm <esd@it-minds.dk>
2015-03-11 17:50:12 +0100

    Added Org OData Controller

---
+++ Presentation.Web/Controllers/OData/OrganizationsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Web.Http.OData;
+using Core.DomainModel;
+using Core.DomainModel.ItSystem;
+using Core.DomainServices;
+
+namespace Presentation.Web.Controllers.OData
+{
+    public class OrganizationsController : BaseController<Organization>
+    {
+        public OrganizationsController(IGenericRepository<Organization> repository)
+            : base(repository)
+        {
+        }
+
+        //// GET /Organizations(1)/ItSystems
+        //[EnableQuery]
+        //public IQueryable<ItSystem> GetItSystems([FromODataUriAttribute] int key)
+        //{
+        //    var result = Repository.AsQueryable().Where(m => m.Id == key).SelectMany(x => x.ItSystems);
+        //    return result;
+        //}
+    }
+}
