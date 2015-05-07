4674326f0d18ecd829f3dcff55c2f98cf710ca6a
esditm <esd@it-minds.dk>
2015-05-07 19:33:43 +0200

    Implemented version field using kendo

--- Presentation.Web/Scripts/app/controllers/it-system/it-interface-edit.js
+++ Presentation.Web/Scripts/app/controllers/it-system/it-interface-edit.js
@@ -27,14 +27,6 @@
                     'userService', function(userService) {
                         return userService.getUser();
                     }
-                ],
-                versionOptions: [
-                    '$http', function ($http) {
-                        return $http.get('api/versionOption/')
-                            .then(function (result) {
-                                return result.data.response;
-                            });
-                    }
                 ]
             }
         });
@@ -42,12 +34,10 @@
 
     app.controller('system.interfaceEditCtrl',
     [
-        '$rootScope', '$scope', '$http', '$state', 'notify', 'itInterface', 'hasWriteAccess', 'autofocus', 'user', 'versionOptions',
-        function ($rootScope, $scope, $http, $state, notify, itInterface, hasWriteAccess, autofocus, user, versionOptions) {
+        '$rootScope', '$scope', '$http', '$state', 'notify', 'itInterface', 'hasWriteAccess', 'autofocus', 'user',
+        function ($rootScope, $scope, $http, $state, notify, itInterface, hasWriteAccess, autofocus, user) {
             $rootScope.page.title = 'Snitflade - Rediger';
             autofocus();
-
-            $scope.versionOptions = versionOptions;
 
             itInterface.belongsTo = (!itInterface.belongsToId) ? null : { id: itInterface.belongsToId, text: itInterface.belongsToName };
             itInterface.updateUrl = 'api/itInterface/' + itInterface.id;
@@ -95,6 +85,21 @@
                     }
                 };
             }
+
+            var versionDataSource = new kendo.data.DataSource({
+                type: "odata-v4",
+                serverFiltering: true,
+                transport: {
+                    read: "/odata/ItInterfaces?$select=version"
+                }
+            });
+            $scope.versionKendoConfig = {
+                dataTextField: "Version",
+                filter: "contains",
+                minLength: 1,
+                delay: 500,
+                dataSource: versionDataSource
+            };
         }
     ]);
 })(angular, app);
