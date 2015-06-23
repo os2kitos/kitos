d65ace733bbe008a3e1cf0489349c92ff3312226
MortenAstrup <morten@it-minds.dk>
2015-06-23 13:56:54 +0200

    Clean up

--- Presentation.Web/Controllers/API/ItProjectController.cs
+++ Presentation.Web/Controllers/API/ItProjectController.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Reflection;
 using System.Text;
 using System.Web.Http;
 using Core.ApplicationServices;
