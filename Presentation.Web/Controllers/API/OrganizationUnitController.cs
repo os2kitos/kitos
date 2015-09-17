4c548337a8a1c98c014d9e4377ec3822c59dc9bb
esditm <esd@it-minds.dk>
2015-09-17 14:19:04 +0200

    Activated lazy loading of default users so their default org unit is correctly set to null

--- Presentation.Web/Controllers/API/OrganizationUnitController.cs
+++ Presentation.Web/Controllers/API/OrganizationUnitController.cs
@@ -215,5 +215,13 @@ namespace Presentation.Web.Controllers.API
                 return Error(e);
             }
         }
+
+        protected override void DeleteQuery(OrganizationUnit entity)
+        {
+            // http://stackoverflow.com/questions/15226312/entityframewok-how-to-configure-cascade-delete-to-nullify-foreign-keys
+            // when children are loaded into memory the foreign key is correctly set to null on children when deleted
+            var dummy = entity.DefaultUsers; // users that have this organization unit set as their default
+            base.DeleteQuery(entity);
+        }
     }
 }
