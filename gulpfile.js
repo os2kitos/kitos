f423f1af4097a5c2f69ec565d533f1e534ef040d
BSO <bso@it-minds.dk>
2015-11-03 13:38:42 +0100

    Pass build on linting errors and bundler added to gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp'),
 	tslint = require('gulp-tslint'),
 	eslint = require('gulp-eslint'),
+    bundle = require('gulp-bundle-assets'),
 	Config = require('./gulpfile.config');
 var config = new Config()
 
@@ -9,11 +10,13 @@ gulp.task('default', function() {
   // place code for your default task here
 });
 
+gulp.task('lint', ['es-lint', 'ts-lint']);
+
 gulp.task('ts-lint', function () {
     return gulp.src(config.allTypeScript)
 		.pipe(tslint())
 		.pipe(tslint.report('prose', {
-			summarizeFailureOutput: true
+            emitError: false // Set to true to fail build on errors
 		}));
 });
 
@@ -25,4 +28,9 @@ gulp.task('es-lint', function() {
 		//.pipe(eslint.failAfterError());
 });
 
-gulp.task('lint', ['es-lint', 'ts-lint']);
+gulp.task('bundle', function () {
+    return gulp.src('./bundle.config.js')
+      .pipe(bundle())
+      .pipe(bundle.results('./public'))
+      .pipe(gulp.dest('./public'));
+});
