55027d8a91336f7990dc2e9ec82c3ed3b7e5b5ee
BSO <bso@it-minds.dk>
2015-11-03 11:10:47 +0100

    Jasmin test in TypeScript.

---
+++ Presentation.Web/Scripts/app/controllers/Home/tstest.spec.ts
@@ -0,0 +1,11 @@
+﻿module Tests.Unit.HomeController {
+    describe("A controller", () => {
+        it("should be true", () => {
+            expect(true).toBe(true);
+        });
+
+        it("should be false", () => {
+            expect(false).toBe(false);
+        });
+    });
+}
