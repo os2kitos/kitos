a4f17c09b884c35c7720f165c699366fe9bc55ee
BSO <bso@it-minds.dk>
2015-11-05 15:46:40 +0100

    Protractor setup. Runs locally with dummy e2e test.

---
+++ Presentation.Web/protractor.conf.js
@@ -0,0 +1,7 @@
+﻿exports.config = {
+    // selenium server address (default)
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+
+    // select all end to end tests
+    specs: ['Tests/**/*.e2e.spec.js']
+};
