09d2802e82130b958324692b141201304e5e7b78
BSO <bso@it-minds.dk>
2015-11-10 11:31:33 +0100

    Change in certificate generator.

--- InstallSelfSignedCert.ps1
+++ InstallSelfSignedCert.ps1
@@ -3,7 +3,7 @@ $rootStore = Get-Item cert:\LocalMachine\Root
 $rootStore.Open("ReadWrite")
 $rootStore.Add($cert)
 $rootStore.Close();
-Import-Module WebAdministration
+# Import-Module WebAdministration
 # Set-Location IIS:\SslBindings
 # New-WebBinding -Name "Kitos" -IP "*" -Port 44300 -Protocol https
-$cert | New-Item 0.0.0.0!44300
+# $cert | New-Item 0.0.0.0!44300
