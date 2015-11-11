8d34655c24cb2ca14e857149eb0ce46e33ec3344
BSO <bso@it-minds.dk>
2015-11-11 09:11:38 +0100

    Silence xUnit, OpenCover and Reportgenerator

--- run-tests.bat
+++ run-tests.bat
@@ -1,1 +1,1 @@
-packages\xunit.runner.console.2.1.0\tools\xunit.console.exe %TEST_ASSEMBLIES% -noshadow -appveyor -nologo
+packages\xunit.runner.console.2.1.0\tools\xunit.console.exe %TEST_ASSEMBLIES% -noshadow -appveyor -nologo -quiet
