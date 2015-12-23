e8a90820994ad9b5b3bfd20a8686f55c309b426b
Bjørn Sørensen <bso@it-minds.dk>
2015-12-23 10:30:17 +0100

    Added NuGet type definitions for protractor-http-mock.

--- Presentation.Web/Scripts/typings/protractor-http-mock/protractor-http-mock.d.ts
+++ Presentation.Web/Scripts/typings/protractor-http-mock/protractor-http-mock.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for protractor-http-mock
+// Type definitions for protractor-http-mock
 // Project: https://github.com/atecarlos/protractor-http-mock
 // Definitions by: Crevil <https://github.com/Crevil>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
