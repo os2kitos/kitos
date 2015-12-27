93551d4bb591edf7936e0c2f79c1fcad68aabc8a
Bjørn Sørensen <bso@it-minds.dk>
2015-12-27 21:12:50 +0100

    Use only chrome on CI and suppress jasmine dot reporter.

--- protractor.conf.js
+++ protractor.conf.js
@@ -21,71 +21,72 @@ exports.config = {
             'os': 'Windows',
             'os_version': '7',
             'resolution': '1280x1024'
-        },
-        {
-            // IE 10
-            'browserstack.user': process.env.BROWSERSTACK_USER,
-            'browserstack.key': process.env.BROWSERSTACK_KEY,
-            'browserstack.local': 'true',
-
-            'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
-            'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
-
-            // settings for the browser to test
-            'browserName': 'IE',
-            'browser_version': '10.0',
-            'os': 'Windows',
-            'os_version': '7',
-            'resolution': '1280x1024'
-        },
-        {
-            // IE 11
-            'browserstack.user': process.env.BROWSERSTACK_USER,
-            'browserstack.key': process.env.BROWSERSTACK_KEY,
-            'browserstack.local': 'true',
-
-            'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
-            'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
-
-            // settings for the browser to test
-            'browserName': 'IE',
-            'browser_version': '11.0',
-            'os': 'Windows',
-            'os_version': '7',
-            'resolution': '1280x1024'
-        },
-        {
-            // Edge 12
-            'browserstack.user': process.env.BROWSERSTACK_USER,
-            'browserstack.key': process.env.BROWSERSTACK_KEY,
-            'browserstack.local': 'true',
-
-            'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
-            'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
-
-            // settings for the browser to test
-            'browserName': 'Edge',
-            'browser_version': '12.0',
-            'os': 'Windows',
-            'os_version': '10',
-            'resolution': '1280x1024'
-        },
-        {
-            // Firefox 42
-            'browserstack.user': process.env.BROWSERSTACK_USER,
-            'browserstack.key': process.env.BROWSERSTACK_KEY,
-            'browserstack.local': 'true',
-
-            'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
-            'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
-
-            // settings for the browser to test
-            'browserName': 'Firefox',
-            'browser_version': '42.0',
-            'os': 'Windows',
-            'os_version': '7',
-            'resolution': '1280x1024'
         }
+        //,
+        //{
+        //    // IE 10
+        //    'browserstack.user': process.env.BROWSERSTACK_USER,
+        //    'browserstack.key': process.env.BROWSERSTACK_KEY,
+        //    'browserstack.local': 'true',
+
+        //    'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
+        //    'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
+
+        //    // settings for the browser to test
+        //    'browserName': 'IE',
+        //    'browser_version': '10.0',
+        //    'os': 'Windows',
+        //    'os_version': '7',
+        //    'resolution': '1280x1024'
+        //},
+        //{
+        //    // IE 11
+        //    'browserstack.user': process.env.BROWSERSTACK_USER,
+        //    'browserstack.key': process.env.BROWSERSTACK_KEY,
+        //    'browserstack.local': 'true',
+
+        //    'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
+        //    'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
+
+        //    // settings for the browser to test
+        //    'browserName': 'IE',
+        //    'browser_version': '11.0',
+        //    'os': 'Windows',
+        //    'os_version': '7',
+        //    'resolution': '1280x1024'
+        //},
+        //{
+        //    // Edge 12
+        //    'browserstack.user': process.env.BROWSERSTACK_USER,
+        //    'browserstack.key': process.env.BROWSERSTACK_KEY,
+        //    'browserstack.local': 'true',
+
+        //    'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
+        //    'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
+
+        //    // settings for the browser to test
+        //    'browserName': 'Edge',
+        //    'browser_version': '12.0',
+        //    'os': 'Windows',
+        //    'os_version': '10',
+        //    'resolution': '1280x1024'
+        //},
+        //{
+        //    // Firefox 42
+        //    'browserstack.user': process.env.BROWSERSTACK_USER,
+        //    'browserstack.key': process.env.BROWSERSTACK_KEY,
+        //    'browserstack.local': 'true',
+
+        //    'project': process.env.APPVEYOR_PROJECT_NAME || 'kitos local',
+        //    'build': process.env.APPVEYOR_BUILD_NUMBER || 'local build',
+
+        //    // settings for the browser to test
+        //    'browserName': 'Firefox',
+        //    'browser_version': '42.0',
+        //    'os': 'Windows',
+        //    'os_version': '7',
+        //    'resolution': '1280x1024'
+        //}
     ],
 
     // select all end to end tests
@@ -95,9 +96,9 @@ exports.config = {
     allScriptsTimeout: 90000,
     baseUrl: 'https://localhost:44300',
 
-    // jasmine timeout options
     jasmineNodeOpts: {
-        defaultTimeoutInterval: 90000
+        defaultTimeoutInterval: 90000,
+        print: function () { }
     },
 
     onPrepare: function () {
