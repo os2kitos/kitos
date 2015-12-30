fd051e1ee30ab1a4cb1dcf896a2a4bb2fa413ea7
Bjørn Sørensen <bso@it-minds.dk>
2015-12-30 15:02:21 +0100

    e2e tests of project edit tab strategy

---
+++ Presentation.Web/app/components/it-project/tabs/it-project-tab-strategy.po.ts
@@ -0,0 +1,18 @@
+﻿import IPageObject = require("../../../../Tests/object-wrappers/IPageObject.po");
+import Select2Wrapper = require("../../../../Tests/object-wrappers/Select2Wrapper");
+
+class ItProjectEditTabStrategy implements IPageObject {
+    controllerVm: string = "projectStatusVm";
+
+    getPage(): webdriver.promise.Promise<void> {
+        return browser.get("https://localhost:44300/#/project/edit/1/strategy");
+    }
+
+    // joint municipal project
+    jointMunicipalSelector = new Select2Wrapper("#s2id_jointDrop");
+
+    // common public project
+    commonPublicSelector = new Select2Wrapper("#s2id_commonDrop");
+}
+
+export = ItProjectEditTabStrategy;
