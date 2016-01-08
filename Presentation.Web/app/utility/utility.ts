aeadd2f07c79d1485559ce58577cf0fe0d878f45
esd <esd@it-minds.dk>
2016-01-08 14:39:31 +0100

    Converted to typescript

--- Presentation.Web/app/utility/utility.ts
+++ Presentation.Web/app/utility/utility.ts
@@ -1,9 +1,15 @@
-﻿function waitCursor() {
-    $("body").addClass("wait");
+﻿// global functions
+function waitCursor() {
+    jQuery("body").addClass("wait");
 }
 
 function normalCursor() {
-    $("body").removeClass("wait");
+    jQuery("body").removeClass("wait");
+}
+
+// array extension
+interface Array<T> {
+    pushArray(arr: Array<any>): void;
 }
 
 // http://stackoverflow.com/questions/4156101/javascript-push-array-values-into-another-array
@@ -11,9 +17,14 @@ Array.prototype.pushArray = function (arr) {
     this.push.apply(this, arr);
 };
 
+// string extension
+interface String {
+    repeat(count: any|number): string;
+}
+
 String.prototype.repeat = function (count) {
-    if (count < 1) return '';
-    var result = '', pattern = this.valueOf();
+    if (count < 1) return "";
+    var result = "", pattern = this.valueOf();
     while (count > 1) {
         if (count & 1) result += pattern;
         count >>= 1, pattern += pattern;
