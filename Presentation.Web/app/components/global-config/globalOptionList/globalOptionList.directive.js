5f451459322cb3d4fd87fa018973f2c6d2621f57
Bjørn Sørensen <bso@it-minds.dk>
2016-01-13 12:44:49 +0100

    Split directives to separate files.

---
+++ Presentation.Web/app/components/global-config/globalOptionList/globalOptionList.directive.js
@@ -0,0 +1,67 @@
+﻿(function(ng, app) {
+    'use strict';
+
+    app.directive('globalOptionList', [
+        '$http', '$timeout', '$state', '$stateParams', 'notify', function($http, $timeout, $state, $stateParams, notify) {
+            return {
+                scope: {
+                    optionsUrl: '@',
+                    title: '@',
+                    orgid: '@'
+                },
+                templateUrl: 'app/shared/optionList/optionList.view.html',
+                link: function(scope, element, attrs) {
+
+                    scope.list = [];
+                    $http.get(scope.optionsUrl + '?organizationId=' + scope.orgid + '&nonsuggestions').success(function(result) {
+                        _.each(result.response, function(v) {
+                            scope.list.push({
+                                id: v.id,
+                                name: v.name,
+                                note: v.note,
+                                isActive: v.isActive
+                            });
+                        });
+                    });
+
+                    scope.suggestions = [];
+                    $http.get(scope.optionsUrl + '?organizationId=' + scope.orgid + '&suggestions').success(function(result) {
+                        _.each(result.response, function(v) {
+                            scope.suggestions.push({
+                                id: v.id,
+                                name: v.name,
+                                note: v.note
+                            });
+                        });
+                    });
+
+                    scope.approve = function(id) {
+                        var msg = notify.addInfoMessage("Gemmer...", false);
+                        $http({ method: 'PATCH', url: scope.optionsUrl + '/' + id + '?organizationId=' + scope.orgid, data: { isSuggestion: false } })
+                            .success(function() {
+                                msg.toSuccessMessage("Valgmuligheden er opdateret.");
+                                // reload page to show changes
+                                reload();
+                            })
+                            .error(function() {
+                                msg.toErrorMessage("Fejl! Valgmuligheden kunne ikke ændres!");
+                            });
+                    };
+
+                    // work around for $state.reload() not updating scope
+                    // https://github.com/angular-ui/ui-router/issues/582
+                    function reload() {
+                        return $state.transitionTo($state.current, $stateParams, {
+                            reload: true
+                        }).then(function() {
+                            scope.hideContent = true;
+                            return $timeout(function() {
+                                return scope.hideContent = false;
+                            }, 1);
+                        });
+                    };
+                }
+            };
+        }
+    ]);
+})(angular, app);
---
+++ Presentation.Web/app/shared/optionList/optionList.directive.js
@@ -0,0 +1,29 @@
+﻿(function(ng, app) {
+    'use strict';
+
+    app.directive('optionList', [
+        '$http', function($http) {
+            return {
+                scope: {
+                    optionsUrl: '@',
+                    title: '@',
+                },
+                templateUrl: 'app/shared/optionList/optionList.view.html',
+                link: function(scope, element, attrs) {
+
+                    scope.list = [];
+
+                    $http.get(scope.optionsUrl + '?nonsuggestions').success(function(result) {
+                        _.each(result.response, function(v) {
+                            scope.list.push({
+                                id: v.id,
+                                name: v.name,
+                                note: v.note
+                            });
+                        });
+                    });
+                }
+            };
+        }
+    ]);
+})(angular, app);
