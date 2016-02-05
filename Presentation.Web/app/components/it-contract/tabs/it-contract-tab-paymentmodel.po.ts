22b9307725510ffd58c2cfc55e5bdcec53868ac5
Bjørn Sørensen <bso@it-minds.dk>
2016-02-05 15:49:06 +0100

    WIP: e2e tests of it contract edit tab paymentModel

---
+++ Presentation.Web/app/components/it-contract/tabs/it-contract-tab-paymentmodel.po.ts
@@ -0,0 +1,17 @@
+﻿import IPageObject = require("../../../../Tests/object-wrappers/IPageObject.po");
+import Select2Wrapper = require("../../../../Tests/object-wrappers/Select2Wrapper");
+import RepeaterWrapper = require("../../../../Tests/object-wrappers/RepeaterWrapper");
+
+class ItContractEditTabPaymentModelPo implements IPageObject {
+    public getPage(): webdriver.promise.Promise<void> {
+        return browser.get("https://localhost:44300/#/contract/edit/1/paymentmodel");
+    }
+
+    // agreementOperation input
+    public agreementOperationElement = element(by.css("#agreement-operation"));
+    public agreementOperationInput = (value: string) => {
+        this.agreementOperationElement.sendKeys(value);
+    }
+}
+
+export = ItContractEditTabPaymentModelPo;
