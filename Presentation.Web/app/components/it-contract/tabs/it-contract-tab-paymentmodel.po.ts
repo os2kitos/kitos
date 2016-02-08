0f02007f73019dd5505c1b5078a7b5870cb86696
Bjørn Sørensen <bso@it-minds.dk>
2016-02-08 08:41:45 +0100

    e2e tests of it contract edit tab payment model

--- Presentation.Web/app/components/it-contract/tabs/it-contract-tab-paymentmodel.po.ts
+++ Presentation.Web/app/components/it-contract/tabs/it-contract-tab-paymentmodel.po.ts
@@ -1,17 +1,25 @@
 ﻿import IPageObject = require("../../../../Tests/object-wrappers/IPageObject.po");
 import Select2Wrapper = require("../../../../Tests/object-wrappers/Select2Wrapper");
-import RepeaterWrapper = require("../../../../Tests/object-wrappers/RepeaterWrapper");
 
 class ItContractEditTabPaymentModelPo implements IPageObject {
     public getPage(): webdriver.promise.Promise<void> {
         return browser.get("https://localhost:44300/#/contract/edit/1/paymentmodel");
     }
 
-    // agreementOperation input
-    public agreementOperationElement = element(by.css("#agreement-operation"));
-    public agreementOperationInput = (value: string) => {
-        this.agreementOperationElement.sendKeys(value);
+    // operationDate input
+    public operationDateElement = element(by.css("#agreement-operation"));
+    public operationDateInput = (value: string) => {
+        this.operationDateElement.sendKeys(value);
     }
+
+    // freq selector
+    public freqSelector = new Select2Wrapper("#s2id_agreement-freq");
+
+    // payment selector
+    public paymentSelector = new Select2Wrapper("#s2id_agreement-payment");
+
+    // price selector
+    public priceSelector = new Select2Wrapper("#s2id_agreement-price");
 }
 
 export = ItContractEditTabPaymentModelPo;
