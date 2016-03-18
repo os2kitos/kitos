d47ed144dd4bf330f32a7217d880d7f2948a65e6
esd <esd@it-minds.dk>
2016-03-18 11:57:54 +0100

    Added get single orgunit action

--- Presentation.Web/Controllers/OData/OrganizationUnitsController.cs
+++ Presentation.Web/Controllers/OData/OrganizationUnitsController.cs
@@ -16,10 +16,19 @@ namespace Presentation.Web.Controllers.OData
 
         // GET /Organizations(1)/OrganizationUnits
         [EnableQuery]
-        [ODataRoute("Organizations({key})/OrganizationUnits")]
-        public IHttpActionResult GetOrganizationUnits(int key)
+        [ODataRoute("Organizations({orgKey})/OrganizationUnits")]
+        public IHttpActionResult GetOrganizationUnits(int orgKey)
         {
-            var result = Repository.AsQueryable().Where(m => m.OrganizationId == key);
+            var result = Repository.AsQueryable().Where(m => m.OrganizationId == orgKey);
+            return Ok(result);
+        }
+
+        // GET /Organizations(1)/OrganizationUnits
+        [EnableQuery]
+        [ODataRoute("Organizations({orgKey})/OrganizationUnits({unitKey})")]
+        public IHttpActionResult GetOrganizationUnit(int orgKey, int unitKey)
+        {
+            var result = Repository.AsQueryable().Where(m => m.OrganizationId == orgKey && m.Id == unitKey);
             return Ok(result);
         }
     }
