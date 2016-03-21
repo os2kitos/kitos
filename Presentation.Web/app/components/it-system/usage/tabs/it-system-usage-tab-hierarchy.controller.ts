e1d79682c947f014a5d43cdf7078079a0437c0ec
esd <esd@it-minds.dk>
2016-03-21 09:02:21 +0100

    Reverted change to hierarchy url as it's actually required here

--- Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-hierarchy.controller.ts
+++ Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-hierarchy.controller.ts
@@ -5,7 +5,7 @@
             templateUrl: "app/components/it-system/usage/tabs/it-system-usage-tab-hierarchy.view.html",
             controller: "system.EditHierarchy",
             resolve: {
-                hierarchyFlat: ["$http", "$stateParams", ($http, $stateParams) => $http.get(`api/itsystem/${$stateParams.id}?hierarchy`).then(result => result.data.response)]
+                hierarchyFlat: ["$http", "$stateParams", ($http, $stateParams) => $http.get(`api/itsystem/${$stateParams.id}?hierarchy=true`).then(result => result.data.response)]
             }
         });
     }]);
