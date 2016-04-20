c7e896e4046360a8f7b481e784a44a0af5d58cdd
esd <esd@it-minds.dk>
2016-04-20 18:43:33 +0200

    Moved toggle logic to controller and removed ngDisabled from label as it isn't a valid state for a label, instead just setting disabled class

--- Presentation.Web/app/components/it-project/it-project-overview.controller.ts
+++ Presentation.Web/app/components/it-project/it-project-overview.controller.ts
@@ -145,6 +145,12 @@
             this.reload();
         }
 
+        public toggleLock(dataItem) {
+            if (dataItem.hasWriteAccess) {
+                dataItem.IsPriorityLocked = !dataItem.IsPriorityLocked;
+            }
+        }
+
         private reload() {
             this.$state.go(".", null, { reload: true });
         }
@@ -570,8 +576,8 @@
                         field: "PriorityPf", title: "Prioritet: Portefølje", width: 150,
                         persistId: "prioritypf", // DON'T YOU DARE RENAME!
                         template: () => `<div class="btn-group btn-group-sm" data-toggle="buttons">
-                                                    <label class="btn btn-star" data-ng-class="{ 'unstarred': !dataItem.IsPriorityLocked }" data-ng-disabled="!dataItem.hasWriteAccess">
-                                                        <input type="checkbox" data-ng-model="dataItem.IsPriorityLocked" data-autosave="api/itproject/{{dataItem.Id}}" data-field="IsPriorityLocked" data-ng-click="dataItem.IsPriorityLocked = !dataItem.IsPriorityLocked" data-ng-disabled="!dataItem.hasWriteAccess">
+                                                    <label class="btn btn-star" data-ng-class="{ 'unstarred': !dataItem.IsPriorityLocked, 'disabled': !dataItem.hasWriteAccess }" data-ng-click="projectOverviewVm.toggleLock(dataItem)">
+                                                        <input type="checkbox" data-ng-model="dataItem.IsPriorityLocked" data-autosave="api/itproject/{{dataItem.Id}}" data-field="IsPriorityLocked" data-ng-disabled="!dataItem.hasWriteAccess">
                                                         <i class="glyphicon glyphicon-lock"></i>
                                                     </label>
                                                 </div>
