03b1ab0e272f5072f19fe0ae2ef392e7c082b770
Martin-Andersen <MartinHerlovAndersen@gmail.com>
2016-07-14 11:29:40 +0200

    Så skal vi se om gitversion virker

--- buildscripts/GetPackages.ps1
+++ buildscripts/GetPackages.ps1
@@ -1,9 +1,12 @@
 # in msbuild <Exec Command="powershell -Command &quot;&amp;{ .\Deploy.ps1 scriptArg1 scriptArg2 }&quot;" />
+# ps cmd line: ./GetPackages.ps1 -packagePath "C:\os2kitos\b1\packages" -packageName "GitVersionCore.dll"
 
-Param($packagePath, $packageName)
+Param([cmdletbinding()]$packagePath, [cmdletbinding()]$packageName)
 
-FunctionÂ GetPackagePath
-{
+function Get-PackagePath
+{   
 	Param($packagePath, $packageName)
 	Get-ChildItem $packagePath\*\$packageName -Recurse | Format-Table FullName
 }
+
+Get-PackagePath -packagePath $packagePath -packageName $packageName
