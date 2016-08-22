c500a05f9c6d4945659293b67e00e01f1e4b2863
Mikkel Andreasen <mema@mema.dk>
2016-08-22 08:47:42 +0200

    Added code to remove xml from webapi as it does not all results can be serialized (loop's) as xml

--- Presentation.Web/App_Start/WebApiConfig.cs
+++ Presentation.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.OData.Builder;
 using System.Web.OData.Extensions;
@@ -38,8 +39,8 @@ namespace Presentation.Web
 
             config.EnableEnumPrefixFree(true);
             config.EnableCaseInsensitive(true);
-            //var formatters = ODataMediaTypeFormatters.Create();
-            //config.Formatters.InsertRange(0, formatters);
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
 
         public static IEdmModel GetModel()
