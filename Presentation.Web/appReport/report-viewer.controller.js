a199a15a38255c312e1ec552906f2a1ff1fd627b
jlemiracle <jle@miracle.dk>
2016-08-30 14:07:52 +0200

    Tilføjet test rapport som kan vises i preview

--- Presentation.Web/appReport/report-viewer.controller.js
+++ Presentation.Web/appReport/report-viewer.controller.js
@@ -2,51 +2,58 @@ var Kitos;
 (function (Kitos) {
     var Reports;
     (function (Reports) {
+        "use strict";
         var ReportViewerController = (function () {
-            function ReportViewerController(stimulsoftService, $timeout) {
-                this.$timeout = $timeout;
+            function ReportViewerController(stimulsoftService, $timeout, reportService, $location) {
+                var _this = this;
+                this.stimulsoftService = stimulsoftService;
+                this.reportService = reportService;
+                this.loadReport = function () {
+                    _this.reportService.GetById(1).then(function (result) {
+                        _this.report = result.data;
+                        var stiReport = _this.stimulsoftService.getReport();
+                        if (_this.report.Definition && _this.report.Definition.length > 0) {
+                            //  Load reports from JSON object
+                            stiReport.load(_this.report.Definition);
+                        }
+                        //Assign the report to the viewer
+                        _this.viewer.report = stiReport;
+                    });
+                };
                 var options = stimulsoftService.getOptions();
                 options.appearance.scrollbarsMode = true;
                 options.toolbar.showDesignButton = true;
                 options.appearance.fullScreenMode = true;
-                var viewer = stimulsoftService.getViewer(options, "Viewer");
+                this.viewer = stimulsoftService.getViewer(options, "Viewer");
                 // Add the design button event
-                viewer.onDesignReport = function (e) {
+                this.viewer.onDesignReport = function (e) {
                     this.visible = false;
                     var designOptions = stimulsoftService.getDesignerOptions();
                     designOptions.appearance.fullScreenMode = true;
                     var designer = stimulsoftService.getDesigner(designOptions, "designer");
                     designer.onSaveReport = function (saveEvent) {
-                        viewer.report = saveEvent.report;
+                        this.viewer.report = saveEvent.report;
                         console.log("saving a report");
                     };
                     designer.onExit = function (exitEvent) {
                         console.log("Closing designer");
                         designer.visible = false;
                         //viewer.report = exitEvent.report;
-                        viewer.visible = true;
+                        this.viewer.visible = true;
                     };
                     designer.renderHtml("reportDesigner");
                     designer.visible = true;
                     designer.report = e.report;
                 };
-                viewer.showProcessIndicator();
-                $timeout(function () {
-                    //Create a new report instance
-                    var stiReport = stimulsoftService.getReport();
-                    //    if (report.Definition && report.Definition.length > 0) {
-                    //        //  Load reports from JSON object
-                    //        stiReport.load(report.Definition);
-                    //    }
-                    //Assign the report to the viewer
-                    viewer.report = stiReport;
-                }, 50);
-                viewer.renderHtml("reportViewer");
+                this.viewer.showProcessIndicator();
+                this.viewer.renderHtml("reportViewer");
+                this.loadReport();
             }
-            ReportViewerController.$inject = ["stimulsoftService", "$timeout"];
+            ReportViewerController.$inject = ["stimulsoftService", "$timeout", "reportService", "$location"];
             return ReportViewerController;
         }());
         Reports.ReportViewerController = ReportViewerController;
+        angular.module("reportApp").controller("reportViewerController", Kitos.Reports.ReportViewerController);
     })(Reports = Kitos.Reports || (Kitos.Reports = {}));
 })(Kitos || (Kitos = {}));
 //# sourceMappingURL=report-viewer.controller.js.map
