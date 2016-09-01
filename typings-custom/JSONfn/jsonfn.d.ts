b3b254e6b993259f612a3fc4de7ec8647993b3a3
Martin Herløv Andersen <MartinHerlovAndersen@gmail.com>
2016-09-01 15:24:47 +0200

    moved more packages from nuget to typings
    CustomTyping in its own folder

---
+++ typings-custom/JSONfn/jsonfn.d.ts
@@ -0,0 +1,9 @@
+﻿declare var JSONfn: JSONfn.JSONfnStatic;
+
+declare module JSONfn {
+    interface JSONfnStatic {
+        stringify(obj: Object): string;
+        parse(str: string|JSON|any, date2obj?): Object;
+        clone(obj: Object, date2obj?);
+    }
+}
