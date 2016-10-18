6f11cc552fb8ec929da750ae75b16bdc3db18c07
Rasmus Lagoni <Rhl@miracle.dk>
2016-10-18 14:25:16 +0200

    Migration added, panes removed from global konfig to global admin. global konfig removed.

---
+++ Presentation.Web/app/components/global-admin/global-admin-system.controller.ts
@@ -0,0 +1,25 @@
+﻿(function(ng, app) {
+    var subnav = [
+        { state: 'global-admin.organizations', text: 'Organisation' },
+        { state: 'global-admin.global-users', text: 'Globale administratorer' },
+        { state: 'global-admin.local-users', text: 'Lokale administratorer' },
+        { state: 'global-admin.org', text: 'Roller' },
+        { state: 'global-admin.project', text: 'IT Projekt' },
+        { state: 'global-admin.system', text: 'IT System' },
+        { state: 'global-admin.contract', text: 'IT Kontrakt' }
+    ];
+
+    app.config(['$stateProvider', function($stateProvider) {
+        $stateProvider.state('global-admin.system', {
+            url: '/system',
+            templateUrl: 'app/components/global-admin/global-admin-system.view.html',
+            controller: 'global-admin.SystemCtrl',
+            authRoles: ['GlobalAdmin']
+        });
+    }]);
+
+    app.controller('global-admin.SystemCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $rootScope.page.title = 'Global Admin';
+        $rootScope.page.subnav = subnav;
+    }]);
+})(angular, app);
