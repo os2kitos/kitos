cd7186d2b0dcbe3cded70e6742b0a12f894a0c31
Rasmus Lagoni <Rhl@miracle.dk>
2016-11-11 08:30:30 +0100

    Advice - Generic contorller and view added

---
+++ Presentation.Web/app/components/It-advice.controller.ts
@@ -0,0 +1,9 @@
+﻿(function (ng, app) {
+    app.controller('object.EditAdviceCtrl', ['$scope', '$http', '$state', '$stateParams', '$timeout', 'notify',
+        function ($scope, $http, $state, $stateParams, $timeout, notify) {
+            
+
+
+
+        }]);
+})(angular, app);
---
+++ Presentation.Web/app/components/it-contract/tabs/it-contract-tab-advice.state.ts
@@ -0,0 +1,28 @@
+﻿(function(ng, app) {
+    app.config(['$stateProvider', function($stateProvider) {
+        $stateProvider.state('it-contract.edit.advice-generic', {
+            url: '/advice/:id/:type',
+            templateUrl: 'app/components/it-contract/tabs/it-contract-tab-advice.view.html',
+            controller: 'object.EditAdviceCtrl',
+            resolve: {
+                
+                itContractRoles: ['$http', function ($http) {
+                  
+                    return $http.get("odata/LocalItContractRoles?$filter=IsLocallyAvailable eq true or IsObligatory eq true")
+                        .then(function (result) {
+                            return result.data.value;
+                        });
+                }],
+                advices: ['$http', '$stateParams', function ($http, $stateParams) {
+                    if ($stateParams.id != undefined) { 
+                    return $http.get('api/itcontract/' + $stateParams.id).then(function (result) {
+                        return result.data.response.advices;
+                        });
+                    }
+                    return null;
+                }]
+            }
+        });
+    }]);
+
+})(angular, app);
