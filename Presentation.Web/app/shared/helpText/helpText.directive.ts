d5232def9106b29eba7657d07e64e1d1dfd2dd8a
JONASMORTHO94B7\jonasmorthorst <jmc@miracle.dk>
2016-11-17 11:10:15 +0100

    OS2KITOS-498 Hjælpetekster directive OS2KITOS-497 Globaladmin hjælpetekster redigering

---
+++ Presentation.Web/app/shared/helpText/helpText.directive.ts
@@ -0,0 +1,33 @@
+﻿(function (ng, app) {
+    'use strict';
+
+    app.directive("helpText", [
+        function () {
+            return {
+                templateUrl: "app/shared/helpText/helpText.view.html",
+                scope: {
+                    id: "@",
+                },
+                controller: [
+                    '$scope', '$http', '$uibModal', function ($scope, $http, $uibModal) {
+                        var parent = $scope;
+                        $scope.showHelpTextModal = function() {
+
+                        var modalInstance = $uibModal.open({
+                            windowClass: "modal fade in",
+                            templateUrl: "app/shared/helpText/helpTextModal.view.html",
+                            controller: ["$scope", "$uibModalInstance", "notify", function ($scope, $modalInstance, nofity) {
+                                $http.get("odata/HelpTexts(" + parent.id + ")")
+                                    .success((result: any) => {
+                                        $scope.title = result.Title;
+                                        $scope.description = result.Description;
+                                    })
+                            }]
+                        });
+                    }
+                    }
+                ]
+            };
+        }
+    ]);
+})(angular, app);
