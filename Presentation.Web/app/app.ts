b188eb37bbc9f5757019de972284d4f3455e611e
JONASMORTHO94B7\jonasmorthorst <jmc@miracle.dk>
2016-11-30 14:35:01 +0100

    Removed directive

--- Presentation.Web/app/app.ts
+++ Presentation.Web/app/app.ts
@@ -110,17 +110,3 @@ app.run([
         });
     }
 ]);
-
-app.directive('convertToNumber', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, element, attrs, ngModel) {
-            ngModel.$parsers.push(function (val) {
-                return parseInt(val, 10);
-            });
-            ngModel.$formatters.push(function (val) {
-                return '' + val;
-            });
-        }
-    };
-});
