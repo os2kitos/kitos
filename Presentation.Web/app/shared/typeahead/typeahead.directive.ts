520616d34780e0663de6d6763411245bd6553b7d
JONASMORTHO94B7\jonasmorthorst <jmc@miracle.dk>
2016-11-30 14:35:24 +0100

    OS2KITOS-528 Lav typeahead-directive

---
+++ Presentation.Web/app/shared/typeahead/typeahead.directive.ts
@@ -0,0 +1,36 @@
+﻿(function (ng, app) {
+    'use strict';
+
+    app.directive('typeahead', function ($timeout) {
+        return {
+            restrict: 'AEC',
+            scope: {
+                items: '=',
+                prompt: '@',
+                title: '@',
+                subtitle: '@',
+                model: '=',
+                onSelect: '&amp;'
+            },
+            link: function (scope:any, elem, attrs) {
+                scope.handleSelection = function (selectedItem) {
+                    scope.model = selectedItem;
+                    scope.current = 0;
+                    scope.selected = true;
+                    $timeout(function () {
+                        scope.onSelect();
+                    }, 200);
+                };
+                scope.current = 0;
+                scope.selected = true; // hides the list initially
+                scope.isCurrent = function (index) {
+                    return scope.current == index;
+                };
+                scope.setCurrent = function (index) {
+                    scope.current = index;
+                };
+            },
+            templateUrl: 'typeahead.view.html'
+        };
+    });
+})(angular, app);
