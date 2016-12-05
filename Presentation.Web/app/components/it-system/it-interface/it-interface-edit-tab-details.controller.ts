3115bd42d9db7576510a2b7e3b253b00d749743a
JONASMORTHO94B7\jonasmorthorst <jmc@miracle.dk>
2016-12-05 15:39:29 +0100

    Small fixes

--- Presentation.Web/app/components/it-system/it-interface/it-interface-edit-tab-details.controller.ts
+++ Presentation.Web/app/components/it-system/it-interface/it-interface-edit-tab-details.controller.ts
@@ -53,8 +53,8 @@
     }]);
 
     app.controller('system.SystemInterfaceDetailsCtrl', [
-        '$scope', '$http', '$timeout', '$state', 'notify', 'user', 'tsas', 'interfaces', 'interfaceTypes', 'methods', 'dataTypes', 'itInterface', 'dataRows',
-        function ($scope, $http, $timeout, $state, notify, user, tsas, interfaces, interfaceTypes, methods, dataTypes, itInterface, dataRows) {
+        '$scope', '$http', '$timeout', '$state', 'notify', 'user', 'tsas', 'interfaces', 'interfaceTypes', 'methods', 'dataTypes', 'itInterface', 'dataRows', 'hasWriteAcess',
+        function ($scope, $http, $timeout, $state, notify, user, tsas, interfaces, interfaceTypes, methods, dataTypes, itInterface, dataRows, hasWriteAcess) {
 
             $scope.hasWriteAcess = hasWriteAcess;
             $scope.tsas = tsas;
