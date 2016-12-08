efa07a4c1254bc92dcc7be12dacc59aa38273a51
Tobias Legind <tol@miracle.dk>
2016-12-08 12:10:11 +0100

    OS2KITOS-635 -Done

--- Core.DomainModel/ItSystem/ItSystem.cs
+++ Core.DomainModel/ItSystem/ItSystem.cs
@@ -3,6 +3,8 @@ using Core.DomainModel.Organization;
 
 namespace Core.DomainModel.ItSystem
 {
+    using System;
+
     /// <summary>
     /// Represents an it system.
     /// </summary>
@@ -17,6 +19,88 @@ namespace Core.DomainModel.ItSystem
             this.Wishes = new List<Wish>();
             this.Usages = new List<ItSystemUsage.ItSystemUsage>();
         }
+
+        public bool IsActive
+        {
+            get
+            {
+                if (!this.Active)
+                {
+                    var today = DateTime.UtcNow;
+                    var startDate = this.Concluded ?? today;
+                    var endDate = DateTime.MaxValue;
+
+                    if (ExpirationDate.HasValue && ExpirationDate.Value != DateTime.MaxValue)
+                    {
+                        endDate = ExpirationDate.Value.Date.AddDays(1).AddTicks(-1);
+                    }
+
+                    if (this.Terminated.HasValue)
+                    {
+                        var terminationDate = this.Terminated;
+                        if (this.TerminationDeadlineInSystem != null)
+                        {
+                            int deadline;
+                            int.TryParse(this.TerminationDeadlineInSystem.Name, out deadline);
+                            terminationDate = this.Terminated.Value.AddMonths(deadline);
+                        }
+                        // indgået-dato <= dags dato <= opsagt-dato + opsigelsesfrist
+                        return today >= startDate.Date && today <= terminationDate.Value.Date.AddDays(1).AddTicks(-1);
+                    }
+
+                    // indgået-dato <= dags dato <= udløbs-dato
+                    return today >= startDate.Date && today <= endDate;
+                }
+                return this.Active;
+            }
+        }
+
+        /// <summary>
+        ///     Gets or sets Active.
+        /// </summary>
+        /// <value>
+        ///   Active.
+        /// </value>
+        public bool Active { get; set; }
+
+        /// <summary>
+        ///     When the system began. (indgået)
+        /// </summary>
+        /// <value>
+        ///     The concluded date.
+        /// </value>
+        public DateTime? Concluded { get; set; }
+
+        /// <summary>
+        ///     When the system expires. (udløbet)
+        /// </summary>
+        /// <value>
+        ///     The expiration date.
+        /// </value>
+        public DateTime? ExpirationDate { get; set; }
+
+        /// <summary>
+        ///     Date the system ends. (opsagt)
+        /// </summary>
+        /// <value>
+        ///     The termination date.
+        /// </value>
+        public DateTime? Terminated { get; set; }
+
+        //public int? TerminationDeadlineId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the termination deadline option. (opsigelsesfrist)
+        /// </summary>
+        /// <remarks>
+        ///     Added months to the <see cref="Terminated" /> contract termination date before the contract expires.
+        ///     It's a string but should be treated as an int.
+        ///     TODO perhaps a redesign of OptionEntity is in order
+        /// </remarks>
+        /// <value>
+        ///     The termination deadline.
+        /// </value>
+        public virtual TerminationDeadlineTypesInSystem TerminationDeadlineInSystem { get; set; }
 
         /// <summary>
         /// Gets or sets the user defined system identifier.
@@ -86,5 +170,10 @@ namespace Core.DomainModel.ItSystem
         /// The usages.
         /// </value>
         public virtual ICollection<ItSystemUsage.ItSystemUsage> Usages { get; set; }
+
+        public class TerminationDeadlineTypesInSystem : OptionEntity<ItSystem>, IOptionReference<ItSystem>
+        {
+            public virtual ICollection<ItSystem> References { get; set; } = new HashSet<ItSystem>();
+        }
     }
 }
