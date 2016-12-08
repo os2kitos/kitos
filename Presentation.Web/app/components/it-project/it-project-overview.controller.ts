c57dd0a53efe35a5aa540c4cf71c4781f5a6fc3a
Tobias Legind <tol@miracle.dk>
2016-12-08 15:02:06 +0100

    OS2KITOS-157

--- Presentation.Web/app/components/it-project/it-project-overview.controller.ts
+++ Presentation.Web/app/components/it-project/it-project-overview.controller.ts
@@ -785,15 +785,8 @@
                         // filtering doesn't allow to sort on an array of values, it needs a single value for each row...
                         field: "Rights.Role", title: `${this.user.fullName}`, width: 150,
                         persistId: "usersRoles", // DON'T YOU DARE RENAME!
-                        //template: item => item.Rights ? item.Rights.every(x => x.Role.Name) : "",
-                        //template: () => `<span data-ng-model="dataItem.usersRoles" value="rights.Role.Name" ng-repeat="rights in dataItem.Rights"> {{rights.Role.Name}}, </span>`,
-                        template: (item) => {
-                            var roles = [];
-                            for (var right of item.Rights) {
-                                roles.push(right.Role.Name);
-                            }
-                            return roles;
-                        },
+                        template: () => `<span data-ng-model="dataItem.usersRoles" value="rights.Role.Name" ng-repeat="rights in dataItem.Rights"> {{rights.Role.Name}}{{$last ? '' : ', '}}</span>`,
+                        
                         attributes: { "class": "might-overflow" },
                         hidden: true,
                         sortable: false,
