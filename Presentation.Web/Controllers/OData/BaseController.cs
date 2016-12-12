49e676e5015b0a1adf1d9516819eaa07b7775516
Mikkel Andreasen <mema@mema.dk>
2016-12-12 10:34:23 +0100

    Added CreateRef to ItSystemUsagesController

--- Presentation.Web/Controllers/OData/BaseController.cs
+++ Presentation.Web/Controllers/OData/BaseController.cs
@@ -1,9 +1,25 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
 using System.Web.OData;
+using System.Web.OData.Extensions;
 using Core.DomainServices;
 using System.Web.OData.Routing;
+using Microsoft.OData.Core;
+using Microsoft.OData.Core.UriParser;
 using Ninject;
 using Ninject.Extensions.Logging;
+using Microsoft.OData.Core;
+using Microsoft.OData.Core.UriParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http.Routing;
+using System.Web.OData.Extensions;
+using System.Web.OData.Routing;
 
 namespace Presentation.Web.Controllers.OData
 {
@@ -41,6 +57,33 @@ namespace Presentation.Web.Controllers.OData
         {
             var entity = Repository.GetByKey(key);
             return Ok(entity);
+        }
+
+
+        protected TKey GetKeyFromUri<TKey>(HttpRequestMessage request, Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri");
+            }
+
+            var urlHelper = request.GetUrlHelper() ?? new UrlHelper(request);
+
+            string serviceRoot = urlHelper.CreateODataLink(
+                request.ODataProperties().RouteName,
+                request.ODataProperties().PathHandler, new List<ODataPathSegment>());
+            var odataPath = request.ODataProperties().PathHandler.Parse(
+                request.ODataProperties().Model,
+                serviceRoot, uri.LocalPath);
+
+            var keySegment = odataPath.Segments.OfType<KeyValuePathSegment>().FirstOrDefault();
+            if (keySegment == null)
+            {
+                throw new InvalidOperationException("The link does not contain a key.");
+            }
+
+            var value = ODataUriUtils.ConvertFromUriLiteral(keySegment.Value, ODataVersion.V4);
+            return (TKey)value;
         }
     }
 }
