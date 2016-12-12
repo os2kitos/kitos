06a76b7bf39b9381021fe78ae14ba1feddb1ac3b
JONASMORTHO94B7\jonasmorthorst <jmc@miracle.dk>
2016-12-12 10:17:59 +0100

    WIP

--- Infrastructure.DataAccess/KitosContext.cs
+++ Infrastructure.DataAccess/KitosContext.cs
@@ -94,6 +94,7 @@ namespace Infrastructure.DataAccess
         public DbSet<Stakeholder> Stakeholders { get; set; }
         public DbSet<TerminationDeadlineType> TerminationDeadlineTypes { get; set; }
         public DbSet<TaskRef> TaskRefs { get; set; }
+        public DbSet<AccessType> AccessTypes { get; set; }
         public DbSet<TaskUsage> TaskUsages { get; set; }
         public DbSet<Text> Texts { get; set; }
         public DbSet<TsaType> TsaTypes { get; set; }
