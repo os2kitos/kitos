16ff811cee2d0c1f5e9e6fe42099c8ddb8f69b0d
Tobias Legind <tol@miracle.dk>
2016-12-19 15:35:03 +0100

    Advis service  - added log

--- Core.ApplicationServices/AdviceService.cs
+++ Core.ApplicationServices/AdviceService.cs
@@ -14,6 +14,8 @@ using System.Text;
 
 namespace Core.ApplicationServices
 {
+    using Serilog;
+
     public class AdviceService: IAdviceService
     {
         [Inject]
@@ -31,7 +33,8 @@ namespace Core.ApplicationServices
         [Inject]
         public IGenericRepository<ItSystemRight> _ItSystemRights { get; set; }
 
-
+        [Inject]
+        public ILogger Logger { get; set; }
 
         public AdviceService() {}
 
@@ -110,6 +113,7 @@ namespace Core.ApplicationServices
                     catch (Exception e)
                     {
                         //todo log exception
+                        Logger.Debug(e, "Error in Advis service");
                         return false;
                     }
                 }
@@ -207,6 +211,7 @@ namespace Core.ApplicationServices
                         catch (Exception e)
                         {
                             //todo log exception
+                            Logger.Debug(e, "Error in Advis service");
                             return false;
                         }
                     }
