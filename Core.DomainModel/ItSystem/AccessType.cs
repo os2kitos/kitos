4faf393903b4209af34d511f6bc09573bf4f51f9
JONASMORTHO94B7\jonasmorthorst <jmc@miracle.dk>
2016-12-20 10:29:35 +0100

    Access Types local admin fix

--- Core.DomainModel/ItSystem/AccessType.cs
+++ Core.DomainModel/ItSystem/AccessType.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.DomainModel.ItSystem
 {
-    public class AccessType : Entity
+    public class AccessType : Entity, ISystemModule
     {
         public AccessType()
         {
