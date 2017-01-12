963688944b6a8517a0f83d092582aa2c454c417c
JONASMORTHO94B7\jonasmorthorst <jmc@miracle.dk>
2017-01-12 11:02:33 +0100

    subnav positioning fix

--- Presentation.Web/app/app.ts
+++ Presentation.Web/app/app.ts
@@ -77,20 +77,22 @@ app.run([
         });
 
         $rootScope.positionSubnav = () => {
-            $timeout(() => {
-                if ($rootScope.subnavPositionCenter) {
-                    $("#subnav").css("text-align", "center");
-                    $("#subnav").css("padding-left", "0");
-                } else {
-                    const buttonWidth = $("#navbar-top a.active").width();
-                    const distanceFromContainerToButton = $("#navbar-top").offset().left - $("#navbar-top a.active").offset().left;
-                    const ulWidth = $("#subnav ul").width();
-                    const subnavWidth = $("#navbar-top").width();
-                    $("#subnav").css("text-align", "left");
-                    $("#subnav").css("padding-left", `${((distanceFromContainerToButton * (-1)) - (ulWidth / 2) + (buttonWidth / 2)) / subnavWidth * 100}%`);
-                }
+            $(document).ready(function () {
+                $timeout(() => {
+                    if ($rootScope.subnavPositionCenter) {
+                        $("#subnav").css("text-align", "center");
+                        $("#subnav").css("padding-left", "0");
+                    } else {
+                        const buttonWidth = $("#navbar-top a.active").width();
+                        const distanceFromContainerToButton = $("#navbar-top").offset().left - $("#navbar-top a.active").offset().left;
+                        const ulWidth = $("#subnav ul").width();
+                        const subnavWidth = $("#navbar-top").width();
+                        $("#subnav").css("text-align", "left");
+                        $("#subnav").css("padding-left", `${((distanceFromContainerToButton * (-1)) - (ulWidth / 2) + (buttonWidth / 2)) / subnavWidth * 100}%`);
+                    }
 
-                $rootScope.subnavNotPositioned = false;
+                    $rootScope.subnavNotPositioned = false;
+                });
             });
         }
 
