56e49bf513991731b6a7123f5bde2268e56c4b0e
Rasmus Lagoni <Rhl@miracle.dk>
2018-02-28 12:46:29 +0100

    Small text changes and cleanup

--- Core.DomainModel/ItSystem/SensitiveDataType.cs
+++ Core.DomainModel/ItSystem/SensitiveDataType.cs
@@ -2,7 +2,7 @@
 
 namespace Core.DomainModel.ItSystem
 {
-    public class SensitiveDataType : OptionHasChecked<ItSystemUsage.ItSystemUsage>, IOptionReference<ItSystemUsage.ItSystemUsage>
+    public class SensitiveDataType : OptionEntity<ItSystemUsage.ItSystemUsage>, IOptionReference<ItSystemUsage.ItSystemUsage>
     {
         public virtual ICollection<ItSystemUsage.ItSystemUsage> References { get; set; } = new HashSet<ItSystemUsage.ItSystemUsage>();
     }
