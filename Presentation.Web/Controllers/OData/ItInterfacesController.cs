c28edc0a1a7b30344105f2ba010553186c1e2fd1
Tobias Legind <tol@miracle.dk>
2018-06-04 15:09:59 +0200

    Revert "OS2KITOS-1702 - changed isActive check"
    This reverts commit 2cf892b9c8f7d18b68f7413e3842f30728f4d42d.

--- Presentation.Web/Controllers/OData/ItInterfacesController.cs
+++ Presentation.Web/Controllers/OData/ItInterfacesController.cs
@@ -32,20 +32,6 @@ namespace Presentation.Web.Controllers.OData
         [ODataRoute("Organizations({key})/ItInterfaces")]
         public IHttpActionResult GetItInterfaces(int key)
         {
-            //var loggedIntoOrgId = _authService.GetCurrentOrganizationId(UserId);
-            //if (!_authService.HasReadAccessOutsideContext(UserId))
-            //{
-            //    if (loggedIntoOrgId != key)
-            //        return StatusCode(HttpStatusCode.Forbidden);
-
-            //    var result = Repository.AsQueryable().Where(m => m.OrganizationId == key);
-            //    return Ok(result);
-            //}
-            //else
-            //{
-            //    var result = Repository.AsQueryable().Where(m => m.OrganizationId == key || m.AccessModifier == AccessModifier.Public);
-            //    return Ok(result);
-            //}
             var result = Repository.AsQueryable().Where(m => m.OrganizationId == key || m.AccessModifier == AccessModifier.Public);
             return Ok(result);
         }
