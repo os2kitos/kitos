6986ee1d78f3cd868715d5c7ca2acfc52a864b9a
Georgi Valentinov Vasov <gvv@miracle.dk>
2018-10-23 16:15:40 +0200

    Empty date fields enabled on ItContracts page

--- Presentation.Web/app/components/it-contract/tabs/it-contract-tab-main.controller.ts
+++ Presentation.Web/app/components/it-contract/tabs/it-contract-tab-main.controller.ts
@@ -148,8 +148,13 @@
                 }
                 $scope.patchDate = (field, value) => {
                     var date = moment(moment(value, "DD-MM-YYYY", true).format());
+                    if(value === "") {
+                        var payload = {};
+                        payload[field] = null;
+                        patch(payload, $scope.autosaveUrl2 + '?organizationId=' + user.currentOrganizationId);
+                    } else if (value == null) {
 
-                    if (!date.isValid() || isNaN(date.valueOf()) || date.year() < 1000 || date.year() > 2099) {
+                    } else if (!date.isValid() || isNaN(date.valueOf()) || date.year() < 1000 || date.year() > 2099) {
                         notify.addErrorMessage("Den indtastede dato er ugyldig.");
 
                     } else {
--- Presentation.Web/app/components/it-contract/tabs/it-contract-tab-paymentmodel.controller.ts
+++ Presentation.Web/app/components/it-contract/tabs/it-contract-tab-paymentmodel.controller.ts
@@ -38,8 +38,13 @@
                 };
                 $scope.patchDate = (field, value) => {
                     var date = moment(value, "DD-MM-YYYY");
-
-                    if (!date.isValid() || isNaN(date.valueOf()) || date.year() < 1000 || date.year() > 2099) {
+                    if (value === "") {
+                        var payload = {};
+                        payload[field] = null;
+                        patch(payload, $scope.autosaveUrl + '?organizationId=' + user.currentOrganizationId);
+                    } else if (value == null) {
+    
+                    } else if (!date.isValid() || isNaN(date.valueOf()) || date.year() < 1000 || date.year() > 2099) {
                         notify.addErrorMessage("Den indtastede dato er ugyldig.");
 
                     }
