db8189d3c7ca1a9768fd2526735d77f766206c43
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-07-04 20:39:25 +0200

    Stop on error

--- DeploymentScripts/DeployIntegration.ps1
+++ DeploymentScripts/DeployIntegration.ps1
@@ -2,8 +2,11 @@
 $Env:EnvironmentName="integration"
 
 # Set access keys passed by user
+
 $Env:AWS_ACCESS_KEY_ID=$Env:IntegrationEnvironment.AwsAccessKeyId
 $Env:AWS_SECRET_ACCESS_KEY=$Env:IntegrationEnvironment.AwsSecretAccessKey
+
+Write-Host "Using Acces Key ID: $Env:AWS_ACCESS_KEY_ID in environment $Env:EnvironmentName"
 
 # Non-secret configuration parameters
 $Env:MsDeployServiceUrl="https://172.26.2.34:8172/msdeploy.axd"
@@ -18,4 +21,6 @@ $Env:MailSuffix="(SAW CI)"
 $Env:KitosEnvironmentName="dev"
 
 # Execute the deployment script
-.$PSScriptRoot\DeployWebsite.ps1
+.$PSScriptRoot\DeployWebsite.ps1 -ErrorAction Stop
+
+exit $lastexitcode
