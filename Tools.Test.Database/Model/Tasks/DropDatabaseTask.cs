2ae5302bbf1efc7f98855ae59fa7bc1c027d9b86
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-07-04 14:52:42 +0200

    use raw api to delete

--- Tools.Test.Database/Model/Tasks/DropDatabaseTask.cs
+++ Tools.Test.Database/Model/Tasks/DropDatabaseTask.cs
@@ -2,18 +2,17 @@
 {
     public class DropDatabaseTask : DatabaseTask
     {
+        private readonly string _connectionString;
+
         public DropDatabaseTask(string connectionString)
         : base(connectionString)
         {
-
+            _connectionString = connectionString;
         }
 
         public override bool Execute()
         {
-            using (var context = CreateKitosContext())
-            {
-                context.Database.Delete();
-            }
+            System.Data.Entity.Database.Delete(_connectionString);
 
             return true;
         }
