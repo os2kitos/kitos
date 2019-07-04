8d681223221f07378e6dfb19bb33bca7e6243b29
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-07-04 13:11:02 +0200

    disallow multiple global admins

--- Tools.Test.Database/Model/Tasks/CreateKitosUserTask.cs
+++ Tools.Test.Database/Model/Tasks/CreateKitosUserTask.cs
@@ -105,6 +105,12 @@ namespace Tools.Test.Database.Model.Tasks
             {
                 throw new ArgumentException($"{nameof(role)} must be one of [{string.Join(",", organizationRoles.Keys)}]");
             }
+
+            if (actualRole == OrganizationRole.GlobalAdmin)
+            {
+                throw new NotSupportedException("Only one global admin allowed");
+            }
+
             return actualRole;
         }
     }
