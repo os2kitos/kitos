5cd90ded7840c262b4c47cfa8bbdc21f7ab59670
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-07-12 11:45:32 +0200

    add usage to it system

--- Tools.Test.Database/Model/Tasks/CreateItSystemTask.cs
+++ Tools.Test.Database/Model/Tasks/CreateItSystemTask.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Data.Entity;
 using System.Linq;
+using Core.DomainModel;
 using Core.DomainModel.ItSystem;
-using Core.DomainModel.Organization;
+using Core.DomainModel.ItSystemUsage;
+using Infrastructure.DataAccess;
 using Tools.Test.Database.Model.Extensions;
 
 namespace Tools.Test.Database.Model.Tasks
@@ -24,7 +25,7 @@ namespace Tools.Test.Database.Model.Tasks
                 var commonOrg = context.GetCommonOrganization();
                 var globalAdmin = context.GetGlobalAdmin();
 
-                var systemWithSameName = context.ItSystems.AsNoTracking().FirstOrDefault(x => x.Name == _name);
+                var systemWithSameName = GetItSystemnByName(context);
                 if (systemWithSameName != null)
                 {
                     Console.Out.WriteLine($"Existing ITSystem with name {_name} already exists in the database.");
@@ -42,9 +43,42 @@ namespace Tools.Test.Database.Model.Tasks
 
                 context.ItSystems.Add(itSystem);
                 context.SaveChanges();
+
+                //Enable usage of the system
+                var itSystemUsage = new ItSystemUsage
+                {
+                    ItSystemId = itSystem.Id,
+                    OrganizationId = commonOrg.Id,
+                    ObjectOwnerId = globalAdmin.Id,
+                    LastChangedByUserId = globalAdmin.Id,
+                    DataLevel = itSystem.DataLevel,
+                    ContainsLegalInfo = itSystem.ContainsLegalInfo,
+                    AssociatedDataWorkers = itSystem.AssociatedDataWorkers.Select(x => new ItSystemUsageDataWorkerRelation
+                    {
+                        LastChangedByUserId = x.LastChangedByUserId,
+                        ObjectOwnerId = x.ObjectOwnerId,
+                        DataWorkerId = x.DataWorkerId
+                    }).ToList()
+                };
+                context.ItSystemUsages.Add(itSystemUsage);
+                context.SaveChanges();
+
+                foreach (var option in context.AttachedOptions.Where(x => x.ObjectId == itSystem.Id && x.ObjectType == EntityType.ITSYSTEM))
+                {
+                    option.ObjectId = itSystemUsage.Id;
+                    option.ObjectType = EntityType.ITSYSTEMUSAGE;
+                    context.AttachedOptions.Add(option);
+                }
+                
+                context.SaveChanges();
             }
 
             return true;
+        }
+
+        private ItSystem GetItSystemnByName(KitosContext context)
+        {
+            return context.ItSystems.AsNoTracking().FirstOrDefault(x => x.Name == _name);
         }
     }
 }
