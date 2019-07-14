f3a9ee952d2c1427d1c019163a7a8cc1af0acdae
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-07-14 08:58:45 +0200

    Added API tests foundation

---
+++ Tests.Integration.Presentation.Web/Tools/Model/KitosCredentials.cs
@@ -0,0 +1,18 @@
+﻿using Core.DomainModel.Organization;
+
+namespace Tests.Integration.Presentation.Web.Tools.Model
+{
+    public class KitosCredentials
+    {
+        public string Username { get; }
+        public string Password { get; }
+        public OrganizationRole Role { get; }
+
+        public KitosCredentials(string username, string password, OrganizationRole role)
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
