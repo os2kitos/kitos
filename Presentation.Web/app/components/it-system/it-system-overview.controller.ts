4e91d0b935c97cd97ecd2855bf21652b8231f719
Mads Brønd Kracht <mbk@strongminds.dk>
2019-07-16 13:12:24 +0200

    Missing data-element-type on it-system

--- Presentation.Web/app/components/it-system/it-system-overview.controller.ts
+++ Presentation.Web/app/components/it-system/it-system-overview.controller.ts
@@ -281,7 +281,7 @@
                     {
                         name: "clearFilter",
                         text: "Nulstil",
-                        template: "<button type='button' class='k-button k-button-icontext' title='Nulstil sortering, filtering og kolonnevisning, -bredde og –rækkefølge' data-ng-click='systemOverviewVm.clearOptions()' data-element-type='resetFilter'>#: text #</button>"
+                        template: "<button type='button' class='k-button k-button-icontext' title='Nulstil sortering, filtering og kolonnevisning, -bredde og –rækkefølge' data-ng-click='systemOverviewVm.clearOptions()' data-element-type='resetFilterButton'>#: text #</button>"
                     },
                     {
                         name: "saveFilter",
@@ -291,12 +291,12 @@
                     {
                         name: "useFilter",
                         text: "Anvend filter",
-                        template: "<button type='button' class='k-button k-button-icontext' title='Anvend gemte filtre og sortering' data-ng-click='systemOverviewVm.loadGridProfile()' data-ng-disabled='!systemOverviewVm.doesGridProfileExist()' data-element-type='useFilter'>#: text #</button>"
+                        template: "<button type='button' class='k-button k-button-icontext' title='Anvend gemte filtre og sortering' data-ng-click='systemOverviewVm.loadGridProfile()' data-ng-disabled='!systemOverviewVm.doesGridProfileExist()' data-element-type='useFilterButton'>#: text #</button>"
                     },
                     {
                         name: "deleteFilter",
                         text: "Slet filter",
-                        template: "<button type='button' class='k-button k-button-icontext' title='Slet filtre og sortering' data-ng-click='systemOverviewVm.clearGridProfile()' data-ng-disabled='!systemOverviewVm.doesGridProfileExist()' data-element-type='removeFilter'>#: text #</button>"
+                        template: "<button type='button' class='k-button k-button-icontext' title='Slet filtre og sortering' data-ng-click='systemOverviewVm.clearGridProfile()' data-ng-disabled='!systemOverviewVm.doesGridProfileExist()' data-element-type='removeFilterButton'>#: text #</button>"
                     },
                     {
                         template: kendo.template(this.$("#role-selector").html())
