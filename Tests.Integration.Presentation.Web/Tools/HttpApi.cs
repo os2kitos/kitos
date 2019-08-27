ce730ffee338ad7889de9d3d620f69212b2becd1
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-08-27 11:54:16 +0200

    prevent conflicting state when working with cookies

--- Tests.Integration.Presentation.Web/Tools/HttpApi.cs
+++ Tests.Integration.Presentation.Web/Tools/HttpApi.cs
@@ -16,13 +16,16 @@ namespace Tests.Integration.Presentation.Web.Tools
 {
     public static class HttpApi
     {
-        private static readonly HttpClient HttpClient = new HttpClient();
+        /// <summary>
+        /// Use for stateless calls only
+        /// </summary>
+        private static readonly HttpClient StatelessHttpClient = new HttpClient();
 
         public static Task<HttpResponseMessage> GetWithTokenAsync(Uri url, string token)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
             requestMessage.Headers.Authorization = AuthenticationHeaderValue.Parse("bearer " + token);
-            return HttpClient.SendAsync(requestMessage);
+            return StatelessHttpClient.SendAsync(requestMessage);
         }
 
         public static Task<HttpResponseMessage> PostWithCookieAsync(Uri url, Cookie cookie, object body)
@@ -32,21 +35,21 @@ namespace Tests.Integration.Presentation.Web.Tools
                 Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json")
             };
 
-            return SendAsync(cookie, requestMessage);
+            return SendWithCookieAsync(cookie, requestMessage);
         }
 
         public static Task<HttpResponseMessage> GetWithCookieAsync(Uri url, Cookie cookie)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
 
-            return SendAsync(cookie, requestMessage);
+            return SendWithCookieAsync(cookie, requestMessage);
         }
 
         public static Task<HttpResponseMessage> DeleteWithCookieAsync(Uri url, Cookie cookie)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Delete, url);
 
-            return SendAsync(cookie, requestMessage);
+            return SendWithCookieAsync(cookie, requestMessage);
         }
 
         public static Task<HttpResponseMessage> PatchWithCookieAsync(Uri url, Cookie cookie, object body)
@@ -56,23 +59,33 @@ namespace Tests.Integration.Presentation.Web.Tools
                 Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json")
             };
 
-            return SendAsync(cookie, requestMessage);
+            return SendWithCookieAsync(cookie, requestMessage);
         }
 
-        private static Task<HttpResponseMessage> SendAsync(Cookie cookie, HttpRequestMessage requestMessage)
+        private static async Task<HttpResponseMessage> SendWithCookieAsync(Cookie cookie, HttpRequestMessage requestMessage)
         {
             requestMessage.Headers.Add("Cookie", cookie.Name + "=" + cookie.Value);
 
-            return HttpClient.SendAsync(requestMessage);
+            //Make sure state does not bleed into stateless handler
+            using (var client = new HttpClient())
+            {
+                return await client.SendAsync(requestMessage);
+            }
         }
 
         public static Task<HttpResponseMessage> PostAsync(Uri url, object body)
+        {
+            var requestMessage = CreatePostMessage(url, body);
+            return StatelessHttpClient.SendAsync(requestMessage);
+        }
+
+        private static HttpRequestMessage CreatePostMessage(Uri url, object body)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Post, url)
             {
                 Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json")
             };
-            return HttpClient.SendAsync(requestMessage);
+            return requestMessage;
         }
 
         public static Task<HttpResponseMessage> PostWithTokenAsync(Uri url, object body, string tokenvalue)
@@ -83,13 +96,13 @@ namespace Tests.Integration.Presentation.Web.Tools
             };
             requestMessage.Headers.Add("Authorization", "bearer " + tokenvalue);
 
-            return HttpClient.SendAsync(requestMessage);
+            return StatelessHttpClient.SendAsync(requestMessage);
         }
 
         public static Task<HttpResponseMessage> GetAsync(Uri url)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
-            return HttpClient.SendAsync(requestMessage);
+            return StatelessHttpClient.SendAsync(requestMessage);
         }
 
 
@@ -102,7 +115,7 @@ namespace Tests.Integration.Presentation.Web.Tools
         public static async Task<List<T>> ReadOdataListResponseBodyAs<T>(this HttpResponseMessage response)
         {
             var responseAsJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-            var spec = new {value = new List<T>()};
+            var spec = new { value = new List<T>() };
             var result = JsonConvert.DeserializeAnonymousType(responseAsJson, spec);
             return result.value;
         }
@@ -157,16 +170,19 @@ namespace Tests.Integration.Presentation.Web.Tools
             var url = TestEnvironment.CreateUrl("api/authorize");
             var loginDto = ObjectCreateHelper.MakeSimpleLoginDto(userCredentials.Username, userCredentials.Password);
 
-            var cookieResponse = await HttpApi.PostAsync(url, loginDto);
-            var cookieParts = cookieResponse.Headers.Where(x => x.Key == "Set-Cookie").First().Value.First().Split('=');
-            var cookieName = cookieParts[0];
-            var cookieValue = cookieParts[1].Split(';')[0];
-
-            return new Cookie(cookieName, cookieValue)
+            using (var client = new HttpClient())
             {
-                Domain = url.Host
-            };
+                var request = CreatePostMessage(url,loginDto);
+                var cookieResponse = await client.SendAsync(request);
+                var cookieParts = cookieResponse.Headers.First(x => x.Key == "Set-Cookie").Value.First().Split('=');
+                var cookieName = cookieParts[0];
+                var cookieValue = cookieParts[1].Split(';')[0];
 
+                return new Cookie(cookieName, cookieValue)
+                {
+                    Domain = url.Host
+                };
+            }
         }
 
         public static async Task<int> CreateOdataUserAsync(ApiUserDTO userDto, string role, int organizationId = 1)
@@ -179,7 +195,7 @@ namespace Tests.Integration.Presentation.Web.Tools
             using (var createdResponse = await PostWithCookieAsync(TestEnvironment.CreateUrl("odata/Users/Users.Create"), cookie, createUserDto))
             {
                 Assert.Equal(HttpStatusCode.Created, createdResponse.StatusCode);
-                var response = await createdResponse.ReadResponseBodyAs<UserDTO>().ConfigureAwait(false);
+                var response = await createdResponse.ReadResponseBodyAs<UserDTO>();
                 userId = response.Id;
 
                 Assert.Equal(userDto.Email, response.Email);
