5b6d7dc282f424a6b5e17484c0a00ab7858f6fe8
Mads Brønd Kracht <mbk@strongminds.dk>
2019-08-28 14:48:28 +0200

    Added API test for IT System Interfaces

---
+++ Tests.Integration.Presentation.Web/ItSystem/ItSystemInterfacesTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DomainModel.Organization;
+using Tests.Integration.Presentation.Web.Tools;
+using Xunit;
+
+namespace Tests.Integration.Presentation.Web.ItSystem
+{
+    public class ItSystemInterfacesTest : WithAutoFixture
+    {
+        [Theory]
+        [InlineData(OrganizationRole.User)]
+        [InlineData(OrganizationRole.GlobalAdmin)]
+        public async Task Api_Users_Can_Get_IT_Interfaces_Data_From_Own_Organization(OrganizationRole role)
+        {
+            //Arrange
+            var token = await HttpApi.GetTokenAsync(role);
+            var url = TestEnvironment.CreateUrl($"odata/Organizations({TestEnvironment.DefaultOrganizationId})/ItInterfaces");
+
+            //Act
+            using (var httpResponse = await HttpApi.GetWithTokenAsync(url, token.Token))
+            {
+                var response = httpResponse.ReadResponseBodyAs<Core.DomainModel.ItSystem.ItInterface>();
+                //Assert
+                Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+                Assert.NotNull(response);
+            }
+        }
+
+        [Theory]
+        [InlineData(OrganizationRole.GlobalAdmin)]
+        public async Task Api_Global_Admin_Can_Get_All_ITsystem_Interfaces(OrganizationRole role)
+        {
+            //Arrange
+            var token = await HttpApi.GetTokenAsync(role);
+            var url = TestEnvironment.CreateUrl($"odata/ItInterfaces");
+
+            //Act
+            using (var httpResponse = await HttpApi.GetWithTokenAsync(url, token.Token))
+            {
+                var response = httpResponse.ReadResponseBodyAs<Core.DomainModel.ItSystem.ItInterface>();
+                //Assert
+                Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+                Assert.NotNull(response);
+            }
+        }
+    }
+}
