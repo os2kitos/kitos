fe84933db828922f25e196faa22770b0433e1ca2
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-08-30 08:35:43 +0200

    made sure queries can compile to sql by adding class constraint to generic entity type

--- Core.DomainModel/Extensions/EntityQueryExtensions.cs
+++ Core.DomainModel/Extensions/EntityQueryExtensions.cs
@@ -4,17 +4,24 @@ namespace Core.DomainModel.Extensions
 {
     public static class EntityQueryExtensions
     {
-        public static IQueryable<T> ByOrganizationId<T>(this IQueryable<T> result, int organizationId) where T : IHasOrganization
+        public static IQueryable<T> ByOrganizationId<T>(this IQueryable<T> result, int organizationId) where T :
+            class,
+            IHasOrganization
         {
             return result.Where(x => x.OrganizationId == organizationId);
         }
 
-        public static IQueryable<T> ByPublicAccessModifier<T>(this IQueryable<T> result) where T : IHasAccessModifier
+        public static IQueryable<T> ByPublicAccessModifier<T>(this IQueryable<T> result) where T :
+            class,
+            IHasAccessModifier
         {
             return result.Where(x => x.AccessModifier == AccessModifier.Public);
         }
 
-        public static IQueryable<T> ByPublicAccessOrOrganizationId<T>(this IQueryable<T> result, int organizationId) where T : IHasAccessModifier, IHasOrganization
+        public static IQueryable<T> ByPublicAccessOrOrganizationId<T>(this IQueryable<T> result, int organizationId) where T :
+            class,
+            IHasAccessModifier,
+            IHasOrganization
         {
             return result.Where(x => x.AccessModifier == AccessModifier.Public || x.OrganizationId == organizationId);
         }
