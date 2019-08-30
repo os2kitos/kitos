6000bb5f80afff47a57a9e3e08fe0fc2ac325dde
Mads Brønd Kracht <mbk@strongminds.dk>
2019-08-30 10:20:14 +0200

    Missing variable from testenviroment

--- Tests.Integration.Presentation.Web/Tools/TestEnvironment.cs
+++ Tests.Integration.Presentation.Web/Tools/TestEnvironment.cs
@@ -178,6 +178,7 @@ namespace Tests.Integration.Presentation.Web.Tools
         }
         
         public static int DefaultItSystemId => 1;
+        public static int SecondItSystemId => 2;
         public static int DefaultUserId => 1;
     }
 }
