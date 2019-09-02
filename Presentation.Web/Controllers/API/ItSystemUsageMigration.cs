b6ec1e1a0076047f902d201fa2bc5b418d661352
JacobMalling <jacob.malling.olesen@gmail.com>
2019-09-02 14:55:48 +0200

    Dummy controller added

---
+++ Presentation.Web/Controllers/API/ItSystemUsageMigration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Presentation.Web.Infrastructure.Attributes;
+using Presentation.Web.Models;
+
+namespace Presentation.Web.Controllers.API
+{
+    [InternalApi]
+    public class ItSystemUsageMigration : BaseApiController
+    {
+
+        public HttpResponseMessage GetUnusedItSystemsBySearchAndOrganization(int organizationId, string q, int limit)
+        {
+            var newDTO = new List<ItSystemDTO>();
+            return CreateResponse(HttpStatusCode.OK, newDTO);
+        }
+
+        public HttpResponseMessage GetMigrationConflicts(int fromSystemId, int toSystemId)
+        {
+            var newDTO = new ItSystemUsageMigrationDTO();
+            return CreateResponse(HttpStatusCode.OK, newDTO);
+        }
+
+        public HttpResponseMessage ExecuteMigration(ItSystemUsageMigrationDTO toExecute)
+        {
+            return CreateResponse(HttpStatusCode.OK);
+        }
+
+    }
+}
---
+++ Presentation.Web/Models/ItSystemUsageMigrationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Presentation.Web.Models
+{
+    public class ItSystemUsageMigrationDTO
+    {
+        public IEnumerable<ItInterfaceExhibitUsageDTO> ItInterfaceExhibitUsages;
+        public ItSystemDTO fromItSystem;
+        public ItSystemDTO toItSystem;
+        public OrganizationDTO Organization;
+
+    }
+}
