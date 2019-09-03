ad0521b7a4592df9165fdd9e4d2fafe3c1d335b7
JacobMalling <jacob.malling.olesen@gmail.com>
2019-09-03 09:33:34 +0200

    Controller working

---
+++ Presentation.Web/Controllers/API/ItSystemUsageMigrationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Presentation.Web.Infrastructure.Attributes;
+using Presentation.Web.Models;
+using Swashbuckle.Swagger.Annotations;
+
+namespace Presentation.Web.Controllers.API
+{
+    [PublicApi]
+    public class ItSystemUsageMigrationController : ApiController
+    {
+        
+        [HttpGet]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        public HttpResponseMessage GetUnusedItSystemsBySearchAndOrganization(int organizationId, string q, int limit)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpGet]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        public HttpResponseMessage GetMigrationConflicts(int fromSystemId, int toSystemId)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        public HttpResponseMessage ExecuteMigration(ItSystemUsageMigrationDTO toExecute)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+    }
+}
