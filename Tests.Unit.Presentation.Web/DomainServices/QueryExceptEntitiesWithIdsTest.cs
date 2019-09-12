9ffeed60c1aade32405f8758e8d83efb2006583f
Morten Rembøl Jacobsen <morten.remboel.jacobsen@lego.com>
2019-09-12 14:58:22 +0200

    added unit tests of queries

---
+++ Tests.Unit.Presentation.Web/DomainServices/QueryExceptEntitiesWithIdsTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using AutoFixture;
+using Core.DomainModel.ItSystem;
+using Core.DomainServices.Queries;
+using Tests.Unit.Presentation.Web.Helpers;
+using Xunit;
+
+namespace Tests.Unit.Presentation.Web.DomainServices
+{
+    public class QueryExceptEntitiesWithIdsTest : WithAutoFixture
+    {
+        protected override void OnFixtureCreated(Fixture fixture)
+        {
+            fixture.Register(() => new ItSystem { Id = fixture.Create<int>() });
+            base.OnFixtureCreated(fixture);
+        }
+
+        [Fact]
+        public void Apply_Removes_Entities_With_Id_Match()
+        {
+            //Arrange
+            var excludedSystems = Many<ItSystem>().ToList();
+            var includedSystems = Many<ItSystem>().ToList();
+            var sut = new QueryExceptEntitiesWithIds<ItSystem>(excludedSystems.Select(x => x.Id));
+            var input = excludedSystems.Concat(includedSystems).AsQueryable();
+
+            //Act
+            var result = sut.Apply(input);
+
+            //Assert
+            Assert.True(includedSystems.SequenceEqual(result));
+        }
+
+        [Fact]
+        public void Apply_Echoes_Input_If_No_Excluded_Ids_Provided()
+        {
+            //Arrange
+            var sut = new QueryExceptEntitiesWithIds<ItSystem>(Enumerable.Empty<int>());
+            var input = Many<ItSystem>().AsQueryable();
+
+            //Act
+            var result = sut.Apply(input);
+
+            //Assert
+            Assert.Same(input,result);
+        }
+    }
+}
---
+++ Tests.Unit.Presentation.Web/DomainServices/RejectAllResultsQueryTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using AutoFixture;
+using Core.DomainModel.ItSystem;
+using Core.DomainServices.Queries;
+using Tests.Unit.Presentation.Web.Helpers;
+using Xunit;
+
+namespace Tests.Unit.Presentation.Web.DomainServices
+{
+    public class RejectAllResultsQueryTest : WithAutoFixture
+    {
+        protected override void OnFixtureCreated(Fixture fixture)
+        {
+            fixture.Register(() => new ItSystem { Id = fixture.Create<int>() });
+            base.OnFixtureCreated(fixture);
+        }
+
+        [Fact]
+        public void Apply_Removes_All_Results_From_Input()
+        {
+            //Arrange
+            var itSystems = Many<ItSystem>().AsQueryable();
+
+
+            //Act
+            var result = new RejectAllResultsQuery<ItSystem>().Apply(itSystems);
+
+            //Assert
+            Assert.Empty(result);
+        }
+    }
+}
