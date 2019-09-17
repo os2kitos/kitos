f4a29c7a6a96122f44f82582b6e68000b93bfdf7
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2019-09-17 08:04:44 +0200

    cleanup

--- Presentation.Web/Extensions/DTOMappingExtensions.cs
+++ Presentation.Web/Extensions/DTOMappingExtensions.cs
@@ -6,15 +6,23 @@ namespace Presentation.Web.Extensions
 {
     public static class DTOMappingExtensions
     {
-        public static NamedEntityDTO MapToNamedEntityDTO<T>(this T result)
+        public static NamedEntityDTO MapToNamedEntityDTO<T>(this T source)
             where T : Entity, IHasName
         {
-            return new NamedEntityDTO { Id = result.Id, Name = result.Name };
+            return new NamedEntityDTO
+            {
+                Id = source.Id,
+                Name = source.Name
+            };
         }
 
-        public static NamedEntityDTO MapToNamedEntityDTO(this NamedEntity result)
+        public static NamedEntityDTO MapToNamedEntityDTO(this NamedEntity source)
         {
-            return new NamedEntityDTO { Id = result.Id, Name = result.Name };
+            return new NamedEntityDTO
+            {
+                Id = source.Id,
+                Name = source.Name
+            };
         }
     }
 }
