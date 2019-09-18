bd1f8004ff6ee175c4f61792f135dddd0ea10680
JacobMalling <jacob.malling.olesen@gmail.com>
2019-09-18 10:29:10 +0200

    Covered LocalConfig

--- Tests.Integration.Presentation.Web/Tools/LocalConfigHelper.cs
+++ Tests.Integration.Presentation.Web/Tools/LocalConfigHelper.cs
@@ -8,12 +8,25 @@ namespace Tests.Integration.Presentation.Web.Tools
     public class LocalConfigHelper
     {
 
-        public static async Task<HttpResponseMessage> SendUpdateConfigRequestAsync(object body, int orgId, Cookie optionalCookie = null)
+        public static async Task<HttpResponseMessage> SendUpdateConfigRequestAsync(
+            object body, 
+            int orgId, 
+            Cookie optionalCookie = null)
         {
             var cookie = optionalCookie ?? await HttpApi.GetCookieAsync(OrganizationRole.LocalAdmin);
             var url = TestEnvironment.CreateUrl($"/odata/Configs({orgId})?organizationId={orgId}");
 
             return await HttpApi.PatchWithCookieAsync(url, cookie, body);
+        }
+
+        public static async Task<HttpResponseMessage> GetLocalConfig(
+            int orgId,
+            Cookie optionalCookie = null)
+        {
+            var cookie = optionalCookie ?? await HttpApi.GetCookieAsync(OrganizationRole.LocalAdmin);
+            var url = TestEnvironment.CreateUrl($"/odata/Configs({orgId})?organizationId={orgId}");
+
+            return await HttpApi.GetWithCookieAsync(url, cookie);
         }
     }
 }
