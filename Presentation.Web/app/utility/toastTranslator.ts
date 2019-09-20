4359ba6e7bbb1a96bab931774bac84fbfd502403
Mads Brønd Kracht <mbk@strongminds.dk>
2019-09-20 14:44:46 +0200

    Code style adjustment

--- Presentation.Web/app/utility/toastTranslator.ts
+++ Presentation.Web/app/utility/toastTranslator.ts
@@ -2,20 +2,16 @@
     export class ToastTranslatorTool {
         static translateItSystemDeletionConflictResponse(response: string): string {
             switch (response) {
-                case "InUse":
-                    {
+                case "InUse":{
                         return "Systemet kan ikke slettes! \nDa Systemet er i brug";
                     }
-                case "HasChildren":
-                    {
+                case "HasChildren":{
                         return "Systemet kan ikke slettes! \nDa andre systemer afhænger af dette system";
                     }
-                case "HasInterfaceExhibits":
-                    {
+                case "HasInterfaceExhibits":{
                         return "Systemet kan ikke slettes! \nDa en snitflade afhænger af dette system";
                     }
-                default:
-                    {
+                default:{
                         return "Systemet kan ikke slettes!";
                     }
             }
