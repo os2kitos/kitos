7506ab5be79a7ff93f13a8f85ac44615b1a549b0
JacobMalling <jacob.malling.olesen@gmail.com>
2019-09-23 11:10:28 +0200

    Refactore Select2 functions in tests

--- Presentation.Web/Tests/Helpers/InterfaceCatalogHelper.ts
+++ Presentation.Web/Tests/Helpers/InterfaceCatalogHelper.ts
@@ -1,12 +1,12 @@
 ﻿import InterfaceCatalogPage = require("../PageObjects/it-system/Interfaces/itSystemInterface.po");
 import CSSLocator = require("../object-wrappers/CSSLocatorHelper");
 import WaitTimers = require("../Utility/WaitTimers");
+import Select2 = require("./Select2Helper");
 
 class InterfaceCatalogHelper {
     private static cssHelper = new CSSLocator();
     private static waitUpTo = new WaitTimers();
     private static interfacePage = new InterfaceCatalogPage();
-    private static EC = protractor.ExpectedConditions;
 
     public static createInterface(name: string) {
         console.log(`Creating interface with name ${name}`);
@@ -22,8 +22,8 @@ class InterfaceCatalogHelper {
         console.log(`Binding interface with name ${interfaceName} to system with name ${systemName}`);
         return this.gotoSpecificInterface(interfaceName)
             .then(() => element(this.cssHelper.byDataElementType("interfaceDetailsLink")).click())
-            .then(() => this.select2SearchForInterface(systemName))
-            .then(() => this.waitForSelect2DataAndSelect());
+            .then(() => Select2.SearchFor(systemName, "s2id_interface-exposed-by"))
+            .then(() => Select2.waitForDataAndSelect());
     }
 
     public static waitForKendoGrid() {
@@ -40,19 +40,6 @@ class InterfaceCatalogHelper {
     private static findSpecificInterfaceInKendo(name: string) {
         console.log(`Finding interface with name : ${name}`);
         return element(by.xpath('//*/tbody/*/td/a[text()="' + name + '"]/parent::*/parent::*//a'));
-    }
-
-    public static waitForSelect2DataAndSelect() {
-        console.log(`waitForSelect2DataAndSelect`);
-        return browser.wait(this.EC.visibilityOf(element(by.className("select2-result-label"))), 20000)
-            .then(() => element(by.id("select2-drop")).element(by.className("select2-input")).sendKeys(protractor.Key.ENTER));
-    }
-
-    public static select2SearchForInterface(name: string) {
-        console.log(`select2SearchForMainSystem: ${name}`);
-        return element(by.id("s2id_interface-exposed-by")).element(by.tagName('a')).click()
-            .then(() => element(by.id("select2-drop")).element(by.className("select2-input")).click())
-            .then(() => element(by.id("select2-drop")).element(by.className("select2-input")).sendKeys(name));
     }
 
 }
