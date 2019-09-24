3102708c657f6c8a8db29611870af4d7e93380e8
JacobMalling <jacob.malling.olesen@gmail.com>
2019-09-24 08:41:25 +0200

    Added missing test file

---
+++ Tests.Unit.Presentation.Web/Extensions/SystemDeleteReturnConflictMapperTest.cs
@@ -0,0 +1,19 @@
+﻿using Core.ApplicationServices.Model.System;
+using Presentation.Web.Extensions;
+using Presentation.Web.Models.ItSystem;
+using Xunit;
+
+namespace Tests.Unit.Presentation.Web.Extensions
+{
+    public class SystemDeleteReturnConflictMapperTest
+    {
+        [Theory]
+        [InlineData(DeleteResult.InUse, SystemDeleteConflict.InUse)]
+        [InlineData(DeleteResult.HasChildren, SystemDeleteConflict.HasChildren)]
+        [InlineData(DeleteResult.HasInterfaceExhibits, SystemDeleteConflict.HasInterfaceExhibits)]
+        public void CanMapToConflict(DeleteResult deleteResult, SystemDeleteConflict conflict)
+        {
+            Assert.Equal(conflict, deleteResult.MapToConflict());
+        }
+    }
+}
