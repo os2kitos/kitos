f036a2e6c9166d820e38bfff96a739b530b73ee9
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2020-01-07 14:27:17 +0100

    Bridge and Repos status implemented

---
+++ Core.DomainServices/Repositories/KLE/KLEStandardRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Infrastructure.Services.KLEDataBridge;
+
+namespace Core.DomainServices.Repositories.KLE
+{
+    public class KLEStandardRepository
+    {
+        private readonly IKLEDataBridge _kleDataBridge;
+
+        public KLEStandardRepository(IKLEDataBridge kleDataBridge)
+        {
+            _kleDataBridge = kleDataBridge;
+        }
+
+        public KLEStatus GetKLEStatus()
+        {
+            var kleXmlData = _kleDataBridge.GetKLEXMLData();
+            var publishedString = kleXmlData.Descendants("UdgivelsesDato").First().Value;
+            var publishedDate = DateTime.Parse(publishedString, CultureInfo.InvariantCulture);
+            return new KLEStatus
+            {
+                UpToDate = DateTime.Now.Date>=publishedDate.Date,
+                Published = publishedDate
+            };
+        }
+    }
+}
---
+++ Infrastructure.Services/KLEDataBridge/KLEDataBridge.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Net;
+using System.Xml.Linq;
+
+namespace Infrastructure.Services.KLEDataBridge
+{
+    public class KLEDataBridge : IKLEDataBridge
+    {
+        public XDocument GetKLEXMLData()
+        {
+            using (var client = new WebClient())
+            using (var stream = client.OpenRead("http://api.kle-online.dk/resources/kle/emneplan"))
+            using (var reader = new StreamReader(stream))
+            {
+                return XDocument.Load(reader);
+            }
+        }
+    }
+}
