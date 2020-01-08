6c8e93f4797d735992294740914896cfaea6f408
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2020-01-08 12:46:54 +0100

    Fixed missing registrations

--- Presentation.Web/Controllers/API/KLEController.cs
+++ Presentation.Web/Controllers/API/KLEController.cs
@@ -7,7 +7,7 @@ using Presentation.Web.Models;
 
 namespace Presentation.Web.Controllers.API
 {
-    [InternalApi]
+    [PublicApi]
     public class KLEController : BaseApiController
     {
         private readonly IKLEApplicationService _kleApplicationService;
