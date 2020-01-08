271d920c88eb068e6ec25a98f9c51742e9f1a8db
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2020-01-08 14:23:45 +0100

    Dummy CSV file

--- Presentation.Web/Controllers/API/KLEController.cs
+++ Presentation.Web/Controllers/API/KLEController.cs
@@ -1,4 +1,12 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
 using Core.ApplicationServices;
 using Core.ApplicationServices.Authorization;
 using Core.ApplicationServices.Model.Result;
@@ -7,7 +15,8 @@ using Presentation.Web.Models;
 
 namespace Presentation.Web.Controllers.API
 {
-    [PublicApi]
+    [PublicApi]//TODO: Internalapi
+    [RoutePrefix("api/v1/kle")]
     public class KLEController : BaseApiController
     {
         private readonly IKLEApplicationService _kleApplicationService;
@@ -17,6 +26,8 @@ namespace Presentation.Web.Controllers.API
             _kleApplicationService = kleApplicationService;
         }
 
+        [HttpGet]
+        [Route("status")]
         public HttpResponseMessage GetKLEStatus()
         {
             var result = _kleApplicationService.GetKLEStatus();
@@ -35,6 +46,46 @@ namespace Presentation.Web.Controllers.API
                 default:
                     return Error($"Something went wrong getting KLE status");
             }
+        }
+
+        [HttpGet]
+        [Route("changes")]
+        public HttpResponseMessage GetKLEChanges()
+        {
+            // TODO: Replace dummy data
+            var list = new List<dynamic>();
+            var header = new ExpandoObject() as IDictionary<string, Object>;
+            header.Add("Uuid", "Identifier");
+            header.Add("Type", "KLE Type");
+            header.Add("TaskKey", "KLE nummer");
+            header.Add("Description", "Beskrivelse");
+            header.Add("Change", "Ændring");
+            list.Add(header);
+            var s = list.ToCsv();
+            var bytes = Encoding.Unicode.GetBytes(s);
+            var stream = new MemoryStream();
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StreamContent(stream)
+            };
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("text/csv");
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileNameStar = "kle-updates.csv",
+                DispositionType = "ISO-8859-1"
+            };
+            return result;
+        }
+
+        [HttpPut]
+        [Route("")]
+        public HttpResponseMessage PutKLEChanges()
+        {
+            // TODO
+            return Ok();
         }
     }
 }
