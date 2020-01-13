840e829cae052889974366f03a364160d1960139
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-01-13 09:08:27 +0100

    added comment for future expansion

--- Core.ApplicationServices/Authorization/AuthorizationContextFactory.cs
+++ Core.ApplicationServices/Authorization/AuthorizationContextFactory.cs
@@ -4,6 +4,7 @@
     {
         public IAuthorizationContext Create(IOrganizationalUserContext userContext)
         {
+            //NOTE: SupplierAccess is injected here because then it is not "organizationAuthorizationContext but supplierauthorizationcontext"
             return userContext is UnauthenticatedUserContext
                 ? new UnauthenticatedAuthorizationContext()
                 : (IAuthorizationContext) new OrganizationAuthorizationContext(userContext);
