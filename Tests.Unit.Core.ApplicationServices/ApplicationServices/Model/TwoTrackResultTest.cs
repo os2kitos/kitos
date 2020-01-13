3a62ea4f146dfd14ebc93f8d5a07168197daebdd
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-01-13 13:25:10 +0100

    added twotrackresult test

---
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/Model/TwoTrackResultTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Core.DomainServices.Model.Result;
+using Xunit;
+
+namespace Tests.Unit.Core.ApplicationServices.Model
+{
+    public class TwoTrackResultTest
+    {
+        [Fact]
+        public void Success_Returns_OkResult_With_Value()
+        {
+            var value = Guid.NewGuid();
+
+            var result = TwoTrackResult<Guid, bool>.Success(value);
+
+            Assert.True(result.Ok);
+            Assert.Equal(value, result.Value);
+            Assert.Throws<InvalidOperationException>(() => result.Error);
+        }
+
+        [Fact]
+        public void Success_Returns_ErrorResult_With_ErrorValue()
+        {
+            var value = Guid.NewGuid();
+
+            var result = TwoTrackResult<bool, Guid>.Failure(value);
+
+            Assert.False(result.Ok);
+            Assert.Equal(value, result.Error);
+            Assert.Throws<InvalidOperationException>(() => result.Value);
+        }
+    }
+}
