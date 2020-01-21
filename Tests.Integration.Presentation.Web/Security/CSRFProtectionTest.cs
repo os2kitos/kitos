6fcdbbe545d23aaaf108cccce179b6bb7d9f313f
JacobMalling <jacob.malling.olesen@gmail.com>
2020-01-21 10:08:10 +0100

    Tests using cookies now works. Getting KITOS-Api-Token through Post request also works.

--- Tests.Integration.Presentation.Web/Security/CSRFProtectionTest.cs
+++ Tests.Integration.Presentation.Web/Security/CSRFProtectionTest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Core.DomainModel;
 using Core.DomainModel.Organization;
+using Newtonsoft.Json;
 using Tests.Integration.Presentation.Web.Tools;
 using Xunit;
 
@@ -25,11 +28,19 @@ namespace Tests.Integration.Presentation.Web.Security
                 AccessModifier = AccessModifier.Public
             };
 
-            //Act
-            using (var httpResponse = await HttpApi.PostWithCookieAsync(TestEnvironment.CreateUrl("/api/itsystem"), cookie, itSystem))
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, TestEnvironment.CreateUrl("api/itsystem"))
             {
+                Content = new StringContent(JsonConvert.SerializeObject(itSystem), Encoding.UTF8, "application/json")
+            };
+            var cookieContainer = new CookieContainer();
+            cookieContainer.Add(cookie);
+
+            //Act
+            using (var client = new HttpClient(new HttpClientHandler { CookieContainer = cookieContainer }))
+            {
+                var response = await client.SendAsync(requestMessage);
                 //Assert
-                Assert.Equal(HttpStatusCode.Forbidden, httpResponse.StatusCode);
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
             }
         }
 
@@ -51,9 +62,8 @@ namespace Tests.Integration.Presentation.Web.Security
             using (var httpResponse = await HttpApi.PostWithCookieAsync(TestEnvironment.CreateUrl("/api/itsystem"), cookie, itSystem))
             {
                 //Assert
-                Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
+                Assert.Equal(HttpStatusCode.BadRequest, httpResponse.StatusCode);
             }
-
         }
     }
 }
