f29c526e211cf30fa1de61f952f1533fd9832611
JacobMalling <jacob.malling.olesen@gmail.com>
2020-01-23 09:44:10 +0100

    CSRF works on reports as well

--- Presentation.Web/appReport/reportApp.module.ts
+++ Presentation.Web/appReport/reportApp.module.ts
@@ -3,7 +3,25 @@ var app = angular.module("reportApp", [
     "ui.bootstrap",
     "ngAnimate",
     "ngSanitize",
-    "notify"]);
+    "notify",
+    "ngCookies"]);
 
 app.constant("$", $)
-   .constant("_", _);
+    .constant("_", _);
+
+app.config([
+    "$httpProvider",
+    ($httpProvider) => {
+
+        $httpProvider.defaults.headers.get = {
+            "Cache-Control": "no-cache"
+        };
+
+        //Disable built-in xsrf in angular - it overrides our interceptor
+        $httpProvider.defaults.xsrfCookieName = "IGNORED-XSRF-TOKEN";
+        $httpProvider.defaults.xsrfHeaderName = "IGNORED-XSRF-TOKEN";
+
+        $httpProvider.interceptors.push("csrfRequestInterceptor");
+
+    }
+]);
