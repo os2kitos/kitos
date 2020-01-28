3b8f87bc2f4b11763b62bfa8f8a15b255184e456
JacobMalling <jacob.malling.olesen@gmail.com>
2020-01-28 09:28:28 +0100

    Added Exposed Interfaces to logical it systems

---
+++ Presentation.Web/app/components/it-system/edit/tabs/it-system-usage-tab-interfaces.config.ts
@@ -0,0 +1,73 @@
+﻿((ng, app) => {
+    app.config(["$stateProvider", $stateProvider => {
+        $stateProvider.state("it-system.edit.interfaces", {
+            url: "/interfaces",
+            templateUrl: "app/components/it-system/edit/tabs/it-system-usage-tab-interfaces.view.html",
+            controller: "system.ExposedInterfaces",
+            resolve: {
+                tsas: [
+                    "$http",
+                    $http => $http
+                        .get(
+                            "odata/LocalTsaTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                        .then(result => result.data.value)
+                ],
+                interfaces: [
+                    "$http",
+                    $http => $http
+                        .get(
+                            "odata/LocalInterfaceTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                        .then(result => result.data.value)
+                ],
+                interfaceTypes: [
+                    "$http",
+                    $http => $http
+                        .get(
+                            "odata/LocalItInterfaceTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                        .then(result => result.data.value)
+                ],
+                methods: [
+                    "$http",
+                    $http => $http
+                        .get(
+                            "odata/LocalMethodTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                        .then(result => result.data.value)
+                ],
+                dataTypes: [
+                    "$http",
+                    $http => $http
+                        .get(
+                            "odata/LocalDataTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                        .then(result => result.data.value)
+                ],
+                frequencies: [
+                    "$http",
+                    $http => $http
+                        .get(
+                            "odata/LocalFrequencyTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                        .then(result => result.data.value)
+                ],
+                systemUsageId: [
+                    "$http", "itSystem", "user",
+                    ($http, itSystem, user) => $http.get(`api/itSystemUsage/?itSystemId=${itSystem.id}&organizationId=${user.currentOrganizationId}`)
+                        .then(systemUsageResult => systemUsageResult.data.response.id)
+                ],
+                exhibits: [
+                    "$http", "itSystem", "user", "systemUsageId", ($http, itSystem, user, systemUsageId) =>
+                        $http.get(`api/exhibit/?interfaces=true&sysId=${itSystem.id}&orgId=${user.currentOrganizationId}`).then(result => {
+                            var interfaces = result.data.response;
+                            _.each(interfaces, (data: { exhibitedById; usage; }) => {
+                                $http.get(`api/itInterfaceExhibitUsage/?usageId=${systemUsageId}&exhibitId=${data.exhibitedById}`).success(usageResult => {
+                                    data.usage = usageResult.response;
+                                });
+                            });
+                            return interfaces;
+                        })
+                ],
+                user: [
+                    "userService", userService => userService.getUser()
+                ]
+            }
+        });
+    }]);
+})(angular, app);
