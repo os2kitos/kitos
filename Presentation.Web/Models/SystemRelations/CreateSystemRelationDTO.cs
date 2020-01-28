c2677b26b7a20a1131f68dc2f8b318069075a53e
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-01-28 13:37:30 +0100

    added controller for POST

---
+++ Presentation.Web/Models/SystemRelations/CreateSystemRelationDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Presentation.Web.Models.SystemRelations
+{
+    public class CreateSystemRelationDTO
+    {
+        public int SourceUsageId { get; set; }
+        public int TargetUsageId { get; set; }
+        public string Description { get; set; }
+        public int? InterfaceId { get; set; }
+        public int? FrequencyTypeId { get; set; }
+        public int? ContractId { get; set; }
+        public string LinkName { get; set; }
+        public string LinkUrl { get; set; }
+    }
+}
