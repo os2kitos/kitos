44d5720dbd04fee4fcb59ed4766ed177446e3fc6
JacobMalling <jacob.malling.olesen@gmail.com>
2020-01-30 12:58:41 +0100

    Fixed review

---
+++ Presentation.Web/app/components/it-system/edit/tabs/it-system-edit-tab-interfaces.controller.ts
@@ -0,0 +1,63 @@
+﻿((ng, app) => {
+    app.config(["$stateProvider", $stateProvider => {
+        $stateProvider.state("it-system.edit.interfaces", {
+            url: "/interfaces",
+            templateUrl: "app/components/it-system/edit/tabs/it-system-edit-tab-interfaces.view.html",
+            controller: "system.ExposedInterfaces",
+            resolve: {
+                exhibits: [
+                    "$http", "itSystem", "user", ($http, itSystem, user) =>
+                        $http.get(`api/exhibit/?interfaces=true&sysId=${itSystem.id}&orgId=${user.currentOrganizationId}`).then(result => {
+                            return result.data.response;
+                        })
+                ],
+                user: [
+                    "userService", userService => userService.getUser()
+                ]
+            }
+        });
+    }]);
+
+    app.controller("system.ExposedInterfaces", ["$scope", "$http", "itSystem", "exhibits", "user",
+        ($scope, $http, itSystem, exhibits, user) => {
+
+            $scope.system = itSystem;
+            
+            $scope.interfaceExposures = exhibits;
+
+            $scope.itSystemUsageSelectOptions = selectLazyLoading("api/itSystemUsage", [`organizationId=${user.organizationId}`]);
+            function selectLazyLoading(url, paramAry) {
+                return {
+                    allowClear: true,
+                    minimumInputLength: 1,
+                    initSelection: (elem, callback) => {
+                    },
+                    ajax: {
+                        data: (term, page) => ({ query: term }),
+                        quietMillis: 500,
+                        transport: queryParams => {
+                            var extraParams = paramAry ? "&" + paramAry.join("&") : "";
+                            var res = $http.get(url + "?q=" + queryParams.data.query + extraParams).then(queryParams.success);
+                            res.abort = () => null;
+
+                            return res;
+                        },
+
+                        results: (data, page) => {
+                            var results = [];
+
+                            _.each(data.data.response, (obj: { id; itSystem; }) => {
+                                results.push({
+                                    id: obj.id,
+                                    text: obj.itSystem.name
+                                });
+                            });
+
+                            return { results: results };
+                        }
+                    }
+                };
+            }
+        }
+    ]);
+})(angular, app);
--- Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-interfaces.controller.ts
+++ Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-interfaces.controller.ts
@@ -1,31 +1,40 @@
 ﻿((ng, app) => {
-    app.controller("system.ExposedInterfaces", ["$scope", "$http", "tsas", "interfaces", "interfaceTypes", "methods", "dataTypes", "frequencies", "systemUsageId", "exhibits", "user",
-        ($scope, $http, tsas, interfaces, interfaceTypes, methods, dataTypes, frequencies, systemUsageId, exhibits, user) => {
-
-            $scope.frequencies = frequencies;
-
-            $scope.filterInterfaces = interfaceSystem => interfaceSystem.canBeUsed || interfaceSystem.usage || $scope.showAllInterfaces;
-
-            // resolving complex types from ids
-            function resolveTypes(theInterface) {
-                theInterface.interfaceType = _.find(interfaceTypes, { Id: theInterface.interfaceTypeId });
-                theInterface.interface = _.find(interfaces, { Id: theInterface.interfaceId });
-                theInterface.method = _.find(methods, { Id: theInterface.methodId });
-                theInterface.tsa = _.find(tsas, { Id: theInterface.tsaId });
-
-                _.each(theInterface.dataRows, (dataRow: { dataTypeId; dataType; }) => {
-                    dataRow.dataType = _.find(dataTypes, { Id: dataRow.dataTypeId });
-                });
-
-                theInterface.numRows = theInterface.dataRows.length;
+    app.config(["$stateProvider", $stateProvider => {
+        $stateProvider.state("it-system.usage.interfaces", {
+            url: "/interfaces",
+            templateUrl: "app/components/it-system/usage/tabs/it-system-usage-tab-interfaces.view.html",
+            controller: "systemUsage.ExposedInterfaces",
+            resolve: {
+                exhibits: [
+                    "$http", "itSystemUsage", "user", ($http, itSystemUsage, user) =>
+                        $http.get(`api/exhibit/?interfaces=true&sysId=${itSystemUsage.itSystem.id}&orgId=${user.currentOrganizationId}`).then(result => {
+                            var interfaces = result.data.response;
+                            _.each(interfaces, (data: { exhibitedById; usage; }) => {
+                                $http.get(`api/itInterfaceExhibitUsage/?usageId=${itSystemUsage.id}&exhibitId=${data.exhibitedById}`).success(usageResult => {
+                                    data.usage = usageResult.response;
+                                });
+                            });
+                            return interfaces;
+                        })
+                ],
+                user: [
+                    "userService", userService => userService.getUser()
+                ]
             }
+        });
+    }]);
 
-            // interface exposures
+    app.controller("systemUsage.ExposedInterfaces", ["$scope", "$http", "itSystemUsage", "exhibits", "user",
+        ($scope, $http, itSystemUsage, exhibits, user) => {
+
+            $scope.systemUsage = itSystemUsage;
+
+            // Enables marking "wished" 
             _.each(exhibits, (interfaceExposure: { updateUrl; urlParams; id; }) => {
                 interfaceExposure.updateUrl = "api/itInterfaceExhibitUsage/";
-                interfaceExposure.urlParams = `&usageId=${systemUsageId}&exhibitId=${interfaceExposure.id}`;
-                resolveTypes(interfaceExposure);
+                interfaceExposure.urlParams = `&usageId=${itSystemUsage.id}&exhibitId=${interfaceExposure.id}`;
             });
+
             $scope.interfaceExposures = exhibits;
 
             $scope.itSystemUsageSelectOptions = selectLazyLoading("api/itSystemUsage", [`organizationId=${user.organizationId}`]);
