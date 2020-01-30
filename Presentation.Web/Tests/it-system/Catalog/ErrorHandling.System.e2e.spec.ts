0c82575473337864f40e527af88d3188d7147268
JacobMalling <39791946+JacobMalling@users.noreply.github.com>
2020-01-30 09:54:08 +0100

    Revert "Revert "Added Exposed Interfaces to logical it systems""

--- Presentation.Web/Tests/it-system/Catalog/ErrorHandling.System.e2e.spec.ts
+++ Presentation.Web/Tests/it-system/Catalog/ErrorHandling.System.e2e.spec.ts
@@ -17,7 +17,6 @@ describe("Getting the correct message when there is a conflict deleting a system
         var findCatalogColumnsFor = CatalogHelper.findCatalogColumnsFor;
         var EC = protractor.ExpectedConditions;
         var waitTimer = new waitUpTo();
-        var interfaceHelper = new InterfaceHelper();
 
         afterEach(() => {
             testFixture.enableAutoBrowserWaits();
@@ -77,8 +76,8 @@ describe("Getting the correct message when there is a conflict deleting a system
                     .then(() => expectCreateButtonVisibility(true))
                     .then(() => CatalogHelper.createSystem(systemName))
                     .then(() => expectSystemWithName(systemName))
-                    .then(() => interfaceHelper.createInterface(interfaceName))
-                    .then(() => interfaceHelper.bindInterfaceToSystem(systemName, interfaceName))
+                    .then(() => InterfaceHelper.createInterface(interfaceName))
+                    .then(() => InterfaceHelper.bindInterfaceToSystem(systemName, interfaceName))
                     .then(() => CatalogHelper.getDeleteButtonForSystem(systemName))
                     .then(() => itSystemFrontPage.getDeleteButton().click())
                     .then(() => testFixture.disableAutoBrowserWaits())
---
+++ Presentation.Web/app/components/it-system/Controllers/exposed-interfaces-controller.ts
@@ -0,0 +1,66 @@
+﻿((ng, app) => {
+    app.controller("system.ExposedInterfaces", ["$scope", "$http", "tsas", "interfaces", "interfaceTypes", "methods", "dataTypes", "frequencies", "systemUsageId", "exhibits", "user",
+        ($scope, $http, tsas, interfaces, interfaceTypes, methods, dataTypes, frequencies, systemUsageId, exhibits, user) => {
+
+            $scope.frequencies = frequencies;
+
+            $scope.filterInterfaces = interfaceSystem => interfaceSystem.canBeUsed || interfaceSystem.usage || $scope.showAllInterfaces;
+
+            // resolving complex types from ids
+            function resolveTypes(theInterface) {
+                theInterface.interfaceType = _.find(interfaceTypes, { Id: theInterface.interfaceTypeId });
+                theInterface.interface = _.find(interfaces, { Id: theInterface.interfaceId });
+                theInterface.method = _.find(methods, { Id: theInterface.methodId });
+                theInterface.tsa = _.find(tsas, { Id: theInterface.tsaId });
+
+                _.each(theInterface.dataRows, (dataRow: { dataTypeId; dataType; }) => {
+                    dataRow.dataType = _.find(dataTypes, { Id: dataRow.dataTypeId });
+                });
+
+                theInterface.numRows = theInterface.dataRows.length;
+            }
+
+            // interface exposures
+            _.each(exhibits, (interfaceExposure: { updateUrl; urlParams; id; }) => {
+                interfaceExposure.updateUrl = "api/itInterfaceExhibitUsage/";
+                interfaceExposure.urlParams = `&usageId=${systemUsageId}&exhibitId=${interfaceExposure.id}`;
+                resolveTypes(interfaceExposure);
+            });
+            $scope.interfaceExposures = exhibits;
+
+            $scope.itSystemUsageSelectOptions = selectLazyLoading("api/itSystemUsage", [`organizationId=${user.organizationId}`]);
+            function selectLazyLoading(url, paramAry) {
+                return {
+                    allowClear: true,
+                    minimumInputLength: 1,
+                    initSelection: (elem, callback) => {
+                    },
+                    ajax: {
+                        data: (term, page) => ({ query: term }),
+                        quietMillis: 500,
+                        transport: queryParams => {
+                            var extraParams = paramAry ? "&" + paramAry.join("&") : "";
+                            var res = $http.get(url + "?q=" + queryParams.data.query + extraParams).then(queryParams.success);
+                            res.abort = () => null;
+
+                            return res;
+                        },
+
+                        results: (data, page) => {
+                            var results = [];
+
+                            _.each(data.data.response, (obj: { id; itSystem; }) => {
+                                results.push({
+                                    id: obj.id,
+                                    text: obj.itSystem.name
+                                });
+                            });
+
+                            return { results: results };
+                        }
+                    }
+                };
+            }
+        }
+    ]);
+})(angular, app);
