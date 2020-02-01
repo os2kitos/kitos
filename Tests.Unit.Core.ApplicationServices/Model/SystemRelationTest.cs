78d58a6e50097605d8e72dfba76ba455ad64ae87
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-02-01 11:39:29 +0100

    do not fall back to local call name.. always use the main system name

---
+++ Tests.Unit.Core.ApplicationServices/Model/SystemRelationTest.cs
@@ -0,0 +1,21 @@
+﻿using Core.DomainModel.ItSystemUsage;
+using Tests.Toolkit.Patterns;
+using Xunit;
+
+namespace Tests.Unit.Core.Model
+{
+    public class SystemRelationTest : WithAutoFixture
+    {
+        private readonly ItSystemUsage _fromSystemUsage;
+        private readonly SystemRelation _sut;
+
+        public SystemRelationTest()
+        {
+            _fromSystemUsage = new ItSystemUsage { Id = A<int>(), OrganizationId = A<int>() };
+            _sut = new SystemRelation(_fromSystemUsage);
+        }
+
+        //[Fact]
+        //public void //TODO:
+    }
+}
