dcc64cd3db6b9db33a2125853b1e5c3b81f6e222
JacobMalling <jacob.malling.olesen@gmail.com>
2020-02-03 15:26:31 +0100

    Fixed review. All data fields are separately available to be changed.

--- Presentation.Web/Tests/Helpers/InterfaceHelper.ts
+++ Presentation.Web/Tests/Helpers/InterfaceHelper.ts
@@ -1,66 +1,20 @@
 ﻿import CSSLocator = require("../object-wrappers/CSSLocatorHelper");
 import Select2 = require("./Select2Helper");
-import constant = require("../Utility/Constants")
-import interfaceCatalogHelper = require("./InterfaceCatalogHelper");
+import constant = require("../Utility/Constants");
 
 class InterfaceHelper {
     private static const = new constant();
     private static cssHelper = new CSSLocator();
 
-    public static writeDataToAllInputs(nameOfInterface: string, data: string, exposedBy: string, sysInterface: string, access: string, belongsTo: string, dataTypeTable: string, version: string) {
-        return interfaceCatalogHelper.gotoSpecificInterface(nameOfInterface).then(() =>
-            this.writeDataToTextInput(data, this.const.interfaceNameInput)
-                .then(() => {
-                    return this.writeDataToTextInput(data + "1", this.const.interfaceIdInput);
-                }).then(() => {
-                    return this.writeDataToTextInput(data + "2", this.const.interfaceDescriptionInput);
-                }).then(() => {
-                    return this.writeDataToTextInput(data + "3", this.const.interfaceDescriptionLinkInput);
-                }).then(() => {
-                    return this.writeDataToTextInput(data + "4", this.const.interfaceNoteInput);
-                }).then(() => {
-                    return this.selectDataFromSelect2Field(exposedBy, this.const.interfaceSelectExhibit);
-                }).then(() => {
-                    return this.selectDataFromSelect2Field(sysInterface, this.const.interfaceSelectInterface);
-                }).then(() => {
-                    return this.selectDataFromSelect2Field(access, this.const.interfaceSelectAccess);
-                }).then(() => {
-                    return this.selectDataFromSelect2Field(belongsTo, this.const.interfaceSelectBelongs);
-                }).then(() => {
-                    return this.writeDataToTextInput(version, this.const.interfaceVersionInput);
-                }).then(() => {
-                    return this.writeDataToTable(data, dataTypeTable);
-                }));
-    }
 
-    public static verifyDataWasSaved(data: string, exposedBy: string, sysInterface: string, access: string, belongsTo: string, dataTypeTable: string, version: string) {
-        return interfaceCatalogHelper.gotoSpecificInterface(data).then(() => {
-            // text 
-            this.verifyDataFromTextInput(data, this.const.interfaceNameInput);
-            this.verifyDataFromTextInput(data + "1", this.const.interfaceIdInput);
-            this.verifyDataFromTextInput(data + "2", this.const.interfaceDescriptionInput);
-            this.verifyDataFromTextInput(data + "3", this.const.interfaceDescriptionLinkInput);
-            this.verifyDataFromTextInput(data + "4", this.const.interfaceNoteInput);
-            this.verifyDataFromTextInput(data, this.const.interfaceDataInput);
-            this.verifyDataFromTextInput(version, this.const.interfaceVersionInput);
-
-            //select2
-            this.verifyDataFromSelect2(exposedBy, this.const.interfaceSelectExhibit);
-            this.verifyDataFromSelect2(sysInterface, this.const.interfaceSelectInterface);
-            this.verifyDataFromSelect2(access, this.const.interfaceSelectAccess);
-            this.verifyDataFromSelect2(belongsTo, this.const.interfaceSelectBelongs);
-            this.verifyDataFromSelect2(dataTypeTable, this.const.interfaceSelectTableDataType);
-        });
-    }
-
-    private static writeDataToTextInput(data: string, ele: string) {
+    public static writeDataToTextInput(data: string, ele: string) {
         console.log(`Writing ${data} To ${ele}`);
         return element(this.cssHelper.byDataElementType(ele)).clear().then(() => {
             return element(this.cssHelper.byDataElementType(ele)).sendKeys(data);
         });
     }
 
-    private static writeDataToTable(data: string, dataType: string) {
+    public static writeDataToTable(data: string, dataType: string) {
         console.log(`Writing ${data} ${dataType} to table`);
         return element(this.cssHelper.byDataElementType(this.const.interfaceNewRowButton)).click().then(() => {
             return this.writeDataToTextInput(data, this.const.interfaceDataInput);
@@ -69,18 +23,18 @@ class InterfaceHelper {
         });
     }
 
-    private static verifyDataFromTextInput(data: string, ele: string) {
+    public static verifyDataFromTextInput(data: string, ele: string) {
         console.log(`Verifying ${ele} Has value - ${data}`);
         expect(element(this.cssHelper.byDataElementType(ele)).isDisplayed()).toBe(true);
         expect(element(this.cssHelper.byDataElementType(ele)).getAttribute('value')).toEqual(data);
     }
 
-    private static verifyDataFromSelect2(data: string, ele: string) {
+    public static verifyDataFromSelect2(data: string, ele: string) {
         console.log(`Verifying ${ele} Has value - ${data}`);
         return expect(Select2.getData(ele).getText()).toEqual(data);
     }
 
-    private static selectDataFromSelect2Field(search: string, id: string) {
+    public static selectDataFromSelect2Field(search: string, id: string) {
         console.log(`Entering data into select2 field ${id}`);
         return Select2.searchFor(search, id).then(() => {
             console.log("Waiting for select2 data");
--- Presentation.Web/Tests/it-system/Interfaces/GlobalAdmin.Interface.Creation.e2e.spec.ts
+++ Presentation.Web/Tests/it-system/Interfaces/GlobalAdmin.Interface.Creation.e2e.spec.ts
@@ -4,11 +4,13 @@ import TestFixtureWrapper = require("../../Utility/TestFixtureWrapper");
 import InterfaceCatalogHelper = require("../../Helpers/InterfaceCatalogHelper");
 import InterfaceHelper = require("../../Helpers/InterfaceHelper");
 import ItSystemHelper = require("../../Helpers/SystemCatalogHelper");
+import constant = require("../../Utility/Constants");
 
 describe("Only Global Administrator is able to create and fill out an interface", () => {
     var loginHelper = new Login();
     var pageObject = new ItSystemInterface();
     var testFixture = new TestFixtureWrapper();
+    var constants = new constant();
 
 
     afterEach(() => {
@@ -23,36 +25,99 @@ describe("Only Global Administrator is able to create and fill out an interface"
         testFixture.disableLongRunningTest();
     });
 
-    
+
 
     it("Global Admin is able to create and fill out data on a interface", () => {
-        var iName = createInterfaceName();
-        var sysName = createItSystem();
-        var sysInterface = "CSV";
+        var interfaceName = createInterfaceName();
+        var id = createString();
+        var description = createString();
+        var descriptionLink = createString();
+        var note = createString();
+        var dataRow = createString();
+        var systemName = createItSystem();
+        var systemInterface = "CSV";
         var access = "Lokal";
         var org = "Fælles Kommune";
         var dataType = "Sag";
-        var data = createData();
         var version = createVersion();
-        
+
         loginHelper.loginAsGlobalAdmin()
             .then(() => {
                 console.log("Getting page");
                 return pageObject.getPage();
             }).then(() => {
                 console.log("Creating interface");
-                return InterfaceCatalogHelper.createInterface(iName);
+                return InterfaceCatalogHelper.createInterface(interfaceName);
             }).then(() => {
                 console.log("Creating IT system");
-                return ItSystemHelper.createSystem(sysName);
+                return ItSystemHelper.createSystem(systemName);
             }).then(() => {
                 console.log("Inserting data");
-                return InterfaceHelper.writeDataToAllInputs(iName, data, sysName, sysInterface, access, org, dataType, version);
+                return writeDataToAllInputs(interfaceName, id, description, descriptionLink, note, dataRow, systemName, systemInterface, access, org, dataType, version);
             }).then(() => {
                 console.log("Verifying data");
-                return InterfaceHelper.verifyDataWasSaved(data, sysName, sysInterface, access, org, dataType, version);
+                return verifyDataWasSaved(interfaceName, id, description, descriptionLink, note, dataRow, systemName, systemInterface, access, org, dataType, version);
             });
     });
+
+    function writeDataToAllInputs(
+        nameOfInterface: string,
+        id: string,
+        description: string,
+        descriptionLink: string,
+        note: string,
+        dataRow: string,
+        systemName: string,
+        systemInterface: string,
+        access: string,
+        belongsTo: string,
+        dataTypeTable: string,
+        version: string) {
+        return InterfaceCatalogHelper.gotoSpecificInterface(nameOfInterface)
+            .then(() => InterfaceHelper.writeDataToTextInput(nameOfInterface, constants.interfaceNameInput)
+                .then(() => InterfaceHelper.writeDataToTextInput(id, constants.interfaceIdInput))
+                .then(() => InterfaceHelper.writeDataToTextInput(description, constants.interfaceDescriptionInput))
+                .then(() => InterfaceHelper.writeDataToTextInput(descriptionLink, constants.interfaceDescriptionLinkInput))
+                .then(() => InterfaceHelper.writeDataToTextInput(note, constants.interfaceNoteInput))
+                .then(() => InterfaceHelper.selectDataFromSelect2Field(systemName, constants.interfaceSelectExhibit))
+                .then(() => InterfaceHelper.selectDataFromSelect2Field(systemInterface, constants.interfaceSelectInterface))
+                .then(() => InterfaceHelper.selectDataFromSelect2Field(access, constants.interfaceSelectAccess))
+                .then(() => InterfaceHelper.selectDataFromSelect2Field(belongsTo, constants.interfaceSelectBelongs))
+                .then(() => InterfaceHelper.writeDataToTextInput(version, constants.interfaceVersionInput))
+                .then(() => InterfaceHelper.writeDataToTable(dataRow, dataTypeTable)));
+    };
+
+    function verifyDataWasSaved(
+        nameOfInterface: string,
+        id: string,
+        description: string,
+        descriptionLink: string,
+        note: string,
+        dataRow: string,
+        systemName: string,
+        systemInterface: string,
+        access: string,
+        belongsTo: string,
+        dataTypeTable: string,
+        version: string) {
+        return InterfaceCatalogHelper.gotoSpecificInterface(nameOfInterface)
+            .then(() => {
+                InterfaceHelper.verifyDataFromTextInput(nameOfInterface, constants.interfaceNameInput);
+                InterfaceHelper.verifyDataFromTextInput(id, constants.interfaceIdInput);
+                InterfaceHelper.verifyDataFromTextInput(description, constants.interfaceDescriptionInput);
+                InterfaceHelper.verifyDataFromTextInput(descriptionLink, constants.interfaceDescriptionLinkInput);
+                InterfaceHelper.verifyDataFromTextInput(note, constants.interfaceNoteInput);
+                InterfaceHelper.verifyDataFromTextInput(version, constants.interfaceVersionInput);
+                InterfaceHelper.verifyDataFromTextInput(dataRow, constants.interfaceDataInput);
+
+                InterfaceHelper.verifyDataFromSelect2(systemName, constants.interfaceSelectExhibit);
+                InterfaceHelper.verifyDataFromSelect2(systemInterface, constants.interfaceSelectInterface);
+                InterfaceHelper.verifyDataFromSelect2(access, constants.interfaceSelectAccess);
+                InterfaceHelper.verifyDataFromSelect2(belongsTo, constants.interfaceSelectBelongs);
+                InterfaceHelper.verifyDataFromSelect2(dataTypeTable, constants.interfaceSelectTableDataType);
+            });
+    };
+
 
     function createInterfaceName() {
         return `Interface${new Date().getTime()}`;
@@ -62,8 +127,8 @@ describe("Only Global Administrator is able to create and fill out an interface"
         return `ItSystem${new Date().getTime()}`;
     }
 
-    function createData() {
-        return `SawData${new Date().getTime()}`;
+    function createString() {
+        return `Data${new Date().getTime()}`;
     }
 
     function createVersion() {
