ab76683ee13111227e4ff859ecebced0f18df5eb
JacobMalling <jacob.malling.olesen@gmail.com>
2020-02-19 12:44:04 +0100

    Aligned the system/interfaces and systemusage/interfaces views

--- Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-interfaces.controller.ts
+++ Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-interfaces.controller.ts
@@ -8,13 +8,7 @@
                 exhibits: [
                     "$http", "itSystemUsage", "user", ($http, itSystemUsage, user) =>
                         $http.get(`api/exhibit/?interfaces=true&sysId=${itSystemUsage.itSystem.id}&orgId=${user.currentOrganizationId}`).then(result => {
-                            var interfaces = result.data.response;
-                            _.each(interfaces, (data: { exhibitedById; usage; }) => {
-                                $http.get(`api/itInterfaceExhibitUsage/?usageId=${itSystemUsage.id}&exhibitId=${data.exhibitedById}`).success(usageResult => {
-                                    data.usage = usageResult.response;
-                                });
-                            });
-                            return interfaces;
+                            return result.data.response;
                         })
                 ],
                 user: [
@@ -29,14 +23,7 @@
 
             $scope.systemUsage = itSystemUsage;
 
-            // Enables marking "wished" 
-            _.each(exhibits, (interfaceExposure: { updateUrl; urlParams; id; }) => {
-                interfaceExposure.updateUrl = "api/itInterfaceExhibitUsage/";
-                interfaceExposure.urlParams = `&usageId=${itSystemUsage.id}&exhibitId=${interfaceExposure.id}`;
-            });
-
             $scope.interfaceExposures = exhibits;
         }
     ]);
-
 })(angular, app);
