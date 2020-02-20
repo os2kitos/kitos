4e2c4aaa22019595fa0604caa25aebc87d0d8133
JacobMalling <jacob.malling.olesen@gmail.com>
2020-02-20 14:32:29 +0100

    Fixed review

--- Presentation.Web/app/utility/tableManipulation.ts
+++ Presentation.Web/app/utility/tableManipulation.ts
@@ -1,18 +1,20 @@
 ﻿module Kitos.Utility {
 
     export class TableManipulation {
+        
 
         static expandRetractParagraphCell(e, shortTextLineCount) {
+            const textExpandedAttribute = "data-textExpanded";
             const element = angular.element(e.currentTarget);
             const para = element.closest("td").find(document.getElementsByClassName("readMoreParagraph"))[0];
             const btn = element[0];
 
-            if (para.getAttribute("data-textExpanded") != null) {
-                para.removeAttribute("data-textExpanded");
+            if (para.getAttribute(textExpandedAttribute) != null) {
+                para.removeAttribute(textExpandedAttribute);
                 para.setAttribute("style", `height: ${shortTextLineCount}em;overflow: hidden;`);
                 btn.innerText = "Se mere";
             } else {
-                para.setAttribute("data-textExpanded", "");
+                para.setAttribute(textExpandedAttribute, "");
                 para.removeAttribute("style");
                 btn.innerText = "Se mindre";
             }
