1d62cca0cc58ba1d0ff3dcd7a145b49f2913ab21
JacobMalling <jacob.malling.olesen@gmail.com>
2020-02-24 12:36:46 +0100

    Fixed reference and KLE view on main page. Cleaned up ts files.

--- Presentation.Web/app/components/it-system/edit/tabs/it-system-edit-tab-main.controller.ts
+++ Presentation.Web/app/components/it-system/edit/tabs/it-system-edit-tab-main.controller.ts
@@ -18,8 +18,9 @@
 
     app.controller("system.SystemMainCtrl",
         [
-            "$rootScope", "$scope", "$http", "itSystem", "businessTypes", "user", "autofocus", "hasWriteAccess",
-            ($rootScope, $scope, $http, itSystem, businessTypes, user, autofocus, hasWriteAccess) => {
+            "$rootScope", "$scope", "itSystem", "businessTypes", "user", "autofocus", "hasWriteAccess", "select2LoadingService",
+            ($rootScope, $scope, itSystem, businessTypes, user, autofocus, hasWriteAccess, select2LoadingService) => {
+
                 $rootScope.page.title = "IT System - Rediger system";
                 autofocus();
 
@@ -27,60 +28,21 @@
                 itSystem.belongsTo = (!itSystem.belongsToId) ? null : { id: itSystem.belongsToId, text: itSystem.belongsToName };
                 itSystem.parent = (!itSystem.parentId) ? null : { id: itSystem.parentId, text: itSystem.parentName };
 
-                $scope.select2AllowClearOpt = {
-                    allowClear: true
-                };
-                
                 $scope.system = itSystem;
                 $scope.businessTypes = businessTypes.data.value;
-                $scope.itSystemsSelectOptions = selectLazyLoading("api/itsystem", true, [`excludeId=${itSystem.id}`, `orgId=${user.currentOrganizationId}`]);
-                $scope.organizationSelectOptions = selectLazyLoading("api/organization", true, [`orgId=${user.currentOrganizationId}`]);
+
+                $scope.itSystemsSelectOptions = select2LoadingService.loadSelect2(
+                    "api/itsystem",
+                    true,
+                    [`excludeId=${itSystem.id}`, `orgId=${user.currentOrganizationId}`],
+                    true);
+                $scope.organizationSelectOptions = select2LoadingService.loadSelect2(
+                    "api/organization",
+                    true,
+                    [`orgId=${user.currentOrganizationId}`],
+                    true);
             
                 $scope.hasWriteAccess = hasWriteAccess;
-
-
-                function selectLazyLoading(url: any, allowClear: any, paramAry: any);
-                function selectLazyLoading(url, allowClear, paramAry) {
-                    return {
-                        minimumInputLength: 1,
-                        initSelection: (elem, callback) => {
-                        },
-                        allowClear: allowClear,
-                        ajax: {
-                            data: (term, page) => ({ query: term }),
-                            quietMillis: 500,
-                            transport: (queryParams) => {
-                                var extraParams = paramAry ? `&${paramAry.join("&")}` : "";
-                                var res = $http.get(url + "?q=" + queryParams.data.query + extraParams).then(queryParams.success);
-                                res.abort = () => null;
-                                return res;
-                            },
-                            
-                            results: (data, page) => {
-                                var results = [];
-                                if (url === "api/itsystem") {
-                                    _.each(data.data.response, (obj: { id; name; disabled; }) => {
-                                        if (obj.disabled === false) {
-                                            results.push({
-                                                id: obj.id,
-                                                text: obj.name
-                                            });
-                                        }
-                                    });
-                                }
-                                else {
-                                    _.each(data.data.response, (obj: { id; name; }) => {
-                                        results.push({
-                                            id: obj.id,
-                                            text: obj.name
-                                        });
-                                    }); 
-                                }
-                                return { results: results };
-                            }
-                        }
-                    };
-                }
             }
         ]);
 })(angular, app);
