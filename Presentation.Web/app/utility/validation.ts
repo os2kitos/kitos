ac1391223dd6f7c74451c9ac15432d605a920847
JacobMalling <jacob.malling.olesen@gmail.com>
2020-02-25 09:46:00 +0100

    Fixed review. Delete some unused files.

--- Presentation.Web/app/utility/validation.ts
+++ Presentation.Web/app/utility/validation.ts
@@ -1,7 +1,9 @@
 ﻿module Kitos.Utility {
     export class Validation {
         static validateUrl(url: string): boolean {
-
+            if (url == null) {
+                return false;
+            }
             const regexp = /(^https?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/])$)?/;
             return regexp.test(url.toLowerCase());
         }
