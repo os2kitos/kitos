e8f0ebc65191d595a8c6fac698778b1abb7371a2
JacobMalling <jacob.malling.olesen@gmail.com>
2020-02-26 12:19:33 +0100

    Working on fixing ui errors

--- Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-main.controller.ts
+++ Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-main.controller.ts
@@ -1,33 +1,21 @@
-﻿(function (ng, app) {
-    app.config(['$stateProvider', function ($stateProvider) {
-        $stateProvider.state('it-system.usage.main', {
-            url: '/main',
-            templateUrl: 'app/components/it-system/usage/tabs/it-system-usage-tab-main.view.html',
-            controller: 'system.EditMain',
+﻿((ng, app) => {
+    app.config(["$stateProvider", $stateProvider => {
+        $stateProvider.state("it-system.usage.main", {
+            url: "/main",
+            templateUrl: "app/components/it-system/usage/tabs/it-system-usage-tab-main.view.html",
+            controller: "system.EditMain",
             resolve: {
                 businessTypes: [
-                    '$http', function ($http) {
-                        return $http.get("odata/LocalBusinessTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
-                            .then(function (result) {
-                                return result.data.value;
-                            });
-                    }
+                    "$http", $http => $http.get("odata/LocalBusinessTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                    .then(result => result.data.value)
                 ],
                 archiveTypes: [
-                    '$http', function ($http) {
-                        return $http.get('odata/LocalArchiveTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc')
-                            .then(function (result) {
-                                return result.data.value;
-                            });
-                    }
+                    "$http", $http => $http.get("odata/LocalArchiveTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                    .then(result => result.data.value)
                 ],
                 sensitiveDataTypes: [
-                    '$http', function ($http) {
-                        return $http.get('odata/LocalSensitiveDataTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc')
-                            .then(function (result) {
-                                return result.data.value;
-                            });
-                    }
+                    "$http", $http => $http.get("odata/LocalSensitiveDataTypes?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
+                    .then(result => result.data.value)
                 ],
                 systemCategories: [
                     "$http", $http => $http.get("odata/LocalItSystemCategories?$filter=IsLocallyAvailable eq true or IsObligatory&$orderby=Priority desc")
@@ -37,12 +25,11 @@
         });
     }]);
 
-    app.controller('system.EditMain', ['$rootScope', '$scope', '$http', '$stateParams', 'notify', 'itSystemUsage', 'user', 'systemCategories',
-        'businessTypes', 'archiveTypes', 'sensitiveDataTypes', 'autofocus', 'hasWriteAccess', 
-        function ($rootScope, $scope, $http, $stateParams, notify, itSystemUsage, user, systemCategories, businessTypes, archiveTypes, sensitiveDataTypes, autofocus, hasWriteAccess) {
-            $rootScope.page.title = 'IT System - Anvendelse';
-            $scope.autoSaveUrl = 'api/itsystemusage/' + $stateParams.id;
-            $scope.autosaveUrl2 = 'api/itsystemusage/' + $scope.usage.id;
+    app.controller("system.EditMain", ["$rootScope", "$scope", "$http", "$stateParams", "notify", "itSystemUsage", "user", "systemCategories",
+        "businessTypes", "archiveTypes", "sensitiveDataTypes", "autofocus", "hasWriteAccess", 
+        ($rootScope, $scope, $http, $stateParams, notify, itSystemUsage, user, systemCategories, businessTypes, archiveTypes, sensitiveDataTypes, autofocus, hasWriteAccess) => {
+            $rootScope.page.title = "IT System - Anvendelse";
+            $scope.autoSaveUrl = `api/itSystemUsage/${$scope.usage.id}`;
             $scope.usage = itSystemUsage;
             $scope.usageId = $stateParams.id;
             $scope.hasWriteAccess = hasWriteAccess;
@@ -51,6 +38,7 @@
             $scope.sensitiveDataTypes = sensitiveDataTypes;
             $scope.hasViewAccess = user.currentOrganizationId == itSystemUsage.organizationId;
             $scope.systemCategories = systemCategories;
+            $scope.system = itSystemUsage.itSystem;
             autofocus();
             var today = new Date();
             if (!itSystemUsage.active) {
@@ -76,34 +64,33 @@
 
             $scope.orgUnits = itSystemUsage.usedBy;
 
-            $scope.itSytemUsagesSelectOptions = selectLazyLoading('api/itSystemUsage', false, ['organizationId=' + itSystemUsage.organizationId]);
+            $scope.itSytemUsagesSelectOptions = selectLazyLoading("api/itSystemUsage", false, ["organizationId=" + itSystemUsage.organizationId]);
 
+            function selectLazyLoading(url: any, excludeSelf: any, paramAry: any);
             function selectLazyLoading(url, excludeSelf, paramAry) {
                 return {
                     minimumInputLength: 1,
                     allowClear: true,
-                    placeholder: ' ',
-                    initSelection: function (elem, callback) {
+                    placeholder: " ",
+                    initSelection(elem, callback) {
                     },
                     ajax: {
-                        data: function (term, page) {
+                        data(term, page) {
                             return { query: term };
                         },
                         quietMillis: 500,
-                        transport: function (queryParams) {
-                            var extraParams = paramAry ? '&' + paramAry.join('&') : '';
-                            var res = $http.get(url + '?q=' + queryParams.data.query + extraParams).then(queryParams.success);
-                            res.abort = function () {
-                                return null;
-                            };
+                        transport(queryParams) {
+                            var extraParams = paramAry ? "&" + paramAry.join("&") : "";
+                            var res = $http.get(url + "?q=" + queryParams.data.query + extraParams).then(queryParams.success);
+                            res.abort = () => null;
 
                             return res;
                         },
 
-                        results: function (data, page) {
+                        results(data, page) {
                             var results = [];
 
-                            _.each(data.data.response, function (obj: { id; itSystem; cvr; }) {
+                            _.each(data.data.response, (obj: { id; itSystem; cvr; }) => {
                                 if (excludeSelf && obj.id == $scope.usageId)
                                     return; // don't add self to result
 
@@ -124,14 +111,14 @@
                 var concluded = $scope.usage.concluded;
                 var formatString = "DD-MM-YYYY";
                 var formatDateString = "YYYY-MM-DD";
-                var fromDate = moment(concluded, [formatString, formatDateString]).startOf('day');
-                var endDate = moment(expirationDate, [formatString, formatDateString]).endOf('day');
+                var fromDate = moment(concluded, [formatString, formatDateString]).startOf("day");
+                var endDate = moment(expirationDate, [formatString, formatDateString]).endOf("day");
                 var date = moment(value, "DD-MM-YYYY");
                 var today = moment();
                 if (value === "" || value == undefined) {
                     var payload = {};
                     payload[field] = null;
-                    patch(payload, $scope.autosaveUrl2 + '?organizationId=' + user.currentOrganizationId);
+                    patch(payload, $scope.autosaveUrl2 + "?organizationId=" + user.currentOrganizationId);
                 } else if (!date.isValid() || isNaN(date.valueOf()) || date.year() < 1000 || date.year() > 2099) {
                     notify.addErrorMessage("Den indtastede dato er ugyldig.");
                 }
@@ -139,9 +126,9 @@
                     notify.addErrorMessage("Den indtastede slutdato er før startdatoen.");
                 }
                 else {
-                    if (today.isBetween(moment($scope.usage.concluded, "DD-MM-YYYY").startOf('day'), moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf('day'), null, '[]') ||
-                        (today.isSameOrAfter(moment($scope.usage.concluded, "DD-MM-YYYY").startOf('day')) && $scope.usage.expirationDate == null) ||
-                        (today.isSameOrBefore(moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf('day')) && $scope.usage.concluded == null) ||
+                    if (today.isBetween(moment($scope.usage.concluded, "DD-MM-YYYY").startOf("day"), moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf("day"), null, "[]") ||
+                        (today.isSameOrAfter(moment($scope.usage.concluded, "DD-MM-YYYY").startOf("day")) && $scope.usage.expirationDate == null) ||
+                        (today.isSameOrBefore(moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf("day")) && $scope.usage.concluded == null) ||
                         ($scope.usage.expirationDate == null && $scope.usage.concluded == null)) {
                         $scope.usage.isActive = true;
                     }
@@ -156,16 +143,18 @@
                     var dateString = date.format("YYYY-MM-DD");
                     var payload = {};
                     payload[field] = dateString;
-                    patch(payload, $scope.autosaveUrl2 + '?organizationId=' + user.currentOrganizationId);
+                    patch(payload, $scope.autosaveUrl2 + "?organizationId=" + user.currentOrganizationId);
                 }
             }
+
+            function patch(payload: any, url: any);
             function patch(payload, url) {
                 var msg = notify.addInfoMessage("Gemmer...", false);
-                $http({ method: 'PATCH', url: url, data: payload })
-                    .success(function () {
+                $http({ method: "PATCH", url: url, data: payload })
+                    .success(() => {
                         msg.toSuccessMessage("Feltet er opdateret.");
                     })
-                    .error(function () {
+                    .error(() => {
                         msg.toErrorMessage("Fejl! Feltet kunne ikke ændres!");
                     });
             }
@@ -173,9 +162,9 @@
             $scope.checkSystemValidity = () => {
                 var today = moment();
 
-                if (today.isBetween(moment($scope.usage.concluded, "DD-MM-YYYY").startOf('day'), moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf('day'), null, '[]') ||
-                    (today > moment($scope.usage.concluded, "DD-MM-YYYY").startOf('day') && $scope.usage.expirationDate == null) ||
-                    (today < moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf('day') && $scope.usage.concluded == null) ||
+                if (today.isBetween(moment($scope.usage.concluded, "DD-MM-YYYY").startOf("day"), moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf("day"), null, "[]") ||
+                    (today > moment($scope.usage.concluded, "DD-MM-YYYY").startOf("day") && $scope.usage.expirationDate == null) ||
+                    (today < moment($scope.usage.expirationDate, "DD-MM-YYYY").endOf("day") && $scope.usage.concluded == null) ||
                     ($scope.usage.expirationDate == null && $scope.usage.concluded == null)) {
                     $scope.usage.isActive = true;
                 }
