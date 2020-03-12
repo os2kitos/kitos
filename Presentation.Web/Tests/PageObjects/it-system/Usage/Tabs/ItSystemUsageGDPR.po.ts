e41abb2fe9a0fe0b84dc059013ae91718ee5771f
Mads Brønd Kracht <mbk@strongminds.dk>
2020-03-12 13:10:29 +0100

    Started protractor test files for the GDPR tab

---
+++ Presentation.Web/Tests/PageObjects/it-system/Usage/Tabs/ItSystemUsageGDPR.po.ts
@@ -0,0 +1,12 @@
+﻿import constants = require("../../../../Utility/Constants");
+import CssHelper = require("../../../../Object-wrappers/CSSLocatorHelper");
+
+class ItSystemUsageGDPR {
+
+    private consts = new constants();
+    private cssHelper = new CssHelper();
+
+
+}
+
+export = ItSystemUsageGDPR;
---
+++ Presentation.Web/Tests/it-system/Usage/Tabs/GDPR.System.e2e.spec.ts
@@ -0,0 +1,36 @@
+﻿import login = require("../../../Helpers/LoginHelper");
+import TestFixtureWrapper = require("../../../Utility/TestFixtureWrapper");
+import ItSystemHelper = require("../../../Helpers/SystemCatalogHelper");
+import Select2Helper = require("../../../Helpers/Select2Helper");
+import TestFixtureWrapper1 = require("../../../Utility/TestFixtureWrapper");
+import ItSystemReference1 = require("../../../PageObjects/It-system/Tabs/ItSystemReference.po");
+import ItSystemReference = require("../../PageObjects/it-system/Tabs/ItSystemFrontpage.po");
+
+describe("Global Admin can",
+    () => {
+        const optionInputs = {
+            Undecided: { text: " " },
+            B: { text: "B" }
+        };
+        var loginHelper = new login();
+        var testFixture = new TestFixtureWrapper1();
+        var pageObject = new ItSystemReference1();
+
+        var itSystemName = `ItSystemMainTabTest${new Date().getTime()}`;
+
+        beforeAll(() => {
+            loginHelper.loginAsGlobalAdmin()
+                .then(() => ItSystemHelper.createSystem(itSystemName));
+        });
+
+        beforeEach(() => {
+            testFixture.enableLongRunningTest();
+        });
+
+        afterAll(() => {
+            testFixture.disableLongRunningTest();
+            testFixture.cleanupState();
+        });
+
+
+    });
