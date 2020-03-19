952d73ea2da884a2c66db9a7c4f2ab2b853f42c5
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-03-19 12:41:06 +0100

    added unit tests for repositories

---
+++ Tests.Unit.Core.ApplicationServices/DomainServices/SSO/SsoOrganizationIdentityRepositoryTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DomainModel.SSO;
+using Core.DomainServices;
+using Core.DomainServices.Repositories.SSO;
+using Moq;
+using Tests.Toolkit.Patterns;
+using Xunit;
+
+namespace Tests.Unit.Core.DomainServices.SSO
+{
+    public class SsoOrganizationIdentityRepositoryTest : WithAutoFixture
+    {
+        private readonly Mock<IGenericRepository<SsoOrganizationIdentity>> _repository;
+        private readonly SsoOrganizationIdentityRepository _sut;
+
+        public SsoOrganizationIdentityRepositoryTest()
+        {
+            _repository = new Mock<IGenericRepository<SsoOrganizationIdentity>>();
+            _sut = new SsoOrganizationIdentityRepository(_repository.Object);
+        }
+
+        [Fact]
+        public void GetByExternalUuid_Returns_Value()
+        {
+            //Arrange
+            var inputId = A<Guid>();
+            var expectedResponse = CreateSsoOrganizationIdentity(inputId);
+
+            ExpectRepositoryContent(CreateSsoOrganizationIdentity(), expectedResponse);
+
+            //Act
+            var result = _sut.GetByExternalUuid(inputId);
+
+            //Assert
+            Assert.True(result.HasValue);
+            Assert.Same(expectedResponse, result.Value);
+        }
+
+        [Fact]
+        public void GetByExternalUuid_Returns_None()
+        {
+            //Arrange
+            var inputId = A<Guid>();
+
+            ExpectRepositoryContent(CreateSsoOrganizationIdentity(), CreateSsoOrganizationIdentity());
+
+            //Act
+            var result = _sut.GetByExternalUuid(inputId);
+
+            //Assert
+            Assert.False(result.HasValue);
+        }
+
+        private void ExpectRepositoryContent(params SsoOrganizationIdentity[] response)
+        {
+            _repository.Setup(x => x.AsQueryable()).Returns(response.AsQueryable());
+        }
+
+        private SsoOrganizationIdentity CreateSsoOrganizationIdentity(Guid? id = null)
+        {
+            return new SsoOrganizationIdentity
+            {
+                ExternalUuid = id.GetValueOrDefault(A<Guid>())
+            };
+        }
+    }
+}
---
+++ Tests.Unit.Core.ApplicationServices/DomainServices/SSO/SsoUserIdentityRepositoryTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using Core.DomainModel.Result;
+using Core.DomainModel.SSO;
+using Core.DomainServices;
+using Core.DomainServices.Repositories.SSO;
+using Moq;
+using Moq.Language.Flow;
+using Tests.Toolkit.Patterns;
+using Xunit;
+
+namespace Tests.Unit.Core.DomainServices.SSO
+{
+    public class SsoUserIdentityRepositoryTest : WithAutoFixture
+    {
+        private readonly Mock<IGenericRepository<SsoUserIdentity>> _repository;
+        private readonly SsoUserIdentityRepository _sut;
+
+        public SsoUserIdentityRepositoryTest()
+        {
+            _repository = new Mock<IGenericRepository<SsoUserIdentity>>();
+            _sut = new SsoUserIdentityRepository(_repository.Object);
+        }
+
+        [Fact]
+        public void GetByExternalUuid_Returns_Value()
+        {
+            //Arrange
+            var inputId = A<Guid>();
+            var expectedResponse = CreateSsoUserIdentity(inputId);
+
+            ExpectRepositoryContent(CreateSsoUserIdentity(), expectedResponse);
+
+            //Act
+            var result = _sut.GetByExternalUuid(inputId);
+
+            //Assert
+            Assert.True(result.HasValue);
+            Assert.Same(expectedResponse, result.Value);
+        }
+
+        [Fact]
+        public void GetByExternalUuid_Returns_None()
+        {
+            //Arrange
+            var inputId = A<Guid>();
+
+            ExpectRepositoryContent(CreateSsoUserIdentity(), CreateSsoUserIdentity());
+
+            //Act
+            var result = _sut.GetByExternalUuid(inputId);
+
+            //Assert
+            Assert.False(result.HasValue);
+        }
+
+        private void ExpectRepositoryContent(params SsoUserIdentity[] response)
+        {
+            _repository.Setup(x => x.AsQueryable()).Returns(response.AsQueryable());
+        }
+
+        private SsoUserIdentity CreateSsoUserIdentity(Guid? id = null)
+        {
+            return new SsoUserIdentity
+            {
+                ExternalUuid = id.GetValueOrDefault(A<Guid>())
+            };
+        }
+    }
+}
