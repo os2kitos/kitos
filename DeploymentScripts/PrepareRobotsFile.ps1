6f9937480c38abb5d3d2a91eadebcdadd081fb38
Mads Brønd Kracht <mbk@strongminds.dk>
2020-04-01 14:53:34 +0200

    Should not copy the robots.txt to the right position.

--- DeploymentScripts/PrepareRobotsFile.ps1
+++ DeploymentScripts/PrepareRobotsFile.ps1
@@ -1,6 +1,18 @@
 Function CopyAndRenameRobots([String] $robotsFileName)
 {
-	Copy-Item "$PSScriptRoot\Robots\$($robotsFileName).txt" -Destination "$PSScriptRoot\..\Presentation.Web\Robots.txt"
+	Write-Debug "Extracting files"
+	Expand-Archive "$PSScriptRoot\..\WebPackage\Presentation.Web.csproj.zip" -DestinationPath "$PSScriptRoot\..\WebPackage\files-to-edit\"
+	
+	Write-Debug "Copying Robot file"
+	Copy-Item "$PSScriptRoot\Robots\$($robotsFileName).txt" -Destination "$PSScriptRoot\..\WebPackage\files-to-edit\Content\C_C\kitos_tmp\app\Robots.txt"
+	
+	Write-Debug "Zipping file"
+	Compress-Archive "$PSScriptRoot\..\WebPackage\files-to-edit\**" -DestinationPath "$PSScriptRoot\..\WebPackage\Presentation.Web.csproj.zip" -Force
+	
+	Write-Debug "Removing temp files"
+	Remove-Item "$PSScriptRoot\..\WebPackage\files-to-edit" -recurse
+	
+	Write-Debug "Robots.txt filetransfer complete"
 }
 
 Function Prepare-RobotsFile([String] $targetEnvironment)
@@ -8,8 +20,8 @@ Function Prepare-RobotsFile([String] $targetEnvironment)
     switch( $targetEnvironment ) 
     {
         "integration" 			
-        {
- 			CopyAndRenameRobots -robotsFileName "Test_Robots"
+			{
+				CopyAndRenameRobots -robotsFileName "Test_Robots"
             break;
         }
         "test"
