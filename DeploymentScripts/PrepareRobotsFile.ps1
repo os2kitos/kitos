84b4590f0ab623b6d0f4fa70138dd4308ca81bec
Mads Brønd Kracht <mbk@strongminds.dk>
2020-04-02 10:32:07 +0200

    Oops should not be under apps/apps

--- DeploymentScripts/PrepareRobotsFile.ps1
+++ DeploymentScripts/PrepareRobotsFile.ps1
@@ -1,7 +1,7 @@
 Function CopyAndRenameRobots([String] $robotsFileName)
 {	
 	Write-Host "Copying Robots.txt"
-	Copy-Item "$PSScriptRoot\Robots\$($robotsFileName).txt" -Destination "C:\kitos_tmp\app\Robots.txt"
+	Copy-Item "$PSScriptRoot\Robots\$($robotsFileName).txt" -Destination "C:\kitos_tmp\Robots.txt"
 }
 
 Function Prepare-RobotsFile([String] $targetEnvironment)
