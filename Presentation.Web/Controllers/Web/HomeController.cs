444d0168b5e8d8dcda03ce287dcb3b9cc950be62
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-04-17 13:06:43 +0200

    removed SSO as temp feature

--- Presentation.Web/Controllers/Web/HomeController.cs
+++ Presentation.Web/Controllers/Web/HomeController.cs
@@ -44,7 +44,6 @@ namespace Presentation.Web.Controllers.Web
         {
             if (ssoErrorCode.HasValue)
             {
-                PushTempVariable(TemporaryFeature.Sso, FeatureToggleKey); //enable sso toggle
                 PushTempVariable(ssoErrorCode, SsoErrorKey);
             }
 
