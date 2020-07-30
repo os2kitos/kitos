3630910161fe630f0fcf16d6be689068288dddf5
JacobMalling <jacob.malling.olesen@gmail.com>
2020-07-30 09:19:21 +0200

    Indexes are fixed

---
+++ Core.ApplicationServices/Model/MaxLength.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ApplicationServices.Model
+{
+    public class MaxLength
+    {
+        public static int OneHundred { get; } = 100;
+        public static int OneHundredFifty { get; } = 150;
+        public static int TwoHundred { get; } = 200;
+    }
+}
