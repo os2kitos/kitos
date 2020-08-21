b5b45c275457ed0e753e7de27aa312f1434de6e0
Mads Brønd Kracht <mbk@strongminds.dk>
2020-08-21 12:12:03 +0200

    Removed unused imports

--- Core.DomainServices/Repositories/Organization/IOrganizationUnitRepository.cs
+++ Core.DomainServices/Repositories/Organization/IOrganizationUnitRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Infrastructure.Services.Types;
+﻿using System.Collections.Generic;
 
 namespace Core.DomainServices.Repositories.Organization
 {
