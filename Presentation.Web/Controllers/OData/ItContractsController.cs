4c1f9be6fe1a4ca0ea9f0ac2fe7dfbb0ec461edd
Mads Brønd Kracht <mbk@strongminds.dk>
2020-08-21 12:10:14 +0200

    Removed unused imports

--- Presentation.Web/Controllers/OData/ItContractsController.cs
+++ Presentation.Web/Controllers/OData/ItContractsController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
--- Presentation.Web/Controllers/OData/ItSystemUsagesController.cs
+++ Presentation.Web/Controllers/OData/ItSystemUsagesController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Microsoft.AspNet.OData;
