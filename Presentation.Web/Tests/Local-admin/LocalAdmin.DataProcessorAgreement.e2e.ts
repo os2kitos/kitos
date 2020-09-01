0600345c2affec18353947e48d092b458ba30a1f
Mads Brønd Kracht <mbk@strongminds.dk>
2020-09-01 15:52:47 +0200

    Initial Test setup

---
+++ Presentation.Web/Tests/Local-admin/LocalAdmin.DataProcessorAgreement.e2e.ts
@@ -0,0 +1,19 @@
+﻿import Login = require("../Helpers/LoginHelper");
+import DPA = require("../PageObjects/Local-admin/LocalDataProcessorAgreement.po");
+
+
+describe("Local admin is able to toggle DataProcessorAgreement", () => {
+
+    var loginHelper = new Login();
+    var DPAPageHelper = new DPA();
+
+    beforeAll(() => {
+        loginHelper.loginAsGlobalAdmin();
+    });
+
+    it("Option to toggle DataProcessorAgreement is visible", () => {
+        DPAPageHelper.getPage();
+    });
+
+});
+
---
+++ Presentation.Web/Tests/PageObjects/Local-admin/LocalDataProcessorAgreement.po.ts
@@ -0,0 +1,13 @@
+﻿import PageObject = require("../IPageObject.po");
+import NavigationHelper = require("../../Utility/NavigationHelper");
+import constants = require("../../Utility/Constants");
+
+class LocalProject implements PageObject {
+    private navigationHelper = new NavigationHelper();
+
+    getPage(): webdriver.promise.Promise<void> {
+        return this.navigationHelper.getPage("/#/local-config/DataProcessorAgreement");
+    }
+}
+
+export = LocalProject;
