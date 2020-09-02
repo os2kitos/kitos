1a9e77f9926af03a8ccd3972434537118ed3ad58
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-09-02 11:03:21 +0200

    cleanup

---
+++ Presentation.Web/Controllers/OData/BaseOdataController.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Core.DomainModel.Result;
+using Microsoft.AspNet.OData;
+
+namespace Presentation.Web.Controllers.OData
+{
+    [Authorize]
+    public class BaseOdataController : ODataController
+    {
+        protected IHttpActionResult FromOperationFailure(OperationFailure failure)
+        {
+            return FromOperationError(failure);
+        }
+
+        protected IHttpActionResult FromOperationError(OperationError failure)
+        {
+            HttpStatusCode statusCode = failure.FailureType switch
+            {
+                OperationFailure.BadInput => HttpStatusCode.BadRequest,
+                OperationFailure.NotFound => HttpStatusCode.NotFound,
+                OperationFailure.Forbidden => HttpStatusCode.Forbidden,
+                OperationFailure.Conflict => HttpStatusCode.Conflict,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            return ResponseMessage(new HttpResponseMessage(statusCode) {Content = new StringContent(failure.Message.GetValueOrFallback(statusCode.ToString("G")))});
+        }
+    }
+}
