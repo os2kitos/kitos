d75e8366df6e4d2a1deb5dff00f92334b4a6acbd
Mads Brønd Kracht <mbk@strongminds.dk>
2020-09-03 12:41:43 +0200

    Renamed migration to DataProcessing + renamed config value on gdpr tab

---
+++ Infrastructure.DataAccess/Migrations/202009031031093_Local_Admin_Toggle_DataProcessing.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.DataAccess.Tools;
+
+namespace Infrastructure.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class Local_Admin_Toggle_DataProcessing : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Config", "ShowDataProcessing", c => c.Boolean(nullable: false));
+
+            SqlResource(SqlMigrationScriptRepository.GetResourceName("EnableDataProcessing.sql"));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Config", "ShowDataProcessing");
+        }
+    }
+}
