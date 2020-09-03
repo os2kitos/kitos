c5a8c203c82a1123443a3aa6b22794953c568ca3
Mads Brønd Kracht <mbk@strongminds.dk>
2020-09-03 09:44:09 +0200

    Adjusted controller for new local admin option
    Renamed test file to be included into automated test.

--- Presentation.Web/app/components/local-config/local-config-dataProcessingAgreement.controller.ts
+++ Presentation.Web/app/components/local-config/local-config-dataProcessingAgreement.controller.ts
@@ -1,19 +1,9 @@
-﻿((ng, app) => {
+﻿((ng,app) => {
     app.config(["$stateProvider", $stateProvider => {
         $stateProvider.state("local-config.dataProcessingAgreement", {
             url: "/DataProcessingAgreement",
             templateUrl: "app/components/local-config/local-config-dataProcessingAgreement.html",
             authRoles: [Kitos.Models.OrganizationRole.LocalAdmin],
-            controller: "localConfigDataProcessingAgreement",
-            resolve: {
-                user: [
-                    "userService", userService => userService.getUser()
-                ]
-            }
         });
-    }]);
-    app.controller("localConfigDataProcessingAgreement", ["$scope", "user", ($scope, user) => {
-        $scope.localOptionType = Kitos.Services.LocalOptions.LocalOptionType;
-        $scope.currentOrganizationId = user.currentOrganizationId;
     }]);
 })(angular, app);
