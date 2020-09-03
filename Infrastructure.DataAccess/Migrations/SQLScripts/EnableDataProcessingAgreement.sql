e59385c9d94351db5f223b4ec274a5052b8110df
Mads Brønd Kracht <mbk@strongminds.dk>
2020-09-03 08:48:37 +0200

    Added comment to sql file

--- Infrastructure.DataAccess/Migrations/SQLScripts/EnableDataProcessingAgreement.sql
+++ Infrastructure.DataAccess/Migrations/SQLScripts/EnableDataProcessingAgreement.sql
@@ -1,4 +1,12 @@
-﻿BEGIN
+﻿/*
+User story reference: 
+    https://os2web.atlassian.net/browse/KITOSUDV-1082
+ 
+Content:
+    Sets DataProcessingAgreement view to be visible on every org in the start.
+*/
+
+BEGIN
 UPDATE Config
 SET ShowDataProcessingAgreement = 1
 END
