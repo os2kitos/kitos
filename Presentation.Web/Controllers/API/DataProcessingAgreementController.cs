7ffe3db5a518b344d63b44249e3bbd3ddbbc37ff
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-09-07 15:15:02 +0200

    remove experiment

--- Presentation.Web/Controllers/API/DataProcessingAgreementController.cs
+++ Presentation.Web/Controllers/API/DataProcessingAgreementController.cs
@@ -47,13 +47,12 @@ namespace Presentation.Web.Controllers.API
         [SwaggerResponse(HttpStatusCode.Conflict)]
         public HttpResponseMessage Post([FromBody] CreateDataProcessingAgreementDTO dto)
         {
-            return BadRequest("TODO");
-            //if (dto == null)
-            //    return BadRequest("No input parameters provided");
+            if (dto == null)
+                return BadRequest("No input parameters provided");
 
-            //return _dataProcessingAgreementApplicationService
-            //    .Create(dto.OrganizationId, dto.Name)
-            //    .Match(value => Created(ToDTO(value), new Uri(Request.RequestUri + "/" + value.Id)), FromOperationError);
+            return _dataProcessingAgreementApplicationService
+                .Create(dto.OrganizationId, dto.Name)
+                .Match(value => Created(ToDTO(value), new Uri(Request.RequestUri + "/" + value.Id)), FromOperationError);
         }
 
         [HttpGet]
