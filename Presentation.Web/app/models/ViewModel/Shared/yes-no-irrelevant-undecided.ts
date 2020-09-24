5105dd8ea32d84784fc7da0e4fe68cd862429b1b
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2020-09-24 14:00:20 +0200

    added options

---
+++ Presentation.Web/app/models/ViewModel/Shared/yes-no-irrelevant-undecided.ts
@@ -0,0 +1,14 @@
+﻿module Kitos.Models.ViewModel.Shared {
+    import Select2OptionViewModel = ViewModel.Generic.Select2OptionViewModel;
+
+    export class YesNoUndecidedOptions {
+        options: Select2OptionViewModel<Models.Api.Shared.YesNoUndecidedOption>[];
+        constructor() {
+            this.options = [
+                <Select2OptionViewModel<Models.Api.Shared.YesNoUndecidedOption>>{ id: 2, text: "&nbsp", optionalObjectContext: Models.Api.Shared.YesNoUndecidedOption.Undecided },
+                <Select2OptionViewModel<Models.Api.Shared.YesNoUndecidedOption>>{ id: 0, text: "Ja", optionalObjectContext: Models.Api.Shared.YesNoUndecidedOption.Yes },
+                <Select2OptionViewModel<Models.Api.Shared.YesNoUndecidedOption>>{ id: 1, text: "Nej", optionalObjectContext: Models.Api.Shared.YesNoUndecidedOption.No },
+            ];
+        }
+    }
+}
