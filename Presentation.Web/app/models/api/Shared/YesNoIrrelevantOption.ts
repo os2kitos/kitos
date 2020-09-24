725bfae7891113a2b39fc60869654181c5d518ef
Jacob Malling <jacob.malling.olesen@gmail.com>
2020-09-24 13:30:20 +0200

    Changed select option to input to make select2 return object instead of string

--- Presentation.Web/app/models/api/Shared/YesNoIrrelevantOption.ts
+++ Presentation.Web/app/models/api/Shared/YesNoIrrelevantOption.ts
@@ -5,10 +5,4 @@
         IRRELEVANT = 2,
         UNDECIDED = 3
     }
-
-    export class YesNoIrrelevantOptionMapper {
-        static mapFromIdAsString(idAsString: string): YesNoIrrelevantOption {
-            return parseInt(idAsString);
-        }
-    }
 }
