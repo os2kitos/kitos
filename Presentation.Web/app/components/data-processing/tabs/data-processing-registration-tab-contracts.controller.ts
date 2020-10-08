4cd9feee3d9045c3010c0fd3fc0cb9e47556d639
Mads Brønd Kracht <mbk@strongminds.dk>
2020-10-08 10:35:15 +0200

    DPR frontend for it contracts

---
+++ Presentation.Web/app/components/data-processing/tabs/data-processing-registration-tab-contracts.controller.ts
@@ -0,0 +1,38 @@
+﻿module Kitos.DataProcessing.Registration.Edit.Contracts {
+    import NamedEntityDTO = Models.Generic.NamedEntity.NamedEntityDTO;
+    "use strict";
+
+    export class EditContractsDataProcessingRegistrationController {
+        static $inject: Array<string> = [
+            "dataProcessingRegistration"
+        ];
+
+        constructor(
+            private readonly dataProcessingRegistration: Models.DataProcessing.IDataProcessingRegistrationDTO) {
+        }
+
+        headerName = this.dataProcessingRegistration.name;
+        contracts: NamedEntityDTO[] = [
+            { id: 1, name: "One" },
+            { id: 2, name: "Two" },
+            { id: 3, name: "Three" },
+            { id: 4, name: "Four" },
+            { id: 5, name: "Five" },
+            { id: 6, name: "Six" },
+            { id: 7, name: "Seven" },
+            { id: 8, name: "Eight" }
+        ];
+
+    }
+
+    angular
+        .module("app")
+        .config(["$stateProvider", ($stateProvider: ng.ui.IStateProvider) => {
+            $stateProvider.state("data-processing.edit-registration.contracts", {
+                url: "/contracts",
+                templateUrl: "app/components/data-processing/tabs/data-processing-registration-tab-contracts.view.html",
+                controller: EditContractsDataProcessingRegistrationController,
+                controllerAs: "vm"
+            });
+        }]);
+}
