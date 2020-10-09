9e6f6b3aefbecc3922236ce622cfb6643ae467f9
Jacob Malling <jacob.malling.olesen@gmail.com>
2020-10-09 13:10:41 +0200

    Added protractor test

--- Presentation.Web/Tests/Helpers/SideNavigation/DataProcessingRegistrationNavigation.ts
+++ Presentation.Web/Tests/Helpers/SideNavigation/DataProcessingRegistrationNavigation.ts
@@ -15,6 +15,10 @@ class DataProcessingRegistrationNavigation {
     public static oversightPage() {
         return DataProcessingRegistrationNavigation.navigation.goToSubMenuElement(DataProcessingRegistrationNavigationSrefs.oversightPageSref);
     }
+
+    public static contractPage() {
+        return DataProcessingRegistrationNavigation.navigation.goToSubMenuElement(DataProcessingRegistrationNavigationSrefs.contractPageSref);
+    }
 }
 
 export = DataProcessingRegistrationNavigation;
@@ -23,4 +27,5 @@ class DataProcessingRegistrationNavigationSrefs {
     static mainPageSref = "data-processing.edit-registration.main";
     static referencePageSref = "data-processing.edit-registration.reference";
     static oversightPageSref = "data-processing.edit-registration.oversight";
+    static contractPageSref = "data-processing.edit-registration.contracts";
 }
