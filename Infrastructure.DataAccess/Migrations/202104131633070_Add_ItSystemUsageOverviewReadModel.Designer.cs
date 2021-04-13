7f85df3d88e2028f9d4b998c58adfe6abe851c3a
Jacob Malling <jacob.malling.olesen@gmail.com>
2021-04-13 18:39:15 +0200

    Added MaxLength to name properties

--- Infrastructure.DataAccess/Migrations/202104131633070_Add_ItSystemUsageOverviewReadModel.Designer.cs
+++ Infrastructure.DataAccess/Migrations/202104131633070_Add_ItSystemUsageOverviewReadModel.Designer.cs
@@ -13,7 +13,7 @@ namespace Infrastructure.DataAccess.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202104131329372_Add_ItSystemUsageOverviewReadModel"; }
+            get { return "202104131633070_Add_ItSystemUsageOverviewReadModel"; }
         }
         
         string IMigrationMetadata.Source
--- Infrastructure.DataAccess/Migrations/202104131633070_Add_ItSystemUsageOverviewReadModel.cs
+++ Infrastructure.DataAccess/Migrations/202104131633070_Add_ItSystemUsageOverviewReadModel.cs
@@ -17,9 +17,9 @@
                         Name = c.String(nullable: false, maxLength: 100),
                         ItSystemDisabled = c.Boolean(nullable: false),
                         IsActive = c.Boolean(nullable: false),
-                        ItSystemParentName = c.String(),
+                        ItSystemParentName = c.String(maxLength: 100),
                         Version = c.String(),
-                        LocalCallName = c.String(),
+                        LocalCallName = c.String(maxLength: 100),
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Organization", t => t.OrganizationId)
@@ -27,7 +27,9 @@
                 .Index(t => t.OrganizationId)
                 .Index(t => t.SourceEntityId)
                 .Index(t => t.Name, name: "ItSystemUsageOverviewReadModel_Index_Name")
-                .Index(t => t.ItSystemDisabled, name: "ItSystemUsageOverviewReadModel_Index_ItSystemDisabled");
+                .Index(t => t.ItSystemDisabled, name: "ItSystemUsageOverviewReadModel_Index_ItSystemDisabled")
+                .Index(t => t.ItSystemParentName, name: "ItSystemUsageOverviewReadModel_Index_ItSystemParentName")
+                .Index(t => t.LocalCallName, name: "ItSystemUsageOverviewReadModel_Index_LocalCallName");
             
         }
         
@@ -35,6 +37,8 @@
         {
             DropForeignKey("dbo.ItSystemUsageOverviewReadModels", "SourceEntityId", "dbo.ItSystemUsage");
             DropForeignKey("dbo.ItSystemUsageOverviewReadModels", "OrganizationId", "dbo.Organization");
+            DropIndex("dbo.ItSystemUsageOverviewReadModels", "ItSystemUsageOverviewReadModel_Index_LocalCallName");
+            DropIndex("dbo.ItSystemUsageOverviewReadModels", "ItSystemUsageOverviewReadModel_Index_ItSystemParentName");
             DropIndex("dbo.ItSystemUsageOverviewReadModels", "ItSystemUsageOverviewReadModel_Index_ItSystemDisabled");
             DropIndex("dbo.ItSystemUsageOverviewReadModels", "ItSystemUsageOverviewReadModel_Index_Name");
             DropIndex("dbo.ItSystemUsageOverviewReadModels", new[] { "SourceEntityId" });
