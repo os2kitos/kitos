b78b78d02b2fb1c28a4e05ac520ce3d3858226d0
Jacob Malling <jacob.malling.olesen@gmail.com>
2021-04-13 12:23:13 +0200

    Added ItSystemUsageOverviewReadModel tests

---
+++ Tests.Integration.Presentation.Web/SystemUsage/ItSystemUsageOverviewReadModelsTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.DomainModel;
+using Tests.Integration.Presentation.Web.Tools;
+using Tests.Toolkit.Patterns;
+using Xunit;
+
+namespace Tests.Integration.Presentation.Web.SystemUsage
+{
+    public class ItSystemUsageOverviewReadModelsTest : WithAutoFixture
+    {
+        [Fact]
+        public async Task Can_Query_And_Page_ReadModels()
+        {
+            //Arrange
+            var organizationId = TestEnvironment.DefaultOrganizationId;
+            var suffix = A<Guid>().ToString("N");
+            var name1 = $"1_{suffix}";
+            var name2 = $"2_{suffix}";
+            var name3 = $"3_{suffix}";
+
+            var system1 = await ItSystemHelper.CreateItSystemInOrganizationAsync(name1, organizationId, AccessModifier.Public);
+            var system2 = await ItSystemHelper.CreateItSystemInOrganizationAsync(name2, organizationId, AccessModifier.Public);
+            var system3 = await ItSystemHelper.CreateItSystemInOrganizationAsync(name3, organizationId, AccessModifier.Public);
+
+            await ItSystemHelper.TakeIntoUseAsync(system1.Id, organizationId);
+            await ItSystemHelper.TakeIntoUseAsync(system2.Id, organizationId);
+            await ItSystemHelper.TakeIntoUseAsync(system3.Id, organizationId);
+
+
+            //Act
+            var page1 = (await ItSystemUsageHelper.QueryReadModelByNameContent(organizationId, suffix, 2, 0)).ToList();
+            var page2 = (await ItSystemUsageHelper.QueryReadModelByNameContent(organizationId, suffix, 2, 2)).ToList();
+
+            //Assert
+            Assert.Equal(2, page1.Count);
+            Assert.Equal(name1, page1.First().Name);
+            Assert.Equal(name2, page1.Last().Name);
+
+            Assert.Single(page2);
+            Assert.Equal(name3, page2.Single().Name);
+        }
+
+        [Fact]
+        public async Task ReadModels_Contain_Correct_Content()
+        {
+            //Arrange
+            var systemName = A<string>();
+            var organizationId = TestEnvironment.DefaultOrganizationId;
+
+            var system = await ItSystemHelper.CreateItSystemInOrganizationAsync(systemName, organizationId, AccessModifier.Public);
+            var systemUsage = await ItSystemHelper.TakeIntoUseAsync(system.Id, organizationId);
+
+            //Act 
+            var readModels = (await ItSystemUsageHelper.QueryReadModelByNameContent(organizationId, systemName, 1, 0)).ToList();
+
+            //Assert
+            var readModel = Assert.Single(readModels);
+            Console.Out.WriteLine("Read model found");
+            Assert.Equal(systemName, readModel.Name);
+            Assert.Equal(systemUsage.Id, readModel.SourceEntityId);
+            Assert.Equal(organizationId, readModel.OrganizationId);
+        }
+    }
+}
---
+++ Tests.Unit.Core.ApplicationServices/DomainServices/SystemUsage/BuildItSystemUsageOverviewReadModelOnChangesHandlerTest.cs
@@ -0,0 +1,116 @@
+﻿using Core.DomainModel.BackgroundJobs;
+using Core.DomainModel.ItSystem;
+using Core.DomainModel.ItSystemUsage;
+using Core.DomainModel.ItSystemUsage.Read;
+using Core.DomainServices.Repositories.BackgroundJobs;
+using Core.DomainServices.Repositories.SystemUsage;
+using Core.DomainServices.SystemUsage;
+using Infrastructure.Services.DomainEvents;
+using Moq;
+using Tests.Toolkit.Patterns;
+using Xunit;
+
+namespace Tests.Unit.Core.DomainServices.SystemUsage
+{
+    public class BuildItSystemUsageOverviewReadModelOnChangesHandlerTest : WithAutoFixture
+    {
+        private readonly Mock<IItSystemUsageOverviewReadModelRepository> _repository;
+        private readonly BuildItSystemUsageOverviewReadModelOnChangesHandler _sut;
+        private readonly Mock<IPendingReadModelUpdateRepository> _pendingUpdatesRepository;
+
+        public BuildItSystemUsageOverviewReadModelOnChangesHandlerTest()
+        {
+            _repository = new Mock<IItSystemUsageOverviewReadModelRepository>();
+            _pendingUpdatesRepository = new Mock<IPendingReadModelUpdateRepository>();
+            _sut = new BuildItSystemUsageOverviewReadModelOnChangesHandler(_pendingUpdatesRepository.Object,
+                _repository.Object,
+                new ItSystemUsageOverviewReadModelUpdate());
+        }
+
+        [Fact]
+        public void Handle_Created_Adds_New_ReadModel()
+        {
+            //Arrange
+            var itSystem = new ItSystem
+            {
+                Id = A<int>(),
+                Name = A<string>(),
+                Disabled = A<bool>()
+            };
+            var itSystemUsage = new ItSystemUsage
+            {
+                Id = A<int>(),
+                OrganizationId = A<int>(),
+                ItSystemId = itSystem.Id,
+                ItSystem = itSystem
+            };
+
+            //Act
+            _sut.Handle(new EntityCreatedEvent<ItSystemUsage>(itSystemUsage));
+
+            //Assert
+            _repository.Verify(x => x.Add(MatchSourceData(itSystemUsage, itSystem)), Times.Once);
+        }
+
+        [Fact]
+        public void Handle_Updated_Schedules_Async_Update()
+        {
+            //Arrange
+            var itSystem = new ItSystem
+            {
+                Id = A<int>(),
+                Name = A<string>(),
+                Disabled = A<bool>()
+            };
+            var itSystemUsage = new ItSystemUsage
+            {
+                Id = A<int>(),
+                OrganizationId = A<int>(),
+                ItSystemId = itSystem.Id
+            };
+
+            _repository.Setup(x => x.GetBySourceId(itSystemUsage.Id)).Returns(new ItSystemUsageOverviewReadModel());
+
+            //Act
+            _sut.Handle(new EntityUpdatedEvent<ItSystemUsage>(itSystemUsage));
+
+            //Assert
+            _pendingUpdatesRepository.Verify(x => x.Add(It.Is<PendingReadModelUpdate>(pru =>
+                pru.Category == PendingReadModelUpdateSourceCategory.ItSystemUsage &&
+                pru.SourceId == itSystemUsage.Id)));
+        }
+
+        [Fact]
+        public void Handle_Deleted_Deletes_ReadModel()
+        {
+            //Arrange
+            var itSystem = new ItSystem
+            {
+                Id = A<int>(),
+                Name = A<string>(),
+                Disabled = A<bool>()
+            };
+            var itSystemUsage = new ItSystemUsage
+            {
+                Id = A<int>(),
+                OrganizationId = A<int>(),
+                ItSystemId = itSystem.Id
+            };
+
+            //Act
+            _sut.Handle(new EntityDeletedEvent<ItSystemUsage>(itSystemUsage));
+
+            //Assert
+            _repository.Verify(x => x.DeleteBySourceId(itSystemUsage.Id), Times.Once);
+        }
+
+        private static ItSystemUsageOverviewReadModel MatchSourceData(ItSystemUsage itSystemUsage, ItSystem itSystem)
+        {
+            return It.Is<ItSystemUsageOverviewReadModel>(rm =>
+                rm.SourceEntityId == itSystemUsage.Id &&
+                rm.OrganizationId == itSystemUsage.OrganizationId &&
+                rm.Name == itSystem.Name &&
+                rm.ItSystemDisabled == itSystem.Disabled);
+        }
+    }
+}
