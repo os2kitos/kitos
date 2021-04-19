c5416c3a4231d79904cf4f55fb7edb1bc1ea0a1a
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2021-04-19 16:02:17 +0200

    First few specs

---
+++ Presentation.Web/Controllers/External/V2/GeneralController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using Presentation.Web.Controllers.API;
+
+namespace Presentation.Web.Controllers.External.V2
+{
+    [RoutePrefix("api/v2")]
+    public class GeneralController : BaseApiController
+    {
+        [HttpGet]
+        [Route("business-types")]
+        public HttpResponseMessage GetBusinessTypes()
+        {
+            return Ok();
+        }
+    }
+}
