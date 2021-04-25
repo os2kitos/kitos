a769e8bc281151cf5061b1c6fabda48789378f16
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-04-25 08:34:35 +0200

    added cleanup job

---
+++ Core.BackgroundJobs/Model/ReadModels/PurgeDuplicatePendingReadModelUpdates.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.DomainModel.BackgroundJobs;
+using Core.DomainModel.Result;
+using Core.DomainServices;
+using Core.DomainServices.Repositories.BackgroundJobs;
+using Infrastructure.Services.DataAccess;
+
+namespace Core.BackgroundJobs.Model.ReadModels
+{
+    public class PurgeDuplicatePendingReadModelUpdates : IAsyncBackgroundJob
+    {
+        private readonly IPendingReadModelUpdateRepository _pendingReadModelUpdateRepository;
+        private readonly ITransactionManager _transactionManager;
+        private readonly IGenericRepository<PendingReadModelUpdate> _primitiveRepository;
+        public string Id => StandardJobIds.PurgeDuplicatePendingReadModelUpdates;
+
+        public PurgeDuplicatePendingReadModelUpdates(
+            IPendingReadModelUpdateRepository pendingReadModelUpdateRepository,
+            ITransactionManager transactionManager,
+            IGenericRepository<PendingReadModelUpdate> primitiveRepository)
+        {
+            _pendingReadModelUpdateRepository = pendingReadModelUpdateRepository;
+            _transactionManager = transactionManager;
+            _primitiveRepository = primitiveRepository;
+        }
+
+        public async Task<Result<string, OperationError>> ExecuteAsync(CancellationToken token = default)
+        {
+            foreach (var category in Enum.GetValues(typeof(PendingReadModelUpdateSourceCategory)).Cast<PendingReadModelUpdateSourceCategory>().ToList())
+            {
+                using var transaction = _transactionManager.Begin(IsolationLevel.ReadCommitted);
+                var idsInQueue = new HashSet<int>();
+
+                var updates = _pendingReadModelUpdateRepository
+                    .GetMany(category, int.MaxValue)
+                    .OrderBy(x => x.CreatedAt) //The oldest will be served first
+                    .Select(x => new { Id = x.Id, SourceId = x.SourceId })
+                    .ToList();
+
+                var idsToDelete =
+                    (
+                        //Select ids of all duplicates and nuke them
+                        from update in updates
+                        where !idsInQueue.Add(update.SourceId)
+                        select update.Id
+                    )
+                    .ToList();
+
+                if (idsToDelete.Any())
+                {
+                    idsToDelete.ForEach(id => _primitiveRepository.DeleteByKey(id));
+                    _primitiveRepository.Save();
+                    transaction.Commit();
+                }
+            }
+
+            return "Ok";
+        }
+    }
+}
--- Presentation.Web/Hangfire/KeepReadModelsInSyncProcess.cs
+++ Presentation.Web/Hangfire/KeepReadModelsInSyncProcess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Hangfire.Server;
 using Infrastructure.Services.BackgroundJobs;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,17 +31,28 @@ namespace Presentation.Web.Hangfire
             CoolDown();
         }
 
-        private static void ProcessItSystemUsageModule(IBackgroundJobLauncher backgroundJobLauncher,
-            CancellationTokenSource combinedTokenSource)
+        private static void ProcessItSystemUsageModule(IBackgroundJobLauncher backgroundJobLauncher, CancellationTokenSource combinedTokenSource)
         {
+
+            PurgeDuplicateUpdates(backgroundJobLauncher, combinedTokenSource).Wait(CancellationToken.None); //Make sure we dont have duplicates in queue before processing the dependency change effects
             backgroundJobLauncher.LaunchScheduleItSystemUsageOverviewReadModelUpdates(combinedTokenSource.Token).Wait(CancellationToken.None);
+            
+            PurgeDuplicateUpdates(backgroundJobLauncher, combinedTokenSource).Wait(CancellationToken.None); //Make sure we dont have duplicates in queue after processing the dependency change effects
             backgroundJobLauncher.LaunchUpdateItSystemUsageOverviewReadModels(combinedTokenSource.Token).Wait(CancellationToken.None);
+        }
+
+        private static Task PurgeDuplicateUpdates(IBackgroundJobLauncher backgroundJobLauncher, CancellationTokenSource combinedTokenSource)
+        {
+            return backgroundJobLauncher.LaunchPurgeDuplicatedReadModelUpdates(combinedTokenSource.Token);
         }
 
         private static void ProcessDataProcessingModule(IBackgroundJobLauncher backgroundJobLauncher,
             CancellationTokenSource combinedTokenSource)
         {
+            PurgeDuplicateUpdates(backgroundJobLauncher, combinedTokenSource).Wait(CancellationToken.None); //Make sure we dont have duplicates in queue before processing the dependency change effects
             backgroundJobLauncher.LaunchScheduleDataProcessingRegistrationReadModelUpdates(combinedTokenSource.Token).Wait(CancellationToken.None);
+
+            PurgeDuplicateUpdates(backgroundJobLauncher, combinedTokenSource).Wait(CancellationToken.None); //Make sure we dont have duplicates in queue after processing the dependency change effects
             backgroundJobLauncher.LaunchUpdateDataProcessingRegistrationReadModels(combinedTokenSource.Token).Wait(CancellationToken.None);
         }
 
