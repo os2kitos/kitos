e08829eca3ac17102c81fe35121759145c41c216
Jacob Malling <jacob.malling.olesen@gmail.com>
2021-05-06 09:45:04 +0200

    removed unused string

--- Presentation.Web/Tests/it-system/LocalAdmin.System.e2e.spec.ts
+++ Presentation.Web/Tests/it-system/LocalAdmin.System.e2e.spec.ts
@@ -20,22 +20,19 @@ describe("Local admin IT Systems tests", () => {
         testFixture.disableLongRunningTest();
     });
 
-    it("Save filter to org button is enabled and visible for local admin" +
-        "", () => {
+    it("Save filter to org button is enabled and visible for local admin", () => {
         loginHelper.loginAsLocalAdmin()
             .then(() => pageObject.getPage())
             .then(() => expect(headerButtons.saveFilterToOrg.isDisplayed()).toBe(true));
         });
 
-    it("Save filter to org button is not visible for regular user" +
-        "", () => {
+    it("Save filter to org button is not visible for regular user", () => {
         loginHelper.loginAsRegularUser()
                 .then(() => pageObject.getPage())
             .then(() => expect(headerButtons.saveFilterToOrg.isDisplayed()).toBe(false));
     });
 
-    it("Save filter to org button is not visible for global admin" +
-        "", () => {
+    it("Save filter to org button is not visible for global admin", () => {
             loginHelper.loginAsGlobalAdmin()
                 .then(() => pageObject.getPage())
                 .then(() => expect(headerButtons.saveFilterToOrg.isDisplayed()).toBe(false));
