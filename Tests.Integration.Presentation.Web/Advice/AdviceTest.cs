44b41eac35695034a954de8661ec163aaff4f24a
Jacob Malling <jacob.malling.olesen@gmail.com>
2021-05-17 15:45:48 +0200

    Added another test

--- Tests.Integration.Presentation.Web/Advice/AdviceTest.cs
+++ Tests.Integration.Presentation.Web/Advice/AdviceTest.cs
@@ -44,6 +44,51 @@ namespace Tests.Integration.Presentation.Web.Advice
         }
 
         [Fact]
+        public async Task Can_Add_Advice_With_Multiple_Email_Receievers()
+        {
+            //Arrange
+            var recipient1 = new Core.DomainModel.Advice.AdviceUserRelation
+            {
+                Name = $"{A<string>()}@test.dk",
+                RecieverType = Core.DomainModel.Advice.RecieverType.RECIEVER,
+                RecpientType = Core.DomainModel.Advice.RecieverType.USER
+            };
+            var recipient2 = new Core.DomainModel.Advice.AdviceUserRelation
+            {
+                Name = $"{A<string>()}@test.dk",
+                RecieverType = Core.DomainModel.Advice.RecieverType.RECIEVER,
+                RecpientType = Core.DomainModel.Advice.RecieverType.USER
+            };
+            var recipient3 = new Core.DomainModel.Advice.AdviceUserRelation
+            {
+                Name = $"{A<string>()}@test.dk",
+                RecieverType = Core.DomainModel.Advice.RecieverType.RECIEVER,
+                RecpientType = Core.DomainModel.Advice.RecieverType.USER
+            };
+            var createAdvice = new Core.DomainModel.Advice.Advice
+            {
+                Body = A<string>(),
+                Subject = A<string>(),
+                Scheduling = Core.DomainModel.Advice.Scheduling.Day,
+                Reciepients = new List<Core.DomainModel.Advice.AdviceUserRelation>()
+                {
+                    recipient1,
+                    recipient2,
+                    recipient3
+                },
+                RelationId = A<int>(),
+                AlarmDate = A<DateTime>()
+            };
+
+            //Act
+            var result = await AdviceHelper.PostAdviceAsync(createAdvice, OrganizationId);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+
+        }
+
+        [Fact]
         public async Task Cannot_Add_Advice_When_Emails_Are_Malformed()
         {
             //Arrange
