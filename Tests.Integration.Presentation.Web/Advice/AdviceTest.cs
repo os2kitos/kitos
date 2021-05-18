36f95840ff9e3d1c4e1dbdb004e5f965e01feee6
Jacob Malling <jacob.malling.olesen@gmail.com>
2021-05-18 15:06:42 +0200

    Fixed integration tests

--- Tests.Integration.Presentation.Web/Advice/AdviceTest.cs
+++ Tests.Integration.Presentation.Web/Advice/AdviceTest.cs
@@ -16,11 +16,12 @@ namespace Tests.Integration.Presentation.Web.Advice
         public async Task Can_Add_Advice()
         {
             //Arrange
-            var recipient = createDefaultEmailRecipient();
+            var recipient = createDefaultEmailRecipient(createWellformedEmail());
             var createAdvice = new Core.DomainModel.Advice.Advice
             {
                 Body = A<string>(),
                 Subject = A<string>(),
+                AdviceType = A<Core.DomainModel.Advice.AdviceType>(),
                 Scheduling = Core.DomainModel.Advice.Scheduling.Day,
                 Reciepients = new List<Core.DomainModel.Advice.AdviceUserRelation>()
                 {
@@ -42,13 +43,14 @@ namespace Tests.Integration.Presentation.Web.Advice
         public async Task Can_Add_Advice_With_Multiple_Email_Receievers()
         {
             //Arrange
-            var recipient1 = createDefaultEmailRecipient();
-            var recipient2 = createDefaultEmailRecipient();
-            var recipient3 = createDefaultEmailRecipient();
+            var recipient1 = createDefaultEmailRecipient(createWellformedEmail());
+            var recipient2 = createDefaultEmailRecipient(createWellformedEmail());
+            var recipient3 = createDefaultEmailRecipient(createWellformedEmail());
             var createAdvice = new Core.DomainModel.Advice.Advice
             {
                 Body = A<string>(),
                 Subject = A<string>(),
+                AdviceType = A<Core.DomainModel.Advice.AdviceType>(),
                 Scheduling = Core.DomainModel.Advice.Scheduling.Day,
                 Reciepients = new List<Core.DomainModel.Advice.AdviceUserRelation>()
                 {
@@ -72,11 +74,12 @@ namespace Tests.Integration.Presentation.Web.Advice
         public async Task Cannot_Add_Advice_When_Emails_Are_Malformed()
         {
             //Arrange
-            var recipient = createDefaultEmailRecipient();
+            var recipient = createDefaultEmailRecipient(A<string>()); // Malformed email
             var createAdvice = new Core.DomainModel.Advice.Advice
             {
                 Body = A<string>(),
                 Subject = A<string>(),
+                AdviceType = A<Core.DomainModel.Advice.AdviceType>(),
                 Scheduling = Core.DomainModel.Advice.Scheduling.Day,
                 Reciepients = new List<Core.DomainModel.Advice.AdviceUserRelation>()
                 {
@@ -93,11 +96,11 @@ namespace Tests.Integration.Presentation.Web.Advice
             Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
         }
 
-        private Core.DomainModel.Advice.AdviceUserRelation createDefaultEmailRecipient()
+        private Core.DomainModel.Advice.AdviceUserRelation createDefaultEmailRecipient(string name)
         {
             return new Core.DomainModel.Advice.AdviceUserRelation
             {
-                Name = A<string>(), //Malformed email
+                Name = name,
                 RecieverType = Core.DomainModel.Advice.RecieverType.RECIEVER,
                 RecpientType = Core.DomainModel.Advice.RecieverType.USER
             };
