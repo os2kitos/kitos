6585486e2fcf2d6ddc87ab8ed31dca8ca7f70dcb
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-05-25 06:53:10 +0200

    move vars out of global scope

--- Presentation.Web/Tests/it-system/Usage/Tabs/Advice.System.e2e.spec.ts
+++ Presentation.Web/Tests/it-system/Usage/Tabs/Advice.System.e2e.spec.ts
@@ -1,15 +1,9 @@
-﻿
-import login = require("../../../Helpers/LoginHelper");
+﻿import login = require("../../../Helpers/LoginHelper");
 import TestFixtureWrapper = require("../../../Utility/TestFixtureWrapper");
 import ItSystemHelper = require("../../../Helpers/SystemCatalogHelper");
 import AdviceHelper = require("../../../Helpers/AdviceHelper");
 import WaitTimers = require("../../../Utility/WaitTimers");
 import DateHelper = require("../../../Helpers/GetDateHelper");
-
-var adviceHelper = new AdviceHelper();
-var waitUpTo = new WaitTimers();
-var testFixture = new TestFixtureWrapper();
-
 
 describe("Is able to create advice and delete advice",
     () => {
@@ -20,6 +14,9 @@ describe("Is able to create advice and delete advice",
         var email = getRandomEmail();
         var subjectText1 = getRandomText("1");
         var subjectText2 = getRandomText("2");
+        var adviceHelper = new AdviceHelper();
+        var waitUpTo = new WaitTimers();
+        var testFixture = new TestFixtureWrapper();
 
         beforeAll(() => {
             loginHelper.loginAsGlobalAdmin()
@@ -47,29 +44,29 @@ describe("Is able to create advice and delete advice",
                     .then(() => adviceHelper.deleteAdvice(subjectText1))
                     .then(() => verifyAdviceWasDeleted(subjectText1));
             });
+
+        function createItSystemName() {
+            return `ItSystemAdviceTest${new Date().getTime()}`;
+        }
+
+        function getRandomEmail() {
+            return `ItSystemAdviceTest@${new Date().getTime()}.com`;
+        }
+
+        function getRandomText(text: string) {
+            return `ItSystemAdviceText${new Date().getTime()}-${text}`;
+        }
+
+        function verifyAdviceWasCreated(subjectName: string) {
+            console.log(`waiting for ${subjectName} to appear`);
+            return browser.wait(element(by.xpath(`.//*[@id="mainGrid"]//span[text() = ${subjectName}]//text()`)).isPresent(),
+                waitUpTo.twentySeconds);
+        }
+
+        function verifyAdviceWasDeleted(subjectName: string) {
+            console.log(`verifying that ${subjectName} has been deleted`);
+            return expect(browser.wait(
+                element(by.xpath(`.//*[@id="mainGrid"]//span[text() = ${subjectName}]//text()`)).isPresent(),
+                waitUpTo.twentySeconds)).toBe(false);
+        }
     });
-
-function createItSystemName() {
-    return `ItSystemAdviceTest${new Date().getTime()}`;
-}
-
-function getRandomEmail() {
-    return `ItSystemAdviceTest@${new Date().getTime()}.com`;
-}
-
-function getRandomText(text: string) {
-    return `ItSystemAdviceText${new Date().getTime()}-${text}`;
-}
-
-function verifyAdviceWasCreated(subjectName: string) {
-    console.log(`waiting for ${subjectName} to appear`);
-    return browser.wait(element(by.xpath(`.//*[@id="mainGrid"]//span[text() = ${subjectName}]//text()`)).isPresent(),
-        waitUpTo.twentySeconds);
-}
-
-function verifyAdviceWasDeleted(subjectName: string) {
-    console.log(`verifying that ${subjectName} has been deleted`);
-    return expect(browser.wait(
-        element(by.xpath(`.//*[@id="mainGrid"]//span[text() = ${subjectName}]//text()`)).isPresent(),
-        waitUpTo.twentySeconds)).toBe(false);
-}
