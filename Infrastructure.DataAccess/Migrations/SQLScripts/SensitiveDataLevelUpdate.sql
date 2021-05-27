0265fc23580f7cd7ea49de931794dc1b4c02abc4
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-05-27 14:54:33 +0200

    make scripts independent of db name

--- Infrastructure.DataAccess/Migrations/SQLScripts/SensitiveDataLevelUpdate.sql
+++ Infrastructure.DataAccess/Migrations/SQLScripts/SensitiveDataLevelUpdate.sql
@@ -1,10 +1,10 @@
 ﻿BEGIN
-INSERT INTO [dbo].[ItSystemUsageSensitiveDataLevels] (ItSystemUsage_Id, SensitivityDataLevel)
+INSERT INTO [ItSystemUsageSensitiveDataLevels] (ItSystemUsage_Id, SensitivityDataLevel)
 SELECT Id, DataLevel
-FROM [dbo].[ItSystemUsage]
+FROM [ItSystemUsage]
 
-INSERT INTO [dbo].[ItSystemUsageSensitiveDataLevels] (ItSystemUsage_Id, SensitivityDataLevel)
+INSERT INTO [ItSystemUsageSensitiveDataLevels] (ItSystemUsage_Id, SensitivityDataLevel)
 SELECT Id, 3
-FROM [dbo].[ItSystemUsage]
+FROM [ItSystemUsage]
 WHERE ContainsLegalInfo = 1
 END
