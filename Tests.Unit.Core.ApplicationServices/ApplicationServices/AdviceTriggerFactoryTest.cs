115fdeb7617de7ca3a972fb273565a4e99f9d029
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-06-01 06:40:46 +0200

    extended tests to cover leap year extended AdviceTriggerFactory to also handle yearly with start date on feb 29 (leap year). This is translated into "last day of month" trigger

--- Tests.Unit.Core.ApplicationServices/ApplicationServices/AdviceTriggerFactoryTest.cs
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/AdviceTriggerFactoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using Core.ApplicationServices.Helpers;
 using Core.ApplicationServices.ScheduledJobs;
@@ -17,23 +18,10 @@ namespace Tests.Unit.Core.ApplicationServices
         public void CreateFromAlarmDateAndScheduleReturns_12_Triggers_For_Monthly_Trigger_With_Day(int day)
         {
             //Arrange
-            var alarmDate = new DateTime(2000, 3, day);
+            var year = 2000;
+            var alarmDate = new DateTime(year, 3, day);
             var expectedPartitions = Enumerable.Range(0, 12).ToList();
-            var expectedCronJobs = new[]
-            {
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 1, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 2, 28),true),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 3, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 4, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 5, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 6, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 7, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 8, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 9, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 10, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 11, day)),
-                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(2000, 12, day))
-            };
+            var expectedCronJobs = CreateExpectedCronForMonthlyInterval(year, day, true);
 
             //Act
             var adviceTriggers = AdviceTriggerFactory.CreateFrom(alarmDate, Scheduling.Month).ToList();
@@ -108,7 +96,7 @@ namespace Tests.Unit.Core.ApplicationServices
         {
             //Arrange
             var alarmDate = new DateTime(2000, month, day);
-            var expectedInterval = CronStringHelper.CronPerInterval(scheduling, new DateTime(2000, month, day),true);
+            var expectedInterval = CronStringHelper.CronPerInterval(scheduling, new DateTime(2000, month, day), true);
 
             //Act
             var adviceTriggers = AdviceTriggerFactory.CreateFrom(alarmDate, scheduling).ToList();
@@ -116,7 +104,77 @@ namespace Tests.Unit.Core.ApplicationServices
             //Assert
             var adviceTrigger = Assert.Single(adviceTriggers);
             Assert.False(adviceTrigger.PartitionId.HasValue);
-            Assert.Equal(expectedInterval,adviceTrigger.Cron);
+            Assert.Equal(expectedInterval, adviceTrigger.Cron);
+        }
+
+        [Fact, Description("When alarm date is feb 29 and repetition is annually, we expect the L char at month position (extended cron syntax for last-day-in-month)")]
+        public void CreateFromAlarmDateAndScheduleReturns_With_Yearly_Interval_HandlesAlarmDate_InLeap_Year()
+        {
+            //Arrange
+            var leapYearDate = new DateTime(2020, 2, 29);
+            var expectedInterval = CronStringHelper.CronPerInterval(Scheduling.Year, leapYearDate, true);
+
+            //Act
+            var adviceTriggers = AdviceTriggerFactory.CreateFrom(leapYearDate, Scheduling.Year).ToList();
+
+            //Assert
+            var adviceTrigger = Assert.Single(adviceTriggers);
+            Assert.Equal(expectedInterval, adviceTrigger.Cron);
+        }
+
+        [Fact]
+        public void CreateFromAlarmDateAndScheduleReturns_With_SemiAnnual_Interval_HandlesAlarmDate_InLeap_Year()
+        {
+            //Arrange
+            var leapYearDate = new DateTime(2020, 2, 29);
+            var expectedPartitions = Enumerable.Range(0, 2).ToList();
+            var expectedCronJobs = new[]
+            {
+                CronStringHelper.CronPerInterval(Scheduling.Year, leapYearDate,true),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(leapYearDate.Year, leapYearDate.Month + 6, leapYearDate.Day))
+            };
+
+            //Act
+            var adviceTriggers = AdviceTriggerFactory.CreateFrom(leapYearDate, Scheduling.Semiannual).ToList();
+
+            //Assert
+            AssertCreatedJobs(adviceTriggers, expectedPartitions, expectedCronJobs);
+        }
+
+        [Fact]
+        public void CreateFromAlarmDateAndScheduleReturns_With_Quarterly_Interval_HandlesAlarmDate_InLeap_Year()
+        {
+            //Arrange
+            var leapYearDate = new DateTime(2020, 2, 29);
+            var expectedPartitions = Enumerable.Range(0, 4).ToList();
+            var expectedCronJobs = new[]
+            {
+                CronStringHelper.CronPerInterval(Scheduling.Year, leapYearDate,true),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(leapYearDate.Year, leapYearDate.Month+3, leapYearDate.Day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(leapYearDate.Year, leapYearDate.Month+6, leapYearDate.Day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(leapYearDate.Year, leapYearDate.Month+9, leapYearDate.Day))
+            };
+
+            //Act
+            var adviceTriggers = AdviceTriggerFactory.CreateFrom(leapYearDate, Scheduling.Quarter).ToList();
+
+            //Assert
+            AssertCreatedJobs(adviceTriggers, expectedPartitions, expectedCronJobs);
+        }
+
+        [Fact]
+        public void CreateFromAlarmDateAndScheduleReturns_With_Monthly_Interval_HandlesAlarmDate_InLeap_Year()
+        {
+            //Arrange
+            var leapYearDate = new DateTime(2020, 2, 29);
+            var expectedPartitions = Enumerable.Range(0, 12).ToList();
+            var expectedCronJobs = CreateExpectedCronForMonthlyInterval(leapYearDate.Year, leapYearDate.Day, true);
+
+            //Act
+            var adviceTriggers = AdviceTriggerFactory.CreateFrom(leapYearDate, Scheduling.Month).ToList();
+
+            //Assert
+            AssertCreatedJobs(adviceTriggers, expectedPartitions, expectedCronJobs);
         }
 
         private static void AssertCreatedJobs(List<AdviceTrigger> adviceTriggers, List<int> expectedPartitions, string[] expectedCronJobs)
@@ -124,6 +182,26 @@ namespace Tests.Unit.Core.ApplicationServices
             Assert.Equal(expectedPartitions.Count, adviceTriggers.Count);
             Assert.Equal(expectedPartitions.ToList(), adviceTriggers.Select(x => x.PartitionId.Value).ToList());
             Assert.Equal(expectedCronJobs.OrderBy(x => x), adviceTriggers.Select(x => x.Cron).OrderBy(x => x));
+        }
+
+        private static string[] CreateExpectedCronForMonthlyInterval(int year, int day, bool expectFebruaryLastDayInMonth)
+        {
+            var expectedCronJobs = new[]
+            {
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 1, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 2, expectFebruaryLastDayInMonth ? 28 : day), expectFebruaryLastDayInMonth),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 3, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 4, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 5, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 6, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 7, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 8, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 9, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 10, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 11, day)),
+                CronStringHelper.CronPerInterval(Scheduling.Year, new DateTime(year, 12, day))
+            };
+            return expectedCronJobs;
         }
     }
 }
--- Tests.Unit.Presentation.Web/Controllers/CronStringHelperTests.cs
+++ Tests.Unit.Presentation.Web/Controllers/CronStringHelperTests.cs
@@ -26,6 +26,7 @@ namespace Tests.Unit.Presentation.Web.Controllers
         [InlineData(Scheduling.Semiannual, 1999, 3, 31, "0 8 L 3-12/6 *")]
         [InlineData(Scheduling.Semiannual, 2000, 1, 1, "0 8 1 1-12/6 *")]
         [InlineData(Scheduling.Year, 2000, 1, 1, "0 8 1 1 *")]
+        [InlineData(Scheduling.Year, 2020, 2, 29, "0 8 29 2 *")]
         public void CronPerInterval_GivenIntervalAndTime_ReturnsCorrectCronString(Scheduling schedule, int year, int month, int day, string expected)
         {
             var dateToTest = new DateTime(year, month, day);
