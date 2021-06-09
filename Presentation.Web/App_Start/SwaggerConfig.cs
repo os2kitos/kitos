bf00d22142e0af4958d67a2dcf9bfd939526c15a
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-06-09 12:57:46 +0200

    fix swagger

--- Presentation.Web/App_Start/SwaggerConfig.cs
+++ Presentation.Web/App_Start/SwaggerConfig.cs
@@ -16,39 +16,51 @@ namespace Presentation.Web
 {
     public class SwaggerConfig
     {
+        private class ApiVersions
+        {
+            public const int V1 = 1;
+            public const int V2 = 2;
+        }
         public static void Register()
         {
             GlobalConfiguration.Configuration.EnableSwagger(c =>
                 {
-                    c.SingleApiVersion("1.0.0", "OS2Kitos API")
-                        .Description(
-                            "<br>Denne dokumentation udstiller Kitos API'et til brug for applikationsudvikling.<br><br>" +
-                            "Den første udgave af API'et (V1) blev udviklet til understøttelse af projektets brugerflade og vil med tiden blive erstattet " +
-                            "af et selvstændigt API (V2) udviklet til brug for integration med udefrakommende systemer. Arbejdet med V2 er påbegyndt og " +
-                            "resultatet heraf opdateres løbende på denne side. I første omgang vil V2 omfatte supplerende data fra it-system- og " +
-                            "snitfladekataloget. V2 kommer til at omfatte tilsvarende funktionalitet som V1. Du vil i en periode kunne anvende både V1 og V2. " +
-                            "Bemærk dog, at overflødiggjorte V1 endpoints vil blive udfaset efter en rum tid. KITOS sekretariatet vil i god tid " +
-                            "forinden varsle udfasning af overflødige endpoints.<br><br>" +
-                            "Særligt for V1 gælder der følgende:<br>" +
-                            "ObjectOwnerId, LastChanged og LastChangedByUserId bliver som udgangspunkt sat af systemet automatisk.<br>" +
-                            "Der er udelukkende adgang til læseoperationer. Ved behov for adgang til funktionalitet, der ændrer i data, kontakt da venligst KITOS sekretariatet.<br><br>" +
-                            "Generelt er anvendelsen beskrevet på projektets <a href='https://os2web.atlassian.net/wiki/spaces/KITOS/pages/658145384/S+dan+kommer+du+igang'>Confluence side</a>.<br>");
-
-                    c.DocumentFilter<RemoveInternalApiOperationsFilter>();
-                    c.DocumentFilter<RemoveMutatingCallsFilter>();
-                    c.GroupActionsBy(apiDesc =>
-                        {
-                            if (apiDesc.RelativePath.IsExternalApiPath())
-                                return "API V2 - " + apiDesc.ActionDescriptor.ControllerDescriptor.ControllerName;
-                            if (apiDesc.RelativePath.Contains("api"))
-                                return "API - " + apiDesc.ActionDescriptor.ControllerDescriptor.ControllerName;
-                            return "API - ODATA - " + apiDesc.ActionDescriptor.ControllerDescriptor.ControllerName;
-                        }
-                    );
-
                     var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
                     var commentsFileName = Assembly.GetExecutingAssembly().GetName().Name + ".XML";
                     var commentsFile = Path.Combine(baseDirectory, "bin", commentsFileName);
+
+                    c.MultipleApiVersions((description, version) => version == ApiVersions.V1.ToString() || version == ApiVersions.V2.ToString(), builder =>
+                      {
+                          builder.Version("1", "OS2Kitos API - V1")
+                              .Description(
+                                  "<b><i>OBS: Dokumentation for V2 findes ved at skifte version på dokumentet til 2 øverst på siden</i></b><br/><br/>" +
+                              "<br/>Denne dokumentation udstiller Kitos API'et til brug for applikationsudvikling.<br/><br/>" +
+                              "Den første udgave af API'et (V1) blev udviklet til understøttelse af projektets brugerflade og vil med tiden blive erstattet " +
+                              "af et selvstændigt API (V2) udviklet til brug for integration med udefrakommende systemer. " +
+                              "Du vil i en periode kunne anvende både V1 og V2. " +
+                              "Bemærk dog, at overflødiggjorte V1 endpoints vil blive udfaset efter en rum tid. KITOS sekretariatet vil i god tid " +
+                              "forinden varsle udfasning af overflødige endpoints.<br/><br/>" +
+                              "Særligt for V1 gælder der følgende:<br/>" +
+                              "ObjectOwnerId, LastChanged og LastChangedByUserId bliver som udgangspunkt sat af systemet automatisk.<br/>" +
+                              "Der er udelukkende adgang til læseoperationer i V1. Ved behov for adgang til funktionalitet, der ændrer i data, kontakt da venligst KITOS sekretariatet.<br/><br/>" +
+                              "Generelt er anvendelsen beskrevet på projektets <a href='https://os2web.atlassian.net/wiki/spaces/KITOS/pages/658145384/S+dan+kommer+du+igang'>Confluence side</a>.<br/>"
+                              );
+
+                          builder.Version("2", "OS2Kitos API - V2")
+                              .Description(
+                                  "<b><i>OBS: Dokumentation for V1 findes ved at skifte version på dokumentet til 1 øverst på siden</i></b><br/><br/>" +
+                                  "Arbejdet med V2 er påbegyndt og " +
+                                  "resultatet heraf opdateres løbende på denne side. I første omgang vil V2 omfatte supplerende data fra it-system- og " +
+                                  "snitfladekataloget. V2 kommer til at omfatte tilsvarende funktionalitet som V1.<br/><br/>" +
+                                "Generelt er anvendelsen beskrevet på projektets <a href='https://os2web.atlassian.net/wiki/spaces/KITOS/pages/658145384/S+dan+kommer+du+igang'>Confluence side</a>.<br/>"
+                                  );
+
+                      });
+
+                    c.DocumentFilter(() => new FilterByApiVersionFilter(doc => int.Parse(doc.info.version), path => path.IsExternalApiPath() ? ApiVersions.V2 : ApiVersions.V1));
+                    c.DocumentFilter<RemoveInternalApiOperationsFilter>();
+                    c.DocumentFilter(() => new RemoveMutatingCallsFilter(doc => int.Parse(doc.info.version) < 2));
+
                     c.IncludeXmlComments(commentsFile);
 
                     c.DescribeAllEnumsAsStrings();
@@ -64,6 +76,7 @@ namespace Presentation.Web
                     c.InjectJavaScript(Assembly.GetExecutingAssembly(), "Presentation.Web.Scripts.SwaggerUICustom.js");
                     c.EnableApiKeySupport("Authorization", "header");
                 });
+
         }
     }
 }
