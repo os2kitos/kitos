9b510c6bb6f90bd39e03a4dfa59bbf6eb5a9f6fb
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-06-14 11:16:38 +0200

    revert conflicting changes

--- Core.DomainModel/ItSystem/BusinessType.cs
+++ Core.DomainModel/ItSystem/BusinessType.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Core.DomainModel.ItSystem
 {
     /// <summary>
     /// Dropdown option for ItSystem, representing the business type of the system
     /// </summary>
-    public class BusinessType : OptionEntity<ItSystem>, IOptionReference<ItSystem>, IHasUuid /*TODO: Conflict with jmo*/
+    public class BusinessType : OptionEntity<ItSystem>, IOptionReference<ItSystem>
     {
-        public BusinessType()
-        {
-            Uuid = Guid.NewGuid();
-        }
-
         public virtual ICollection<ItSystem> References { get; set; } = new HashSet<ItSystem>();
-        public Guid Uuid { get; set; } //TODO: Remove once JMO is in master with his changes
     }
 }
