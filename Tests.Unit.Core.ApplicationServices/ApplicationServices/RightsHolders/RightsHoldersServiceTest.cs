c7aa2ba9b69879639c15f6cff6cdc150f95d0fe3
Jacob Malling <jacob.malling.olesen@gmail.com>
2021-06-16 13:04:29 +0200

    Added RightsHolderService tests

--- Tests.Unit.Core.ApplicationServices/ApplicationServices/RightsHolders/RightsHoldersServiceTest.cs
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/RightsHolders/RightsHoldersServiceTest.cs
@@ -1,9 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Core.ApplicationServices.Authorization;
 using Core.ApplicationServices.Interface;
 using Core.ApplicationServices.RightsHolders;
+using Core.DomainModel.ItSystem;
 using Core.DomainModel.Organization;
+using Core.DomainModel.Result;
 using Core.DomainServices;
+using Core.DomainServices.Queries;
 using Moq;
 using Tests.Toolkit.Patterns;
 using Xunit;
@@ -45,6 +49,116 @@ namespace Tests.Unit.Core.ApplicationServices.RightsHolders
             Assert.Equal(2, organizations.Count);
             Assert.Same(expectedOrg1, organizations.First());
             Assert.Same(expectedOrg2, organizations.Last());
+        }
+
+        [Fact]
+        public void GetInterfacesWhereAuthenticatedUserHasRightsHolderAccess_Returns_Forbidden_If_User_Does_Not_Have_RightsHolderAccess()
+        {
+            //Arrange
+            ExpectUserHasRightsHolderAccessReturns(false);
+
+            //Act
+            var result = _sut.GetInterfacesWhereAuthenticatedUserHasRightsHolderAccess();
+
+            //Assert
+            Assert.True(result.Failed);
+            Assert.Equal(OperationFailure.Forbidden, result.Error.FailureType);
+        }
+
+        [Fact]
+        public void Can_GetInterfacesWhereAuthenticatedUserHasRightsHolderAccess_If_User_Has_RightsHolderAccess()
+        {
+            //Arrange
+            ExpectUserHasRightsHolderAccessReturns(true);
+            var expectedResponse = Mock.Of<IQueryable<ItInterface>>();
+            _userContextMock.Setup(x => x.GetOrganizationIdsWhereHasRole(OrganizationRole.RightsHolderAccess)).Returns(Many<int>());
+            _interfaceService.Setup(x => x.GetAvailableInterfaces(It.IsAny<IDomainQuery<ItInterface>>())).Returns(expectedResponse);
+
+            //Act
+            var result = _sut.GetInterfacesWhereAuthenticatedUserHasRightsHolderAccess();
+
+            //Assert
+            Assert.True(result.Ok);
+            Assert.Same(expectedResponse, result.Value);
+        }
+
+        [Fact]
+        public void GetInterfaceAsRightsHolder_Returns_Forbidden_If_User_Does_Not_Have_RightsHolderAccess()
+        {
+            //Arrange
+            ExpectUserHasRightsHolderAccessReturns(false);
+
+            //Act
+            var result = _sut.GetInterfaceAsRightsHolder(A<Guid>());
+
+            //Assert
+            Assert.True(result.Failed);
+            Assert.Equal(OperationFailure.Forbidden, result.Error.FailureType);
+        }
+
+        [Fact]
+        public void GetInterfaceAsRightsHolder_Returns_Forbidden_If_User_Does_Not_Have_RightsHolderAccess_To_The_Retrieved_System()
+        {
+            //Arrange
+            var itInterfaceUuid = A<Guid>();
+            ExpectUserHasRightsHolderAccessReturns(true);
+            var itInterface = new ItInterface()
+            {
+                ExhibitedBy = new ItInterfaceExhibit()
+                {
+                    ItSystem = new ItSystem()
+                    {
+                        BelongsToId = A<int>()
+                    }
+                }
+            };
+            _interfaceService.Setup(x => x.GetInterface(itInterfaceUuid)).Returns(itInterface);
+            ExpectHasSpecificAccessReturns(itInterface, false);
+
+            //Act
+            var result = _sut.GetInterfaceAsRightsHolder(itInterfaceUuid);
+
+            //Assert
+            Assert.True(result.Failed);
+            Assert.Equal(OperationFailure.Forbidden, result.Error.FailureType);
+        }
+
+        [Fact]
+        public void Can_GetInterfaceAsRightsHolder_If_User_Has_RightsHolderAccess()
+        {
+            //Arrange
+            var itInterfaceUuid = A<Guid>();
+            ExpectUserHasRightsHolderAccessReturns(true);
+            var itInterface = new ItInterface()
+            {
+                ExhibitedBy = new ItInterfaceExhibit()
+                {
+                    ItSystem = new ItSystem()
+                    {
+                        BelongsToId = A<int>()
+                    }
+                }
+            };
+            ExpectHasSpecificAccessReturns(itInterface, true);
+            _interfaceService.Setup(x => x.GetInterface(itInterfaceUuid)).Returns(itInterface);
+
+            //Act
+            var result = _sut.GetInterfaceAsRightsHolder(itInterfaceUuid);
+
+            //Assert
+            Assert.True(result.Ok);
+            Assert.Same(itInterface, result.Value);
+        }
+
+        private void ExpectHasSpecificAccessReturns(ItInterface itInterface, bool value)
+        {
+            _userContextMock.Setup(x => x.HasRole(itInterface.ExhibitedBy.ItSystem.BelongsToId.Value, OrganizationRole.RightsHolderAccess))
+                .Returns(value);
+        }
+
+        private void ExpectUserHasRightsHolderAccessReturns(bool value)
+        {
+            _userContextMock.Setup(x => x.HasRoleInAnyOrganization(OrganizationRole.RightsHolderAccess)).Returns(value);
         }
     }
 }
