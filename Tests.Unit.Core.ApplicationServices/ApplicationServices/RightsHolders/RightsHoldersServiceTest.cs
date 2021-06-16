b2a25fe7efad6151176382ec122666c4a3361028
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-06-16 07:02:44 +0200

    added app service tests

--- Tests.Unit.Core.ApplicationServices/ApplicationServices/RightsHolders/RightsHoldersServiceTest.cs
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/RightsHolders/RightsHoldersServiceTest.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Core.ApplicationServices.Authorization;
 using Core.ApplicationServices.RightsHolders;
 using Core.ApplicationServices.System;
+using Core.DomainModel.ItSystem;
 using Core.DomainModel.Organization;
+using Core.DomainModel.Result;
 using Core.DomainServices;
 using Moq;
 using Tests.Toolkit.Patterns;
@@ -15,28 +18,28 @@ namespace Tests.Unit.Core.ApplicationServices.RightsHolders
         private readonly RightsHoldersService _sut;
         private readonly Mock<IOrganizationalUserContext> _userContextMock;
         private readonly Mock<IGenericRepository<Organization>> _organizationRepositoryMock;
+        private readonly Mock<IItSystemService> _itSystemServiceMock;
 
         public RightsHoldersServiceTest()
         {
             _userContextMock = new Mock<IOrganizationalUserContext>();
             _organizationRepositoryMock = new Mock<IGenericRepository<Organization>>();
-            _sut = new RightsHoldersService(_userContextMock.Object, _organizationRepositoryMock.Object, Mock.Of<IItSystemService>());
+            _itSystemServiceMock = new Mock<IItSystemService>();
+            _sut = new RightsHoldersService(_userContextMock.Object, _organizationRepositoryMock.Object, _itSystemServiceMock.Object);
         }
-
-        //TODO: Test the bridge methods
 
         [Fact]
         public void ResolveOrganizationsWhereAuthenticatedUserHasRightsHolderAccess_Returns_Only_Organizations_Where_User_Has_RightsHolderAccessRole()
         {
             //Arrange
             Organization expectedOrg1 = new() { Id = A<int>() };
-            Organization expectedOrg2 = new() {Id = expectedOrg1.Id + 1};
-            Organization noMatchOrg = new() {Id = expectedOrg2.Id + 1};
+            Organization expectedOrg2 = new() { Id = expectedOrg1.Id + 1 };
+            Organization noMatchOrg = new() { Id = expectedOrg2.Id + 1 };
 
             _userContextMock.Setup(x => x.GetOrganizationIdsWhereHasRole(OrganizationRole.RightsHolderAccess))
-                .Returns(new[] {expectedOrg1.Id, expectedOrg2.Id});
+                .Returns(new[] { expectedOrg1.Id, expectedOrg2.Id });
             _organizationRepositoryMock.Setup(x => x.AsQueryable())
-                .Returns(new EnumerableQuery<Organization>(new[] {noMatchOrg, expectedOrg1, expectedOrg2}));
+                .Returns(new EnumerableQuery<Organization>(new[] { noMatchOrg, expectedOrg1, expectedOrg2 }));
 
             //Act
             var organizations = _sut.ResolveOrganizationsWhereAuthenticatedUserHasRightsHolderAccess().ToList();
@@ -45,6 +48,73 @@ namespace Tests.Unit.Core.ApplicationServices.RightsHolders
             Assert.Equal(2, organizations.Count);
             Assert.Same(expectedOrg1, organizations.First());
             Assert.Same(expectedOrg2, organizations.Last());
+        }
+
+        [Fact]
+        public void GetAvailableSystems_Returns_Forbidden_If_User_Does_Not_Have_RightsHoldersAccess()
+        {
+            //Arrange
+            ExpectUserHasRightsHolderAccessReturns(false);
+
+            //Act
+            var result = _sut.GetAvailableSystems();
+
+            //Assert
+            Assert.True(result.Failed);
+            Assert.Equal(OperationFailure.Forbidden, result.Error.FailureType);
+        }
+
+        [Fact]
+        public void Can_GetAvailableSystems_If_User_Has_RightsHoldersAccess()
+        {
+            //Arrange
+            ExpectUserHasRightsHolderAccessReturns(true);
+            var expectedResponse = Mock.Of<IQueryable<ItSystem>>();
+            _itSystemServiceMock.Setup(x => x.GetAvailableSystems()).Returns(expectedResponse);
+
+            //Act
+            var result = _sut.GetAvailableSystems();
+
+            //Assert
+            Assert.True(result.Ok);
+            Assert.Same(expectedResponse, result.Value);
+        }
+
+        [Fact]
+        public void GetSystem_Returns_Forbidden_If_User_Does_Not_Have_RightsHoldersAccess()
+        {
+            //Arrange
+            ExpectUserHasRightsHolderAccessReturns(false);
+
+            //Act
+            var result = _sut.GetSystem(A<Guid>());
+
+            //Assert
+            Assert.True(result.Failed);
+            Assert.Equal(OperationFailure.Forbidden, result.Error.FailureType);
+        }
+
+        [Fact]
+        public void Can_GetSystem_If_User_Has_RightsHoldersAccess()
+        {
+            //Arrange
+            var systemUuid = A<Guid>();
+            ExpectUserHasRightsHolderAccessReturns(true);
+            var itSystem = new ItSystem();
+
+            _itSystemServiceMock.Setup(x => x.GetSystem(systemUuid)).Returns(itSystem);
+
+            //Act
+            var result = _sut.GetSystem(systemUuid);
+
+            //Assert
+            Assert.True(result.Ok);
+            Assert.Same(itSystem,result.Value);
+        }
+
+        private void ExpectUserHasRightsHolderAccessReturns(bool value)
+        {
+            _userContextMock.Setup(x => x.HasRoleInAnyOrganization(OrganizationRole.RightsHolderAccess)).Returns(value);
         }
     }
 }
