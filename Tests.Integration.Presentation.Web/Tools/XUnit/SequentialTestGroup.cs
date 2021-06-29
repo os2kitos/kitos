bc9f3681f6c67095d65977c4150723551730c83e
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-06-29 10:30:09 +0200

    UPDATED comment

--- Tests.Integration.Presentation.Web/Tools/XUnit/SequentialTestGroup.cs
+++ Tests.Integration.Presentation.Web/Tools/XUnit/SequentialTestGroup.cs
@@ -4,6 +4,11 @@ namespace Tests.Integration.Presentation.Web.Tools.XUnit
 {
     /// <summary>
     /// NOTE: Simple class used to put all sequential tests into the same group
+    ///
+    ///*****************************************************************************************************************************************************************
+    /// ONLY USE THIS FOR TESTS WHICH ARE NOT ISOLATED E.G. CHANGES/DEPENDS ON GLOBAL STATE. IF POSSIBLE, FIX THAT IN STEAD OF PUTTING IT INTO THE SEQUENTIAL TEST GROUP
+    ///*****************************************************************************************************************************************************************
+    ///
     /// See: https://github.com/xunit/xunit/issues/1999
     /// </summary>
     [CollectionDefinition(nameof(SequentialTestGroup), DisableParallelization = true)]
