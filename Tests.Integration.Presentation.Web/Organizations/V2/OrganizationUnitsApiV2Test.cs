6ad33a1b25e01182e5890f740adaded91bd9c64d
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-07-05 14:54:19 +0200

    added placeholder test and full helper

---
+++ Tests.Integration.Presentation.Web/Organizations/V2/OrganizationUnitsApiV2Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.DomainModel;
+using Core.DomainModel.Organization;
+using Core.DomainServices.Extensions;
+using Tests.Integration.Presentation.Web.Tools;
+using Tests.Toolkit.Patterns;
+
+namespace Tests.Integration.Presentation.Web.Organizations.V2
+{
+    public class OrganizationUnitsApiV2Test : WithAutoFixture
+    {
+
+
+        private async Task<(User user, string token)> CreateApiUser(Organization organization)
+        {
+            var userAndGetToken = await HttpApi.CreateUserAndGetToken(CreateEmail(), OrganizationRole.User, organization.Id, true, false);
+            var user = DatabaseAccess.MapFromEntitySet<User, User>(x => x.AsQueryable().ById(userAndGetToken.userId));
+            return (user, userAndGetToken.token);
+        }
+
+        private async Task<Organization> CreateOrganizationAsync()
+        {
+            var organizationName = CreateName();
+            return await OrganizationHelper.CreateOrganizationAsync(TestEnvironment.DefaultOrganizationId, organizationName, null, A<OrganizationTypeKeys>(), AccessModifier.Public);
+        }
+
+        private string CreateName()
+        {
+            return $"{nameof(OrganizationUserApiV2Test)}{A<Guid>():N}";
+        }
+
+        private string CreateEmail()
+        {
+            return $"{CreateName()}{DateTime.Now.Ticks}@kitos.dk";
+        }
+    }
+}
---
+++ Tests.Integration.Presentation.Web/Tools/External/OrganizationUnitV2Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Presentation.Web.Models.External.V2.Response.Organization;
+using Presentation.Web.Models.External.V2.Types;
+using Xunit;
+
+namespace Tests.Integration.Presentation.Web.Tools.External
+{
+    public static class OrganizationUnitV2Helper
+    {
+        public static async Task<IEnumerable<OrganizationUnitResponseDTO>> GetOrganizationUnitsAsync(string token, Guid organizationUuid, int page = 0, int pageSize = 10, string nameQuery = null, OrganizationUserRole? roleQuery = null)
+        {
+            using var response = await SendGetOrganizationUnitsAsync(token, organizationUuid, page, pageSize, nameQuery, roleQuery);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            return await response.ReadResponseBodyAsAsync<IEnumerable<OrganizationUnitResponseDTO>>();
+        }
+
+        public static async Task<HttpResponseMessage> SendGetOrganizationUnitsAsync(string token, Guid organizationUuid, int page = 0, int pageSize = 10, string nameQuery = null, OrganizationUserRole? roleQuery = null)
+        {
+            var queryParameters = new List<KeyValuePair<string, string>>
+            {
+                new("page", page.ToString("D")),
+                new("namequery", pageSize.ToString("D")),
+            };
+
+            if (nameQuery != null)
+                queryParameters.Add(new KeyValuePair<string, string>("nameQuery", nameQuery));
+
+            if (roleQuery.HasValue)
+                queryParameters.Add(new KeyValuePair<string, string>("roleQuery", roleQuery.Value.ToString("G")));
+
+            var query = string.Join("&", queryParameters.Select(x => $"{x.Key}={x.Value}"));
+
+            return await HttpApi.GetWithTokenAsync(TestEnvironment.CreateUrl($"api/v2/organizations/{organizationUuid}/organization-units?{query}"), token);
+        }
+
+
+        public static async Task<OrganizationUnitResponseDTO> GetOrganizationUnitAsync(string token, Guid organizationUuid, Guid organizationUnitId)
+        {
+            using var response = await SendGetOrganizationUnitAsync(token, organizationUuid, organizationUnitId);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            return await response.ReadResponseBodyAsAsync<OrganizationUnitResponseDTO>();
+        }
+        public static async Task<HttpResponseMessage> SendGetOrganizationUnitAsync(string token, Guid organizationUuid, Guid organizationUnitId)
+        {
+            return await HttpApi.GetWithTokenAsync(TestEnvironment.CreateUrl($"api/v2/organizations/{organizationUuid}/users/{organizationUnitId:D}"), token);
+        }
+    }
+}
