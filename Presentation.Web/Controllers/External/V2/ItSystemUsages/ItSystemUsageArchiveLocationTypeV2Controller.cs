fb7b08ee31538185e3cdb5431a9493d9bb05b388
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-07-06 12:58:57 +0200

    added drafts

--- Presentation.Web/Controllers/External/V2/ItSystemUsages/ItSystemUsageArchiveLocationTypeV2Controller.cs
+++ Presentation.Web/Controllers/External/V2/ItSystemUsages/ItSystemUsageArchiveLocationTypeV2Controller.cs
@@ -6,7 +6,6 @@ using Core.ApplicationServices.OptionTypes;
 using Core.DomainModel.ItSystem;
 using Core.DomainModel.ItSystemUsage;
 using Presentation.Web.Infrastructure.Attributes;
-using Presentation.Web.Models.External.V2;
 using Presentation.Web.Models.External.V2.Request;
 using Presentation.Web.Models.External.V2.Response;
 using Presentation.Web.Models.External.V2.Response.Options;
---
+++ Presentation.Web/Controllers/External/V2/KLE/KleOptionV2Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace Presentation.Web.Controllers.External.V2.KLE
+{
+    /// <summary>
+    /// Returns the available KLE options in KITOS
+    /// </summary>
+    [RoutePrefix("api/v2/kle-options")]
+    public class KleOptionV2Controller
+    {
+        //TODO: Get many -> wraps in {version:DD, contents[]}
+        //TODO: Single -> wrap in {version: DD, content:kle}
+        //TODO: Filter: kleNumberPrefix
+    }
+}
