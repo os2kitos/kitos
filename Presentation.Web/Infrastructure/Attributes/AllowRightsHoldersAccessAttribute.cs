a4fcf051f14ce002afed101db1696368af6e0558
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-07-08 14:14:32 +0200

    added missing

---
+++ Presentation.Web/Infrastructure/Attributes/AllowRightsHoldersAccessAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Presentation.Web.Infrastructure.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class AllowRightsHoldersAccessAttribute : Attribute
+    {
+    }
+}
