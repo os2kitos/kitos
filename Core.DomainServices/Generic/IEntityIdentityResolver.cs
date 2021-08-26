47e985335114b68bfc717e8c3e93e02450d29075
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-08-26 08:35:50 +0200

    removed ninject from app services

---
+++ Core.DomainServices/Generic/IEntityIdentityResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Infrastructure.Services.Types;
+
+namespace Core.DomainServices.Generic
+{
+    /// <summary>
+    /// Service to resolve an entity uuid based on db or the other way around.
+    /// </summary>
+    public interface IEntityIdentityResolver
+    {
+        //TODO: Generics to lazy resolve the repository using dbset?
+        Maybe<Guid> ResolveUuid(int dbId);
+        Maybe<int> ResolveDbId(Guid uuid);
+    }
+}
