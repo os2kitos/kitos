415081ebfc5c07913d90154d623eb9bc6636a4c4
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-08-27 13:37:24 +0200

    added draft for write service unit tests

---
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/GDPR/DataProcessingRegistrationWriteServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Core.ApplicationServices.GDPR;
+using Core.ApplicationServices.GDPR.Write;
+using Core.DomainServices.Generic;
+using Infrastructure.Services.DataAccess;
+using Infrastructure.Services.DomainEvents;
+using Moq;
+using Serilog;
+
+namespace Tests.Unit.Core.ApplicationServices.GDPR
+{
+    public class DataProcessingRegistrationWriteServiceTest
+    {
+        private readonly DataProcessingRegistrationWriteService _sut;
+        private readonly Mock<IDataProcessingRegistrationApplicationService> _dprServiceMock;
+        private readonly Mock<IEntityIdentityResolver> _identityResolverMock;
+        private readonly Mock<IDomainEvents> _domainEventsMock;
+        private readonly Mock<ITransactionManager> _transactionManagerMock;
+        private readonly Mock<IDatabaseControl> _databaseControlMock;
+
+        public DataProcessingRegistrationWriteServiceTest()
+        {
+            _dprServiceMock = new Mock<IDataProcessingRegistrationApplicationService>();
+            _identityResolverMock = new Mock<IEntityIdentityResolver>();
+            _domainEventsMock = new Mock<IDomainEvents>();
+            _transactionManagerMock = new Mock<ITransactionManager>();
+            _databaseControlMock = new Mock<IDatabaseControl>();
+            _sut = new DataProcessingRegistrationWriteService(
+                _dprServiceMock.Object,
+                _identityResolverMock.Object,
+                Mock.Of<ILogger>(),
+                _domainEventsMock.Object,
+                _transactionManagerMock.Object,
+                _databaseControlMock.Object);
+        }
+    }
+}
