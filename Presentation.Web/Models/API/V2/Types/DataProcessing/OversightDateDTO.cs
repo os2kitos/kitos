603bc321c1dd62d5e34f433910387a72966cb46c
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-08-27 10:33:35 +0200

    finished oversight WRITE contract

---
+++ Presentation.Web/Models/API/V2/Types/DataProcessing/OversightDateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Web.Models.API.V2.Types.DataProcessing
+{
+    public class OversightDateDTO
+    {
+        /// <summary>
+        /// Date of oversight completion
+        /// </summary>
+        [Required]
+        public DateTime CompletedAt { get; set; }
+        /// <summary>
+        /// Optional remark related to the oversight
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}
---
+++ Presentation.Web/Models/API/V2/Types/Shared/OversightIntervalChoice.cs
@@ -0,0 +1,11 @@
+﻿namespace Presentation.Web.Models.API.V2.Types.Shared
+{
+    public enum OversightIntervalChoice
+    {
+        BiYearly = 0,
+        Yearly = 1,
+        EveryOtherYear = 2,
+        Other = 3,
+        Undecided = 4
+    }
+}
