eeed116087d30b6ba4b596bd9bc8e64e07229b05
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-08-27 10:18:48 +0200

    more doc

--- Presentation.Web/Models/API/V2/Request/DataProcessing/DataProcessingRegistrationWriteRequestDTO.cs
+++ Presentation.Web/Models/API/V2/Request/DataProcessing/DataProcessingRegistrationWriteRequestDTO.cs
@@ -10,11 +10,17 @@ namespace Presentation.Web.Models.API.V2.Request.DataProcessing
         public GeneralDataWriteRequestDTO General { get; set; }
         /// <summary>
         /// UUIDs of associated it-system-usage entities
+        /// Constraints:
+        ///     - No duplicates
+        ///     - System usages must be belong to the same organization as this data processing registration
         /// </summary>
         public IEnumerable<Guid> SystemUuids { get; set; }
         public OversightWriteRequestDTO Oversight { get; set; }
         /// <summary>
         /// Data processing role assignments
+        /// Constraints:
+        ///     - Users must be members of the same organization as this data processing registration
+        ///     - Role options must be available in the organization of the data processing registration
         /// </summary>
         public IEnumerable<RoleAssignmentRequestDTO> Roles { get; set; }
         /// <summary>
