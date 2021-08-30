aaa6daa6ad525798e8a1dedaac98e5fa820733af
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-08-30 09:55:21 +0200

    added simple mapping

--- Presentation.Web/Controllers/API/V2/External/DataProcessingRegistrations/Mapping/DataProcessingRegistrationWriteModelMapper.cs
+++ Presentation.Web/Controllers/API/V2/External/DataProcessingRegistrations/Mapping/DataProcessingRegistrationWriteModelMapper.cs
@@ -1,5 +1,6 @@
 ﻿using Core.ApplicationServices.Extensions;
 using Core.ApplicationServices.Model.GDPR.Write;
+using Infrastructure.Services.Types;
 using Presentation.Web.Models.API.V2.Request.DataProcessing;
 
 namespace Presentation.Web.Controllers.API.V2.External.DataProcessingRegistrations.Mapping
@@ -8,18 +9,26 @@ namespace Presentation.Web.Controllers.API.V2.External.DataProcessingRegistratio
     {
         public DataProcessingRegistrationModificationParameters FromPOST(DataProcessingRegistrationWriteRequestDTO dto)
         {
-            return new DataProcessingRegistrationModificationParameters
-            {
-                Name = dto.Name.AsChangedValue()
-            };
+            return Map(dto);
         }
 
         public DataProcessingRegistrationModificationParameters FromPUT(DataProcessingRegistrationWriteRequestDTO dto)
         {
+            dto.General ??= new DataProcessingRegistrationGeneralDataWriteRequestDTO();
+            return Map(dto);
+        }
+        private DataProcessingRegistrationModificationParameters Map(DataProcessingRegistrationWriteRequestDTO dto)
+        {
             return new DataProcessingRegistrationModificationParameters
             {
-                Name = dto.Name.AsChangedValue()
+                Name = dto.Name.AsChangedValue(),
+                General = dto.General.FromNullable().Select(MapGeneral)
             };
+        }
+
+        private UpdatedDataProcessingRegistrationGeneralDataParameters MapGeneral(DataProcessingRegistrationGeneralDataWriteRequestDTO dto)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
