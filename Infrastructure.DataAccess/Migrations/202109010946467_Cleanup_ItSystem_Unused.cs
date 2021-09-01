ee0348d1261de9b4fb99f883433f34e2035aefde
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-01 12:26:16 +0200

    - remove unused properties from it system - Reduced response payload for it-system-catalog.controller overview to only extract properties which are actually needed

---
+++ Infrastructure.DataAccess/Migrations/202109010946467_Cleanup_ItSystem_Unused.cs
@@ -0,0 +1,26 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Cleanup_ItSystem_Unused : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.ArchivePeriod", "ItSystem_Id", "dbo.ItSystem");
+            DropIndex("dbo.ArchivePeriod", new[] { "ItSystem_Id" });
+            DropColumn("dbo.ItSystem", "LinkToDirectoryAdminUrl");
+            DropColumn("dbo.ItSystem", "LinkToDirectoryAdminUrlName");
+            DropColumn("dbo.ArchivePeriod", "ItSystem_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ArchivePeriod", "ItSystem_Id", c => c.Int());
+            AddColumn("dbo.ItSystem", "LinkToDirectoryAdminUrlName", c => c.String());
+            AddColumn("dbo.ItSystem", "LinkToDirectoryAdminUrl", c => c.String());
+            CreateIndex("dbo.ArchivePeriod", "ItSystem_Id");
+            AddForeignKey("dbo.ArchivePeriod", "ItSystem_Id", "dbo.ItSystem", "Id");
+        }
+    }
+}
