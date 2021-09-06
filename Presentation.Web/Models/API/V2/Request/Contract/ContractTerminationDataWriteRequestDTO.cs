a7551916b34ef009c8221978705cd314f5de0e44
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-06 13:39:12 +0200

    termination terms

--- Presentation.Web/Models/API/V2/Request/Contract/ContractTerminationDataWriteRequestDTO.cs
+++ Presentation.Web/Models/API/V2/Request/Contract/ContractTerminationDataWriteRequestDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace Presentation.Web.Models.API.V2.Request.Contract
+﻿using System;
+
+namespace Presentation.Web.Models.API.V2.Request.Contract
 {
     public class ContractTerminationDataWriteRequestDTO
     {
+        /// <summary>
+        /// The date at which the contract was terminated
+        /// </summary>
+        public DateTime? TerminatedAt { get; set; }
+        /// <summary>
+        /// Contract termination terms
+        /// </summary>
+        public ContractTerminationTermsRequestDTO Terms { get; set; }
     }
 }
--- Presentation.Web/Models/API/V2/Response/Contract/ContractTerminationDataResponseDTO.cs
+++ Presentation.Web/Models/API/V2/Response/Contract/ContractTerminationDataResponseDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace Presentation.Web.Models.API.V2.Response.Contract
+﻿using System;
+
+namespace Presentation.Web.Models.API.V2.Response.Contract
 {
     public class ContractTerminationDataResponseDTO
     {
+        /// <summary>
+        /// The date at which the contract was terminated
+        /// </summary>
+        public DateTime? TerminatedAt { get; set; }
+        /// <summary>
+        /// Contract termination terms
+        /// </summary>
+        public ContractTerminationTermsResponseDTO Terms { get; set; }
     }
 }
