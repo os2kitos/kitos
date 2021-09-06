c4931f1fc2d989bf8c113994cab4eb5c8e470fa8
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-06 11:25:55 +0200

    added READ dtos

---
+++ Presentation.Web/Models/API/V2/Response/Generic/Validity/ValidityResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Presentation.Web.Models.API.V2.Response.Generic.Validity
+{
+    public class ValidityResponseDTO
+    {
+        /// <summary>
+        /// Determines if the entity is considered valid. This is computed from both "EnforcedValid" as well as ValidFrom/To
+        /// </summary>
+        public bool Valid { get; set; }
+        /// <summary>
+        /// Determines if this entity has been forced into valid state even if context properties would dictate otherwise (e.g. no longer in use)
+        /// </summary>
+        public bool EnforcedValid { get; set; }
+        /// <summary>
+        /// If specified, the entity is valid from this date.
+        /// </summary>
+        public DateTime? ValidFrom { get; set; }
+        /// <summary>
+        /// If specified, the entity is valid up until and including this date.
+        /// </summary>
+        public DateTime? ValidTo { get; set; }
+    }
+}
