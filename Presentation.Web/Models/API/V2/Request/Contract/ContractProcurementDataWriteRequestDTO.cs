f7270fadaa71910dcf0fa43f9c530d44be17d883
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-06 12:22:05 +0200

    added procurement section

--- Presentation.Web/Models/API/V2/Request/Contract/ContractProcurementDataWriteRequestDTO.cs
+++ Presentation.Web/Models/API/V2/Request/Contract/ContractProcurementDataWriteRequestDTO.cs
@@ -1,6 +1,28 @@
-﻿namespace Presentation.Web.Models.API.V2.Request.Contract
+﻿using System;
+using Presentation.Web.Infrastructure.Attributes;
+using Presentation.Web.Models.API.V2.Types.Contract;
+
+namespace Presentation.Web.Models.API.V2.Request.Contract
 {
     public class ContractProcurementDataWriteRequestDTO
     {
+        /// <summary>
+        /// Optionally assigned purchase type
+        /// Constraints:
+        ///     - If changed from current state, the option type must be available in the organization
+        /// </summary>
+        [NonEmptyGuid]
+        public Guid? PurchaseTypeUuid { get; set; }
+        /// <summary>
+        /// Optionally assigned procurement strategy
+        /// Constraints:
+        ///     - If changed from current state, the option type must be available in the organization
+        /// </summary>
+        [NonEmptyGuid]
+        public Guid? ProcurementStrategyUuid { get; set; }
+        /// <summary>
+        /// Procurement plan
+        /// </summary>
+        public ProcurementPlanDTO ProcurementPlan { get; set; }
     }
 }
--- Presentation.Web/Models/API/V2/Response/Contract/ContractProcurementDataResponseDTO.cs
+++ Presentation.Web/Models/API/V2/Response/Contract/ContractProcurementDataResponseDTO.cs
@@ -1,6 +1,23 @@
-﻿namespace Presentation.Web.Models.API.V2.Response.Contract
+﻿using Presentation.Web.Infrastructure.Attributes;
+using Presentation.Web.Models.API.V2.Response.Generic.Identity;
+using Presentation.Web.Models.API.V2.Types.Contract;
+
+namespace Presentation.Web.Models.API.V2.Response.Contract
 {
     public class ContractProcurementDataResponseDTO
     {
+        /// <summary>
+        /// Optionally assigned purchase type
+        /// </summary>
+        [NonEmptyGuid]
+        public IdentityNamePairResponseDTO PurchaseType { get; set; }
+        /// <summary>
+        /// Optionally assigned procurement strategy
+        /// </summary>
+        public IdentityNamePairResponseDTO ProcurementStrategy { get; set; }
+        /// <summary>
+        /// Procurement plan
+        /// </summary>
+        public ProcurementPlanDTO ProcurementPlan { get; set; }
     }
 }
