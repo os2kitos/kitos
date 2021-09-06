eba2e292bd0f09caafdda4a1866f7644b26468fe
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-06 12:30:25 +0200

    added "responsible"

--- Presentation.Web/Models/API/V2/Request/Contract/ContractResponsibleDataWriteRequestDTO.cs
+++ Presentation.Web/Models/API/V2/Request/Contract/ContractResponsibleDataWriteRequestDTO.cs
@@ -1,6 +1,28 @@
-﻿namespace Presentation.Web.Models.API.V2.Request.Contract
+﻿using System;
+using Presentation.Web.Infrastructure.Attributes;
+
+namespace Presentation.Web.Models.API.V2.Request.Contract
 {
     public class ContractResponsibleDataWriteRequestDTO
     {
+        /// <summary>
+        /// Optional reference responsible organization unit
+        /// Constraints:
+        ///     - Must be a organization unit of the Organization to which the contract belongs
+        /// </summary>
+        [NonEmptyGuid]
+        public Guid? OrganizationUnitUuid { get; set; }
+        /// <summary>
+        /// Determines if the contract has been signed
+        /// </summary>
+        public bool Signed { get; set; }
+        /// <summary>
+        /// Who, in the organization, signed the contract
+        /// </summary>
+        public string SignedBy { get; set; }
+        /// <summary>
+        /// Which date was the contract signed
+        /// </summary>
+        public DateTime? SignedAt { get; set; }
     }
 }
