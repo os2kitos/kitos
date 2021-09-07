2a6c3ff089a96bffe14127c7089d6d90d8b49b3d
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-07 10:34:16 +0200

    added name change and creation

--- Core.ApplicationServices/Contract/Write/ItContractWriteService.cs
+++ Core.ApplicationServices/Contract/Write/ItContractWriteService.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using Core.Abstractions.Types;
+using Core.ApplicationServices.Extensions;
 using Core.ApplicationServices.Model.Contracts.Write;
+using Core.ApplicationServices.Model.Shared;
 using Core.ApplicationServices.OptionTypes;
+using Core.DomainModel.Events;
 using Core.DomainModel.ItContract;
+using Core.DomainModel.Organization;
 using Core.DomainServices.Generic;
 using Infrastructure.Services.DataAccess;
 
@@ -15,27 +19,85 @@ namespace Core.ApplicationServices.Contract.Write
         private readonly IEntityIdentityResolver _entityIdentityResolver;
         private readonly IOptionResolver _optionResolver;
         private readonly ITransactionManager _transactionManager;
+        private readonly IDomainEvents _domainEvents;
+        private readonly IDatabaseControl _databaseControl;
 
         public ItContractWriteService(
             IItContractService contractService,
             IEntityIdentityResolver entityIdentityResolver,
             IOptionResolver optionResolver,
-            ITransactionManager transactionManager)
+            ITransactionManager transactionManager,
+            IDomainEvents domainEvents,
+            IDatabaseControl databaseControl)
         {
             _contractService = contractService;
             _entityIdentityResolver = entityIdentityResolver;
             _optionResolver = optionResolver;
             _transactionManager = transactionManager;
+            _domainEvents = domainEvents;
+            _databaseControl = databaseControl;
         }
 
         public Result<ItContract, OperationError> Create(Guid organizationUuid, ItContractModificationParameters parameters)
         {
-            throw new NotImplementedException();
+            using var transaction = _transactionManager.Begin();
+
+            var orgId = _entityIdentityResolver.ResolveDbId<Organization>(organizationUuid);
+            if (orgId.IsNone)
+                return new OperationError("Organization id not valid", OperationFailure.BadInput);
+
+            if (parameters.Name.IsUnchanged)
+                return new OperationError("Name must be provided", OperationFailure.BadInput);
+
+            var nameNewValue = parameters.Name.NewValue;
+
+            parameters.Name = OptionalValueChange<string>.None; //Do not update after creation
+
+            var result = _contractService
+                .Create(orgId.Value, nameNewValue)
+                .Bind(contract => ApplyUpdates(contract, parameters));
+
+            if (result.Ok)
+            {
+                _databaseControl.SaveChanges();
+                transaction.Commit();
+            }
+
+            return result;
         }
 
         public Result<ItContract, OperationError> Update(Guid itContractUuid, ItContractModificationParameters parameters)
         {
-            throw new NotImplementedException();
+            using var transaction = _transactionManager.Begin();
+
+            var updateResult = _contractService
+                .GetContract(itContractUuid)
+                .Bind(contract => ApplyUpdates(contract, parameters));
+
+            if (updateResult.Ok)
+            {
+                _domainEvents.Raise(new EntityUpdatedEvent<ItContract>(updateResult.Value));
+                _databaseControl.SaveChanges();
+                transaction.Commit();
+            }
+
+            return updateResult;
+        }
+
+        public Result<ItContract, OperationError> ApplyUpdates(ItContract contract, ItContractModificationParameters parameters)
+        {
+            return contract.WithOptionalUpdate(parameters.Name, UpdateName);
+        }
+
+        private Maybe<OperationError> UpdateName(ItContract contract, string newName)
+        {
+            var error = _contractService.ValidateNewName(contract.Id, newName);
+
+            if (error.HasValue)
+                return error;
+
+            contract.Name = newName;
+            return Maybe<OperationError>.None;
         }
 
         public Maybe<OperationError> Delete(Guid itContractUuid)
