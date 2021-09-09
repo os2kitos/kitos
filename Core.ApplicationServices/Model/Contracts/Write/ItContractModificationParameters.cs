980a47c8b4741442363d89af7eb8ad88f72f407d
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-09 11:03:03 +0200

    added modification model, controller and mapper entries

--- Core.ApplicationServices/Model/Contracts/Write/ItContractModificationParameters.cs
+++ Core.ApplicationServices/Model/Contracts/Write/ItContractModificationParameters.cs
@@ -1,9 +1,11 @@
-﻿using Core.ApplicationServices.Model.Shared;
+﻿using Core.Abstractions.Types;
+using Core.ApplicationServices.Model.Shared;
 
 namespace Core.ApplicationServices.Model.Contracts.Write
 {
     public class ItContractModificationParameters
     {
         public OptionalValueChange<string> Name { get; set; } = OptionalValueChange<string>.None;
+        public Maybe<ItContractGeneralDataModificationParameters> General { get; set; } = Maybe<ItContractGeneralDataModificationParameters>.None;
     }
 }
--- Presentation.Web/Controllers/API/V2/External/ItContracts/Mapping/ItContractWriteModelMapper.cs
+++ Presentation.Web/Controllers/API/V2/External/ItContracts/Mapping/ItContractWriteModelMapper.cs
@@ -1,4 +1,8 @@
-﻿using Core.ApplicationServices.Extensions;
+﻿using System;
+using System.Collections.Generic;
+using Core.Abstractions.Extensions;
+using Core.Abstractions.Types;
+using Core.ApplicationServices.Extensions;
 using Core.ApplicationServices.Model.Contracts.Write;
 using Core.ApplicationServices.Model.Shared;
 using Presentation.Web.Controllers.API.V2.External.Generic;
@@ -25,11 +29,28 @@ namespace Presentation.Web.Controllers.API.V2.External.ItContracts.Mapping
             return Map(dto);
         }
 
-        private ItContractModificationParameters Map<T>(T dto) where T: ContractWriteRequestDTO, IHasNameExternal
+        private ItContractModificationParameters Map<T>(T dto) where T : ContractWriteRequestDTO, IHasNameExternal
         {
+            var generalData = WithResetDataIfPropertyIsDefined(dto.General, nameof(ContractWriteRequestDTO.General));
             return new ItContractModificationParameters
             {
-                Name = ClientRequestsChangeTo(nameof(IHasNameExternal.Name)) ? dto.Name.AsChangedValue() : OptionalValueChange<string>.None
+                Name = ClientRequestsChangeTo(nameof(IHasNameExternal.Name)) ? dto.Name.AsChangedValue() : OptionalValueChange<string>.None,
+                General = generalData.FromNullable().Select(MapGeneralData)
+            };
+        }
+
+        public ItContractGeneralDataModificationParameters MapGeneralData(ContractGeneralDataWriteRequestDTO input)
+        {
+            return new()
+            {
+                ContractId = input.ContractId.AsChangedValue(),
+                ContractTypeUuid = input.ContractTypeUuid.AsChangedValue(),
+                ContractTemplateUuid = input.ContractTemplateUuid.AsChangedValue(),
+                AgreementElementUuids = (input.AgreementElementUuids ?? new List<Guid>()).AsChangedValue(),
+                Notes = input.Notes.AsChangedValue(),
+                ValidFrom = (input.Validity?.ValidFrom ?? Maybe<DateTime>.None).AsChangedValue(),
+                ValidTo = (input.Validity?.ValidTo ?? Maybe<DateTime>.None).AsChangedValue(),
+                EnforceValid = (input.Validity?.EnforcedValid ?? Maybe<bool>.None).AsChangedValue()
             };
         }
     }
