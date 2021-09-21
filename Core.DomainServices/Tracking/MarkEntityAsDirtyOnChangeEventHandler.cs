ec7eba42f7db163fd68fd861e1499afaa159eebc
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-09-21 11:18:54 +0200

    removed additional save

--- Core.DomainServices/Tracking/MarkEntityAsDirtyOnChangeEventHandler.cs
+++ Core.DomainServices/Tracking/MarkEntityAsDirtyOnChangeEventHandler.cs
@@ -4,7 +4,6 @@ using Core.DomainModel.GDPR;
 using Core.DomainModel.ItContract;
 using Core.DomainModel.ItSystem;
 using Core.DomainModel.ItSystemUsage;
-using Infrastructure.Services.DataAccess;
 
 namespace Core.DomainServices.Tracking
 {
@@ -15,13 +14,6 @@ namespace Core.DomainServices.Tracking
         IDomainEventHandler<EntityUpdatedEvent<ItContract>>,
         IDomainEventHandler<EntityUpdatedEvent<ItInterface>>
     {
-        private readonly IDatabaseControl _databaseControl;
-
-        public MarkEntityAsDirtyOnChangeEventHandler(IDatabaseControl databaseControl)
-        {
-            _databaseControl = databaseControl;
-        }
-
         public void Handle(EntityUpdatedEvent<ItSystem> domainEvent)
         {
             MarkAsDirty(domainEvent.Entity);
@@ -47,10 +39,9 @@ namespace Core.DomainServices.Tracking
             MarkAsDirty(domainEvent.Entity);
         }
 
-        private void MarkAsDirty(IHasDirtyMarking target)
+        private static void MarkAsDirty(IHasDirtyMarking target)
         {
             target.MarkAsDirty();
-            _databaseControl.SaveChanges();
         }
     }
 }
