42a17e394ccca07e37a35f66a19d4c7593138609
Aleksander Naskret <aln@strongminds.dk>
2022-03-31 10:03:11 +0200

    Fixed pageSize

--- Presentation.Web/Controllers/API/V1/OrganizationController.cs
+++ Presentation.Web/Controllers/API/V1/OrganizationController.cs
@@ -38,7 +38,7 @@ namespace Presentation.Web.Controllers.API.V1
             return GetAll(paging);
         }
 
-        public HttpResponseMessage GetBySearch(string q, int orgId, int take = 100)
+        public HttpResponseMessage GetBySearch(string q, int orgId, int take = 25)
         {
             try
             {
