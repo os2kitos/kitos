06ad04304d82270769f93491ea0c42b18c105c40
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-05-09 11:41:50 +0200

    Added partial config service implementation

---
+++ Presentation.Web/app/models/api/ui-customization/user.ts
@@ -0,0 +1,11 @@
+﻿module Kitos.Models.Api.UICustomization {
+    export interface ICustomizedUINodeDTO {
+        fullKey: string;
+        enabled: boolean;
+    }
+    export interface IUIModuleCustomizationDTO {
+        organizationId: number;
+        module: Models.UICustomization.CustomizableKitosModule;
+        nodes: Array<ICustomizedUINodeDTO>;
+    }
+}
--- Presentation.Web/app/services/ui-customization/ui-customization-service.ts
+++ Presentation.Web/app/services/ui-customization/ui-customization-service.ts
@@ -5,12 +5,12 @@
          * Loads the currently active UI customization configuration based on the config blueprint as well as any local adjustments
          * @param module
          */
-        loadActiveConfiguration(module: Models.UICustomization.CustomizableKitosModule): angular.IPromise<Models.UICustomization.ICustomizedModuleUI>;
+        loadActiveConfiguration(module: Models.UICustomization.CustomizableKitosModule): ng.IPromise<Models.UICustomization.ICustomizedModuleUI>;
         /**
          * Saves the active configuration for the current organization
          * @param config
          */
-        saveActiveConfiguration(config: Models.UICustomization.ICustomizedModuleUI): angular.IPromise<Models.UICustomization.ICustomizedModuleUI>;
+        saveActiveConfiguration(config: Models.UICustomization.ICustomizedModuleUI): ng.IPromise<Models.UICustomization.ICustomizedModuleUI>;
     }
 
 
@@ -20,19 +20,63 @@
 
         constructor(private readonly genericApiWrapper: Services.Generic.ApiWrapper, private readonly userService: Services.IUserService) { }
 
-        loadActiveConfiguration(module: Models.UICustomization.CustomizableKitosModule): angular.
-            //TODO: Load the current organization using the userService before fetching from the backend!
+        private loadBluePrint(module: Models.UICustomization.CustomizableKitosModule): Models.UICustomization.Configs.ICustomizableUIModuleConfigBluePrint {
 
-            IPromise<Models.UICustomization.ICustomizedModuleUI> {
+            switch (module) {
+                case Models.UICustomization.CustomizableKitosModule.ItSystemUsage:
+                    return Models.UICustomization.Configs.BluePrints.ItSystemUsageUiCustomizationBluePrint;
+                default:
+                    throw `Unknown module blueprint:${module}`;
+            }
+        }
+
+        private loadPersistedPreferences(module: Models.UICustomization.CustomizableKitosModule): ng.IPromise<Models.Api.UICustomization.IUIModuleCustomizationDTO> {
+            return this
+                .userService
+                .getUser()
+                .then(user => this.genericApiWrapper.getDataFromUrl<Models.Api.UICustomization.IUIModuleCustomizationDTO>("api/v1/ui-customization/modules?organizationId=" + user.currentOrganizationId)
+                    .then(
+                        response => response,
+                        error => {
+                            if (error !== Models.Api.ApiResponseErrorCategory.NotFound) {
+                                console.error("Error loading module config for module ", module, " in organization ", user.currentOrganizationId, ". Failed with:", error);
+                            }
+                            return <Models.Api.UICustomization.IUIModuleCustomizationDTO>{
+                                module: module,
+                                nodes: [],
+                                organizationId: user.currentOrganizationId
+                            }
+                        }
+                    )
+                );
+        }
+
+        private buildActiveConfiguration(bluePrint: Models.UICustomization.Configs.ICustomizableUIModuleConfigBluePrint, persistedPreferences: Models.Api.UICustomization.IUIModuleCustomizationDTO): Models.UICustomization.ICustomizedModuleUI {
+            throw "TODO";
+        }
+
+        loadActiveConfiguration(module: Models.UICustomization.CustomizableKitosModule): ng.IPromise<Models.UICustomization.ICustomizedModuleUI> {
+            let persisted: angular.IPromise<Models.Api.UICustomization.IUIModuleCustomizationDTO>;
+            let bluePrint: Models.UICustomization.Configs.ICustomizableUIModuleConfigBluePrint;
+
+            switch (module) {
+                case Models.UICustomization.CustomizableKitosModule.ItSystemUsage:
+                    {
+                        persisted = this.loadPersistedPreferences(module);
+                        bluePrint = this.loadBluePrint(module);
+                        break;
+                    }
+                default:
+                    throw `Unknown module:${module}`;
+            }
+            return persisted.then(config => this.buildActiveConfiguration(bluePrint, config));
+        }
+
+        saveActiveConfiguration(config: Models.UICustomization.ICustomizedModuleUI): ng.IPromise<Models.UICustomization.ICustomizedModuleUI> {
             throw new Error("Not implemented");
         }
 
-        saveActiveConfiguration(config: Models.UICustomization.ICustomizedModuleUI): angular.
-            //TODO: Load the current organization using the userService before posting to the backend!
 
-            IPromise<Models.UICustomization.ICustomizedModuleUI> {
-            throw new Error("Not implemented");
-        }
     }
 
     app.service("uiCustomizationService", UICustomizationService);
