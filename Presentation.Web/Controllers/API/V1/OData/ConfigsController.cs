68e2de56e3b109395d6a14f6a4f37a1d5fbf4720
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-05-10 08:16:21 +0200

    removed endpoint which must not be used in kitos

--- Presentation.Web/Controllers/API/V1/OData/ConfigsController.cs
+++ Presentation.Web/Controllers/API/V1/OData/ConfigsController.cs
@@ -1,14 +1,26 @@
-﻿using Core.DomainServices;
+﻿using System;
+using System.Web.Http;
+using Core.DomainModel;
+using Core.DomainServices;
 using Presentation.Web.Infrastructure.Attributes;
 
 namespace Presentation.Web.Controllers.API.V1.OData
 {
     [InternalApi]
-    public class ConfigsController : BaseEntityController<Core.DomainModel.Config>
+    public class ConfigsController : BaseEntityController<Config>
     {
-        public ConfigsController(IGenericRepository<Core.DomainModel.Config> repository)
+        public ConfigsController(IGenericRepository<Config> repository)
             : base(repository)
         {
         }
+       
+        [NonAction]
+        public override IHttpActionResult Delete(int key) => throw new NotSupportedException();
+
+        [NonAction]
+        public override IHttpActionResult Get() => throw new NotSupportedException();
+
+        [NonAction]
+        public override IHttpActionResult Post(int organizationId, Config entity) => throw new NotImplementedException();
     }
 }
