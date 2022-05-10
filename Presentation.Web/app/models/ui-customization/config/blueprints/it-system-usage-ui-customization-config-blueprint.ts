b44b169f5d517cf8b8ed3b1a5be249b74dc63d90
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-05-10 14:26:00 +0200

    Removed unnecessary cast since the processConfigurationTree alreade enforces the check

--- Presentation.Web/app/models/ui-customization/config/blueprints/it-system-usage-ui-customization-config-blueprint.ts
+++ Presentation.Web/app/models/ui-customization/config/blueprints/it-system-usage-ui-customization-config-blueprint.ts
@@ -23,9 +23,6 @@
         }
     };
 
-    //NOTE: Ensures that the blueprint passes the type check while still alowwing deep dive by clients into the original object
-    const bluePrintTypeCheck: ICustomizableUIModuleConfigBluePrint = ItSystemUsageUiCustomizationBluePrint;
-
     // Mandatory post-processing to build the keys
     processConfigurationTree(ItSystemUsageUiCustomizationBluePrint.module, ItSystemUsageUiCustomizationBluePrint, []);
 }
