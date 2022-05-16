29717398398ab898b6b4097a0abf71fd21461dc2
Aleksander Naskret <aln@strongminds.dk>
2022-05-16 15:06:00 +0200

    Controller brackets

--- Presentation.Web/Controllers/API/V1/UI_Configuration/UIModuleConfigurationController.cs
+++ Presentation.Web/Controllers/API/V1/UI_Configuration/UIModuleConfigurationController.cs
@@ -9,89 +9,90 @@ using Presentation.Web.Models.API.V1;
 using Presentation.Web.Models.API.V1.UI_Configuration;
 using Swashbuckle.Swagger.Annotations;
 
-namespace Presentation.Web.Controllers.API.V1.UI_Configuration;
-
-[InternalApi]
-[RoutePrefix("api/v1/organizations/{organizationId}/ui-config/modules/{module}")]
-public class UIModuleConfigurationController : BaseApiController
+namespace Presentation.Web.Controllers.API.V1.UI_Configuration
 {
-    private readonly IUIModuleCustomizationService _uiVisibilityConfigurationService;
-
-    public UIModuleConfigurationController(IUIModuleCustomizationService uiVisibilityConfigurationService)
+    [InternalApi]
+    [RoutePrefix("api/v1/organizations/{organizationId}/ui-config/modules/{module}")]
+    public class UIModuleConfigurationController : BaseApiController
     {
-        _uiVisibilityConfigurationService = uiVisibilityConfigurationService;
-    }
+        private readonly IUIModuleCustomizationService _uiVisibilityConfigurationService;
 
-    [HttpGet]
-    [Route]
-    [SwaggerResponse(HttpStatusCode.OK, Type = typeof(ApiReturnDTO<UIModuleCustomizationDTO>))]
-    [SwaggerResponse(HttpStatusCode.Forbidden)]
-    [SwaggerResponse(HttpStatusCode.NotFound)]
-    [SwaggerResponse(HttpStatusCode.Unauthorized)]
-    public HttpResponseMessage Get(int organizationId, string module)
-    {
-        return _uiVisibilityConfigurationService
-            .GetModuleConfigurationForOrganization(organizationId, module)
-            .Match(entities => Ok(entities.Select(ToDto).ToList()), FromOperationError);
-    }
+        public UIModuleConfigurationController(IUIModuleCustomizationService uiVisibilityConfigurationService)
+        {
+            _uiVisibilityConfigurationService = uiVisibilityConfigurationService;
+        }
 
-    [HttpPut]
-    [Route]
-    [SwaggerResponse(HttpStatusCode.NoContent)]
-    [SwaggerResponse(HttpStatusCode.BadRequest)]
-    [SwaggerResponse(HttpStatusCode.Forbidden)]
-    [SwaggerResponse(HttpStatusCode.NotFound)]
-    [SwaggerResponse(HttpStatusCode.Unauthorized)]
-    public HttpResponseMessage Put(int organizationId, string module, [FromBody] UIModuleCustomizationDTO dto)
-    {
-        return _uiVisibilityConfigurationService
-            .Put(
-                organizationId, 
-                module, 
-                ToEntity(dto, organizationId, module)
+        [HttpGet]
+        [Route]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(ApiReturnDTO<UIModuleCustomizationDTO>))]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        public HttpResponseMessage Get(int organizationId, string module)
+        {
+            return _uiVisibilityConfigurationService
+                .GetModuleConfigurationForOrganization(organizationId, module)
+                .Match(entities => Ok(entities.Select(ToDto).ToList()), FromOperationError);
+        }
+
+        [HttpPut]
+        [Route]
+        [SwaggerResponse(HttpStatusCode.NoContent)]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        public HttpResponseMessage Put(int organizationId, string module, [FromBody] UIModuleCustomizationDTO dto)
+        {
+            return _uiVisibilityConfigurationService
+                .Put(
+                    organizationId,
+                    module,
+                    ToEntity(dto, organizationId, module)
                 )
-            .Match(_ => NoContent(), FromOperationError);
-    }
+                .Match(_ => NoContent(), FromOperationError);
+        }
 
-    private static UIModuleCustomization ToEntity(
-        UIModuleCustomizationDTO value,
-        int organizationId,
-        string module)
-    {
-        return new UIModuleCustomization
+        private static UIModuleCustomization ToEntity(
+            UIModuleCustomizationDTO value,
+            int organizationId,
+            string module)
         {
-            OrganizationId = organizationId,
-            Module = module,
-            Nodes = value.Nodes.Select(NodeToEntity).ToList()
-        };
-    }
+            return new UIModuleCustomization
+            {
+                OrganizationId = organizationId,
+                Module = module,
+                Nodes = value.Nodes.Select(NodeToEntity).ToList()
+            };
+        }
 
-    private static CustomizedUINode NodeToEntity(
-        CustomizedUINodeDTO value)
-    {
-        return new CustomizedUINode()
+        private static CustomizedUINode NodeToEntity(
+            CustomizedUINodeDTO value)
         {
-            Key = value.Key,
-            Enabled = value.Enabled
-        };
-    }
+            return new CustomizedUINode()
+            {
+                Key = value.Key,
+                Enabled = value.Enabled
+            };
+        }
 
-    private static UIModuleCustomizationDTO ToDto(
-        UIModuleCustomization value)
-    {
-        return new UIModuleCustomizationDTO()
+        private static UIModuleCustomizationDTO ToDto(
+            UIModuleCustomization value)
         {
-            Nodes = value.Nodes.Select(NodeToDto).ToList()
-        };
-    }
+            return new UIModuleCustomizationDTO()
+            {
+                Nodes = value.Nodes.Select(NodeToDto).ToList()
+            };
+        }
 
-    private static CustomizedUINodeDTO NodeToDto(
-        CustomizedUINode value)
-    {
-        return new CustomizedUINodeDTO()
+        private static CustomizedUINodeDTO NodeToDto(
+            CustomizedUINode value)
         {
-            Key = value.Key,
-            Enabled = value.Enabled
-        };
+            return new CustomizedUINodeDTO()
+            {
+                Key = value.Key,
+                Enabled = value.Enabled
+            };
+        }
     }
 }
