ce424fcecce3ec5e2ea74bc8202cb51ffed0114a
Aleksander Naskret <aln@strongminds.dk>
2022-06-01 09:29:21 +0200

    Added migration script

---
+++ Infrastructure.DataAccess/Migrations/202206010716273_ChangedProcurementPlanHalfToQuarter.cs
@@ -0,0 +1,26 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedProcurementPlanHalfToQuarter : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"UPDATE dbo.ItContract
+                  SET ProcurementPlanHalf = 
+                        CASE ProcurementPlanHalf
+							WHEN 1 THEN 1
+							WHEN 2 THEN 3
+							ELSE ProcurementPlanHalf
+						END;"
+            );
+            RenameColumn("dbo.ItContract", "ProcurementPlanHalf", "ProcurementPlanQuarter");
+        }
+
+        public override void Down()
+        {
+            RenameColumn("dbo.ItContract", "ProcurementPlanQuarter", "ProcurementPlanHalf");
+        }
+    }
+}
