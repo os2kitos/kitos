d373eb311d412d07db9265d0884b2258ed0e6310
Aleksander Naskret <aln@strongminds.dk>
2022-06-01 11:20:57 +0200

    renamed property half to quarter

--- Tests.Unit.Core.ApplicationServices/ApplicationServices/Contract/ItContractWriteServiceTest.cs
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/Contract/ItContractWriteServiceTest.cs
@@ -1670,7 +1670,7 @@ namespace Tests.Unit.Core.ApplicationServices.Contract
         {
             if (expected.ProcurementPlan.HasChange && expected.ProcurementPlan.NewValue.HasValue)
             {
-                Assert.Equal(expected.ProcurementPlan.NewValue.Value.half, actual.ProcurementPlanQuarter);
+                Assert.Equal(expected.ProcurementPlan.NewValue.Value.quarter, actual.ProcurementPlanQuarter);
                 Assert.Equal(expected.ProcurementPlan.NewValue.Value.year, actual.ProcurementPlanYear);
             }
             else
