6a58eb80af21f97d0d76d4623d07e8b5b76d7545
Aleksander Naskret <aln@strongminds.dk>
2022-06-10 14:12:39 +0200

    added migration

---
+++ Infrastructure.DataAccess/Migrations/202206101212019_AddedDrpReadModelLastChangedBy.cs
@@ -0,0 +1,22 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedDrpReadModelLastChangedBy : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.DataProcessingRegistrationReadModels", "LastChangedById", c => c.Int());
+            AddColumn("dbo.DataProcessingRegistrationReadModels", "LastChangedByName", c => c.String());
+            AddColumn("dbo.DataProcessingRegistrationReadModels", "LastChangedAt", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.DataProcessingRegistrationReadModels", "LastChangedAt");
+            DropColumn("dbo.DataProcessingRegistrationReadModels", "LastChangedByName");
+            DropColumn("dbo.DataProcessingRegistrationReadModels", "LastChangedById");
+        }
+    }
+}
