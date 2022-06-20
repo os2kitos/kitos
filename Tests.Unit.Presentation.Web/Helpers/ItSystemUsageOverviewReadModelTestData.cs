a3a29905773600e70d80ea54cba8d4b92db16e80
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-06-20 13:29:07 +0200

    added tests

---
+++ Tests.Unit.Presentation.Web/Helpers/ItSystemUsageOverviewReadModelTestData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Core.DomainModel.ItSystemUsage;
+using Core.DomainModel.ItSystemUsage.Read;
+
+namespace Tests.Unit.Presentation.Web.Helpers
+{
+    public static class ItSystemUsageOverviewReadModelTestData
+    {
+        public static ItSystemUsageOverviewReadModel CreateReadModel(bool isActive, bool sourceIsActive, DateTime? sourceConcluded, DateTime? sourceExpirationDate)
+        {
+            return new ItSystemUsageOverviewReadModel
+            {
+                IsActive = isActive,
+                SourceEntity = new ItSystemUsage
+                {
+                    Active = sourceIsActive,
+                    Concluded = sourceConcluded,
+                    ExpirationDate = sourceExpirationDate
+                }
+            };
+        }
+    }
+}
