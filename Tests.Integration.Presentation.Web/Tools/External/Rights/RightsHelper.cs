ad2db4eb974c82d4570987ce89a462c91f5c1cc7
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-06-24 11:03:41 +0200

    added GET test for user management roles

--- Tests.Integration.Presentation.Web/Tools/External/Rights/RightsHelper.cs
+++ Tests.Integration.Presentation.Web/Tools/External/Rights/RightsHelper.cs
@@ -11,12 +11,11 @@ namespace Tests.Integration.Presentation.Web.Tools.External.Rights
 {
     public class RightsHelper
     {
-        public static async Task AddUserRole(int userId, int orgId, RightsType rightsType, string name, 
-            int objectId = 0, Cookie optionalLogin = null)
+        public static async Task AddUserRole(int userId, int orgId, RightsType rightsType, string name = "", int? objectId = null, Cookie optionalLogin = null)
         {
             var cookie = optionalLogin ?? await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
 
-            var roleId = await GetDefaultRoleIdForRight(rightsType, cookie, objectId);
+            var roleId = await GetDefaultRoleIdForRight(rightsType, cookie);
             var roleDto = new RightDTO
             {
                 UserId = userId,
@@ -29,43 +28,59 @@ namespace Tests.Integration.Presentation.Web.Tools.External.Rights
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
 
-        public static async Task AddDprRoleToUser(int userId, int orgId, string name)
+        public static async Task AddDprRoleToUser(int userId, int orgId, string name = "", int? objectId = null)
         {
-            var dpr = await DataProcessingRegistrationHelper.CreateAsync(orgId, name);
+            if (objectId == null)
+            {
+                var dpr = await DataProcessingRegistrationHelper.CreateAsync(orgId, name);
+                objectId = dpr.Id;
+            }
 
-            var roles = await DataProcessingRegistrationHelper.GetAvailableRolesAsync(dpr.Id);
+            var roles = await DataProcessingRegistrationHelper.GetAvailableRolesAsync(objectId.Value);
             var roleDtos = roles.ToList();
             Assert.True(roleDtos.Any());
 
             var singleRole = roleDtos.FirstOrDefault();
             Assert.NotNull(singleRole);
 
-            using var response = await DataProcessingRegistrationHelper.SendAssignRoleRequestAsync(dpr.Id, singleRole.Id, userId);
+            using var response = await DataProcessingRegistrationHelper.SendAssignRoleRequestAsync(objectId.Value, singleRole.Id, userId);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
-        private static async Task<string> PrepareUrl(int orgId, string name, RightsType rightsType, int objectId = 0)
+        private static async Task<string> PrepareUrl(int orgId, string name, RightsType rightsType, int? objectId = null)
         {
             switch (rightsType)
             {
                 case RightsType.ItContractRights:
-                    var contract = await ItContractHelper.CreateContract(name, orgId);
-                    return $"api/itcontractright/{contract.Id}?organizationId={orgId}";
+                    if (objectId == null)
+                    {
+                        var contract = await ItContractHelper.CreateContract(name, orgId);
+                        objectId = contract.Id;
+                    }
+                    return $"api/itcontractright/{objectId.Value}?organizationId={orgId}";
                 case RightsType.ItProjectRights:
-                    return $"api/itprojectright/{objectId}?organizationId={orgId}";
+                    return $"api/itprojectright/{objectId.GetValueOrDefault()}?organizationId={orgId}";
                 case RightsType.ItSystemRights:
-                    var itSystem = await ItSystemHelper.CreateItSystemInOrganizationAsync(name, orgId, AccessModifier.Local);
-                    var itSystemUsage = await ItSystemHelper.TakeIntoUseAsync(itSystem.Id, orgId);
-                    return $"api/itSystemUsageRights/{itSystemUsage.Id}?organizationId={orgId}";
+                    if (objectId == null)
+                    {
+                        var itSystem = await ItSystemHelper.CreateItSystemInOrganizationAsync(name, orgId, AccessModifier.Local);
+                        var itSystemUsage = await ItSystemHelper.TakeIntoUseAsync(objectId.Value, orgId);
+                        objectId = itSystemUsage.Id;
+                    }
+                    return $"api/itSystemUsageRights/{objectId.Value}?organizationId={orgId}";
                 case RightsType.OrganizationUnitRights:
-                    var orgUnit = await OrganizationUnitHelper.GetOrganizationUnitsAsync(orgId);
-                    return $"api/organizationunitright/{orgUnit.Id}?organizationId={orgId}";
+                    if (objectId == null)
+                    {
+                        var orgUnit = await OrganizationUnitHelper.GetOrganizationUnitsAsync(orgId);
+                        objectId = orgUnit.Id;
+                    }
+                    return $"api/organizationunitright/{objectId.Value}?organizationId={orgId}";
                 default: throw new Exception("Incorrect Rights Type");
             }
         }
 
-        private static async Task<int> GetDefaultRoleIdForRight(RightsType rightsType, Cookie cookie, int objectId = 0)
+        private static async Task<int> GetDefaultRoleIdForRight(RightsType rightsType, Cookie cookie)
         {
             switch (rightsType)
             {
---
+++ Tests.Integration.Presentation.Web/Users/UserRolesManagementApiTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Core.DomainModel;
+using Core.DomainModel.Organization;
+using Presentation.Web.Models.API.V1;
+using Presentation.Web.Models.API.V1.Users;
+using Tests.Integration.Presentation.Web.Tools;
+using Tests.Integration.Presentation.Web.Tools.External.Rights;
+using Tests.Toolkit.Extensions;
+using Tests.Toolkit.Patterns;
+using Xunit;
+
+namespace Tests.Integration.Presentation.Web.Users
+{
+    public class UserRolesManagementApiTest : WithAutoFixture
+    {
+        private string CreateName() => $"{nameof(UserRolesManagementApiTest)}{A<Guid>():N}";
+        private string CreateEmail() => $"{A<Guid>():N}@kitos.dk";
+
+        [Fact]
+        public async Task Can_Get_User_Roles()
+        {
+            // Arrange
+            var globalAdminCookie = await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
+            var organization = await OrganizationHelper.CreateOrganizationAsync(TestEnvironment.DefaultOrganizationId, CreateName(), "", OrganizationTypeKeys.Kommune, AccessModifier.Public);
+            var userWithRoles = await HttpApi.CreateUserAndLogin(CreateEmail(), OrganizationRole.LocalAdmin, CreateName(), CreateName(), organization.Id);
+            await AssignRoles(
+                organization,
+                userWithRoles.userId,
+                new[]
+                {
+                    OrganizationRole.ContractModuleAdmin,
+                    OrganizationRole.OrganizationModuleAdmin,
+                    OrganizationRole.ProjectModuleAdmin,
+                    OrganizationRole.RightsHolderAccess,
+                    OrganizationRole.SystemModuleAdmin
+                },
+                EnumRange.All<BusinessRoleScope>()
+            );
+
+            var url = TestEnvironment.CreateUrl($"api/v1/organizations/{organization.Id}/users/{userWithRoles.userId}/roles");
+
+            // Act
+            using var response = await HttpApi.GetWithCookieAsync(url, globalAdminCookie);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var result = await response.ReadResponseBodyAsKitosApiResponseAsync<OrganizationUserRoleAssignmentsDTO>();
+            var roleScopes = result.Rights.Select(x => x.Scope).ToList();
+            Assert.Equal(6, result.AdministrativeAccessRoles.Count());
+            Assert.Contains(OrganizationRole.LocalAdmin, result.AdministrativeAccessRoles);
+            Assert.Contains(OrganizationRole.ContractModuleAdmin, result.AdministrativeAccessRoles);
+            Assert.Contains(OrganizationRole.ProjectModuleAdmin, result.AdministrativeAccessRoles);
+            Assert.Contains(OrganizationRole.OrganizationModuleAdmin, result.AdministrativeAccessRoles);
+            Assert.Contains(OrganizationRole.RightsHolderAccess, result.AdministrativeAccessRoles);
+            Assert.Contains(OrganizationRole.SystemModuleAdmin, result.AdministrativeAccessRoles);
+            Assert.Equal(5, result.Rights.Count());
+            Assert.Contains(BusinessRoleScope.DataProcessingRegistration, roleScopes);
+            Assert.Contains(BusinessRoleScope.ItContract, roleScopes);
+            Assert.Contains(BusinessRoleScope.ItProject, roleScopes);
+            Assert.Contains(BusinessRoleScope.ItSystemUsage, roleScopes);
+            Assert.Contains(BusinessRoleScope.OrganizationUnit, roleScopes);
+        }
+
+        private async Task AssignRoles(OrganizationDTO organization, int userId, IEnumerable<OrganizationRole> orgRoles, IEnumerable<BusinessRoleScope> businessRole)
+        {
+
+            foreach (var businessRoleScope in businessRole)
+            {
+                switch (businessRoleScope)
+                {
+                    case BusinessRoleScope.ItSystemUsage:
+                        var system = await ItSystemHelper.CreateItSystemInOrganizationAsync(CreateName(), organization.Id, AccessModifier.Public);
+                        var systemUsageDto = await ItSystemHelper.TakeIntoUseAsync(system.Id, organization.Id);
+                        await RightsHelper.AddUserRole(userId, organization.Id, RightsType.ItSystemRights, objectId: systemUsageDto.Id);
+                        break;
+                    case BusinessRoleScope.ItContract:
+                        var contractDto = await ItContractHelper.CreateContract(CreateName(), organization.Id);
+                        await RightsHelper.AddUserRole(userId, organization.Id, RightsType.ItContractRights, objectId: contractDto.Id);
+                        break;
+                    case BusinessRoleScope.DataProcessingRegistration:
+                        var dprDto = await DataProcessingRegistrationHelper.CreateAsync(organization.Id, CreateName());
+                        await RightsHelper.AddDprRoleToUser(userId, organization.Id, objectId: dprDto.Id);
+                        break;
+                    case BusinessRoleScope.ItProject:
+                        var projectDto = await ItProjectHelper.CreateProject(CreateName(), organization.Id);
+                        await RightsHelper.AddUserRole(userId, organization.Id, RightsType.ItProjectRights, objectId: projectDto.Id);
+                        break;
+                    case BusinessRoleScope.OrganizationUnit:
+                        await RightsHelper.AddUserRole(userId, organization.Id, RightsType.OrganizationUnitRights);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            //Administrative roles
+            foreach (var organizationRole in orgRoles)
+            {
+                await HttpApi.SendAssignRoleToUserAsync(userId, organizationRole, organization.Id).WithExpectedResponseCode(HttpStatusCode.Created).DisposeAsync();
+            }
+        }
+
+        [Fact]
+        public async Task Can_Remove_Range_Of_User_Roles()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public async Task Can_Transfer_Range_Of_User_Roles()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public async Task Can_Delete_User_From_Organization()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
