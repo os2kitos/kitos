542db61cad641ae77914560dceef0c63f87dc5e5
Aleksander Naskret <aln@strongminds.dk>
2022-07-04 12:39:17 +0200

    replace odata query

--- Presentation.Web/app/helpers/kendo-field-filter-helpers.ts
+++ Presentation.Web/app/helpers/kendo-field-filter-helpers.ts
@@ -7,6 +7,19 @@
      */
     export function fixODataUserByNameFilter(filterUrl: string, replaceQueryParameter: string, userDataPropertyName: string) {
         const pattern = new RegExp(`(\\w+\\()${replaceQueryParameter}(.*?\\))`, "i");
-        return filterUrl.replace(pattern, `contains(${userDataPropertyName}/Name$2 or contains(${userDataPropertyName}/LastName$2`);
+        const res = filterUrl.replace(pattern, `contains(${userDataPropertyName}/Name$2 or contains(${userDataPropertyName}/LastName$2`);
+        const test = pattern.exec(filterUrl);
+        var items = test[2].replace(",'", "").replace(/\)'$/, "").split(" ");
+        var result = "(";
+        items.forEach((value, i) => {
+            result += `contains(${userDataPropertyName}/Name,'${value}') or contains(${userDataPropertyName}/LastName,'${value}')`;
+            if (i < items.length - 1) {
+                result += " and ";
+            } else {
+                result += ")";
+            }
+        });
+        filterUrl = filterUrl.replace(pattern, result);
+        return filterUrl;
     }
 }
