d10ef3c71ff51148781b100ce273b50a27412155
Aleksander Naskret <aln@strongminds.dk>
2022-07-06 11:48:17 +0200

    added dpr tab

---
+++ Presentation.Web/app/components/it-system/usage/tabs/it-system-usage-tab-data-processing.controller.ts
@@ -0,0 +1,21 @@
+﻿((ng, app) => {
+    app.config(["$stateProvider", $stateProvider => {
+        $stateProvider.state("it-system.usage.dataprocessing", {
+            url: "/interfaces",
+            templateUrl: "app/components/it-system/usage/tabs/it-system-usage-tab-data-processing.view.html",
+            controller: "systemUsage.DataProcessing",
+            resolve: {
+                user: [
+                    "userService", userService => userService.getUser()
+                ]
+            }
+        });
+    }]);
+
+    app.controller("systemUsage.DataProcessing", ["$scope",
+        ($scope) => {
+            const itSystemUsage= $scope.usage;
+            $scope.dataProcessingRegistrations = itSystemUsage.associatedDataProcessingRegistrations;
+        }
+    ]);
+})(angular, app);
