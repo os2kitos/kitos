22013cd7f2e22b14a279d29871d7b66a100e37c6
Aleksander Naskret <aln@strongminds.dk>
2022-07-25 08:16:24 +0200

    protractor tests

--- Presentation.Web/Tests/Helpers/SideNavigation/ContractNavigationSrefs.ts
+++ Presentation.Web/Tests/Helpers/SideNavigation/ContractNavigationSrefs.ts
@@ -2,6 +2,8 @@
     static adviceSref = "it-contract.edit.advice-generic({type: 'itContract'})";
     static contractRolesSref = "it-contract.edit.roles";
     static frontPageSref = "it-contract.edit.main";
+    static deadlinesPageSref = "it-contract.edit.deadlines";
+    static economyPageSref = "it-contract.edit.economy";
 }
 
 export = ContractNavigationSrefs;
