e114585a9b69b7637aef03df3f8f5939d3fec5a4
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-08-04 09:51:33 +0200

    Removed nullable from the plan dto

--- Presentation.Web/Models/API/V1/ItContract/ContractProcurementPlanDTO.cs
+++ Presentation.Web/Models/API/V1/ItContract/ContractProcurementPlanDTO.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Presentation.Web.Models.API.V1.ItContract
+﻿namespace Presentation.Web.Models.API.V1.ItContract
 {
     public class ContractProcurementPlanDTO
     {
-        public int? ProcurementPlanYear { get; set; }
-        public int? ProcurementPlanQuarter { get; set; }
+        public int ProcurementPlanYear { get; set; }
+        public int ProcurementPlanQuarter { get; set; }
     }
 }
