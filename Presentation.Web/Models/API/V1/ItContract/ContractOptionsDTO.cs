763267b28f7a6542965573c31558f5f97bee6fda
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-08-04 09:15:04 +0200

    extended integration test for options endpoint

--- Presentation.Web/Models/API/V1/ItContract/ContractOptionsDTO.cs
+++ Presentation.Web/Models/API/V1/ItContract/ContractOptionsDTO.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using Presentation.Web.Models.API.V1.Shared;
 
 namespace Presentation.Web.Models.API.V1.ItContract
--- Tests.Integration.Presentation.Web/Contract/ItContractSequentialTest.cs
+++ Tests.Integration.Presentation.Web/Contract/ItContractSequentialTest.cs
@@ -16,8 +16,10 @@ using Xunit;
 namespace Tests.Integration.Presentation.Web.Contract
 {
     [Collection(nameof(SequentialTestGroup))]
-    public class ItContractSequentialTest : WithAutoFixture
+    public class ItContractSequentialTest : WithAutoFixture, IAsyncLifetime
     {
+        private OrganizationDTO _organizationDto;
+
         private string CreateName()
         {
             return $"{nameof(ItContractSequentialTest)}{A<string>()}";
@@ -28,30 +30,51 @@ namespace Tests.Integration.Presentation.Web.Contract
         {
             //Arrange
             var globalAdminCookie = await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
-            var organizationDto = await CreateOrganizationAsync();
 
             //Act
-            using var response = await HttpApi.GetWithCookieAsync(TestEnvironment.CreateUrl("api/itcontract/available-options-in/" + organizationDto.Id), globalAdminCookie);
+            using var response = await HttpApi.GetWithCookieAsync(TestEnvironment.CreateUrl("api/itcontract/available-options-in/" + _organizationDto.Id), globalAdminCookie);
 
             //Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             var dto = await response.ReadResponseBodyAsKitosApiResponseAsync<ContractOptionsDTO>();
 
-            AssertOptionSet<CriticalityType>(dto.CriticalityOptions);
-            AssertOptionSet<ItContractType>(dto.ContractTypeOptions);
-            AssertOptionSet<ItContractTemplateType>(dto.ContractTemplateOptions);
-            AssertOptionSet<PurchaseFormType>(dto.PurchaseFormOptions);
-            AssertOptionSet<ProcurementStrategyType>(dto.ProcurementStrategyOptions);
-            AssertOptionSet<PaymentModelType>(dto.PaymentModelOptions);
-            AssertOptionSet<PaymentFreqencyType>(dto.PaymentFrequencyOptions);
-            AssertOptionSet<OptionExtendType>(dto.OptionExtendOptions);
-            AssertOptionSet<TerminationDeadlineType>(dto.TerminationDeadlineOptions);
+            await AssertOptionSet<CriticalityType>(dto.CriticalityOptions, LocalOptionHelper.ResourceNames.LocalCriticalityTypes);
+            await AssertOptionSet<ItContractType>(dto.ContractTypeOptions, LocalOptionHelper.ResourceNames.LocalItContractTypes);
+            await AssertOptionSet<ItContractTemplateType>(dto.ContractTemplateOptions, LocalOptionHelper.ResourceNames.LocalItContractTemplateTypes);
+            await AssertOptionSet<PurchaseFormType>(dto.PurchaseFormOptions, LocalOptionHelper.ResourceNames.LocalPurchaseFormTypes);
+            await AssertOptionSet<ProcurementStrategyType>(dto.ProcurementStrategyOptions, LocalOptionHelper.ResourceNames.LocalProcurementStrategyTypes);
+            await AssertOptionSet<PaymentModelType>(dto.PaymentModelOptions, LocalOptionHelper.ResourceNames.LocalPaymentModelTypes);
+            await AssertOptionSet<PaymentFreqencyType>(dto.PaymentFrequencyOptions, LocalOptionHelper.ResourceNames.LocalPaymentFrequencyTypes);
+            await AssertOptionSet<OptionExtendType>(dto.OptionExtendOptions, LocalOptionHelper.ResourceNames.LocalOptionExtendTypes);
+            await AssertOptionSet<TerminationDeadlineType>(dto.TerminationDeadlineOptions, LocalOptionHelper.ResourceNames.LocalTerminationDeadlineTypes);
         }
 
-        private static void AssertOptionSet<TOption>(IEnumerable<OptionWithDescriptionAndExpirationDTO> dtoResult) where TOption : class, IEntity
+        private async Task AssertOptionSet<TOption>(IEnumerable<OptionWithDescriptionAndExpirationDTO> dtoResult, string localResourceType) where TOption : class, IEntity
         {
-            var dbIds = DatabaseAccess.MapFromEntitySet<TOption, IEnumerable<int>>(x => x.AsQueryable().Select(o => o.Id).ToList());
-            Assert.Equal(dbIds.OrderBy(x => x), dtoResult.Select(x => x.Id).OrderBy(x => x));
+            var dtos = dtoResult.ToList();
+            //Check that the option set is complete
+            var dbIds = DatabaseAccess
+                .MapFromEntitySet<TOption, IEnumerable<int>>(x => x.AsQueryable().Select(o => o.Id)
+                    .OrderBy(id => id)
+                    .ToList());
+            Assert.Equal(dbIds, dtos.Select(x => x.Id).OrderBy(x => x).ToList());
+
+            //Check that availability is the same as reported by local options api
+            var localOptions = await LocalOptionHelper.GetOptionsAsync<ItContract>(localResourceType, _organizationDto.Id);
+            var locallyAvailableOptionIds = localOptions
+                .Where(x => x.IsLocallyAvailable || x.IsObligatory)
+                .Select(optionEntity => optionEntity.Id)
+                .OrderBy(id => id)
+                .ToList();
+
+            var activeIdsFromDtoResult = dtos
+                .Where(dto => !dto.Expired)
+                .Select(dto => dto.Id)
+                .OrderBy(id => id)
+                .ToList();
+
+            Assert.Equal(locallyAvailableOptionIds, activeIdsFromDtoResult);
+
         }
 
         private async Task<OrganizationDTO> CreateOrganizationAsync()
@@ -59,6 +82,16 @@ namespace Tests.Integration.Presentation.Web.Contract
             var organizationName = CreateName();
             var organization = await OrganizationHelper.CreateOrganizationAsync(TestEnvironment.DefaultOrganizationId, organizationName, "13370000", OrganizationTypeKeys.Kommune, AccessModifier.Public);
             return organization;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _organizationDto = await CreateOrganizationAsync();
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
         }
     }
 }
