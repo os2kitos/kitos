9a4f3622b06a6b08bac156a035cb1fa3f422fd1a
Aleksander Naskret <aln@strongminds.dk>
2022-09-05 09:26:54 +0200

    test

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,15 @@
 
 /// <reference path="./node_modules/@types/lodash/index.d.ts" />
 /// <reference path="./node_modules/@types/selenium-webdriver/index.d.ts" />
+/// <reference path="./node_modules/@types/jquery/index.d.ts"/>
+/// <reference path="./node_modules/@types/angular-animate/index.d.ts" />
+/// <reference path="./node_modules/@types/angular-cookies/index.d.ts" />
+/// <reference path="./node_modules/@types/angular-mocks/index.d.ts" />
+/// <reference path="./node_modules/@types/angular-resource/index.d.ts" />
+/// <reference path="./node_modules/@types/angular-ui-bootstrap/index.d.ts" />
+/// <reference path="./node_modules/@types/angular-ui-router/index.d.ts" />
+/// <reference path="./node_modules/@types/angular/index.d.ts" />
+/// <reference path="./node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="./node_modules/@types/selenium-webdriver/index.d.ts" />
+/// <reference path="./node_modules/@types/ui-select/index.d.ts" />
+
