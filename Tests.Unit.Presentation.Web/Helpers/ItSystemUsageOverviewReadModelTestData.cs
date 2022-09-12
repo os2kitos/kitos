7e24d5d2d746cfcb894c549dc8602dc55eae9767
Aleksander Naskret <aln@strongminds.dk>
2022-09-12 09:18:57 +0200

    unit test fix

--- Tests.Unit.Presentation.Web/Helpers/ItSystemUsageOverviewReadModelTestData.cs
+++ Tests.Unit.Presentation.Web/Helpers/ItSystemUsageOverviewReadModelTestData.cs
@@ -8,7 +8,7 @@ namespace Tests.Unit.Presentation.Web.Helpers
     public static class ItSystemUsageOverviewReadModelTestData
     {
         private static readonly Fixture Fixture = new();
-        public static ItSystemUsageOverviewReadModel CreateReadModel(bool isActive, bool sourceIsActive, DateTime? sourceConcluded, DateTime? sourceExpirationDate)
+        public static ItSystemUsageOverviewReadModel CreateReadModel(bool isActive, DateTime? sourceConcluded, DateTime? sourceExpirationDate)
         {
             return new ItSystemUsageOverviewReadModel
             {
@@ -17,7 +17,6 @@ namespace Tests.Unit.Presentation.Web.Helpers
                 SourceEntity = new ItSystemUsage
                 {
                     Id = Fixture.Create<int>(),
-                    Active = sourceIsActive,
                     Concluded = sourceConcluded,
                     ExpirationDate = sourceExpirationDate
                 }
