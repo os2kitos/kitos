949a2a98bf6cf776ced698e5ca04b52935b56e9b
Aleksander Naskret <aln@strongminds.dk>
2022-09-14 10:08:11 +0200

    added migration

---
+++ Infrastructure.DataAccess/Migrations/202209140750404_RemovedHasMainContract.cs
@@ -0,0 +1,22 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovedHasMainContract : DbMigration
+    {
+        public override void Up()
+        {
+            DropIndex("dbo.ItSystemUsageOverviewReadModels", "ItSystemUsageOverviewReadModel_Index_HasMainContract");
+            AlterColumn("dbo.ItSystemUsageOverviewReadModels", "MainContractIsActive", c => c.Boolean(nullable: false));
+            DropColumn("dbo.ItSystemUsageOverviewReadModels", "HasMainContract");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ItSystemUsageOverviewReadModels", "HasMainContract", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.ItSystemUsageOverviewReadModels", "MainContractIsActive", c => c.Boolean());
+            CreateIndex("dbo.ItSystemUsageOverviewReadModels", "HasMainContract", name: "ItSystemUsageOverviewReadModel_Index_HasMainContract");
+        }
+    }
+}
