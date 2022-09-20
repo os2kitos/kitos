13960c9fe5df8f137ec376fcb33c61e40d77b112
Aleksander Naskret <aln@strongminds.dk>
2022-09-20 10:28:45 +0200

    cleanup 2

--- Presentation.Web/app/models/it-system-usage/it-system-usage-life-cycle-status.ts
+++ Presentation.Web/app/models/it-system-usage/it-system-usage-life-cycle-status.ts
@@ -15,13 +15,13 @@
                 case LifeCycleStatusType.Undecided:
                     return { text: Constants.Select2.EmptyField, id: value, optionalObjectContext: value };
                 case LifeCycleStatusType.NotInUse:
-                    return { text: "Under indfasning", id: value, optionalObjectContext: value };
+                    return { text: Constants.LifeCycleStatus.NotInUseDescription, id: value, optionalObjectContext: value };
                 case LifeCycleStatusType.PhasingIn:
-                    return { text: "I drift", id: value, optionalObjectContext: value };
+                    return { text: Constants.LifeCycleStatus.PhasingInDescription, id: value, optionalObjectContext: value };
                 case LifeCycleStatusType.Operational:
-                    return { text: "Under udfasning", id: value, optionalObjectContext: value };
+                    return { text: Constants.LifeCycleStatus.OperationalDescription, id: value, optionalObjectContext: value };
                 case LifeCycleStatusType.PhasingOut:
-                    return { text: "Ikke i drift", id: value, optionalObjectContext: value };
+                    return { text: Constants.LifeCycleStatus.PhasingOutDescription, id: value, optionalObjectContext: value };
                 default:
                     return null;
             }
