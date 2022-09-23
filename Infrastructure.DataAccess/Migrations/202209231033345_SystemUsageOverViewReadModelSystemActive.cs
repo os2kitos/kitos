901af1fb7c2ac4261c071c98f6f85f8f31d6370e
Aleksander Naskret <aln@strongminds.dk>
2022-09-23 14:48:34 +0200

    migration

---
+++ Infrastructure.DataAccess/Migrations/202209231033345_SystemUsageOverViewReadModelSystemActive.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SystemUsageOverViewReadModelSystemActive : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ItSystemUsageOverviewReadModels", "SystemActive", c => c.Boolean(nullable: false));
+            CreateIndex("dbo.ItSystemUsageOverviewReadModels", "SystemActive", name: "ItSystemUsageOverviewReadModel_Index_SystemActive");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.ItSystemUsageOverviewReadModels", "ItSystemUsageOverviewReadModel_Index_SystemActive");
+            DropColumn("dbo.ItSystemUsageOverviewReadModels", "SystemActive");
+        }
+    }
+}
