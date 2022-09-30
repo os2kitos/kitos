b1f7e24ffd9fcc860a748e3207d230711645d79f
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-09-30 10:42:08 +0200

    fixed

--- Presentation.Web/app/models/ViewModel/feature-toggle/feature-toggle-view-model.ts
+++ Presentation.Web/app/models/ViewModel/feature-toggle/feature-toggle-view-model.ts
@@ -1,6 +1,6 @@
 ﻿module Kitos.Models.ViewModel.FeatureToggle {
     export interface IFeatureToggleViewModel {
-        featureToggle: string | null;
+        featureToggle: Kitos.Services.FeatureToggle.TemporaryFeature | null;
     }
 
     export class FeatureToggleViewModelFactory {
@@ -10,7 +10,7 @@
         }
 
         createFromViewState() {
-            let featureToggleValue: string | null = null;
+            let featureToggleValue: Kitos.Services.FeatureToggle.TemporaryFeature | null = null;
             const state = new Utility.ViewDataState(this.$);
 
             const featureToggleElement = state.getStateOrNull("feature-toggle");
