8c90b4e697698c33b4777181becf3b05c6523e12
Aleksander Naskret <aln@strongminds.dk>
2022-10-03 14:23:11 +0200

    fix

--- Presentation.Web/app/helpers/date-validation-helper.ts
+++ Presentation.Web/app/helpers/date-validation-helper.ts
@@ -2,8 +2,8 @@
     export class DateValidationHelper {
         static validateInterval(startDate:string , endDate: string, notify, startDateFieldName: string, endDateFieldName: string) {
             const formatDateString = "YYYY-MM-DD";
-            const formattedFromDate = moment(startDate, [Kitos.Constants.DateFormat.DanishDateFormat, formatDateString]).startOf("day");
-            const formattedEndDate = moment(endDate, [Kitos.Constants.DateFormat.DanishDateFormat, formatDateString]).endOf("day");
+            const formattedFromDate = startDate != null ? moment(startDate, [Kitos.Constants.DateFormat.DanishDateFormat, formatDateString]).startOf("day") : null;
+            const formattedEndDate = endDate != null ? moment(endDate, [Kitos.Constants.DateFormat.DanishDateFormat, formatDateString]).endOf("day") : null;
 
             return DateValidationHelper.checkIfDateIsValid(formattedFromDate, notify)
                 && DateValidationHelper.checkIfDateIsValid(formattedEndDate, notify)
@@ -15,7 +15,7 @@
         }
 
         static checkIfStartDateIsSmallerThanEndDate(startDate: moment.Moment, endDate: moment.Moment, notify, startDateFieldName: string, endDateFieldName: string): boolean {
-            if (startDate >= endDate) {
+            if (startDate > endDate) {
                 notify.addErrorMessage(`Den indtastede \"${endDateFieldName}\" er før \"${startDateFieldName}\".`);
                 return false;
             }
@@ -24,7 +24,7 @@
         }
 
         static checkIfDateIsValid(date: moment.Moment, notify): boolean {
-            if (!date.isValid() || isNaN(date.valueOf()) || date.year() < 1000 || date.year() > 2099) {
+            if (date != null && !date.isValid() || isNaN(date.valueOf()) || date.year() < 1000 || date.year() > 2099) {
                 notify.addErrorMessage("Den indtastede dato er ugyldig.");
                 return false;
             }
