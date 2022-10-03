cf1a18f1880683fdbe83574bebe3fa9dd673c860
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2022-10-03 11:32:35 +0200

    added new sync status

--- Presentation.Web/app/components/local-config/import/fk-organization-import-config.component.ts
+++ Presentation.Web/app/components/local-config/import/fk-organization-import-config.component.ts
@@ -12,14 +12,23 @@
         };
     }
 
+    interface IFkOrganizationSynchronizationStatus {
+        connected: boolean
+        synchronizationDepth: number | null
+    }
+
     interface IFkOrganizationImportController extends ng.IComponentController {
         currentOrganizationUuid: string
+        accessGranted: boolean | null;
+        accessError: string | null
+        synchronizationStatus: IFkOrganizationSynchronizationStatus | null
     }
 
     class FkOrganizationImportController implements IFkOrganizationImportController {
         currentOrganizationUuid: string | null = null; //note set by bindings
         accessGranted: boolean | null = null;
         accessError: string | null = null;
+        synchronizationStatus: IFkOrganizationSynchronizationStatus | null = null;
 
         static $inject: string[] = ["stsOrganizationSyncService"];
         constructor(private readonly stsOrganizationSyncService: Kitos.Services.Organization.IStsOrganizationSyncService) {
@@ -32,31 +41,44 @@
                 this.stsOrganizationSyncService
                     .getConnectionStatus(this.currentOrganizationUuid)
                     .then(result => {
-                        if (result.accessStatus.accessGranted) {
-                            this.accessGranted = true;
-                        } else {
-                            this.accessGranted = false;
-                            switch (result.accessStatus.error) {
-                                case Models.Api.Organization.CheckConnectionError.ExistingServiceAgreementIssue:
-                                    this.accessError = "Der er problemer med den eksisterende serviceaftale, der giver KITOS adgang til data fra din kommune i FK Organisatoin. Kontakt venligst den KITOS ansvarlige i din kommune for hjælp."
-                                    break;
-                                case Models.Api.Organization.CheckConnectionError.InvalidCvrOnOrganization:
-                                    this.accessError = "Der enten mangler eller er registreret et ugyldigt CVR nummer på din kommune i KITOS."
-                                    break;
-                                case Models.Api.Organization.CheckConnectionError.MissingServiceAgreement:
-                                    this.accessError = "Din organisation mangler en gyldig serviceaftale der giver KITOS adgang til data fra din kommune i FK Organisation. Kontakt venligst den KITOS ansvarlige i din kommune for hjælp."
-                                    break;
-                                case Models.Api.Organization.CheckConnectionError.Unknown: //intended fallthrough
-                                default:
-                                    this.accessError = "Der skete en ukendt fejl ifm. tjek for forbindelsen til FK Organisation. Genindlæs venligst siden for at prøve igen."
-                                    break;
-                            }
-                        }
+                        this.bindAccessProperties(result);
+                        this.bindSynchronizationStatus(result);
                     }, error => {
                         console.error(error);
                         this.accessGranted = false;
                         this.accessError = "Der skete en fejl ifm. tjek for forbindelsen til FK Organisation. Genindlæs venligst siden for at prøve igen."
                     });
+            }
+        }
+
+        private bindSynchronizationStatus(result: Models.Api.Organization.StsOrganizationSynchronizationStatusResponseDTO) {
+            this.synchronizationStatus = {
+                connected: result.connected,
+                synchronizationDepth: result.synchronizationDepth
+            };
+            //TODO: Bind the available commands
+        }
+
+        private bindAccessProperties(result: Models.Api.Organization.StsOrganizationSynchronizationStatusResponseDTO) {
+            if (result.accessStatus.accessGranted) {
+                this.accessGranted = true;
+            } else {
+                this.accessGranted = false;
+                switch (result.accessStatus.error) {
+                    case Models.Api.Organization.CheckConnectionError.ExistingServiceAgreementIssue:
+                        this.accessError = "Der er problemer med den eksisterende serviceaftale, der giver KITOS adgang til data fra din kommune i FK Organisatoin. Kontakt venligst den KITOS ansvarlige i din kommune for hjælp.";
+                        break;
+                    case Models.Api.Organization.CheckConnectionError.InvalidCvrOnOrganization:
+                        this.accessError = "Der enten mangler eller er registreret et ugyldigt CVR nummer på din kommune i KITOS.";
+                        break;
+                    case Models.Api.Organization.CheckConnectionError.MissingServiceAgreement:
+                        this.accessError = "Din organisation mangler en gyldig serviceaftale der giver KITOS adgang til data fra din kommune i FK Organisation. Kontakt venligst den KITOS ansvarlige i din kommune for hjælp.";
+                        break;
+                    case Models.Api.Organization.CheckConnectionError.Unknown: //intended fallthrough
+                    default:
+                        this.accessError = "Der skete en ukendt fejl ifm. tjek for forbindelsen til FK Organisation. Genindlæs venligst siden for at prøve igen.";
+                        break;
+                }
             }
         }
     }
