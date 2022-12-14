0369d0c3c5460ff1a06f2faf8c5d897c3e807847
Aleksander Naskret <aln@strongminds.dk>
2022-12-14 09:41:51 +0100

    added ui-customization to the gdpr columns in the overview

--- Presentation.Web/app/components/it-system/it-system-overview.controller.ts
+++ Presentation.Web/app/components/it-system/it-system-overview.controller.ts
@@ -819,7 +819,8 @@
                         .withDataSourceType(Utility.KendoGrid.KendoGridColumnDataSourceType.Date)
                         .withFilteringOperation(Utility.KendoGrid.KendoGridColumnFiltering.Date)
                         .withStandardWidth(170)
-                        .withRendering(dataItem => Helpers.RenderFieldsHelper.renderDate(dataItem.RiskAssessmentDate)))
+                        .withRendering(dataItem => Helpers.RenderFieldsHelper.renderDate(dataItem.RiskAssessmentDate))
+                        .withInclusionCriterion(() => uiState.isBluePrintNodeAvailable(uiBluePrint.children.gdpr)))
                 .withColumn(builder =>
                     builder
                         .withDataSourceName("PlannedRiskAssessmentDate")
@@ -828,7 +829,8 @@
                         .withDataSourceType(Utility.KendoGrid.KendoGridColumnDataSourceType.Date)
                         .withFilteringOperation(Utility.KendoGrid.KendoGridColumnFiltering.Date)
                         .withStandardWidth(170)
-                        .withRendering(dataItem => Helpers.RenderFieldsHelper.renderDate(dataItem.PlannedRiskAssessmentDate)));
+                        .withRendering(dataItem => Helpers.RenderFieldsHelper.renderDate(dataItem.PlannedRiskAssessmentDate))
+                        .withInclusionCriterion(() => uiState.isBluePrintNodeAvailable(uiBluePrint.children.gdpr.children.plannedRiskAssessmentDate)));
 
             //Launch kendo grid
             launcher.launch();
