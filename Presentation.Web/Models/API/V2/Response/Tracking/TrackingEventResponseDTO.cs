893fa516300cfc6c6fba10b23c90c4c4c9a5ed0b
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-01-11 11:29:58 +0100

    updated generics

--- Presentation.Web/Models/API/V2/Response/Tracking/TrackingEventResponseDTO.cs
+++ Presentation.Web/Models/API/V2/Response/Tracking/TrackingEventResponseDTO.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Presentation.Web.Models.API.V2.Types.Shared;
 
 namespace Presentation.Web.Models.API.V2.Response.Tracking
 {
     public class TrackingEventResponseDTO
     {
+        [Required]
         public DateTime OccurredAtUtc { get; set; }
+        [Required]
         public Guid EntityUuid { get; set; }
+        [Required]
         public TrackedEntityTypeChoice EntityType { get; set; }
     }
 }
--- Presentation.Web/Models/API/V2/Types/SystemUsage/LifeCycleStatusChoice.cs
+++ Presentation.Web/Models/API/V2/Types/SystemUsage/LifeCycleStatusChoice.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Presentation.Web.Models.API.V2.Types.SystemUsage
+﻿namespace Presentation.Web.Models.API.V2.Types.SystemUsage
 {
     public enum LifeCycleStatusChoice
     {
