397157332a34742fa0b02d562719d23e33365143
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-02-02 11:29:45 +0100

    fix

--- Presentation.Web/Models/API/V2/Internal/Request/PublicMessagesRequestDTO.cs
+++ Presentation.Web/Models/API/V2/Internal/Request/PublicMessagesRequestDTO.cs
@@ -3,13 +3,9 @@
     public class PublicMessagesRequestDTO
     {
         public string About { get; set; }
-        
         public string Guides { get; set; }
-        
         public string StatusMessages { get; set; }
-        
         public string Misc { get; set; }
-        
         public string ContactInfo { get; set; }
     }
 }
