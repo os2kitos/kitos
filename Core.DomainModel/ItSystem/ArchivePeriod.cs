449730ef9070d88b0d78973bc2fcb4b18773b2b4
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-02-08 15:28:13 +0100

    Added uuid to archive period + migrations

--- Core.DomainModel/ItSystem/ArchivePeriod.cs
+++ Core.DomainModel/ItSystem/ArchivePeriod.cs
@@ -2,13 +2,19 @@
 
 namespace Core.DomainModel.ItSystem
 {
-    public class ArchivePeriod : Entity, ISystemModule
+    public class ArchivePeriod : Entity, ISystemModule, IHasUuid
     {
+        public ArchivePeriod()
+        {
+            Uuid = Guid.NewGuid();
+        }
+
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string UniqueArchiveId { get; set; }
         public int ItSystemUsageId { get; set; }
         public ItSystemUsage.ItSystemUsage ItSystemUsage { get; set; }
         public bool Approved { get; set; }
+        public Guid Uuid { get; set; }
     }
 }
