f9df847f66b02cdad5c0ffae2e6b588db2f07500
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-02-17 11:53:53 +0100

    Added new controller and endpoint

---
+++ Presentation.Web/Models/API/V2/Internal/Response/ItSystem/ItSystemSearchResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Presentation.Web.Models.API.V2.Response.Generic.Identity;
+
+namespace Presentation.Web.Models.API.V2.Internal.Response.ItSystem
+{
+    public class ItSystemSearchResponseDTO : IdentityNamePairResponseDTO
+    {
+
+        [Required]
+        public bool Disabled { get; set; }
+
+        public ItSystemSearchResponseDTO()
+        {
+        }
+
+        public ItSystemSearchResponseDTO(Guid uuid, string name, bool disabled) 
+            : base(uuid, name)
+        {
+            Disabled = disabled;
+        }
+    }
+}
