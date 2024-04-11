cfff8824654c987cd0aae591eda02814593a4211
Aleksander Naskret <aln@strongminds.dk>
2024-04-11 14:33:40 +0200

    hierarchy

--- Tests.Integration.Presentation.Web/Tools/External/ItContractV2Helper.cs
+++ Tests.Integration.Presentation.Web/Tools/External/ItContractV2Helper.cs
@@ -13,6 +13,7 @@ using Xunit;
 using Core.DomainModel.Organization;
 using Presentation.Web.Models.API.V1;
 using Presentation.Web.Models.API.V2.Response.Generic.Identity;
+using Presentation.Web.Models.API.V2.Response.Generic.Hierarchy;
 
 namespace Tests.Integration.Presentation.Web.Tools.External
 {
@@ -177,6 +178,17 @@ namespace Tests.Integration.Presentation.Web.Tools.External
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             return await response.ReadResponseBodyAsAsync<IEnumerable<IdentityNamePairResponseDTO>>();
+        }
+
+        public static async Task<IEnumerable<RegistrationHierarchyNodeResponseDTO>> GetHierarchyAsync(Guid uuid, Cookie optionalLogin = null)
+        {
+            var cookie = optionalLogin ?? await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
+
+            var path = $"api/v2/internal/it-contracts/{uuid}/hierarchy";
+            using var response = await HttpApi.GetWithCookieAsync(TestEnvironment.CreateUrl(path), cookie);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            return await response.ReadResponseBodyAsAsync<IEnumerable<RegistrationHierarchyNodeResponseDTO>>();
         }
     }
 }
