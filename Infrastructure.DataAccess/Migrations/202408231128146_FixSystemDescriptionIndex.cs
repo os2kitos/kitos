316eddef5160a6dbaed91f03acba28c3445ad8b2
Aleksander Naskret <aln@strongminds.dk>
2024-08-23 14:24:17 +0200

    index fix

---
+++ Infrastructure.DataAccess/Migrations/202408231128146_FixSystemDescriptionIndex.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixSystemDescriptionIndex : DbMigration
+    {
+        public override void Up()
+        {
+            RenameIndex(table: "dbo.ItSystemUsageOverviewReadModels", name: "ItSystemUsageOverviewReadModel_Index_System_Description", newName: "ItSystemUsageOverviewReadModel_Index_Name");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.ItSystemUsageOverviewReadModels", name: "ItSystemUsageOverviewReadModel_Index_Name", newName: "ItSystemUsageOverviewReadModel_Index_System_Description");
+        }
+    }
+}
