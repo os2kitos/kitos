87f7de88ec1497faaab232f4d0aab28e8b83fd4d
Aleksander Naskret <aln@strongminds.dk>
2024-08-23 11:12:10 +0200

    controller

---
+++ Presentation.Web/Controllers/API/V2/Internal/Organizations/OrganizationsInternalV2Controller.cs
@@ -0,0 +1,14 @@
+﻿using Core.ApplicationServices.Organizations;
+using System.Web.Http;
+
+namespace Presentation.Web.Controllers.API.V2.Internal.Organizations
+{
+    /// <summary>
+    /// Internal API for the local registrations related to it-systems in KITOS
+    /// </summary>
+    [RoutePrefix("api/v2/internal/organizations")]
+    public class OrganizationsInternalV2Controller : InternalApiV2Controller
+    {
+        private readonly IOrganizationService _organizationService;
+    }
+}
