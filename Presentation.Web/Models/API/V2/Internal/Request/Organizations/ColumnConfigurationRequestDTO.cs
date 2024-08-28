ddf95cb82b9cc1b4abaa44104f283600e95b5cc2
JacobBorchMadsen <jbma@strongminds.dk>
2024-08-28 08:53:46 +0200

    actually added now

---
+++ Presentation.Web/Models/API/V2/Internal/Request/Organizations/ColumnConfigurationRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.Web.Models.API.V2.Internal.Request.Organizations
+{
+    public class ColumnConfigurationRequestDTO
+    {
+        public string PersistId { get; set; }
+        public int Index { get; set; }
+    }
+}
