a5d353491453fc85f2b012fd62450ce99d96f02d
JacobBorchMadsen <jbma@strongminds.dk>
2024-09-04 15:08:56 +0200

    initial commit

--- Presentation.Web/Models/API/V2/Request/OrganizationUnit/CreateOrganizationUnitRequestDTO.cs
+++ Presentation.Web/Models/API/V2/Request/OrganizationUnit/CreateOrganizationUnitRequestDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace Presentation.Web.Models.API.V2.Request.OrganizationUnit
+﻿using System;
+using Core.DomainModel.Organization;
+
+namespace Presentation.Web.Models.API.V2.Request.OrganizationUnit
 {
     public class CreateOrganizationUnitRequestDTO : BaseOrganizationUnitRequestDTO
     {
-    }
+        public Guid ParentUnitUuid { get; set; }
+        public string Name { get; set; }
+        public int EAN { get; set; }
+        public int Id { get; set; }
+        public OrganizationUnitOrigin Origin { get; set; }
+}
 }
