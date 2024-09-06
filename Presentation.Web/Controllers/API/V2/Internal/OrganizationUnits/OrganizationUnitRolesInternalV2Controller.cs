0a8d840e6353ca6a22fd694636f173fb55cde63f
JacobBorchMadsen <jbma@strongminds.dk>
2024-09-06 13:00:08 +0200

    initial commit

---
+++ Presentation.Web/Controllers/API/V2/Internal/OrganizationUnits/OrganizationUnitRolesInternalV2Controller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.Web.Controllers.API.V2.Internal.OrganizationUnits
+{
+    public class OrganizationUnitRolesInternalV2Controller
+    {
+    }
+}
