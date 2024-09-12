f7676918f9df5a3bd07ce6a994642cfb442f4537
Aleksander Naskret <aln@strongminds.dk>
2024-09-12 13:13:41 +0200

    fix

--- Presentation.Web/Models/API/V2/Internal/Common/NamedEntityWithUserFullNameV2DTO.cs
+++ Presentation.Web/Models/API/V2/Internal/Common/NamedEntityWithUserFullNameV2DTO.cs
@@ -5,7 +5,7 @@ namespace Presentation.Web.Models.API.V2.Internal.Common
     public class NamedEntityWithUserFullNameV2DTO : NamedEntityV2DTO
     {
         public string UserFullName { get; set; }
-        public NamedEntityWithUserFullNameV2DTO(int id, Guid uuid, string name, string userName)
+        public NamedEntityWithUserFullNameV2DTO(int id, Guid? uuid, string name, string userName)
             : base(id, uuid, name)
         {
             UserFullName = userName;
