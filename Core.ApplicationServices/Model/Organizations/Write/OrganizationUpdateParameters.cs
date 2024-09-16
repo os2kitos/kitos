425b6ef7f3a5ff7a29d79e46b9ad22d35e4c5447
NikolajNielsen <nan@strongminds.dk>
2024-09-16 12:43:02 +0200

    Missing param object

---
+++ Core.ApplicationServices/Model/Organizations/Write/OrganizationUpdateParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.ApplicationServices.Model.Shared;
+
+namespace Core.ApplicationServices.Model.Organizations.Write
+{
+    public class OrganizationUpdateParameters
+    {
+        public OptionalValueChange<string> Cvr { get; set; }
+        public OptionalValueChange<string> Phone { get; set; }
+        public OptionalValueChange<string> Address { get; set; }
+        public OptionalValueChange<string> Email { get; set; }
+    }
+}
