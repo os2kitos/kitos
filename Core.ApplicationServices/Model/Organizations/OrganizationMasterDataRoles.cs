a9cf11d391f3158ead21c5ccebcd487fd9d2f261
NikolajNielsen <nan@strongminds.dk>
2024-09-18 07:50:25 +0200

    Missing files

---
+++ Core.ApplicationServices/Model/Organizations/OrganizationMasterDataRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DomainModel;
+
+namespace Core.ApplicationServices.Model.Organizations
+{
+    public class OrganizationMasterDataRoles
+    {
+        public ContactPerson ContactPerson { get; set; }
+    }
+}
---
+++ Presentation.Web/Models/API/V2/Internal/Response/Organizations/OrganizationMasterDataRolesResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Presentation.Web.Models.API.V1;
+
+namespace Presentation.Web.Models.API.V2.Response.Organization
+{
+    public class OrganizationMasterDataRolesResponseDTO
+    {
+        public ContactPersonDTO ContactPerson { get; set; }
+    }
+}
