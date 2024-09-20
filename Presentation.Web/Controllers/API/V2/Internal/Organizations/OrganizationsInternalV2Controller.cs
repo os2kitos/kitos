03e67fbcc0fef3405ff2ccc5cc34fc4f405eab10
JacobBorchMadsen <jbma@strongminds.dk>
2024-09-20 11:52:32 +0200

    removed unneded stuff

--- Presentation.Web/Controllers/API/V2/Internal/Organizations/OrganizationsInternalV2Controller.cs
+++ Presentation.Web/Controllers/API/V2/Internal/Organizations/OrganizationsInternalV2Controller.cs
@@ -6,17 +6,6 @@ using Presentation.Web.Infrastructure.Attributes;
 using Swashbuckle.Swagger.Annotations;
 using Presentation.Web.Controllers.API.V2.External.Generic;
 using Presentation.Web.Models.API.V2.Response.Shared;
-using Presentation.Web.Models.API.V2.Internal.Response.Roles;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Core.ApplicationServices;
-using Core.ApplicationServices.Model.Users;
-using Core.ApplicationServices.Rights;
-using Core.DomainModel;
-using Core.DomainModel.Organization;
-using Core.DomainServices.Generic;
-using Presentation.Web.Controllers.API.V2.Internal.Mapping;
 
 namespace Presentation.Web.Controllers.API.V2.Internal.Organizations
 {
@@ -27,16 +16,12 @@ namespace Presentation.Web.Controllers.API.V2.Internal.Organizations
     public class OrganizationsInternalV2Controller : InternalApiV2Controller
     {
         private readonly IOrganizationService _organizationService;
-        private readonly IUserRightsService _userRightsService;
-        private readonly IEntityIdentityResolver _entityIdentityResolver;
         private readonly IResourcePermissionsResponseMapper _permissionsResponseMapper;
 
-        public OrganizationsInternalV2Controller(IOrganizationService organizationService, IResourcePermissionsResponseMapper permissionsResponseMapper, IUserRightsService userRightsService, IEntityIdentityResolver identity)
+        public OrganizationsInternalV2Controller(IOrganizationService organizationService, IResourcePermissionsResponseMapper permissionsResponseMapper)
         {
             _organizationService = organizationService;
             _permissionsResponseMapper = permissionsResponseMapper;
-            _userRightsService = userRightsService;
-            _entityIdentityResolver = identity;
         }
 
         [Route("permissions")]
@@ -49,28 +34,6 @@ namespace Presentation.Web.Controllers.API.V2.Internal.Organizations
             return _organizationService.GetPermissions(organizationUuid)
                 .Select(_permissionsResponseMapper.Map)
                 .Match(Ok, FromOperationError);
-        }
-
-        [Route("user/{userUuid}/rights")]
-        public IHttpActionResult GetUserRights([NonEmptyGuid] Guid organizationUuid, [NonEmptyGuid] Guid userUuid)
-        {
-            var orgIdRes = _entityIdentityResolver.ResolveDbId<Organization>(organizationUuid);
-            var userIdRes = _entityIdentityResolver.ResolveDbId<User>(userUuid);
-            if (orgIdRes.IsNone || userIdRes.IsNone)
-            {
-                return BadRequest("The provided UUID's could not be resolved");
-            }
-
-            var orgId = orgIdRes.Value;
-            var userId = userIdRes.Value;
-            return _userRightsService.GetUserRights(userId, orgId)
-                .Select(mapUserRightAssignmentsToDTO)
-                .Match(Ok, FromOperationError);
-        }
-
-        private object mapUserRightAssignmentsToDTO(UserRightsAssignments userRights)
-        {
-            return userRights;
         }
     }
 }
