836f89088df90aec17644f39cd3c947741c878c8
NikolajNielsen <nan@strongminds.dk>
2024-09-20 13:32:39 +0200

    Refactor mapper

--- Presentation.Web/Controllers/API/V2/Internal/Organizations/OrganizationsInternalV2Controller.cs
+++ Presentation.Web/Controllers/API/V2/Internal/Organizations/OrganizationsInternalV2Controller.cs
@@ -112,39 +112,13 @@ namespace Presentation.Web.Controllers.API.V2.Internal.Organizations
             OrganizationMasterDataRolesRequestDTO dto)
         {
             var contactPersonDto = dto.ContactPerson;
-            var contactPersonParameters = contactPersonDto != null
-            ? new ContactPersonUpdateParameters()
-            {
-                Email = OptionalValueChange<string>.With(contactPersonDto.Email),
-                LastName = OptionalValueChange<string>.With(contactPersonDto.LastName),
-                Name = OptionalValueChange<string>.With(contactPersonDto.Name),
-                PhoneNumber = OptionalValueChange<string>.With(contactPersonDto.PhoneNumber)
-            }
-            : Maybe<ContactPersonUpdateParameters>.None;
+            var contactPersonParameters = ToContactPersonUpdateParameters(contactPersonDto);
 
             var dataResponsibleDto = dto.DataResponsible;
-            var dataResponsibleParameters = dataResponsibleDto != null 
-            ? new DataResponsibleUpdateParameters()
-            {
-                Address = OptionalValueChange<string>.With(dataResponsibleDto?.Address),
-                Cvr = OptionalValueChange<string>.With(dataResponsibleDto.Cvr),
-                Email = OptionalValueChange<string>.With(dataResponsibleDto.Email),
-                Name = OptionalValueChange<string>.With(dataResponsibleDto.Name),
-                Phone = OptionalValueChange<string>.With(dataResponsibleDto.Phone)
-            }
-            : Maybe<DataResponsibleUpdateParameters>.None;
+            var dataResponsibleParameters = ToDataResponsibleUpdateParameters(dataResponsibleDto);
 
             var dataProtectionAdvisorDto = dto.DataProtectionAdvisor;
-            var dataProtectionAdvisorParameters = dataProtectionAdvisorDto != null
-            ? new DataProtectionAdvisorUpdateParameters()
-            {
-                Address = OptionalValueChange<string>.With(dataProtectionAdvisorDto.Address),
-                Cvr = OptionalValueChange<string>.With(dataProtectionAdvisorDto.Cvr),
-                Email = OptionalValueChange<string>.With(dataProtectionAdvisorDto.Email),
-                Name = OptionalValueChange<string>.With(dataProtectionAdvisorDto.Name),
-                Phone = OptionalValueChange<string>.With(dataProtectionAdvisorDto.Phone)
-            }
-            : Maybe<DataProtectionAdvisorUpdateParameters>.None;
+            var dataProtectionAdvisorParameters = ToDataProtectionAdvisorUpdateParameters(dataProtectionAdvisorDto);
 
             return new()
             {
@@ -154,5 +128,47 @@ namespace Presentation.Web.Controllers.API.V2.Internal.Organizations
                 DataProtectionAdvisor = dataProtectionAdvisorParameters
             };
         }
+
+        private static Maybe<ContactPersonUpdateParameters> ToContactPersonUpdateParameters(ContactPersonRequestDTO dto)
+        {
+            return dto != null
+            ? new ContactPersonUpdateParameters()
+            {
+                Email = OptionalValueChange<string>.With(dto.Email),
+                LastName = OptionalValueChange<string>.With(dto.LastName),
+                Name = OptionalValueChange<string>.With(dto.Name),
+                PhoneNumber = OptionalValueChange<string>.With(dto.PhoneNumber)
+            }
+            : Maybe<ContactPersonUpdateParameters>.None;
+        }
+
+        private static Maybe<DataResponsibleUpdateParameters> ToDataResponsibleUpdateParameters(DataResponsibleRequestDTO dto)
+        {
+            return dto != null
+                ? new DataResponsibleUpdateParameters()
+                {
+                    Email = OptionalValueChange<string>.With(dto.Email),
+                    Cvr = OptionalValueChange<string>.With(dto.Cvr),
+                    Name = OptionalValueChange<string>.With(dto.Name),
+                    Phone = OptionalValueChange<string>.With(dto.Phone),
+                    Address = OptionalValueChange<string>.With(dto.Address)
+                }
+                : Maybe<DataResponsibleUpdateParameters>.None;
+        }
+
+        private static Maybe<DataProtectionAdvisorUpdateParameters> ToDataProtectionAdvisorUpdateParameters(DataProtectionAdvisorRequestDTO dto)
+        {
+            return dto != null
+                ? new DataProtectionAdvisorUpdateParameters()
+                {
+                    Email = OptionalValueChange<string>.With(dto.Email),
+                    Cvr = OptionalValueChange<string>.With(dto.Cvr),
+                    Name = OptionalValueChange<string>.With(dto.Name),
+                    Phone = OptionalValueChange<string>.With(dto.Phone),
+                    Address = OptionalValueChange<string>.With(dto.Address)
+                }
+                : Maybe<DataProtectionAdvisorUpdateParameters>.None;
+        }
+
     }
 }
