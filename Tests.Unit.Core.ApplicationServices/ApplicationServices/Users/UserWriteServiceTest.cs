5bd067a0285df47171cfc9a42a12d32523a064eb
Aleksander Naskret <aln@strongminds.dk>
2024-09-23 11:48:25 +0200

    unit tests fix 1

--- Tests.Unit.Core.ApplicationServices/ApplicationServices/Users/UserWriteServiceTest.cs
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/Users/UserWriteServiceTest.cs
@@ -4,7 +4,6 @@ using Core.ApplicationServices;
 using Core.ApplicationServices.Model.Users.Write;
 using Core.ApplicationServices.Users.Write;
 using Core.DomainModel;
-using Core.DomainServices.Generic;
 using Infrastructure.Services.DataAccess;
 using Moq;
 using Tests.Toolkit.Patterns;
@@ -21,7 +20,6 @@ namespace Tests.Unit.Core.ApplicationServices.Users
         private readonly UserWriteService _sut;
 
         private readonly Mock<IUserService> _userServiceMock;
-        private readonly Mock<IEntityIdentityResolver> _entityIdentityResolverMock;
         private readonly Mock<IOrganizationRightsService> _organizationRightsServiceMock;
         private readonly Mock<ITransactionManager> _transactionManagerMock;
         private readonly Mock<IAuthorizationContext> _authorizationContextMock;
@@ -30,14 +28,12 @@ namespace Tests.Unit.Core.ApplicationServices.Users
         public UserWriteServiceTest()
         {
             _userServiceMock = new Mock<IUserService>();
-            _entityIdentityResolverMock = new Mock<IEntityIdentityResolver>();
             _organizationRightsServiceMock = new Mock<IOrganizationRightsService>();
             _transactionManagerMock = new Mock<ITransactionManager>();
             _authorizationContextMock = new Mock<IAuthorizationContext>();
             _organizationServiceMock = new Mock<IOrganizationService>();
 
             _sut = new UserWriteService(_userServiceMock.Object, 
-                _entityIdentityResolverMock.Object,
                 _organizationRightsServiceMock.Object, 
                 _transactionManagerMock.Object,
                 _authorizationContextMock.Object,
@@ -51,13 +47,15 @@ namespace Tests.Unit.Core.ApplicationServices.Users
             var createParams = SetupUserParameters();
             var orgUuid = A<Guid>();
             var orgId = A<int>();
+            var org = new Organization { Id = A<int>() };
             var transaction = ExpectTransactionBegins();
 
-            AssertResolveIdReturns(orgUuid, orgId);
-            AssertAddUserReturns(createParams.User, createParams.SendMailOnCreation, orgId);
+            ExpectGetOrganizationReturns(orgUuid, org);
+            ExpectPermissionsReturn(org, true);
+            ExpectAddUserReturns(createParams.User, createParams.SendMailOnCreation, orgId);
             foreach (var organizationRole in createParams.Roles)
             {
-                AssertAddRoleReturns(organizationRole, orgId, createParams.User.Id, Result<OrganizationRight, OperationFailure>.Success(It.IsAny<OrganizationRight>()));
+                ExpectAddRoleReturns(organizationRole, orgId, createParams.User.Id, Result<OrganizationRight, OperationFailure>.Success(It.IsAny<OrganizationRight>()));
             }
 
             //Act
@@ -75,14 +73,16 @@ namespace Tests.Unit.Core.ApplicationServices.Users
             var createParams = SetupUserParameters();
             var orgUuid = A<Guid>();
             var orgId = A<int>();
+            var org = new Organization { Id = A<int>() };
             var transaction = ExpectTransactionBegins();
             var error = A<OperationFailure>();
 
-            AssertResolveIdReturns(orgUuid, orgId);
-            AssertAddUserReturns(createParams.User, createParams.SendMailOnCreation, orgId);
+            ExpectGetOrganizationReturns(orgUuid, org);
+            ExpectPermissionsReturn(org, true);
+            ExpectAddUserReturns(createParams.User, createParams.SendMailOnCreation, orgId);
             foreach (var organizationRole in createParams.Roles)
             {
-                AssertAddRoleReturns(organizationRole, orgId, createParams.User.Id, error);
+                ExpectAddRoleReturns(organizationRole, orgId, createParams.User.Id, error);
             }
 
             //Act
@@ -97,38 +97,60 @@ namespace Tests.Unit.Core.ApplicationServices.Users
         }
 
         [Fact]
-        public void Create_Fails_If_OrgId_Is_Not_Found()
+        public void Create_Fails_If_No_Create_Permission()
         {
             //Arrange
             var createParams = SetupUserParameters();
             var orgUuid = A<Guid>();
+            var org = new Organization { Id = A<int>() };
 
-            AssertResolveIdReturns(orgUuid, Maybe<int>.None);
+            ExpectGetOrganizationReturns(orgUuid, org);
+            ExpectPermissionsReturn(org, false);
 
             //Act
             var result = _sut.Create(orgUuid, createParams);
 
             //Assert
             Assert.True(result.Failed);
-            Assert.Equal(OperationFailure.NotFound, result.Error.FailureType);
+            Assert.Equal(OperationFailure.Forbidden, result.Error.FailureType);
         }
 
-        private void AssertAddUserReturns(User user, bool sendMailOnCreation, int orgId)
+        private void ExpectAddUserReturns(User user, bool sendMailOnCreation, int orgId)
         {
             _userServiceMock.Setup(x => x.AddUser(user, sendMailOnCreation, orgId)).Returns(user);
         }
-
-        private void AssertResolveIdReturns(Guid uuid, Maybe<int> idResult)
-        {
-            _entityIdentityResolverMock.Setup(x => x.ResolveDbId<Organization>(uuid)).Returns(idResult);
-        }
-
-        private void AssertAddRoleReturns(OrganizationRole role, int organizationId, int userId, Result<OrganizationRight, OperationFailure> result)
+        
+        private void ExpectAddRoleReturns(OrganizationRole role, int organizationId, int userId, Result<OrganizationRight, OperationFailure> result)
         {
             _organizationRightsServiceMock.Setup(x => x.AssignRole(organizationId, userId, role)).Returns(result);
         }
 
-        private void AssertGetOrganizationReturns
+        private void ExpectGetOrganizationReturns(Guid organizationUuid, Result<Organization, OperationError> result)
+        {
+            _organizationServiceMock.Setup(x => x.GetOrganization(organizationUuid, null)).Returns(result);
+        }
+
+        private void ExpectCreatePermissionReturns(int organizationId, bool result)
+        {
+            _authorizationContextMock.Setup(x => x.AllowCreate<User>(organizationId)).Returns(result);
+        }
+
+        private void ExpectModifyPermissionReturns(Organization organization, bool result)
+        {
+            _authorizationContextMock.Setup(x => x.AllowModify(organization)).Returns(result);
+        }
+
+        private void ExpectDeletePermissionReturns(int organizationId, bool result)
+        {
+            _authorizationContextMock.Setup(x => x.HasPermission(It.Is<DeleteAnyUserPermission>(parameter => parameter.OptionalOrganizationScopeId.Value == organizationId))).Returns(result);
+        }
+
+        private void ExpectPermissionsReturn(Organization organization, bool result)
+        {
+            ExpectCreatePermissionReturns(organization.Id, result);
+            ExpectModifyPermissionReturns(organization, result);
+            ExpectDeletePermissionReturns(organization.Id, result);
+        }
 
         private Mock<IDatabaseTransaction> ExpectTransactionBegins()
         {
