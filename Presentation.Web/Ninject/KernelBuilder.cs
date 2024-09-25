9153ab2c5283dc1af95034f09a7c3191dbeddd83
NikolajNielsen <nan@strongminds.dk>
2024-09-25 10:44:30 +0200

    Add write service to DI

--- Presentation.Web/Ninject/KernelBuilder.cs
+++ Presentation.Web/Ninject/KernelBuilder.cs
@@ -224,6 +224,7 @@ namespace Presentation.Web.Ninject
             kernel.Bind<IRegistrationNotificationUserRelationsService>().To<RegistrationNotificationUserRelationsService>().InCommandScope(Mode);
             kernel.Bind<INotificationService>().To<NotificationService>().InCommandScope(Mode);
             kernel.Bind<IOrganizationService>().To<OrganizationService>().InCommandScope(Mode);
+            kernel.Bind<IOrganizationWriteService>().To<OrganizationWriteService>().InCommandScope(Mode);
             kernel.Bind<IItSystemService>().To<ItSystemService>().InCommandScope(Mode);
             kernel.Bind<IItSystemUsageService>().To<ItSystemUsageService>().InCommandScope(Mode);
             kernel.Bind<IItSystemUsageMigrationServiceAdapter>().To<ItSystemUsageMigrationServiceAdapter>().InCommandScope(Mode);
