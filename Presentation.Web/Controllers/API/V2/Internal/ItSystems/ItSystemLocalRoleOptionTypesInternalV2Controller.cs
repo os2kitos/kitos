bfd48f6eb1c393a54e12a0009fd7adec99323ec9
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-17 13:35:15 +0200

    added it system roles controller

---
+++ Presentation.Web/Controllers/API/V2/Internal/ItSystems/ItSystemLocalRoleOptionTypesInternalV2Controller.cs
@@ -0,0 +1,91 @@
+﻿using Core.ApplicationServices.LocalOptions;
+using Core.DomainModel.LocalOptions;
+using Presentation.Web.Controllers.API.V2.Common.Mapping;
+using Presentation.Web.Infrastructure.Attributes;
+using Swashbuckle.Swagger.Annotations;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using System;
+using Core.DomainModel.ItSystem;
+using Presentation.Web.Controllers.API.V2.Internal.LocalOptionTypes;
+using Presentation.Web.Models.API.V2.Internal.Response.LocalOptions;
+using Presentation.Web.Models.API.V2.Internal.Request.Options;
+
+namespace Presentation.Web.Controllers.API.V2.Internal.OrganizationUnits
+{
+    [RoutePrefix("api/v2/internal/it-systems/{organizationUuid}/local-option-types/it-systems-roles")]
+    public class ItSystemLocalRoleOptionTypesInternalV2Controller : BaseLocalRoleOptionTypesInternalV2Controller<LocalItSystemRole, ItSystemRight, ItSystemRole>
+    {
+        private readonly IGenericLocalOptionsService<LocalItSystemRole, ItSystemRight, ItSystemRole> _localOrganizationUnitRoleOptionTypeService;
+        private readonly ILocalOptionTypeResponseMapper _responseMapper;
+        private readonly ILocalOptionTypeWriteModelMapper _writeModelMapper;
+
+        public ItSystemLocalRoleOptionTypesInternalV2Controller(IGenericLocalOptionsService<LocalItSystemRole, ItSystemRight, ItSystemRole> localRoleOptionTypeService, ILocalOptionTypeResponseMapper responseMapper, ILocalOptionTypeWriteModelMapper writeModelMapper) : base(localRoleOptionTypeService, responseMapper, writeModelMapper)
+        {
+        }
+
+        [HttpGet]
+        [Route]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(IEnumerable<LocalRoleOptionResponseDTO>))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult GetLocalOrganizationUnitRoles([NonEmptyGuid][FromUri] Guid organizationUuid)
+        {
+            return GetAll(organizationUuid);
+        }
+
+        [HttpGet]
+        [Route("{optionUuid}")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRoleOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult GetLocalOrganizationUnitRole([NonEmptyGuid][FromUri] Guid organizationUuid, [FromUri] Guid optionUuid)
+        {
+            return GetSingle(organizationUuid, optionUuid);
+        }
+
+        [HttpPost]
+        [Route]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRoleOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult CreateLocalOrganizationUnitRole([NonEmptyGuid][FromUri] Guid organizationUuid, LocalOptionCreateRequestDTO dto)
+        {
+            return Create(organizationUuid, dto);
+        }
+
+        [HttpPatch]
+        [Route("{optionUuid}")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRoleOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult PatchLocalOrganizationUnitRole([NonEmptyGuid][FromUri] Guid organizationUuid,
+            [FromUri] Guid optionUuid,
+            LocalRegularOptionUpdateRequestDTO dto)
+        {
+            return Patch(organizationUuid, optionUuid, dto);
+        }
+
+        [HttpDelete]
+        [Route("{optionUuid}")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRoleOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult DeleteLocalOrganizationUnitRole([NonEmptyGuid][FromUri] Guid organizationUuid,
+            [FromUri] Guid optionUuid)
+        {
+            return Delete(organizationUuid, optionUuid);
+        }
+    }
+}
