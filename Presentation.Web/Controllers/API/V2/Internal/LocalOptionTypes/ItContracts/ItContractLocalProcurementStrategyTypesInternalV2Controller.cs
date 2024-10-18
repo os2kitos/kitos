10c2aab5bbaf933587491133af3a2d10b55097f2
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-18 11:49:59 +0200

    added it contract procurement strategy types option controller

---
+++ Presentation.Web/Controllers/API/V2/Internal/LocalOptionTypes/ItContracts/ItContractLocalProcurementStrategyTypesInternalV2Controller.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using Core.ApplicationServices.LocalOptions;
+using Core.DomainModel.ItContract;
+using Core.DomainModel.LocalOptions;
+using Presentation.Web.Controllers.API.V2.Common.Mapping;
+using Presentation.Web.Infrastructure.Attributes;
+using Presentation.Web.Models.API.V2.Internal.Request.Options;
+using Presentation.Web.Models.API.V2.Internal.Response;
+using Swashbuckle.Swagger.Annotations;
+
+namespace Presentation.Web.Controllers.API.V2.Internal.LocalOptionTypes.ItContracts
+{
+    [RoutePrefix("api/v2/internal/it-contracts/{organizationUuid}/local-option-types/procurement-strategy-types")]
+    public class ItContractLocalProcurementStrategyTypesInternalV2Controller : BaseLocalRegularOptionTypesInternalV2Controller<LocalProcurementStrategyType, ItContract, ProcurementStrategyType>
+    {
+        private readonly IGenericLocalOptionsService<LocalProcurementStrategyType, ItContract, ProcurementStrategyType> _localOptionTypeService;
+        private readonly ILocalOptionTypeResponseMapper _responseMapper;
+        private readonly ILocalOptionTypeWriteModelMapper _writeModelMapper;
+
+        public ItContractLocalProcurementStrategyTypesInternalV2Controller(IGenericLocalOptionsService<LocalProcurementStrategyType, ItContract, ProcurementStrategyType> localOptionTypeService, ILocalOptionTypeResponseMapper responseMapper, ILocalOptionTypeWriteModelMapper writeModelMapper)
+            : base(localOptionTypeService, responseMapper, writeModelMapper)
+        {
+        }
+
+        [HttpGet]
+        [Route]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(IEnumerable<LocalRegularOptionResponseDTO>))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult GetLocalProcurementStrategyTypes([NonEmptyGuid][FromUri] Guid organizationUuid)
+        {
+            return GetAll(organizationUuid);
+        }
+
+        [HttpGet]
+        [Route("{optionUuid}")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRegularOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult GetLocalProcurementStrategyTypeByOptionId([NonEmptyGuid][FromUri] Guid organizationUuid, [FromUri] Guid optionUuid)
+        {
+            return GetSingle(organizationUuid, optionUuid);
+        }
+
+        [HttpPost]
+        [Route]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRegularOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult CreateLocalProcurementStrategyType([NonEmptyGuid][FromUri] Guid organizationUuid, LocalOptionCreateRequestDTO dto)
+        {
+            return Create(organizationUuid, dto);
+        }
+
+        [HttpPatch]
+        [Route("{optionUuid}")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRegularOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult PatchLocalProcurementStrategyType([NonEmptyGuid][FromUri] Guid organizationUuid,
+            [FromUri] Guid optionUuid,
+            LocalRegularOptionUpdateRequestDTO dto)
+        {
+            return Patch(organizationUuid, optionUuid, dto);
+        }
+
+        [HttpDelete]
+        [Route("{optionUuid}")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(LocalRegularOptionResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IHttpActionResult DeleteLocalProcurementStrategyType([NonEmptyGuid][FromUri] Guid organizationUuid,
+            [FromUri] Guid optionUuid)
+        {
+            return Delete(organizationUuid, optionUuid);
+        }
+    }
+}
