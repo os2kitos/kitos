e20953bcd601a11116b4f49caa2227588cdecb91
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-31 16:51:45 +0100

    add base class file

---
+++ Core.ApplicationServices/Model/Organizations/Write/OrganizationBaseParameters.cs
@@ -0,0 +1,13 @@
+﻿using Core.Abstractions.Types;
+using Core.ApplicationServices.Model.Shared;
+
+namespace Core.ApplicationServices.Model.Organizations.Write
+{
+    public class OrganizationBaseParameters
+    {
+        public OptionalValueChange<Maybe<string>> Name { get; set; }
+        public OptionalValueChange<Maybe<int>> TypeId { get; set; }
+        public OptionalValueChange<Maybe<string>> Cvr { get; set; }
+        public OptionalValueChange<Maybe<string>> ForeignCvr { get; set; }
+    }
+}
