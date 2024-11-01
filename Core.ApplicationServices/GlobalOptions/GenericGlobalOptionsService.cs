965895753cd16136516a1791a6e8f9e01085aa08
NikolajNielsen <nan@strongminds.dk>
2024-11-01 11:18:39 +0100

    Refactor reduce indentation

--- Core.ApplicationServices/GlobalOptions/GenericGlobalOptionsService.cs
+++ Core.ApplicationServices/GlobalOptions/GenericGlobalOptionsService.cs
@@ -60,29 +60,31 @@ public class GenericGlobalOptionsService<TOptionType, TReferenceType> : IGeneric
 
     public Result<TOptionType, OperationError> PatchGlobalOption(Guid optionUuid, GlobalOptionUpdateParameters updateParameters)
     {
-        return WithGlobalAdminRights($"User is not allowed to patch global option with uuid {optionUuid}")
-            .Match(error => error,
-                () => GetOption(optionUuid)
-                        .Bind(existingOption => PerformGlobalOptionUpdates(existingOption, updateParameters)
-                                .Bind(updatedOption =>
-                                {
-                                    _globalOptionsRepository.Update(updatedOption);
-                                    _globalOptionsRepository.Save();
-                                    _domainEvents.Raise(new EntityUpdatedEvent<TOptionType>(updatedOption));
+        return GetOptionWithGlobalAdminRights(optionUuid)
+            .Bind(existingOption => PerformGlobalOptionUpdates(existingOption, updateParameters)
+                    .Bind(updatedOption =>
+                    {
+                        _globalOptionsRepository.Update(updatedOption);
+                        _globalOptionsRepository.Save();
+                        _domainEvents.Raise(new EntityUpdatedEvent<TOptionType>(updatedOption));
 
-                                    return Result<TOptionType, OperationError>.Success(updatedOption);
-                                })
-                        )
-                );
+                        return Result<TOptionType, OperationError>.Success(updatedOption);
+                    })
+            );
     }
 
-    private Result<TOptionType, OperationError> GetOption(Guid optionUuid)
+    private Result<TOptionType, OperationError> GetOptionWithGlobalAdminRights(Guid optionUuid)
     {
-        var existingOption = _globalOptionsRepository.AsQueryable().ByUuid(optionUuid);
-        return (existingOption != null)
-            ? Result<TOptionType, OperationError>.Success(existingOption)
-            : new OperationError($"Unable to find global option with uuid {optionUuid}",
-                OperationFailure.NotFound);
+        return WithGlobalAdminRights($"User is not allowed to patch global option with uuid {optionUuid}")
+            .Match(error => error,
+                () =>
+                {
+                    var existingOption = _globalOptionsRepository.AsQueryable().ByUuid(optionUuid);
+                    return (existingOption != null)
+                        ? Result<TOptionType, OperationError>.Success(existingOption)
+                        : new OperationError($"Unable to find global option with uuid {optionUuid}",
+                            OperationFailure.NotFound);
+                });
     }
 
     private Result<TOptionType, OperationError> PerformGlobalOptionUpdates(TOptionType option, GlobalOptionUpdateParameters parameters)
