222f33504b85da0e04f9dbb052235a920edfb89b
NikolajNielsen <nan@strongminds.dk>
2024-11-04 08:50:58 +0100

    Setup for role case api tests

--- Tests.Integration.Presentation.Web/GlobalAdminArea/ItSystemGlobalRoleOptionTypesInternalV2ApiTest.cs
+++ Tests.Integration.Presentation.Web/GlobalAdminArea/ItSystemGlobalRoleOptionTypesInternalV2ApiTest.cs
@@ -1,21 +1,20 @@
-﻿
-using Presentation.Web.Models.API.V2.Internal.Response;
+﻿using Core.DomainModel.ItSystem;
+using Presentation.Web.Models.API.V2.Internal.Request.Options;
+using Presentation.Web.Models.API.V2.Internal.Response.GlobalOptions;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
-using Tests.Integration.Presentation.Web.Tools;
-using Tests.Integration.Presentation.Web.Tools.Internal;
-using Tests.Toolkit.Patterns;
-using Xunit;
-using Core.DomainModel.ItSystem;
 using System;
 using System.Linq;
-using Presentation.Web.Models.API.V2.Internal.Request.Options;
-using Presentation.Web.Models.API.V2.Internal.Response.GlobalOptions;
+using Presentation.Web.Models.API.V2.Internal.Request;
+using Tests.Integration.Presentation.Web.Tools.Internal;
+using Tests.Integration.Presentation.Web.Tools;
+using Tests.Toolkit.Patterns;
+using Xunit;
 
 namespace Tests.Integration.Presentation.Web.GlobalAdminArea
 {
-    public class ItSystemGlobalOptionTypesInternalV2ApiTest: WithAutoFixture
+    public class ItSystemGlobalRoleOptionTypesInternalV2ApiTest: WithAutoFixture
     {
         private const string BusinessTypesUrlSuffix = "business-types";
         private const string ItSystemsApiPrefix = "api/v2/internal/it-systems";
@@ -23,12 +22,12 @@ namespace Tests.Integration.Presentation.Web.GlobalAdminArea
         [Fact]
         public async Task Can_Get_Global_Business_Types()
         {
-            var expectedGlobalOption = SetupCreateGlobalBusinessTypeInDatabase();
+            var expectedGlobalOption = SetupCreateGlobalItSystemRoleInDatabase();
 
             using var response = await GlobalOptionTypeV2Helper.GetGlobalOptionTypes(BusinessTypesUrlSuffix, ItSystemsApiPrefix);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            var responseDtos = await response.ReadResponseBodyAsAsync<IEnumerable<LocalRegularOptionResponseDTO>>();
+            var responseDtos = await response.ReadResponseBodyAsAsync<IEnumerable<GlobalRoleOptionResponseDTO>>();
             Assert.NotNull(responseDtos);
             var actualIncludedOption = responseDtos.First(dto => dto.Uuid == expectedGlobalOption.Uuid);
             Assert.NotNull(actualIncludedOption);
@@ -38,63 +37,66 @@ namespace Tests.Integration.Presentation.Web.GlobalAdminArea
         [Fact]
         public async Task Can_Create_Global_Business_Type()
         {
-            var dto = new GlobalRegularOptionCreateRequestDTO()
+            var dto = new GlobalRoleOptionCreateRequestDTO()
             {
                 Name = A<string>(),
                 Description = A<string>(),
-                IsObligatory = A<bool>()
+                IsObligatory = A<bool>(),
+                WriteAccess = A<bool>()
             };
 
             using var response = await GlobalOptionTypeV2Helper.CreateGlobalOptionType(BusinessTypesUrlSuffix, dto, ItSystemsApiPrefix);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            var responseDto = await response.ReadResponseBodyAsAsync<GlobalRegularOptionResponseDTO>();
+            var responseDto = await response.ReadResponseBodyAsAsync<GlobalRoleOptionResponseDTO>();
             Assert.NotNull(responseDto);
             Assert.Equal(dto.Name, responseDto.Name);
             Assert.False(responseDto.IsEnabled);
+            Assert.Equal(dto.WriteAccess, responseDto.WriteAccess);
         }
 
         [Fact]
         public async Task Can_Patch_Global_Business_Type()
         {
-            var globalOption = SetupCreateGlobalBusinessTypeInDatabase();
+            var globalOption = SetupCreateGlobalItSystemRoleInDatabase();
 
-            var dto = new GlobalRegularOptionUpdateRequestDTO()
+            var dto = new GlobalRoleOptionUpdateRequestDTO()
             {
                 Description = A<string>(),
                 IsObligatory = A<bool>(),
                 IsEnabled = A<bool>(),
-                Name = A<string>()
+                Name = A<string>(),
+                WriteAccess = A<bool>()
             };
 
             using var response = await GlobalOptionTypeV2Helper.PatchGlobalOptionType(globalOption.Uuid, BusinessTypesUrlSuffix, dto, ItSystemsApiPrefix);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            var responseDto = await response.ReadResponseBodyAsAsync<GlobalRegularOptionResponseDTO>();
+            var responseDto = await response.ReadResponseBodyAsAsync<GlobalRoleOptionResponseDTO>();
             Assert.Equal(dto.Description, responseDto.Description);
             Assert.Equal(dto.Name, responseDto.Name);
             Assert.Equal(dto.IsObligatory, responseDto.IsObligatory);
             Assert.Equal(dto.IsEnabled, responseDto.IsEnabled);
             Assert.Equal(globalOption.Uuid, responseDto.Uuid);
+            Assert.Equal(dto.WriteAccess, responseDto.WriteAccess);
         }
 
-        private BusinessType SetupCreateGlobalBusinessTypeInDatabase()
+        private ItSystemRole SetupCreateGlobalItSystemRoleInDatabase()
         {
-            var globalOption = new BusinessType()
+            var globalOption = new ItSystemRole()
             {
                 Uuid = A<Guid>(),
                 Id = A<int>(),
                 Name = A<string>(),
-                IsObligatory = false
+                IsObligatory = false,
+                HasWriteAccess = A<bool>()
             };
-            DatabaseAccess.MutateEntitySet<BusinessType>(repository =>
+            DatabaseAccess.MutateEntitySet<ItSystemRole>(repository =>
             {
                 repository.Insert(globalOption);
                 repository.Save();
             });
             return globalOption;
         }
-
-
     }
 }
