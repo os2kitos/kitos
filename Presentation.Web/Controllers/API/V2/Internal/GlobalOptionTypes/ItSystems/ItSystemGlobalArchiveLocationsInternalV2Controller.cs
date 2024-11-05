27511ff31faa520fff4b31adb1faa3571ecaf799
NikolajNielsen <nan@strongminds.dk>
2024-11-05 10:34:08 +0100

    Fix curlies

--- Presentation.Web/Controllers/API/V2/Internal/GlobalOptionTypes/ItSystems/ItSystemGlobalArchiveLocationsInternalV2Controller.cs
+++ Presentation.Web/Controllers/API/V2/Internal/GlobalOptionTypes/ItSystems/ItSystemGlobalArchiveLocationsInternalV2Controller.cs
@@ -18,9 +18,15 @@ namespace Presentation.Web.Controllers.API.V2.Internal.GlobalOptionTypes.ItSyste
 {
     [RoutePrefix("api/v2/internal/it-systems/global-option-types/archive-location-types")]
 
-    public class ItSystemGlobalArchiveLocationsInternalV2Controller: BaseGlobalRegularOptionTypesInternalV2Controller<ItSystemUsage, ArchiveLocation>
+    public class
+        ItSystemGlobalArchiveLocationsInternalV2Controller : BaseGlobalRegularOptionTypesInternalV2Controller<
+        ItSystemUsage,
+        ArchiveLocation>
     {
-        public ItSystemGlobalArchiveLocationsInternalV2Controller(IGlobalRegularOptionsService<ArchiveLocation, ItSystemUsage> globalRegularOptionsService, IGlobalOptionTypeResponseMapper responseMapper, IGlobalOptionTypeWriteModelMapper writeModelMapper) : base(globalRegularOptionsService, responseMapper, writeModelMapper)
+        public ItSystemGlobalArchiveLocationsInternalV2Controller(
+            IGlobalRegularOptionsService<ArchiveLocation, ItSystemUsage> globalRegularOptionsService,
+            IGlobalOptionTypeResponseMapper responseMapper, IGlobalOptionTypeWriteModelMapper writeModelMapper) : base(
+            globalRegularOptionsService, responseMapper, writeModelMapper)
         {
         }
 
@@ -55,9 +61,11 @@ namespace Presentation.Web.Controllers.API.V2.Internal.GlobalOptionTypes.ItSyste
         [SwaggerResponse(HttpStatusCode.Unauthorized)]
         [SwaggerResponse(HttpStatusCode.Forbidden)]
         [SwaggerResponse(HttpStatusCode.NotFound)]
-        public IHttpActionResult PatchGlobalArchiveLocation([NonEmptyGuid][FromUri] Guid optionUuid,
+        public IHttpActionResult PatchGlobalArchiveLocation([NonEmptyGuid] [FromUri] Guid optionUuid,
             GlobalRegularOptionUpdateRequestDTO dto)
         {
             return Patch(optionUuid, dto);
         }
+    }
+}
     
